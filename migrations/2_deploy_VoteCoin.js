d2f0ff1de7e8dbee758510137ab81c572523eb84
Matt Rice <matthewcrice32@gmail.com>
2018-10-08 12:22:43 -0400

    Various updates to contract logic (#23)
    Updates to contract logic in prep for demo

--- migrations/2_deploy_VoteCoin.js
+++ migrations/2_deploy_VoteCoin.js
@@ -1,7 +1,9 @@
-var VoteCoin = artifacts.require("VoteCoin");
+var VoteTokenMock = artifacts.require("VoteTokenMock");
+var Registry = artifacts.require("Registry");
 
 module.exports = function(deployer, network, accounts) {
     // Send 0.5 ether with our account so it can update price repeatedly
-    deployer.deploy(VoteCoin, {from: accounts[0], value: 500000000000000000});
+    deployer.deploy(VoteTokenMock, {from: accounts[0], value: 0});
+    deployer.deploy(Registry, {from: accounts[0], value: 0});
 };
 
