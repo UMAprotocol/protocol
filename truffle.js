a32d72c1db372471a458deff375799aac028bb1a
Matt Rice <matthewcrice32@gmail.com>
2018-10-08 19:13:48 -0400

    moar ci

--- truffle.js
+++ truffle.js
@@ -4,8 +4,14 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
+      port: 9545,
+      network_id: "*", // Match any network id
+      gas: 4700000
+    },
+    ci: {
+      host: "127.0.0.1",
       port: 8545,
-      network_id: 1234, // Match any network id
+      network_id: 1234,
       gas: 4700000
     }
   }
