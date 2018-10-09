1bc7897fc6aa729c1ca3fc6d2249ca96e51434fc
Matt Rice <matthewcrice32@gmail.com>
2018-10-09 13:53:18 -0400

    Add first JS test (#29)

--- truffle.js
+++ truffle.js
@@ -6,7 +6,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 9545,
       network_id: "*", // Match any network id
-      gas: 5000000
+      gas: 4700000
+    },
+    ci: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 1234,
+      gas: 4700000
     }
   }
 };
