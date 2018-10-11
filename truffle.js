12411d27a582e2092bf0e3903c5eda86adfee8c7
Matt Rice <matthewcrice32@gmail.com>
2018-10-10 20:30:15 -0400

    WIP:

--- truffle.js
+++ truffle.js
@@ -14,13 +14,6 @@ module.exports = {
       port: 8545,
       gas: 0xfffffffffff,
       gasPrice: 0x01
-    },
-    ci_coverage: {
-      host: "127.0.0.1",
-      network_id: 12345,
-      port: 8545,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01
     }
   }
 };
