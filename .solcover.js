1404b61685ca2273bd14f63cefa9844b2d09f896
Matt Rice <matthewcrice32@gmail.com>
2018-12-18 13:56:55 -0500

    WIP

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
-  copyPackages: ["openzeppelin-solidity", "truffle",],
+  copyPackages: ["openzeppelin-solidity", "truffle"],
   compileCommand: "$(npm bin)/truffle compile",
   testCommand: "$(npm bin)/truffle test --network coverage",
   port: 8545,
