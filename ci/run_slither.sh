2e5006ebf0e313764326853983e58c363b47bdae
Matt Rice <matthewcrice32@gmail.com>
2019-01-03 19:16:28 -0500

    WIP

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -3,7 +3,7 @@
 sudo apt update -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository -y ppa:jonathonf/python-3.6
-sudo apt update --allow-unauthenticated -y
+sudo apt update --allow-unauthenticated --force-yes --yes
 sudo apt install python3.6 -y
 python3.6 -m pip install --upgrade pip setuptools wheel
 git clone https://github.com/trailofbits/slither.git
