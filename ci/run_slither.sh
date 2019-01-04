c9ba8ac5aa0d75292e74efe79a04c02841285774
Matt Rice <matthewcrice32@gmail.com>
2019-01-03 19:09:56 -0500

    WIP

---
+++ ci/run_slither.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository -y ppa:jonathonf/python-3.6
+sudo apt update --allow-unauthenticated -y
+sudo apt install python3.6 -y
+python3.6 -m pip install --upgrade pip setuptools wheel
+git clone https://github.com/trailofbits/slither.git
+cd slither
+sudo python3.6 setup.py install
+cd ..
+python3.6 -m slither --exclude=naming-convention,solc-version,pragma,external-function .
