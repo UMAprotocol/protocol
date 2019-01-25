4e4365abf4b9a996bb4c334fe6344e053c0e9345
Matt Rice <matthewcrice32@gmail.com>
2019-01-25 00:45:42 -0500

    WIP

--- test/LeveragedReturnCalculator.js
+++ test/LeveragedReturnCalculator.js
@@ -9,31 +9,182 @@ contract("LeveragedReturnCalculator", function(accounts) {
 
   const getReturnCalculator = async leverage => {
     return await LeveragedReturnCalculator.new(leverage, { from: owner });
-  }
+  };
 
   it("No Leverage", async function() {
     const returnCalculator = await getReturnCalculator(1);
 
     // No leverage newPrice = 2*oldPrice -> return of 200%.
-    const doubleReturn = await returnCalculator.computeReturn(web3.utils.toWei("0.5", "ether"), web3.utils.toWei("1", "ether"));
+    const doubleReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("0.5", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
     assert.equal(doubleReturn.toString(), web3.utils.toWei("2", "ether"));
 
     // 0 newPrice should always generate a 0 return.
-    const zeroReturn = await returnCalculator.computeReturn(web3.utils.toWei("1", "ether"), web3.utils.toWei("0", "ether"));
+    const zeroReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("0", "ether")
+    );
     assert.equal(zeroReturn.toString(), "0");
 
     // 0 oldPrice should always generate a 0 return.
-    const zeroStartingPrice = await returnCalculator.computeReturn(web3.utils.toWei("0", "ether"), web3.utils.toWei("1", "ether"));
+    const zeroStartingPrice = await returnCalculator.computeReturn(
+      web3.utils.toWei("0", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
     assert.equal(zeroStartingPrice.toString(), "0");
 
     // Sign flip on price causes a negative return.
-    const negativeReturn = await returnCalculator.computeReturn(web3.utils.toWei("1", "ether"), web3.utils.toWei("-1", "ether"));
+    const negativeReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("-1", "ether")
+    );
     assert.equal(negativeReturn.toString(), web3.utils.toWei("-1", "ether"));
 
     // Increase in magnitude causes a positive return.
-    const allNegativeInputs = await returnCalculator.computeReturn(web3.utils.toWei("-1", "ether"), web3.utils.toWei("-2", "ether"));
+    const allNegativeInputs = await returnCalculator.computeReturn(
+      web3.utils.toWei("-1", "ether"),
+      web3.utils.toWei("-2", "ether")
+    );
     assert.equal(allNegativeInputs.toString(), web3.utils.toWei("2", "ether"));
   });
 
+  it("Leverage >1", async function() {
+    const returnCalculator = await getReturnCalculator(2);
 
+    // 2x leverage newPrice = 2*oldPrice -> return of 300%.
+    const doubleUnderlying = await returnCalculator.computeReturn(
+      web3.utils.toWei("0.5", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
+    assert.equal(doubleUnderlying.toString(), web3.utils.toWei("3", "ether"));
+
+    // 2x leverage newPrice = (1/2)*oldPrice -> return of 0%.
+    const halfUnderlying = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("0.5", "ether")
+    );
+    assert.equal(halfUnderlying.toString(), "0");
+
+    // 0 newPrice should generate a -100% return.
+    const zeroReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("0", "ether")
+    );
+    assert.equal(zeroReturn.toString(), web3.utils.toWei("-1", "ether"));
+
+    // 0 oldPrice should always generate a 0 return.
+    const zeroStartingPrice = await returnCalculator.computeReturn(
+      web3.utils.toWei("0", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
+    assert.equal(zeroStartingPrice.toString(), "0");
+
+    // Loss of 200% -> return of -300%.
+    const negativeReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("-1", "ether")
+    );
+    assert.equal(negativeReturn.toString(), web3.utils.toWei("-3", "ether"));
+
+    // Increase in magnitude causes a 2x positive return on the difference.
+    const allNegativeInputs = await returnCalculator.computeReturn(
+      web3.utils.toWei("-1", "ether"),
+      web3.utils.toWei("-2", "ether")
+    );
+    assert.equal(allNegativeInputs.toString(), web3.utils.toWei("3", "ether"));
+  });
+
+  it("Leverage <0", async function() {
+    const returnCalculator = await getReturnCalculator(-1);
+
+    // -1x leverage newPrice = 2*oldPrice -> short return of 0%.
+    const doubleUnderlying = await returnCalculator.computeReturn(
+      web3.utils.toWei("0.5", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
+    assert.equal(doubleUnderlying.toString(), web3.utils.toWei("0", "ether"));
+
+    // -1x leverage newPrice = (1/2)*oldPrice -> return of 150%.
+    const halfUnderlying = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("0.5", "ether")
+    );
+    assert.equal(halfUnderlying.toString(), web3.utils.toWei("1.5", "ether"));
+
+    // 0 newPrice should generate a -100% return.
+    const zeroReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("0", "ether")
+    );
+    assert.equal(zeroReturn.toString(), web3.utils.toWei("2", "ether"));
+
+    // 0 oldPrice should always generate a 0 return.
+    const zeroStartingPrice = await returnCalculator.computeReturn(
+      web3.utils.toWei("0", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
+    assert.equal(zeroStartingPrice.toString(), "0");
+
+    // Loss of 200% -> return of 300%.
+    const negativeReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("-1", "ether")
+    );
+    assert.equal(negativeReturn.toString(), web3.utils.toWei("3", "ether"));
+
+    // Increase in magnitude causes a negative return on the difference for a short.
+    const allNegativeInputs = await returnCalculator.computeReturn(
+      web3.utils.toWei("-1", "ether"),
+      web3.utils.toWei("-2", "ether")
+    );
+    assert.equal(allNegativeInputs.toString(), web3.utils.toWei("0", "ether"));
+  });
+
+  it("Leverage <-1", async function() {
+    const returnCalculator = await getReturnCalculator(-2);
+
+    // -1x leverage newPrice = 2*oldPrice -> short return of 0%.
+    const doubleUnderlying = await returnCalculator.computeReturn(
+      web3.utils.toWei("0.5", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
+    assert.equal(doubleUnderlying.toString(), web3.utils.toWei("-1", "ether"));
+
+    // 2x leverage newPrice = (1/2)*oldPrice -> return of 150%.
+    const halfUnderlying = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("0.5", "ether")
+    );
+    assert.equal(halfUnderlying.toString(), web3.utils.toWei("2", "ether"));
+
+    // 0 newPrice should generate a -100% return.
+    const zeroReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("0", "ether")
+    );
+    assert.equal(zeroReturn.toString(), web3.utils.toWei("3", "ether"));
+
+    // 0 oldPrice should always generate a 0 return.
+    const zeroStartingPrice = await returnCalculator.computeReturn(
+      web3.utils.toWei("0", "ether"),
+      web3.utils.toWei("1", "ether")
+    );
+    assert.equal(zeroStartingPrice.toString(), "0");
+
+    // Loss of 200% -> return of 300%.
+    const negativeReturn = await returnCalculator.computeReturn(
+      web3.utils.toWei("1", "ether"),
+      web3.utils.toWei("-1", "ether")
+    );
+    assert.equal(negativeReturn.toString(), web3.utils.toWei("5", "ether"));
+
+    // Increase in magnitude causes a 2x negative return on the difference for a short.
+    const allNegativeInputs = await returnCalculator.computeReturn(
+      web3.utils.toWei("-1", "ether"),
+      web3.utils.toWei("-2", "ether")
+    );
+    assert.equal(allNegativeInputs.toString(), web3.utils.toWei("-1", "ether"));
+  });
 });
