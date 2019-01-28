3a49b5f520aa2e6ea514f6e0243e30df03545aae
Prasad Tare <prasad.tare@gmail.com>
2019-01-28 10:43:01 -0500

    Add (somewhat general) comments to AdminInterface

--- contracts/AdminInterface.sol
+++ contracts/AdminInterface.sol
@@ -7,7 +7,10 @@ pragma solidity ^0.5.0;
 
 // The functionality that all derivative contracts expose to the admin.
 interface AdminInterface {
+    // Initiates the shutdown process, in case of an emergency.
     function emergencyShutdown() external;
 
+    // A core contract method called immediately before or after any financial transaction. It pays fees and moves money
+    // between margin accounts to make sure they reflect the NAV of the contract.
     function remargin() external;
 }
