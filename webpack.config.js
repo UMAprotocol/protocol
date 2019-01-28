8b6cef9a6f3cfaafb86a4b552979466aaaacf346
Matt Rice <matthewcrice32@gmail.com>
2019-01-28 10:37:44 -0500

    Correct prettier command and format files (#151)

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,30 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './app/scripts/index.js',
-  mode: 'production',
+  entry: "./app/scripts/index.js",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.js'
+    path: path.resolve(__dirname, "build"),
+    filename: "app.js"
   },
   plugins: [
     // Copy our app's index.html to the build folder.
-    new CopyWebpackPlugin([
-      { from: './app/index.html', to: 'index.html' }
-    ])
+    new CopyWebpackPlugin([{ from: "./app/index.html", to: "index.html" }])
   ],
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
-      { test: /\.s?css$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      { test: /\.s?css$/, use: ["style-loader", "css-loader", "sass-loader"] },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['env'],
-          plugins: ['transform-react-jsx', 'transform-object-rest-spread', 'transform-runtime']
+          presets: ["env"],
+          plugins: ["transform-react-jsx", "transform-object-rest-spread", "transform-runtime"]
         }
       }
     ]
   }
-}
-
+};
