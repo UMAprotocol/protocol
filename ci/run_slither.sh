ca3bbace1dd52c798de32dacc41f73acad0b4ff3
Matt Rice <matthewcrice32@gmail.com>
2019-02-01 11:30:59 -0500

    WIP

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-slither --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,reentrancy-eth .
+sudo env "PATH=$PATH" truffle compile 
+sudo env "PATH=$PATH" slither --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,reentrancy-eth .
