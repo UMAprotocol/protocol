a566042d191c3f338fa536874da38f23c6b4733b
Matt Rice <matthewcrice32@gmail.com>
2019-02-01 11:24:26 -0500

    simplify slither CI

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -1,31 +1,3 @@
 #!/usr/bin/env bash
 
-cd ~
-DIRECTORY=~/Python-3.6.3
-if [ ! -d "$DIRECTORY" ]
-then
-    # Only run when the pre-built python download isn't found.
-    echo "Building a fresh Python 3.6..."
-    sudo apt update -y && sudo apt upgrade
-    wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tgz
-    tar xvf Python-3.6.3.tgz
-    cd $DIRECTORY
-    ./configure --enable-optimizations --with-ensurepip=install
-    make
-    sudo rm -rf Python-3.6.3.tgz
-fi
-
-echo "Installing Python 3.6..."
-cd $DIRECTORY
-sudo make altinstall
-
-echo "Installing Slither..."
-cd ~
-git clone https://github.com/trailofbits/slither.git
-cd slither
-sudo python3.6 setup.py install
-sudo rm -rf slither
-
-cd ~/protocol
-npx truffle compile
-python3.6 -m slither --truffle-version=latest --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,reentrancy-eth .
+slither --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,reentrancy-eth .
