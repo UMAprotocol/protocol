8ad3ec973453da3d86c577477c6a6f27ebbc76ad
Matt Rice <matthewcrice32@gmail.com>
2019-02-07 09:53:32 -0500

    Update README and minor cleanup (#177)

--- .solcover-ci.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  copyPackages: ["openzeppelin-solidity"],
-  compileCommand: "../node_modules/.bin/truffle compile",
-  testCommand: "../node_modules/.bin/truffle test --network ci_coverage",
-  norpc: true,
-  port: 8545,
-  skipFiles: ["Migrations.sol", "echidna_tests/"]
-};
