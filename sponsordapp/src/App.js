338c1d635cfe761b31afad29d4e9663d09343ff9
Prasad Tare <prasad.tare@gmail.com>
2019-02-11 16:12:46 -0500

    Add empty sponsor DApp

---
+++ sponsordapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Sponsor DApp
+      </div>
+    );
+  }
+}
+
+export default App;
