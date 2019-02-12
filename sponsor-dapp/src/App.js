2e654c4605c10bcbdf8cad2a875c635786a59426
Prasad Tare <ptare@users.noreply.github.com>
2019-02-12 17:50:02 -0500

    Incorporate Material UI into Sponsor DApp with a test modal (#190)

--- sponsor-dapp/src/App.js
+++ sponsor-dapp/src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import './App.css';
 
 class App extends Component {
+  state = {
+    open: false
+  };
+
+  handleModalOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleModalClose = () => {
+    this.setState({ open: false} );
+  };
+
   render() {
     return (
       <div className="App">
         Sponsor DApp
+        <Button onClick={this.handleModalOpen}>
+          Open contract details
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleModalClose}
+          aria-labelledby="contract-details"
+        >
+          <DialogTitle>
+            Contract Details
+          </DialogTitle>
+          <DialogContent>
+            Contents of modal
+          </DialogContent>
+        </Dialog>
       </div>
     );
   }
