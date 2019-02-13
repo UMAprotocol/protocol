ea16ac39814995a0ba89f3a0d43f88bdf5efa56f
Matt Rice <matthewcrice32@gmail.com>
2019-02-13 18:40:49 -0500

    WIP

--- sponsor-dapp/src/App.test.js
+++ sponsor-dapp/src/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Drizzle, generateStore } from "drizzle";
 import App from "./App";
+import drizzleOptions from "./drizzleOptions";
 
 it("renders without crashing", () => {
+  // Setup drizzle.
+  const drizzleStore = generateStore(drizzleOptions);
+  const drizzle = new Drizzle(drizzleOptions, drizzleStore);
+
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App drizzle={drizzle} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
