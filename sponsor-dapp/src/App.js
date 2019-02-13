9fcea22492ba92423ee0e79a9ae2d49cd8a70353
Matt Rice <matthewcrice32@gmail.com>
2019-02-13 18:03:29 -0500

    Drizzle integration (#191)

--- sponsor-dapp/src/App.js
+++ sponsor-dapp/src/App.js
@@ -1,44 +1,25 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import './App.css';
-import ContractDetails from './components/ContractDetails.js';
+import React, { Component } from "react";
+import { DrizzleContext } from "drizzle-react";
+import "./App.css";
+import DerivativeList from "./DerivativeList.js";
 
 class App extends Component {
-  state = {
-    open: false
-  };
-
-  handleModalOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleModalClose = () => {
-    this.setState({ open: false} );
-  };
-
   render() {
     return (
-      <div className="App">
-        Sponsor DApp
-        <Button onClick={this.handleModalOpen}>
-          Open contract details
-        </Button>
-        <Dialog
-          open={this.state.open}
-          onClose={this.handleModalClose}
-          aria-labelledby="contract-details"
-        >
-          <DialogTitle>
-            Contract Details
-          </DialogTitle>
-          <DialogContent>
-            <ContractDetails />
-          </DialogContent>
-        </Dialog>
-      </div>
+      <DrizzleContext.Provider drizzle={this.props.drizzle}>
+        <DrizzleContext.Consumer>
+          {drizzleContext => {
+            const { drizzle, initialized, drizzleState } = drizzleContext;
+
+            // If drizzle hasn't gotten any state, don't load the application.
+            if (!initialized) {
+              return "Loading...";
+            }
+
+            return <DerivativeList drizzle={drizzle} drizzleState={drizzleState} />;
+          }}
+        </DrizzleContext.Consumer>
+      </DrizzleContext.Provider>
     );
   }
 }
