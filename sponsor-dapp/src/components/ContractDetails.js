2e782623c9d0db6281b553fb70b023447d0c9905
Prasad Tare <prasad.tare@gmail.com>
2019-02-13 12:03:37 -0500

    Use better object initialization in JS

--- sponsor-dapp/src/components/ContractDetails.js
+++ sponsor-dapp/src/components/ContractDetails.js
@@ -5,32 +5,33 @@ import ContractParameters from './ContractParameters.js';
 class ContractDetails extends Component {
   render() {
     // TODO(ptare): Retrieve these values from the blockchain via Drizzle.
-    const lastRemarginContractFinancials = {};
-    lastRemarginContractFinancials.assetPrice = "$33";
-    lastRemarginContractFinancials.tokenPrice = "217 Dai/token";
-    lastRemarginContractFinancials.totalHoldings = "0";
-    lastRemarginContractFinancials.totalHoldings = "0";
-    lastRemarginContractFinancials.longMargin = "0";
-    lastRemarginContractFinancials.shortMargin = "0";
-    lastRemarginContractFinancials.tokenSupply = "0";
-    lastRemarginContractFinancials.yourTokens = "0";
-    const estimatedCurrentContractFinancials = {};
-    estimatedCurrentContractFinancials.assetPrice = "$35 (+2)";
-    estimatedCurrentContractFinancials.tokenPrice = "221 Dai/token (+4)";
-    estimatedCurrentContractFinancials.totalHoldings = "0";
-    estimatedCurrentContractFinancials.longMargin = "0";
-    estimatedCurrentContractFinancials.shortMargin = "0";
-    estimatedCurrentContractFinancials.tokenSupply = "0";
-    estimatedCurrentContractFinancials.yourTokens = "0";
-
+    const lastRemarginContractFinancials = {
+      assetPrice: "$33",
+      tokenPrice: "217 Dai/token",
+      totalHoldings: "0",
+      totalHoldings: "0",
+      longMargin: "0",
+      shortMargin: "0",
+      tokenSupply: "0",
+      yourTokens: "0"
+    };
+    const estimatedCurrentContractFinancials = {
+      assetPrice: "$35 (+2)",
+      tokenPrice: "221 Dai/token (+4)",
+      totalHoldings: "0",
+      longMargin: "0",
+      shortMargin: "0",
+      tokenSupply: "0",
+      yourTokens: "0"
+    };
     const contractParameters = {
-      contractAddress: '0x12345',
-      creatorAddress: '0x67890',
-      creationTime: '2018-12-10 T13:45:30',
-      expiryTime: '2018-12-30 T17:00:00',
-      priceFeedAddress: '0x54321',
-      marginCurrency: 'Dai (0x09876)',
-      returnCalculator: '2x (0x23456)'
+      contractAddress: "0x12345",
+      creatorAddress: "0x67890",
+      creationTime: "2018-12-10 T13:45:30",
+      expiryTime: "2018-12-30 T17:00:00",
+      priceFeedAddress: "0x54321",
+      marginCurrency: "Dai (0x09876)",
+      returnCalculator: "2x (0x23456)"
     };
 
     return (
