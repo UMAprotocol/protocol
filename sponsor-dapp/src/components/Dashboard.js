5f20cc4fc03334b597ca8af847124ddb040b44b7
Matt Rice <matthewcrice32@gmail.com>
2019-02-15 13:05:02 -0500

    button styling

--- sponsor-dapp/src/components/Dashboard.js
+++ sponsor-dapp/src/components/Dashboard.js
@@ -29,16 +29,6 @@ const styles = theme => ({
       duration: theme.transitions.duration.leavingScreen
     })
   },
-  paper: {
-    marginTop: theme.spacing.unit * 3,
-    marginBottom: theme.spacing.unit * 3,
-    padding: theme.spacing.unit * 2,
-    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-      marginTop: theme.spacing.unit * 2,
-      marginBottom: theme.spacing.unit * 6,
-      padding: theme.spacing.unit * 3
-    }
-  },
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
--- sponsor-dapp/src/components/DerivativeListTable.js
+++ sponsor-dapp/src/components/DerivativeListTable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -7,9 +8,22 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
-const DerivativeListTable = ({ derivatives, buttonPushFn }) => (
-  <Paper align="center">
-    <Table align="center">
+const styles = theme => ({
+  root: {
+    width: "100%",
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 700
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+});
+
+const DerivativeListTable = ({ derivatives, buttonPushFn, classes }) => (
+  <Paper align="center" className={classes.root}>
+    <Table align="center" className={classes.table}>
       <TableHead>
         <TableRow>
           <TableCell padding="dense">Address</TableCell>
@@ -26,7 +40,14 @@ const DerivativeListTable = ({ derivatives, buttonPushFn }) => (
           return (
             <TableRow key={n.id}>
               <TableCell padding="dense">
-                <Button onClick={e => buttonPushFn(n.address, e)}>Open details for {n.address}</Button>
+                <Button
+                  onClick={e => buttonPushFn(n.address, e)}
+                  className={classes.button}
+                  variant="outlined"
+                  color="primary"
+                >
+                  {n.address}
+                </Button>
               </TableCell>
               <TableCell padding="dense">{n.tokenName}</TableCell>
               <TableCell padding="dense">{n.symbol}</TableCell>
@@ -42,4 +63,4 @@ const DerivativeListTable = ({ derivatives, buttonPushFn }) => (
   </Paper>
 );
 
-export default DerivativeListTable;
+export default withStyles(styles)(DerivativeListTable);
