588020f5767700cb06ed83919bb4263f122cc89a
Prasad Tare <ptare@users.noreply.github.com>
2019-02-19 17:46:48 -0500

    Fill in more contract details by making a second request to Drizzle (#203)
    The design here is somewhat weird, it may be possible to refactor into a sequence of operations, rather than one function that gets called multiple times and keeps track of its previous callbacks internally.

--- sponsor-dapp/src/components/ContractFinancialsTable.js
+++ sponsor-dapp/src/components/ContractFinancialsTable.js
@@ -2,10 +2,24 @@ import React from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
 const ContractFinancialsTable = ({ lastRemargin, estimatedCurrent }) => (
   <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell></TableCell>
+        <TableCell>
+          <div>Values at last remargin</div>
+          <div>as of {lastRemargin.time}</div>
+        </TableCell>
+        <TableCell>
+          <div>Estimated current values</div>
+          <div>as of {estimatedCurrent.time}</div>
+        </TableCell>
+      </TableRow>
+    </TableHead>
     <TableBody>
       <TableRow key="assetPrice">
         <TableCell>Asset price:</TableCell>
