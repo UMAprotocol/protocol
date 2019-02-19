896b02f9de590c43423e6eafaf76f831a8b661f2
Yutaro Mori <yutaro@umaproject.org>
2019-02-19 10:28:37 -0500

    Check if web3 account is in the sponsor whitelist

--- sponsor-dapp/src/drizzleOptions.js
+++ sponsor-dapp/src/drizzleOptions.js
@@ -1,11 +1,13 @@
 import Registry from "./contracts/Registry.json";
 import TokenizedDerivativeCreator from "./contracts/TokenizedDerivativeCreator.json";
+import AddressWhitelist from "./contracts/AddressWhitelist.json";
 
 const options = {
-  contracts: [Registry, TokenizedDerivativeCreator],
+  contracts: [Registry, TokenizedDerivativeCreator, AddressWhitelist],
   events: {},
   polls: {
-    accounts: 1500
+    accounts: 1500,
+    blocks: 1500
   }
 };
 
