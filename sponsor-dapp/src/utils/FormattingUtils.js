a2f94c03d48bae95f1503090c6f1d19c50dce42d
Yutaro Mori <yutaro@umaproject.org>
2019-03-04 20:03:43 -0500

    Move display logic from ContractDetails.js -> ContractFinancialsTable.js  - Add drizzleHelper.getCached

--- sponsor-dapp/src/utils/FormattingUtils.js
+++ sponsor-dapp/src/utils/FormattingUtils.js
@@ -1,4 +1,4 @@
-function formatDate(timestampInSeconds, web3) {
+export function formatDate(timestampInSeconds, web3) {
   return new Date(
     parseInt(
       web3.utils
@@ -10,6 +10,10 @@ function formatDate(timestampInSeconds, web3) {
   ).toString();
 }
 
-module.exports = {
-  formatDate
-};
+// formatWei converts a string or BN instance from Wei to Ether, e.g., 1e19 -> 10.
+export function formatWei(num, web3) {
+  // Web3's `fromWei` function doesn't work on BN objects in minified mode (e.g.,
+  // `web3.utils.isBN(web3.utils.fromBN("5"))` is false), so we use a workaround where we always pass in strings.
+  // See https://github.com/ethereum/web3.js/issues/1777.
+  return web3.utils.fromWei(num.toString());
+}
