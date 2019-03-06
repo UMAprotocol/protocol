7afa5c855ac6d77ab0c1b0a9645f6e7e79b3f8b7
Yutaro Mori <ymori123@gmail.com>
2019-03-05 20:04:07 -0500

    Render expired contract details (#242)
    * Move display logic from ContractDetails.js -> ContractFinancialsTable.js
     - Add drizzleHelper.getCached
    
    * run prettier

--- sponsor-dapp/src/utils/FormattingUtils.js
+++ sponsor-dapp/src/utils/FormattingUtils.js
@@ -9,3 +9,11 @@ export function formatDate(timestampInSeconds, web3) {
     )
   ).toString();
 }
+
+// formatWei converts a string or BN instance from Wei to Ether, e.g., 1e19 -> 10.
+export function formatWei(num, web3) {
+  // Web3's `fromWei` function doesn't work on BN objects in minified mode (e.g.,
+  // `web3.utils.isBN(web3.utils.fromBN("5"))` is false), so we use a workaround where we always pass in strings.
+  // See https://github.com/ethereum/web3.js/issues/1777.
+  return web3.utils.fromWei(num.toString());
+}
