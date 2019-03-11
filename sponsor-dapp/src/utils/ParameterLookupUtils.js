fe60eaf8fc0ae17318fbdf1a2afe4c6a285e59f2
Matt Rice <matthewcrice32@gmail.com>
2019-03-11 16:28:32 -0400

    respond to comment

--- sponsor-dapp/src/utils/ParameterLookupUtils.js
+++ sponsor-dapp/src/utils/ParameterLookupUtils.js
@@ -1,7 +1,5 @@
 // Returns the currency name if its name is listed in the params.
-// Otherwise, returns null.
+// Otherwise, returns undefined.
 export function currencyAddressToName(params, address) {
-  const currencyName = params.currencies[address];
-  // This is just to force the output to null rather than undefined.
-  return currencyName == null ? null : currencyName;
+  return params.currencies[address];
 }
