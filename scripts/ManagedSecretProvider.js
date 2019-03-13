ba33fe1c5bcdfc61f566a68362c045b86e649c14
Prasad Tare <prasad.tare@gmail.com>
2019-03-13 19:04:52 -0400

    Fire off promise from constructor and keep attaching callbacks onto it

--- scripts/ManagedSecretProvider.js
+++ scripts/ManagedSecretProvider.js
@@ -4,10 +4,6 @@ const { readFileSync } = require("fs");
 
 // Wraps HDWalletProvider, deferring construction and allowing a Cloud KMS managed secret to be fetched asynchronously
 // and used to initialize an HDWalletProvider.
-// TODO(ptare): Two features need to be added.
-// 1. A way to "lock" construction to prevent multiple initializations (though Truffle will initialize your provider multiple times anyway if you use a thunk in truffle.js).
-// 2. An initialization method that our scripts can call before interacting with the provider. Note that this won't prevent
-// Truffle itself from interacting with the provider before it's been initialized.
 class ManagedSecretProvider {
   // cloudKmsSecretConfig must have the fields:
   //   projectId: ID of a Google Cloud project
@@ -19,26 +15,33 @@ class ManagedSecretProvider {
     this.cloudKmsSecretConfig = cloudKmsSecretConfig;
     this.remainingArgs = remainingArgs;
     this.wrappedProvider = null;
+    this.wrappedProviderPromise = this.getOrConstructWrappedProvider();
   }
 
-  // Constructs the wrapped provider if needed and passes the call through.
+  // Kicks off the construction of the wrapper provider. Call (and await on) this method before invoking any other
+  // methods.
+  async constructWrappedProvider() {
+    return this.wrappedProviderPromise;
+  }
+
+  // Passes the call through, by attaching a callback to the wrapper provider promise.
   sendAsync(...all) {
-    this.getOrConstructWrappedProvider().then(wrappedProvider => {
+    this.wrappedProviderPromise.then(wrappedProvider => {
       wrappedProvider.sendAsync(...all);
     });
   }
 
-  // Passes the call through. Requires that the wrapped provider has been created via, e.g., `sendAsync`.
+  // Passes the call through. Requires that the wrapped provider has been created via, e.g., `constructWrappedProvider`.
   send(...all) {
     return getWrappedProviderOrThrow().send(...all);
   }
 
-  // Passes the call through. Requires that the wrapped provider has been created via, e.g., `sendAsync`.
+  // Passes the call through. Requires that the wrapped provider has been created via, e.g., `constructWrappedProvider`.
   getAddress(idx) {
     return getWrappedProviderOrThrow().getAddress(idx);
   }
 
-  // Passes the call through. Requires that the wrapped provider has been created via, e.g., `sendAsync`.
+  // Passes the call through. Requires that the wrapped provider has been created via, e.g., `constructWrappedProvider`.
   getAddress(...all) {
     return getWrappedProviderOrThrow().getAddress(...all);
   }
