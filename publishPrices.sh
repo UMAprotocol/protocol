65b723bf20c85b430b7c2bf782889be624f5487e
Matt Rice <matthewcrice32@gmail.com>
2019-03-13 23:28:40 -0400

    Reduce number of arguments required for publishPrices

--- publishPrices.sh
+++ publishPrices.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Usage: ./publishPrices.sh <ManualPriceFeed address> <network>
-# Example:  ./publishPrices.s 0x5b48d54581246863913c704551b2CA6248223f32 develop
+# Usage: ./publishPrices.sh <network>
+# Example:  ./publishPrices.sh develop
 while sleep 15; do $(npm bin)/truffle exec ./scripts/PublishPrices.js --network $2 >>out.log 2>&1; done
