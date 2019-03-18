dd5f9b4a248cf17385444376bd411a8a84b3f5f8
Matt Rice <matthewcrice32@gmail.com>
2019-03-18 10:18:17 -0400

    WIP

--- scripts/publishPrices.sh
+++ scripts/publishPrices.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Usage: ./publishPrices.sh <network>
-# Example: ./publishPrices.sh develop
+# Usage: ./scripts/publishPrices.sh <network>
+# Example: ./scripts/publishPrices.sh develop
+# Note: this script must be run from the top level directory (not scripts/).
 while sleep 60; do $(npm bin)/truffle exec ./scripts/PublishPrices.js --network $1 --keys priceFeed >>out.log 2>&1; done
