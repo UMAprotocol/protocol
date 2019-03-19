98826027bd8b67ea48b9f7646391cc1ea48249b6
Matt Rice <matthewcrice32@gmail.com>
2019-03-18 22:18:12 -0400

    WIP:

---
+++ ci/run_truffle_tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Test migration
+$(npm bin)/truffle migrate --reset --network ci
+
+# Ensure the migration is recoverable with only the aritifacts saved in networks/.
+rm -rf build
+$(npm bin)/truffle compile
+$(npm bin)/apply-registry
+
+# Verify the validity of the deployment
+$(npm bin)/truffle exec ./scripts/CheckDeploymentValidity.js --network ci
+
+# Run standard truffle tests
+$(npm bin)/truffle test --network ci
