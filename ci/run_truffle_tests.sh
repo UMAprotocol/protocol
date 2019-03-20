b8f9834b2d4891d7e225006c8bacdcbc01e6b1be
Matt Rice <matthewcrice32@gmail.com>
2019-03-20 11:41:47 -0400

    Add and verify mainnet and ropsten deployments (#345)

--- ci/run_truffle_tests.sh
+++ ci/run_truffle_tests.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+# Verifies persistent deployments stored in the networks/ directory.
+check_deployment() {
+    # $1 is network_id
+    local fname=networks/$1.json
+    local network_name=$2
+    if [ -e $fname ]
+    then
+        echo "Checking ${network_name} deployment stored in ${fname}."
+        $(npm bin)/truffle exec ./scripts/CheckDeploymentValidity.js --network $network_name
+    else
+        echo "No ${network_name} deployment to verify."
+    fi
+}
+
 # Test migration
 $(npm bin)/truffle migrate --reset --network ci
 
@@ -9,8 +23,12 @@ rm -rf build
 $(npm bin)/truffle compile
 $(npm bin)/apply-registry
 
-# Verify the validity of the deployment
+# Verify the validity of the ci migration.
 $(npm bin)/truffle exec ./scripts/CheckDeploymentValidity.js --network ci
+
+# Verify the validity of mainnet and ropsten deployments if they exist.
+check_deployment 1 mainnet
+check_deployment 3 ropsten
 
 # Run standard truffle tests
 $(npm bin)/truffle test --network ci
