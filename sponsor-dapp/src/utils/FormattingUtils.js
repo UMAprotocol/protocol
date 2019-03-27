0d6fa22d7aa81a6faf7d234f72bc4d8affb578df
Matt Rice <matthewcrice32@gmail.com>
2019-03-27 18:26:20 -0400

    Limit decimal length for under-box text (#359)

--- sponsor-dapp/src/utils/FormattingUtils.js
+++ sponsor-dapp/src/utils/FormattingUtils.js
@@ -1,3 +1,5 @@
+const BigNumber = require("bignumber.js");
+
 export function formatDate(timestampInSeconds, web3) {
   return new Date(
     parseInt(
@@ -16,4 +18,19 @@ export function formatWei(num, web3) {
   // `web3.utils.isBN(web3.utils.fromBN("5"))` is false), so we use a workaround where we always pass in strings.
   // See https://github.com/ethereum/web3.js/issues/1777.
   return web3.utils.fromWei(num.toString());
+}
+
+// Formats the input to round to decimalPlaces number of decimals.
+export function formatWithMaxDecimals(num, decimalPlaces, roundUp) {
+  if (roundUp) {
+    BigNumber.set({ ROUNDING_MODE: BigNumber.ROUND_UP });
+  } else {
+    BigNumber.set({ ROUNDING_MODE: BigNumber.ROUND_DOWN });
+  }
+
+  const fullPrecisionFloat = BigNumber(num);
+
+  // Convert back to BN to truncate any trailing 0s that the toFixed() output would print.
+  const fixedPrecisionFloat = BigNumber(fullPrecisionFloat.toFixed(decimalPlaces));
+  return fixedPrecisionFloat.toString();
 }
