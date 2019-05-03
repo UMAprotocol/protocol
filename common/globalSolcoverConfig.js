2887979c6763158a20be2bb54477eeb60595a687
Matt Rice <matthewcrice32@gmail.com>
2019-05-03 19:13:44 -0400

    WIP

--- common/globalSolcoverConfig.js
+++ common/globalSolcoverConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   copyPackages: ["openzeppelin-solidity"],
   compileCommand: "../../node_modules/.bin/truffle compile",
-  testCommand: "cp ../../common/globalTruffleConfig.js ./truffle-config.js && ../../node_modules/.bin/truffle test --network coverage",
+  testCommand: "../../node_modules/.bin/truffle test --network coverage",
   port: 8545,
   skipFiles: ["Migrations.sol", "echidna_tests", "test"]
 };
