2e07eec578f8bffee998b5da4866ca59ab450527
Prasad Tare <prasad.tare@gmail.com>
2019-05-08 14:37:08 -0400

    Rename FixedPointUint to Unsigned
    In preparation for renaming the file to just FixedPoint.

--- v1/contracts/UnsignedFixedPoint.sol
+++ v1/contracts/UnsignedFixedPoint.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 /**
  * @title Library for fixed point arithmetic on uints
- * TODO(ptare): Add operations against non-fixed point number, e.g., add(FixedPointUint, uint).
+ * TODO(ptare): Add operations against non-fixed point number, e.g., add(Unsigned, uint).
  */
 library UnsignedFixedPoint {
 
@@ -15,37 +15,37 @@ library UnsignedFixedPoint {
     // Can represent a value up to (2^256 - 1)/10^18 = ~10^59.
     uint private constant FP_SCALING_FACTOR = 10**18;
 
-    struct FixedPointUint {
+    struct Unsigned {
         uint value;
     }
 
-    /** @dev Adds two `FixedPointUint`s, reverting on overflow. */
-    function add(FixedPointUint memory a, FixedPointUint memory b) internal pure returns (FixedPointUint memory) {
-        return FixedPointUint(a.value.add(b.value));
+    /** @dev Adds two `Unsigned`s, reverting on overflow. */
+    function add(Unsigned memory a, Unsigned memory b) internal pure returns (Unsigned memory) {
+        return Unsigned(a.value.add(b.value));
     }
 
-    /** @dev Subtracts two `FixedPointUint`s, reverting on underflow. */
-    function sub(FixedPointUint memory a, FixedPointUint memory b) internal pure returns (FixedPointUint memory) {
-        return FixedPointUint(a.value.sub(b.value));
+    /** @dev Subtracts two `Unsigned`s, reverting on underflow. */
+    function sub(Unsigned memory a, Unsigned memory b) internal pure returns (Unsigned memory) {
+        return Unsigned(a.value.sub(b.value));
     }
 
-    /** @dev Multiplies two `FixedPointUint`s, reverting on overflow. */
-    function mul(FixedPointUint memory a, FixedPointUint memory b) internal pure returns (FixedPointUint memory) {
+    /** @dev Multiplies two `Unsigned`s, reverting on overflow. */
+    function mul(Unsigned memory a, Unsigned memory b) internal pure returns (Unsigned memory) {
         // There are two caveats with this computation:
         // 1. Max output is ~10^41 (vs ~10^59 as the max value that can be represented), otherwise an intermediate value
         // overflows.
         // 2. Results that can't be represented exactly are truncated not rounded. E.g., 1.4 * 2e-18 = 2.8e-18, which
         // would round to 3, but this computation produces the result 2.
         // No need to use SafeMath because FP_SCALING_FACTOR != 0.
-        return FixedPointUint(a.value.mul(b.value) / FP_SCALING_FACTOR);
+        return Unsigned(a.value.mul(b.value) / FP_SCALING_FACTOR);
     }
 
-    /** @dev Divides with truncation two `FixedPointUint`s, reverting on overflow or division by 0. */
-    function div(FixedPointUint memory a, FixedPointUint memory b) internal pure returns (FixedPointUint memory) {
+    /** @dev Divides with truncation two `Unsigned`s, reverting on overflow or division by 0. */
+    function div(Unsigned memory a, Unsigned memory b) internal pure returns (Unsigned memory) {
         // There are two caveats with this computation:
         // 1. Max value for the dividend `a` is ~10^41, otherwise an intermediate value overflows.
         // 2. Results that can't be represented exactly are truncated not rounded. E.g., 2 / 3 = 0.6 repeating, which
         // would round to 0.666666666666666667, but this computation produces the result 0.666666666666666666.
-        return FixedPointUint(a.value.mul(FP_SCALING_FACTOR).div(b.value));
+        return Unsigned(a.value.mul(FP_SCALING_FACTOR).div(b.value));
     }
 }
--- v1/contracts/test/UnsignedFixedPointTest.sol
+++ v1/contracts/test/UnsignedFixedPointTest.sol
@@ -5,21 +5,21 @@ import "../UnsignedFixedPoint.sol";
 
 // Wraps the UnsignedFixedPoint library for testing purposes.
 contract UnsignedFixedPointTest {
-    using UnsignedFixedPoint for UnsignedFixedPoint.FixedPointUint;
+    using UnsignedFixedPoint for UnsignedFixedPoint.Unsigned;
 
     function wrapAdd(uint a, uint b) external view returns (uint) {
-        return UnsignedFixedPoint.FixedPointUint(a).add(UnsignedFixedPoint.FixedPointUint(b)).value;
+        return UnsignedFixedPoint.Unsigned(a).add(UnsignedFixedPoint.Unsigned(b)).value;
     }
 
     function wrapSub(uint a, uint b) external view returns (uint) {
-        return UnsignedFixedPoint.FixedPointUint(a).sub(UnsignedFixedPoint.FixedPointUint(b)).value;
+        return UnsignedFixedPoint.Unsigned(a).sub(UnsignedFixedPoint.Unsigned(b)).value;
     }
 
     function wrapMul(uint a, uint b) external view returns (uint) {
-        return UnsignedFixedPoint.FixedPointUint(a).mul(UnsignedFixedPoint.FixedPointUint(b)).value;
+        return UnsignedFixedPoint.Unsigned(a).mul(UnsignedFixedPoint.Unsigned(b)).value;
     }
 
     function wrapDiv(uint a, uint b) external view returns (uint) {
-        return UnsignedFixedPoint.FixedPointUint(a).div(UnsignedFixedPoint.FixedPointUint(b)).value;
+        return UnsignedFixedPoint.Unsigned(a).div(UnsignedFixedPoint.Unsigned(b)).value;
     }
 }
