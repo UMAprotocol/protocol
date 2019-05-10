64c9a26722f77ba39c201bae4646673c3d1b3a28
Roz Stengle <rosalind.stengle@gmail.com>
2019-05-10 17:50:59 -0400

    WIP
    Signed-off-by: Roz Stengle <rosalind.stengle@gmail.com>

---
+++ v1/migrations/3_deploy_store.js
@@ -0,0 +1,12 @@
+const Store = artifacts.require("Store");
+const { getKeysForNetwork, deployAndGet, addToTdr } = require("../../common/MigrationUtils.js");
+
+	module.exports = async function(deployer, network, accounts) {
+		const keys = getKeysForNetwork(network, accounts);
+
+		const store = await deployAndGet(deployer, Store, { from: keys.store});
+		await addToTdr(store, network);
+
+		//TODO set oracle fees & constants
+		const secondsPerYear = 31536000;
+	};
