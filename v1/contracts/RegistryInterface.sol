5cdc6e2f653e6f7fefd7a953736fcf747a4a5131
Prasad Tare <ptare@users.noreply.github.com>
2019-05-14 18:16:57 -0400

    Slight improvements to Registry (#412)
    Signed-off-by: Roz Stengle <rosalind.stengle@gmail.com>

--- v1/contracts/RegistryInterface.sol
+++ v1/contracts/RegistryInterface.sol
@@ -1,27 +1,35 @@
-/*
-  Registry Interface
-*/
 pragma solidity ^0.5.0;
 
 pragma experimental ABIEncoderV2;
 
 
+/**
+ * @title Interface for a registry of derivatives and derivative creators
+ */
 interface RegistryInterface {
     struct RegisteredDerivative {
         address derivativeAddress;
         address derivativeCreator;
     }
 
-    // Registers a new derivative. Only authorized derivative creators can call this method.
+    /**
+     * @dev Registers a new derivative. Only authorized derivative creators can call this method.
+     */
     function registerDerivative(address[] calldata counterparties, address derivativeAddress) external;
 
-    // Returns whether the derivative has been registered with the registry (and is therefore an authorized participant
-    // in the UMA system).
+    /**
+     * @dev Returns whether the derivative has been registered with the registry (and is therefore an authorized
+     * participant in the UMA system).
+     */
     function isDerivativeRegistered(address derivative) external view returns (bool isRegistered);
 
-    // Returns a list of all derivatives that are associated with a particular party.
+    /**
+     * @dev Returns a list of all derivatives that are associated with a particular party.
+     */
     function getRegisteredDerivatives(address party) external view returns (RegisteredDerivative[] memory derivatives);
 
-    // Returns all registered derivatives.
+    /**
+     * @dev Returns all registered derivatives.
+     */
     function getAllRegisteredDerivatives() external view returns (RegisteredDerivative[] memory derivatives);
 }
