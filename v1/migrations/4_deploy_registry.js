dc32c29511916f39c8e160cb95bca154dbdb72df
Prasad Tare <prasad.tare@gmail.com>
2019-05-17 11:28:49 -0400

    Rename v0/ to core/ and delete v1/

--- v1/migrations/4_deploy_registry.js
+++
@@ -1,9 +0,0 @@
-const Registry = artifacts.require("Registry");
-const { getKeysForNetwork, deployAndGet, addToTdr } = require("../../common/MigrationUtils.js");
-
-module.exports = async function(deployer, network, accounts) {
-  const keys = getKeysForNetwork(network, accounts);
-
-  const registry = await deployAndGet(deployer, Registry, { from: keys.deployer });
-  await addToTdr(registry, network);
-};
--- v1/utils/Constants.js
+++
@@ -1,8 +0,0 @@
-// The interface names that Finder.sol uses to refer to interfaces in the UMA system.
-const interfaceName = {
-  Registry: "Registry"
-};
-
-module.exports = {
-  interfaceName
-};
