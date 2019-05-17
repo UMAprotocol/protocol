2b90623b51f2714c8c4dbdea11620bc1abb813e3
Prasad Tare <prasad.tare@gmail.com>
2019-05-17 15:38:57 -0400

    UNTEST UNTEST UNTEST

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -6,7 +6,7 @@ sudo chmod -R a+rwx /usr/local/lib/node_modules
 
 run_slither() {
     cd $1
-    $(npm bin)/truffle compile 
+    truffle compile 
 
     cd $PROTOCOL_DIR
     slither --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,locked-ether,reentrancy-eth $1
