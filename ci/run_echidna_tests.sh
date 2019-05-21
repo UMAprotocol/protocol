a0f5fac7a499f4dbe141bdae4fd2c266feacd2dd
Prasad Tare <prasad.tare@gmail.com>
2019-05-21 13:47:07 -0400

    Bring back run_echida_tests.sh script

---
+++ ci/run_echidna_tests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# TODO(ptare): This script is NOT currently run from CI.
+# Note: this script should be run from inside the trailofbits/eth-security-toolbox docker container with this
+# repository mounted at ~/protocol.
+# An example run command would look like the following:
+# docker run -v `pwd`:/home/ethsec/protocol -w /home/ethsec/protocol trailofbits/eth-security-toolbox ci/run_echidna_tests.sh
+
+# Note: this assumes the .sol and .yaml files are 1:1 and have the same name.
+run_echidna_test() {
+    local prefix=v0/contracts/echidna_tests/
+    local solidity_fname=$prefix$1.sol
+    local config_fname=$prefix$1.yaml
+    local contract_name=$2
+    local output=$(echidna-test $solidity_fname $contract_name --config=$config_fname)
+    echo "$output"
+    local num_failures=$(echo $output | grep -ci "failed")
+    if [ $num_failures -ne 0 ]
+    then
+        # If we found failures, exit.
+        exit 1
+    fi
+}
+
+
+run_echidna_test LeveragedReturnCalculatorTest Leveraged1xTest
+run_echidna_test LeveragedReturnCalculatorTest Leveraged4xTest
+run_echidna_test LeveragedReturnCalculatorTest LeveragedShort1xTest
+run_echidna_test LeveragedReturnCalculatorTest LeveragedShort3xTest
