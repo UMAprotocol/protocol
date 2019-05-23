c5cfcd8426ad9f1801c66ad3114b6783edd3c90b
Yutaro Mori <yutaro@umaproject.org>
2019-05-23 11:29:05 -0400

    solhint disable

--- core/contracts/test/TestableTest.sol
+++ core/contracts/test/TestableTest.sol
@@ -5,7 +5,6 @@ import "../Testable.sol";
 
 // TestableTest is derived from the abstract contract Testable for testing purposes.
 contract TestableTest is Testable {
-    constructor(bool _isTest) public Testable(_isTest) {
-        return;
-    }
+    // solhint-disable-next-line no-empty-blocks
+    constructor(bool _isTest) public Testable(_isTest) {}
 }
