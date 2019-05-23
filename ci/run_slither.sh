a990ec8fb7eae87576c55785fa3f0ae84339f98d
Matthew Rice <matthewcrice32@gmail.com>
2019-05-23 10:54:13 -0400

    WIP
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -13,7 +13,7 @@ run_slither() {
     truffle compile 
 
     cd $PROTOCOL_DIR
-    slither --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,locked-ether,reentrancy-eth $1
+    slither --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,locked-ether $1
 }
 
 run_slither $PROTOCOL_DIR/core
