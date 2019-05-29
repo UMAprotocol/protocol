2991cfe0ee9e99a67fd87d0278fdab6bf2e44989
Roz Stengle <rosalind.stengle@gmail.com>
2019-05-29 13:25:14 -0400

    Mixed comparison tests for FixedPoint library (fixes #458) (#460)
    Signed-off-by: Roz Stengle <rosalind.stengle@gmail.com>

--- core/contracts/test/FixedPointTest.sol
+++ core/contracts/test/FixedPointTest.sol
@@ -17,8 +17,24 @@ contract FixedPointTest {
         return FixedPoint.Unsigned(a).isGreaterThan(FixedPoint.Unsigned(b));
     }
 
+    function wrapMixedIsGreaterThan(uint a, uint b) external pure returns (bool) {
+        return FixedPoint.Unsigned(a).isGreaterThan(b);
+    }
+
+    function wrapMixedIsGreaterThanOpposite(uint a, uint b) external pure returns (bool) {
+        return a.isGreaterThan(FixedPoint.Unsigned(b));
+    }
+
     function wrapIsLessThan(uint a, uint b) external pure returns (bool) {
         return FixedPoint.Unsigned(a).isLessThan(FixedPoint.Unsigned(b));
+    }
+
+    function wrapMixedIsLessThan(uint a, uint b) external pure returns (bool) {
+        return FixedPoint.Unsigned(a).isLessThan(b);
+    }
+
+    function wrapMixedIsLessThanOpposite(uint a, uint b) external pure returns (bool) {
+        return a.isLessThan(FixedPoint.Unsigned(b));
     }
 
     function wrapAdd(uint a, uint b) external pure returns (uint) {
