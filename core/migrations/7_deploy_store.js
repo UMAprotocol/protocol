2a5715c4d1e6645d6666ad31b72503ed237c5ecc
Yutaro Mori <yutaro@umaproject.org>
2019-05-29 10:43:10 -0400

    Store compute fees (#463)
    * Store compute fees
    
    Signed-off-by: Yutaro Mori <yutaro@umaproject.org>
    
    * Fix order of migration scripts
    
    Signed-off-by: Yutaro Mori <yutaro@umaproject.org>
    
    * Fix CI
    
    Signed-off-by: Yutaro Mori <yutaro@umaproject.org>

--- core/migrations/7_deploy_store.js
+++ core/migrations/7_deploy_store.js
@@ -1,8 +1,13 @@
 const CentralizedStore = artifacts.require("CentralizedStore");
+const Store = artifacts.require("Store");
+
 const { getKeysForNetwork, deployAndGet, addToTdr } = require("../../common/MigrationUtils.js");
 
 module.exports = async function(deployer, network, accounts) {
   const keys = getKeysForNetwork(network, accounts);
+
+  const store = await deployAndGet(deployer, Store, { from: keys.store });
+  await addToTdr(store, network);
 
   const centralizedStore = await deployAndGet(deployer, CentralizedStore, { from: keys.store });
   await addToTdr(centralizedStore, network);
