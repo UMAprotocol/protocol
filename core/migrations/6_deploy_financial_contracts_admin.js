ad56977a6e80b08d143cccaade66f12988d60979
Prasad Tare <prasad.tare@gmail.com>
2019-05-31 15:43:08 -0400

    Move over several helper scripts
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- core/migrations/6_deploy_financial_contracts_admin.js
+++ core/migrations/6_deploy_financial_contracts_admin.js
@@ -6,4 +6,7 @@ module.exports = async function(deployer, network, accounts) {
 
   const financialContractsAdmin = await deployAndGet(deployer, FinancialContractsAdmin, { from: keys.deployer });
   await addToTdr(financialContractsAdmin, network);
+
+  const remarginRole = "1"; // Corresponds to FinancialContractsAdmin.Roles.Remargin.
+  await financialContractsAdmin.addMember(remarginRole, keys.deployer);
 };
