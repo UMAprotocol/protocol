9c569b40be5174850cf04a691c5e43ab2685ef8d
Prasad Tare <ptare@users.noreply.github.com>
2019-06-04 15:00:45 -0400

    Move over several helper scripts (#477)
    I moved some of these from scripts/test to scripts because they look like they could be useful for interacting with the main deployments as well, e.g., WhitelistToken.
    
    I've only tested these scripts against local Ganache, they might need to modified a bit to interact with the testnet/mainnet after we get the keys set up.
    
    Issue #473

--- core/migrations/7_deploy_financial_contracts_admin.js
+++ core/migrations/7_deploy_financial_contracts_admin.js
@@ -9,6 +9,9 @@ module.exports = async function(deployer, network, accounts) {
   const financialContractsAdmin = await deployAndGet(deployer, FinancialContractsAdmin, { from: keys.deployer });
   await addToTdr(financialContractsAdmin, network);
 
+  const remarginRole = "1"; // Corresponds to FinancialContractsAdmin.Roles.Remargin.
+  await financialContractsAdmin.addMember(remarginRole, keys.deployer);
+
   const finder = await Finder.deployed();
   await finder.changeImplementationAddress(
     web3.utils.utf8ToHex(interfaceName.FinancialContractsAdmin),
