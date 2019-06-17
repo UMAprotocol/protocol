d37951d5f391ee5b7f10c3080c86601fb9a38b81
Prasad Tare <ptare@users.noreply.github.com>
2019-06-17 15:42:54 -0400

    Update comment in publishPrice.sh (#497)

--- core/scripts/publishPrices.sh
+++ core/scripts/publishPrices.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Usage: ./scripts/publishPrices.sh <args to pass to PublishPrices>
 # Example: ./scripts/publishPrices.sh --network mainnet --keys priceFeed
-# Note: this script must be run from the top level directory (not scripts/).
+# Note: this script must be run from the core/ directory (not core/scripts/).
 
 while sleep 60; do $(npm bin)/truffle exec ./scripts/PublishPrices.js "$@" >>out.log 2>&1; done
