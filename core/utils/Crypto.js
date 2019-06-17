c4b55386e1bbfcb2523b207b0ddb0bff4068ebeb
Matthew Rice <matthewcrice32@gmail.com>
2019-06-17 16:10:22 -0400

    comments
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- core/utils/Crypto.js
+++ core/utils/Crypto.js
@@ -3,17 +3,20 @@ const EthCrypto = require("eth-crypto");
 // Encrypts a message using an ethereum public key. To decrypt messages that are encrypted with this method, use
 // decryptMessage().
 async function encryptMessage(publicKey, message) {
+    // substr(2) removes the web3 friendly "0x" from the public key.
     const encryptedMessageObject = await EthCrypto.encryptWithPublicKey(publicKey.substr(2), message);
     return "0x" + EthCrypto.cipher.stringify(encryptedMessageObject);
 }
 
 // Converts an ethereum public key to the corresponding address.
 function addressFromPublicKey(publicKey) {
+    // substr(2) just removes the web3 friendly "0x" from the public key.
     return EthCrypto.publicKey.toAddress(publicKey.substr(2));
 }
 
 // Recovers a public key from a private key.
 function recoverPublicKey(privateKey) {
+    // The "0x" is added to make the public key web3 friendly.
     return "0x" + EthCrypto.publicKeyByPrivateKey(privateKey);
 }
 
