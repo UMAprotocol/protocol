3343a58435691fe448dee3e5188b0fc6f73844b9
Roz Stengle <rosalind.stengle@gmail.com>
2019-06-18 16:40:24 -0400

    first pass at retrieving historical prices
    Signed-off-by: Roz Stengle <rosalind.stengle@gmail.com>

--- core/scripts/Voting.js
+++ core/scripts/Voting.js
@@ -1,9 +1,53 @@
 const Voting = artifacts.require("Voting");
 const { VotePhasesEnum } = require("../utils/Enums");
+const fetch = require("node-fetch");
 
-// TODO(#492): Implement price fetching logic.
+//require("dotenv").config();
+// Get API keys from environment variables or the `.env` file.
+// const cryptocompareKey = process.env.CRYPTO_COMPARE_KEY;
+
+function stripApiKey(str, key) {
+  return str.replace(key, "{redacted}");
+}
+
+// Gets JSON from a URL or throws.
+const getJson = async url => {
+  const response = await fetch(url);
+  const json = await response.json();
+  if (!json) {
+    throw `Query [${url}] failed to get JSON`;
+  }
+  return json;
+};
+
 async function fetchPrice(request) {
-  return web3.utils.toWei("1.5");
+
+  const identifier = request.identifier;
+  const time = request.time;
+
+  // Temporary price feed until we sort historical data.
+  const url = `https://min-api.cryptocompare.com/data/histohour?fsym=${identifier.first}&tsym=${identifier.second}&limit=3`;
+  console.log(`Querying with [${url}]`);
+  const jsonOutput = await getJson(url);
+  console.log(`Response [${JSON.stringify(jsonOutput)}]`);
+
+  if (jsonOutput.Type != "100") {
+    throw "Request failed";
+  }
+
+  if (jsonOutput.Data == null) {
+    throw "Unexpected number of results in json response";
+  }
+
+  const price = jsonOutput.Data[0].open;
+  if (!price) {
+    throw "Failed to get valid price out of JSON response";
+  }
+
+  const tradeTime = jsonOutput.Data[0].time;
+  console.log(`Retrieved quote [${price}] at [${tradeTime}] for asset [${identifier.first}${identifier.second}]`);
+
+  return { price, tradeTime };
 }
 
 // TODO(#493): Implement persistance.
@@ -61,7 +105,8 @@ async function runVoting() {
 }
 
 run = async function(callback) {
-  await runVoting();
+  var request = {identifier:{first:"BTC", second:"USD"}, time:"1560762000"};
+  await fetchPrice(request);
   callback();
 };
 run.VotingSystem = VotingSystem;
