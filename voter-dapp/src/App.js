8d8c1df412abf5a784fef4cfd2966786e27611ee
Prasad Tare <prasad.tare@gmail.com>
2019-06-28 11:31:30 -0400

    Remove unneeded boilerplate
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- voter-dapp/src/App.js
+++ voter-dapp/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Voter DApp</div>;
+  return <div className="App">Voter dApp</div>;
 }
 
 export default App;
