d76e02ee3a7907b3ee994f0cbc2c970eb72dc206
Prasad Tare <prasad.tare@gmail.com>
2019-07-02 10:23:49 -0400

    Add table showing active (i.e., pending) requests
    This PR integrates the voter-dapp with Drizzle. We still have to use our
    fork of Drizzle because the version of web3 Drizzle uses doesn't support
    structs as return values (e.g., from `getPendingRequests`).
    
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- voter-dapp/src/App.js
+++ voter-dapp/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { drizzleReactHooks } from "drizzle-react";
 import "./App.css";
+import ActiveRequests from "./ActiveRequests.js";
 
 function App() {
-  return <div className="App">Voter dApp</div>;
+  const drizzleState = drizzleReactHooks.useDrizzleState(drizzleState => {
+    return {
+      initialized: drizzleState.drizzleStatus.initialized
+    };
+  });
+  if (!drizzleState.initialized) {
+    return <div>Loading</div>;
+  } else {
+    return (
+      <div>
+        Voter dApp <ActiveRequests />
+      </div>
+    );
+  }
 }
 
 export default App;
