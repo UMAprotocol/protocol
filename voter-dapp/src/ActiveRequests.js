cbf1c96c1270b4285b8a82cb430985f589f67165
Prasad Tare <prasad.tare@gmail.com>
2019-07-15 14:29:15 -0400

    Add column for vote status
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- voter-dapp/src/ActiveRequests.js
+++ voter-dapp/src/ActiveRequests.js
@@ -7,19 +7,69 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
 import { formatDate } from "./common/FormattingUtils.js";
+import { VotePhasesEnum } from "./common/Enums.js";
 
 function ActiveRequests() {
   const { drizzle, useCacheCall } = drizzleReactHooks.useDrizzle();
+  const { web3 } = drizzle;
+
   const pendingRequests = useCacheCall("Voting", "getPendingRequests");
-  if (!pendingRequests) {
+  const currentRoundId = useCacheCall("Voting", "getCurrentRoundId");
+  const votePhase = useCacheCall("Voting", "getVotePhase");
+  const account = drizzleReactHooks.useDrizzleState(drizzleState => ({
+    account: drizzleState.accounts[0]
+  })).account;
+  const initialFetchComplete = pendingRequests && currentRoundId && votePhase && account;
+
+  const voteStatuses = useCacheCall(["Voting"], call => {
+    if (!initialFetchComplete) {
+      return null;
+    }
+    return pendingRequests.map(request => ({
+      committedValue: call(
+        "Voting",
+        "getMessage",
+        account,
+        web3.utils.soliditySha3(request.identifier, request.time, currentRoundId)
+      ),
+      // Technically this call can be avoided if `votePhase == COMMIT`.
+      hasRevealed: call("Voting", "hasRevealedVote", request.identifier, request.time)
+    }));
+  });
+  const subsequentFetchComplete =
+    Boolean(voteStatuses) &&
+    // Each of the subfetches has to complete. In drizzle, `undefined` means incomplete, while `null` means complete
+    // but the fetched value was null, e.g., no `comittedValue` existed.
+    voteStatuses.every(
+      voteStatus => typeof voteStatus.committedValue !== "undefined" || typeof voteStatus.hasRevealed !== "undefined"
+    );
+
+  if (!initialFetchComplete || !subsequentFetchComplete) {
     return <div>Looking up requests</div>;
   }
+
+  const statusStrings = voteStatuses.map(voteStatus => {
+    if (votePhase.toString() === VotePhasesEnum.COMMIT) {
+      return "Commit";
+    }
+    // In the REVEAL phase.
+    if (voteStatus.hasRevealed) {
+      return "Revealed";
+    }
+    // In the REVEAL phase, but the vote hasn't been revealed (yet).
+    if (voteStatus.committedValue) {
+      return "Reveal";
+    } else {
+      return "Cannot be revealed";
+    }
+  });
   return (
     <Table>
       <TableHead>
         <TableRow>
           <TableCell>Price Feed</TableCell>
           <TableCell>Timestamp</TableCell>
+          <TableCell>Status</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
@@ -28,6 +78,7 @@ function ActiveRequests() {
             <TableRow key={index}>
               <TableCell>{drizzle.web3.utils.hexToUtf8(pendingRequest.identifier)}</TableCell>
               <TableCell>{formatDate(pendingRequest.time, drizzle.web3)}</TableCell>
+              <TableCell>{statusStrings[index]}</TableCell>
             </TableRow>
           );
         })}
--- voter-dapp/src/drizzleOptions.js
+++ voter-dapp/src/drizzleOptions.js
@@ -1,7 +1,12 @@
 import Voting from "./contracts/Voting.json";
 
 const options = {
-  contracts: [Voting]
+  contracts: [Voting],
+  polls: {
+    accounts: 1000,
+    blocks: 3000
+  },
+  syncAlways: true
 };
 
 export default options;
