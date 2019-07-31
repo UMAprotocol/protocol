f278847dbbc1c047ad1948983b958c2636b2e925
Roz Stengle <rosalind.stengle@gmail.com>
2019-07-31 19:28:17 -0400

    Voter Dapp Header (#577)
    Signed-off-by: Roz Stengle <rosalind.stengle@gmail.com>

--- voter-dapp/src/App.js
+++ voter-dapp/src/App.js
@@ -1,24 +1,47 @@
 import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import { drizzleReactHooks } from "drizzle-react";
 import "./App.css";
+import Header from "./Header.js";
 import ActiveRequests from "./ActiveRequests.js";
-import ResolvedRequests from "./ResolvedRequests.js";
+import AppBar from "@material-ui/core/AppBar";
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#FF4A4A"
+      },
+      secondary: {
+        main: "#272528"
+      }
+    },
+    typography: {
+      useNextVariants: true,
+      fontFamily: "Verdana"
+    }
+  });
+
   const drizzleState = drizzleReactHooks.useDrizzleState(drizzleState => {
     return {
       initialized: drizzleState.drizzleStatus.initialized
     };
   });
   if (!drizzleState.initialized) {
-    return <div>Loading</div>;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Typography variant="body2">Loading...</Typography>
+      </MuiThemeProvider>
+    );
   } else {
     return (
-      <div>
-        Voter dApp
-        <ActiveRequests />
-        <ResolvedRequests />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <AppBar color="secondary">
+          <Header />
+        </AppBar>
+        Voter dApp <ActiveRequests />
+      </MuiThemeProvider>
     );
   }
 }
