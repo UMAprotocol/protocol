b8e2bef9f6f12548f0e966aeb89dead092cefbee
Matthew Rice <matthewcrice32@gmail.com>
2019-07-31 17:04:45 -0400

    finish comm
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- voter-dapp/src/DrizzleInit.js
+++ voter-dapp/src/DrizzleInit.js
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { drizzleReactHooks } from "drizzle-react";
 
 // This component is just a wrapper to trigger a callback once drizzle has been initialized.
-// It must be a separate compon
+// It must be a separate component to allow useDrizzleState to only be called after the user has initiated the login
+// flow.
 function DrizzleInit(props) {
   const { initializationFinished } = props;
 
