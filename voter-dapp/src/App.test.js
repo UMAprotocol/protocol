80b9e37a670ce0386f7cd0d453696cee0535019d
Matthew Rice <matthewcrice32@gmail.com>
2019-07-31 19:18:26 -0400

    more test changes
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- voter-dapp/src/App.test.js
+++ voter-dapp/src/App.test.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Dashboard from "./Dashboard.js";
-import { Drizzle, generateStore } from "drizzle";
-import { drizzleReactHooks } from "drizzle-react";
-import drizzleOptions from "./drizzleOptions";
 
 const fakeEthereumObject = {
   // Enable goes through immediately and returns 0x0 as the selected address.
