0899187a423304edfebc2fd8fb77b54806da4dce
Prasad Tare <ptare@users.noreply.github.com>
2019-08-16 15:18:01 -0400

    Setup Drizzle (#594)
    Issue #589
    
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- sponsor-dapp-v2/src/components/application/index.js
+++ sponsor-dapp-v2/src/components/application/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Switch, Route, withRouter } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
@@ -6,29 +6,36 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 import routes from "lib/routes";
 
 import { fetchAllPositions } from "store/state/positions/actions";
+import { drizzleReactHooks } from "drizzle-react";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchAllPositions();
+function App(props) {
+  const { fetchAllPositions } = props;
+  useEffect(() => {
+    fetchAllPositions();
+  }, [fetchAllPositions]);
+
+  const isDrizzleInitialized = drizzleReactHooks.useDrizzleState(drizzleState => ({
+    initialized: drizzleState.drizzleStatus.initialized
+  })).initialized;
+
+  if (!isDrizzleInitialized) {
+    return <div>Loading</div>;
   }
 
-  render() {
-    const { location } = this.props;
-
-    return (
-      <div className="App">
-        <TransitionGroup>
-          <CSSTransition key={location.key} timeout={{ enter: 300, exit: 300 }} classNames={"fade"}>
-            <Switch>
-              {routes.map(route => (
-                <Route key={route.path} {...route} />
-              ))}
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
-      </div>
-    );
-  }
+  const { location } = props;
+  return (
+    <div className="App">
+      <TransitionGroup>
+        <CSSTransition key={location.key} timeout={{ enter: 300, exit: 300 }} classNames={"fade"}>
+          <Switch>
+            {routes.map(route => (
+              <Route key={route.path} {...route} />
+            ))}
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
 }
 
 export default withRouter(
