0ada91cf88e6fda39e55e8696e69f9136973f079
Prasad Tare <ptare@users.noreply.github.com>
2019-08-19 16:47:52 -0400

    Start filling in ManagePositions page (#598)
    Several fields in the details dropdown aren't filled in yet.
    
    This PR replaces the contract address with the token name. The contract address is still available in the details dropdown.
    
    Issue #595.
    
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- sponsor-dapp-v2/src/views/ManagePositions.js
+++ sponsor-dapp-v2/src/views/ManagePositions.js
@@ -1,21 +1,142 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { drizzleReactHooks } from "drizzle-react";
 
 import Header from "components/common/Header";
 import ExpandBox from "components/common/ExpandBox";
 import Tooltip from "components/common/Tooltip";
+import { withAddedContract } from "lib/contracts";
+import TokenizedDerivative from "contracts/TokenizedDerivative.json";
+import { formatWei, formatWithMaxDecimals } from "common/FormattingUtils";
 
-class ManagePositions extends Component {
-  render() {
-    const { managePosition } = this.props;
+function getStateDescription(derivativeStorage) {
+  switch (derivativeStorage.state) {
+    case "0":
+      return {
+        stateText: "Live",
+        stateColor: "green"
+      };
+    case "1":
+      return {
+        stateText: "Disputed",
+        stateColor: "yellow"
+      };
+    case "2":
+      return {
+        stateText: "Expired",
+        stateColor: "yellow"
+      };
+    case "3":
+      return {
+        stateText: "Defaulted",
+        stateColor: "red"
+      };
+    case "4":
+      return {
+        stateText: "Emergency",
+        stateColor: "red"
+      };
+    case "5":
+      return {
+        stateText: "Settled",
+        stateColor: "blue"
+      };
+    default:
+      throw new Error("Contract is in unknown state: " + derivativeStorage.state);
+  }
+}
 
-    if (!managePosition) {
-      return null;
+// Returns an object that contains data about the financial contract (TokenizedDerivative) at `tokenAddress`. The
+// returned object contains a field `ready` that should be checked first.
+function useFinancialContractData(tokenAddress) {
+  const { drizzle, useCacheCall } = drizzleReactHooks.useDrizzle();
+  const { web3 } = drizzle;
+
+  const { account } = drizzleReactHooks.useDrizzleState(drizzleState => ({
+    account: drizzleState.accounts[0]
+  }));
+
+  const data = {};
+
+  data.derivativeStorage = useCacheCall(tokenAddress, "derivativeStorage");
+  data.name = useCacheCall(tokenAddress, "name");
+  data.symbol = useCacheCall(tokenAddress, "symbol");
+  data.nav = useCacheCall(tokenAddress, "calcNAV");
+  data.tokenValue = useCacheCall(tokenAddress, "calcTokenValue");
+  data.shortMarginBalance = useCacheCall(tokenAddress, "calcShortMarginBalance");
+  data.excessMargin = useCacheCall(tokenAddress, "calcExcessMargin");
+  // TODO(ptare): This may revert in certain cases (which are unlikely to come up now).
+  data.updatedUnderlyingPrice = useCacheCall(tokenAddress, "getUpdatedUnderlyingPrice");
+
+  data.totalSupply = useCacheCall(tokenAddress, "totalSupply");
+  data.tokenBalance = useCacheCall(tokenAddress, "balanceOf", account);
+
+  if (!Object.values(data).every(Boolean)) {
+    return { ready: false };
+  }
+  data.ready = true;
+
+  // Format financial contract data for display.
+  const { toBN } = web3.utils;
+  const scalingFactor = toBN(web3.utils.toWei("1"));
+  const computeSafePercentage = (numerator, denominator) =>
+    denominator.isZero()
+      ? "0"
+      : toBN(numerator)
+          .div(denominator)
+          .muln(100);
+
+  const totalSupplyBn = toBN(data.totalSupply);
+  data.tokenOwnershipPercentage = computeSafePercentage(data.tokenBalance, totalSupplyBn);
+  data.tokenOwnershipValue = totalSupplyBn.mul(toBN(data.tokenValue)).div(scalingFactor);
+  const navBn = toBN(data.nav);
+  const shortMarginBalanceBn = toBN(data.shortMarginBalance);
+  data.totalHoldings = navBn.add(shortMarginBalanceBn);
+  data.collateralizationRatio = computeSafePercentage(data.totalHoldings, navBn);
+  data.minRequiredMargin = shortMarginBalanceBn.sub(toBN(data.excessMargin));
+  data.minCollateralizationPercentage = computeSafePercentage(data.minRequiredMargin.add(navBn), navBn);
+  const { stateText, stateColor } = getStateDescription(data.derivativeStorage);
+  data.stateText = stateText;
+  data.stateColor = stateColor;
+  // TODO(ptare): The following fields still need to be added: Created, Expiry, and Price feed.
+  data.detailsContent = [
+    { type: "timestamp", title: "Last contract valuation", timestamp: data.updatedUnderlyingPrice.time },
+    { type: "address", title: "Address", address: { display: tokenAddress } },
+    { type: "address", title: "Sponsor", address: { display: data.derivativeStorage.externalAddresses.sponsor } },
+    {
+      type: "namedAddress",
+      title: "Denomination",
+      name: "DAI",
+      address: { display: data.derivativeStorage.externalAddresses.marginCurrency }
+    },
+    {
+      type: "namedAddress",
+      title: "Return calculator",
+      name: "1x",
+      address: { display: data.derivativeStorage.externalAddresses.returnCalculator }
     }
+  ];
 
-    const { address } = managePosition.contractStatus;
+  return data;
+}
 
+function ManagePositions(props) {
+  const {
+    drizzle: { web3 }
+  } = drizzleReactHooks.useDrizzle();
+
+  const data = useFinancialContractData(props.match.params.tokenAddress);
+  if (!data.ready) {
+    return <div>Loading data</div>;
+  }
+
+  const numDisplayedDecimals = 4;
+  const format = valInWei => formatWithMaxDecimals(formatWei(valInWei, web3), numDisplayedDecimals, false);
+
+  // This function is only added temporarily to make the diff more readable. Otherwise, github's diff tool isn't able
+  // to recognize that the JSX below is mostly unchanged.
+  function render() {
     return (
       <div className="wrapper">
         <Header />
@@ -31,25 +152,24 @@ class ManagePositions extends Component {
                 <div className="section__head-aside">
                   <div className="section__status">
                     <span>
-                      {address.display.slice(0, 8)}...
-                      {address.display.slice(-1)}
+                      {data.name} ({data.symbol})
                     </span>
 
                     <div className="indicator">
                       <span
                         className="icon"
                         style={{
-                          backgroundColor: `${managePosition.contractStatus.statusColor}`
+                          backgroundColor: `${data.stateColor}`
                         }}
                       />
-                      {managePosition.contractStatus.statusText}
+                      {data.stateText}
                     </div>
                   </div>
                 </div>
 
-                {managePosition.details && (
+                {data.detailsContent && (
                   <div className="section__head-content">
-                    <ExpandBox title="Details" content={managePosition.details} />
+                    <ExpandBox title="Details" content={data.detailsContent} />
                   </div>
                 )}
               </div>
@@ -77,7 +197,7 @@ class ManagePositions extends Component {
                             </td>
 
                             <td>
-                              <strong>{managePosition.assets[0].items.value}</strong>
+                              <strong>{format(data.updatedUnderlyingPrice.underlyingPrice)}</strong>
                             </td>
                           </tr>
 
@@ -94,7 +214,7 @@ class ManagePositions extends Component {
                             </td>
 
                             <td>
-                              <strong>{managePosition.assets[1].items.value} DAI</strong>
+                              <strong>{format(data.tokenValue)} DAI</strong>
                             </td>
                           </tr>
                         </tbody>
@@ -125,15 +245,12 @@ class ManagePositions extends Component {
 
                             <td>
                               <strong>
-                                {managePosition.collateral[0].items.amount.absolute} DAI (
-                                {managePosition.collateral[0].items.amount.percentage}%)
+                                {format(data.totalHoldings)} DAI ({data.collateralizationRatio}%)
                               </strong>
                             </td>
 
                             <td>
-                              <strong>
-                                (min. {managePosition.collateral[0].items.liquidation} needed to avoid liquidation)
-                              </strong>
+                              <strong>(min. {data.minCollateralizationPercentage}% needed to avoid liquidation)</strong>
                             </td>
                           </tr>
 
@@ -148,7 +265,7 @@ class ManagePositions extends Component {
                             </td>
 
                             <td>
-                              <strong>{managePosition.collateral[1].items.amount.absolute} DAI</strong>
+                              <strong>{format(data.nav)} DAI</strong>
                             </td>
 
                             <td>&nbsp;</td>
@@ -166,13 +283,11 @@ class ManagePositions extends Component {
                             </td>
 
                             <td>
-                              <strong>{managePosition.collateral[2].items.amount.absolute} DAI</strong>
+                              <strong>{format(data.excessMargin)} DAI</strong>
                             </td>
 
                             <td>
-                              <strong>
-                                (min. {managePosition.collateral[2].items.liquidation} DAI needed to avoid liquidation)
-                              </strong>
+                              <strong>(min. {format(data.minRequiredMargin)} DAI needed to avoid liquidation)</strong>
                             </td>
                           </tr>
                         </tbody>
@@ -211,7 +326,7 @@ class ManagePositions extends Component {
                             </td>
 
                             <td>
-                              <strong>{managePosition.tokens[0].items.amount.absolute} Tokens</strong>
+                              <strong>{format(data.totalSupply)} Tokens</strong>
                             </td>
 
                             <td>&nbsp;</td>
@@ -230,13 +345,12 @@ class ManagePositions extends Component {
 
                             <td>
                               <strong>
-                                {managePosition.tokens[1].items.amount.absolute} (
-                                {managePosition.tokens[1].items.amount.percentage}%)
+                                {format(data.tokenBalance)} ({data.tokenOwnershipPercentage}%)
                               </strong>
                             </td>
 
                             <td>
-                              <strong>({managePosition.tokens[1].items.value} DAI)</strong>
+                              <strong>({format(data.tokenOwnershipValue)} DAI)</strong>
                             </td>
                           </tr>
                         </tbody>
@@ -261,13 +375,9 @@ class ManagePositions extends Component {
       </div>
     );
   }
+  return render();
 }
 
-export default connect(
-  state => ({
-    managePosition: state.positionsData.managePositions
-  }),
-  {
-    // fetchAllPositions
-  }
-)(ManagePositions);
+export default withAddedContract(TokenizedDerivative.abi, props => props.match.params.tokenAddress)(
+  connect()(ManagePositions)
+);
