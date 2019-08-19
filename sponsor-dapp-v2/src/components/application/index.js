e8919dbc3d01b88faf0b232cdf596b5ee90be1c4
Lê Minh Hiệp <53987873+leminhhiep20691@users.noreply.github.com>
2019-08-20 01:54:45 +0700

    Revert "Merge in burger updates (#599)"
    This reverts commit e4e8c15c09718e49d19c6c31a133957a3ad124ea.

--- sponsor-dapp-v2/src/components/application/index.js
+++ sponsor-dapp-v2/src/components/application/index.js
@@ -26,18 +26,7 @@ function App(props) {
   return (
     <div className="App">
       <TransitionGroup>
-        <CSSTransition
-          key={location.key}
-          timeout={{ enter: 300, exit: 300 }}
-          classNames={"fade"}
-          onExit={node => {
-            if (node) {
-              node.style.position = "fixed";
-              node.style.width = "100%";
-              node.style.top = -1 * window.scrollY + "px";
-            }
-          }}
-        >
+        <CSSTransition key={location.key} timeout={{ enter: 300, exit: 300 }} classNames={"fade"}>
           <Switch>
             {routes.map(route => (
               <Route key={route.path} {...route} />
