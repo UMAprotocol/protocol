ffeb092293b24a3b6d80ea2b57f1c1fa8dca3fd2
Matthew Rice <matthewcrice32@gmail.com>
2019-08-20 14:37:18 -0400

    Remove render functions from hookified components
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- sponsor-dapp-v2/src/views/Landing.js
+++ sponsor-dapp-v2/src/views/Landing.js
@@ -10,93 +10,90 @@ import Footer from "components/common/Footer";
 function Landing() {
   const numContracts = useNumRegisteredContracts();
 
-  function render() {
-    if (numContracts === undefined) {
-      return null;
-    }
-
-    if (numContracts !== 0) {
-      return <Redirect to="/ViewPositions" />;
-    }
-
-    return (
-      <div className="wrapper">
-        <Header />
-
-        <div className="main">
-          <div className="shell">
-            <section className="section section--about">
-              <h2>This is a developer tool that lets you:</h2>
-              <div className="section__entry">
-                <p>Demonstrate how you can use UMA infrastructure </p>
-
-                <p>Deploy a custom token facility</p>
-
-                <p>Borrow synthetic ERC20 tokens and track the price of anything</p>
-              </div>
-
-              <Link to="/Start" className="btn">
-                Get started
-              </Link>
-            </section>
-
-            <section className="section section--service-items">
-              <h2>How it works:</h2>
-
-              <div className="service-items">
-                <div className="service-item">
-                  <h3>Create a derivative</h3>
-
-                  <ul className="list-examples">
-                    <li>
-                      <span className="icon">
-                        <IconSvgComponent iconPath="svg/ico-open-door.svg" additionalClass="ico-open-door" />
-                      </span>
-
-                      <p>Open up a facility</p>
-                    </li>
-
-                    <li>
-                      <span className="icon">
-                        <IconSvgComponent iconPath="svg/ico-deposit.svg" additionalClass="ico-deposit" />
-                      </span>
-
-                      <p>Deposit collateral</p>
-                    </li>
-
-                    <li>
-                      <span className="icon">
-                        <IconSvgComponent iconPath="svg/ico-borrow.svg" additionalClass="ico-borrow" />
-                      </span>
-
-                      <p>Borrow synthetic token</p>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="service-item">
-                  <h3>Manage your risk</h3>
-
-                  <ul className="list-examples">
-                    <li>
-                      <span className="icon">
-                        <IconSvgComponent iconPath="svg/ico-sell-tokens.svg" additionalClass="ico-sell-tokens" />
-                      </span>
-
-                      <p>Sell tokens to leverage risk exposure</p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </section>
-          </div>
-        </div>
-
-        <Footer />
-      </div>
-    );
+  if (numContracts === undefined) {
+    return null;
   }
-  return render();
+
+  if (numContracts !== 0) {
+    return <Redirect to="/ViewPositions" />;
+  }
+
+  return (
+    <div className="wrapper">
+      <Header />
+
+      <div className="main">
+        <div className="shell">
+          <section className="section section--about">
+            <h2>This is a developer tool that lets you:</h2>
+            <div className="section__entry">
+              <p>Demonstrate how you can use UMA infrastructure </p>
+
+              <p>Deploy a custom token facility</p>
+
+              <p>Borrow synthetic ERC20 tokens and track the price of anything</p>
+            </div>
+
+            <Link to="/Start" className="btn">
+              Get started
+            </Link>
+          </section>
+
+          <section className="section section--service-items">
+            <h2>How it works:</h2>
+
+            <div className="service-items">
+              <div className="service-item">
+                <h3>Create a derivative</h3>
+
+                <ul className="list-examples">
+                  <li>
+                    <span className="icon">
+                      <IconSvgComponent iconPath="svg/ico-open-door.svg" additionalClass="ico-open-door" />
+                    </span>
+
+                    <p>Open up a facility</p>
+                  </li>
+
+                  <li>
+                    <span className="icon">
+                      <IconSvgComponent iconPath="svg/ico-deposit.svg" additionalClass="ico-deposit" />
+                    </span>
+
+                    <p>Deposit collateral</p>
+                  </li>
+
+                  <li>
+                    <span className="icon">
+                      <IconSvgComponent iconPath="svg/ico-borrow.svg" additionalClass="ico-borrow" />
+                    </span>
+
+                    <p>Borrow synthetic token</p>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="service-item">
+                <h3>Manage your risk</h3>
+
+                <ul className="list-examples">
+                  <li>
+                    <span className="icon">
+                      <IconSvgComponent iconPath="svg/ico-sell-tokens.svg" additionalClass="ico-sell-tokens" />
+                    </span>
+
+                    <p>Sell tokens to leverage risk exposure</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Landing;
--- sponsor-dapp-v2/src/views/ViewPositions.js
+++ sponsor-dapp-v2/src/views/ViewPositions.js
@@ -135,93 +135,89 @@ function ViewPositions() {
   const positions = usePositionList();
   const faucetUrls = useFaucetUrls();
 
-  function render() {
-    // TODO(mrice32): should we have some sort of loading screen to show while data is being pulled?
-    if (positions === undefined) {
-      return null;
-    }
-
-    // TODO(mrice32): potentially merge Start and ViewPositions pages to simplify.
-    // Always redirect to the start screen if there are no positions.
-    if (positions.length === 0) {
-      return <Redirect to="/Start" />;
-    }
-
-    return (
-      <div className="wrapper">
-        <Header />
-
-        <div className="main">
-          <div className="shell">
-            <section className="section section--intro section--intro-alt">
-              <div className="section__actions">
-                <Link to="/Steps" className="btn btn--size1">
-                  Open token facility
-                </Link>
-
-                <div className="section__actions-inner">
-                  {faucetUrls.eth ? (
-                    <a
-                      href={faucetUrls.eth}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn btn--grey btn--size1"
-                    >
-                      Testnet ETH faucet
-                    </a>
-                  ) : null}
-
-                  {faucetUrls.dai ? (
-                    <a
-                      href={faucetUrls.dai}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn btn--grey btn--size1"
-                    >
-                      Testnet DAI faucet
-                    </a>
-                  ) : null}
-                </div>
-              </div>
-
-              <div className="section__content">
-                <h2>Current risk exposure</h2>
-
-                <div className="positions">
-                  {positions.map((position, pIdx, positionsArr) => {
-                    return (
-                      <Position
-                        key={`position-${pIdx}`}
-                        position={position}
-                        index={pIdx}
-                        totalLength={positionsArr.length}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
-
-              <div className="section__entry">
-                <h2>Ready to create a new position?</h2>
-              </div>
-
-              <div className="section__actions">
-                <Link to="/Steps" className="btn btn--size1">
-                  Open token facility
-                </Link>
-              </div>
-
-              <div className="section__hint">
-                <p>*You will need Testnet ETH and DAI before opening token facility</p>
-              </div>
-            </section>
-          </div>
-        </div>
-      </div>
-    );
+  // TODO(mrice32): should we have some sort of loading screen to show while data is being pulled?
+  if (positions === undefined) {
+    return null;
   }
 
-  return render();
+  // TODO(mrice32): potentially merge Start and ViewPositions pages to simplify.
+  // Always redirect to the start screen if there are no positions.
+  if (positions.length === 0) {
+    return <Redirect to="/Start" />;
+  }
+
+  return (
+    <div className="wrapper">
+      <Header />
+
+      <div className="main">
+        <div className="shell">
+          <section className="section section--intro section--intro-alt">
+            <div className="section__actions">
+              <Link to="/Steps" className="btn btn--size1">
+                Open token facility
+              </Link>
+
+              <div className="section__actions-inner">
+                {faucetUrls.eth ? (
+                  <a
+                    href={faucetUrls.eth}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn--grey btn--size1"
+                  >
+                    Testnet ETH faucet
+                  </a>
+                ) : null}
+
+                {faucetUrls.dai ? (
+                  <a
+                    href={faucetUrls.dai}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn--grey btn--size1"
+                  >
+                    Testnet DAI faucet
+                  </a>
+                ) : null}
+              </div>
+            </div>
+
+            <div className="section__content">
+              <h2>Current risk exposure</h2>
+
+              <div className="positions">
+                {positions.map((position, pIdx, positionsArr) => {
+                  return (
+                    <Position
+                      key={`position-${pIdx}`}
+                      position={position}
+                      index={pIdx}
+                      totalLength={positionsArr.length}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="section__entry">
+              <h2>Ready to create a new position?</h2>
+            </div>
+
+            <div className="section__actions">
+              <Link to="/Steps" className="btn btn--size1">
+                Open token facility
+              </Link>
+            </div>
+
+            <div className="section__hint">
+              <p>*You will need Testnet ETH and DAI before opening token facility</p>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default connect(
