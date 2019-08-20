b7663df4339ac763db2d1796f0179d6106c73320
Matthew Rice <matthewcrice32@gmail.com>
2019-08-20 16:57:54 -0400

    fix ci
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- ci/run_dapp_tests.sh
+++ ci/run_dapp_tests.sh
@@ -2,7 +2,7 @@
 
 run_test() {
     local test_dir=$1
-    cd test_dir
+    cd $test_dir
     output=$(CI=true npm run test)
     echo "$output"
     num_failures=$(echo $output | grep -ci "error")
@@ -10,9 +10,6 @@ run_test() {
     then
         # If we found failures, exit 1.
         exit 1
-    else
-        # If not failures were found, exit 0.
-        exit 0
     fi
 }
 
