45cce461908d7f42aaff0352a7543529fd2658eb
Matt Rice <matthewcrice32@gmail.com>
2019-08-20 14:58:46 -0400

    Remove render functions from hookified components (#610)
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- sponsor-dapp-v2/src/views/Start.js
+++ sponsor-dapp-v2/src/views/Start.js
@@ -9,75 +9,71 @@ function StartScreen() {
   const numContracts = useNumRegisteredContracts();
   const faucetUrls = useFaucetUrls();
 
-  function render() {
-    if (numContracts === undefined) {
-      return null;
-    }
-
-    if (numContracts !== 0) {
-      return <Redirect to="/ViewPositions" />;
-    }
-
-    return (
-      <div className="wrapper">
-        <Header />
-
-        <div className="main">
-          <div className="shell">
-            <section className="section section--intro">
-              <div className="section__actions">
-                <Link to="/Steps" className="btn btn--size1">
-                  Open token facility
-                </Link>
-
-                <div className="section__actions-inner">
-                  {faucetUrls.eth ? (
-                    <a
-                      href={faucetUrls.eth}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn btn--grey btn--size1"
-                    >
-                      Testnet ETH faucet
-                    </a>
-                  ) : null}
-
-                  {faucetUrls.dai ? (
-                    <a
-                      href={faucetUrls.dai}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="btn btn--grey btn--size1"
-                    >
-                      Testnet DAI faucet
-                    </a>
-                  ) : null}
-                </div>
-              </div>
-
-              <div className="section__entry">
-                <h2>You currently have no risk exposure.</h2>
-
-                <h2>Ready to create a new position?</h2>
-              </div>
-
-              <div className="section__actions">
-                <Link to="/Steps" className="btn btn--size1">
-                  Open token facility
-                </Link>
-              </div>
-
-              <div className="section__hint">
-                <p>*You will need Testnet ETH and DAI before opening token facility</p>
-              </div>
-            </section>
-          </div>
-        </div>
-      </div>
-    );
+  if (numContracts === undefined) {
+    return null;
   }
 
-  return render();
+  if (numContracts !== 0) {
+    return <Redirect to="/ViewPositions" />;
+  }
+
+  return (
+    <div className="wrapper">
+      <Header />
+
+      <div className="main">
+        <div className="shell">
+          <section className="section section--intro">
+            <div className="section__actions">
+              <Link to="/Steps" className="btn btn--size1">
+                Open token facility
+              </Link>
+
+              <div className="section__actions-inner">
+                {faucetUrls.eth ? (
+                  <a
+                    href={faucetUrls.eth}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn--grey btn--size1"
+                  >
+                    Testnet ETH faucet
+                  </a>
+                ) : null}
+
+                {faucetUrls.dai ? (
+                  <a
+                    href={faucetUrls.dai}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn--grey btn--size1"
+                  >
+                    Testnet DAI faucet
+                  </a>
+                ) : null}
+              </div>
+            </div>
+
+            <div className="section__entry">
+              <h2>You currently have no risk exposure.</h2>
+
+              <h2>Ready to create a new position?</h2>
+            </div>
+
+            <div className="section__actions">
+              <Link to="/Steps" className="btn btn--size1">
+                Open token facility
+              </Link>
+            </div>
+
+            <div className="section__hint">
+              <p>*You will need Testnet ETH and DAI before opening token facility</p>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default connect(
