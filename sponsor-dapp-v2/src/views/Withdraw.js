9e65e4ec4df520b196da435f528c7709389af4a7
Prasad Tare <ptare@users.noreply.github.com>
2019-08-22 10:13:35 -0400

    Removes `render` functions from functional components (#620)
    Fixes #614 and #604.

--- sponsor-dapp-v2/src/views/Withdraw.js
+++ sponsor-dapp-v2/src/views/Withdraw.js
@@ -34,99 +34,96 @@ function Withdraw(props) {
   const allowedToProceed = withdrawAmount !== "";
   const isLoading = status === "pending";
 
-  const render = () => {
-    return (
-      <div className="popup">
-        <Header />
+  return (
+    <div className="popup">
+      <Header />
 
-        <Link to={"/ManagePositions/" + tokenAddress} className="btn-close">
-          <IconSvgComponent iconPath="svg/ico-close.svg" additionalClass="ico-close" />
-        </Link>
+      <Link to={"/ManagePositions/" + tokenAddress} className="btn-close">
+        <IconSvgComponent iconPath="svg/ico-close.svg" additionalClass="ico-close" />
+      </Link>
 
-        <div className="popup__inner">
-          <div className="shell">
-            <div className="popup__head">
-              <h3>Withdraw collateral from facility</h3>
+      <div className="popup__inner">
+        <div className="shell">
+          <div className="popup__head">
+            <h3>Withdraw collateral from facility</h3>
 
-              <div className="popup__head-entry">
+            <div className="popup__head-entry">
+              <p>
+                <strong>
+                  Your facility has a {fromWei(data.collateralizationRequirement)}% collateralization requirement.
+                </strong>{" "}
+                You can withdraw collateral from your facility as long as you maintain this requirement.{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="popup__body">
+            <div className="popup__col">
+              <div className="form-group">
+                <label htmlFor="field-withdraw" className="form__label">
+                  Withdraw margin
+                </label>
+
+                <div className="form__controls">
+                  <input
+                    type="text"
+                    className="field"
+                    id="field-withdraw"
+                    name="field-withdraw"
+                    maxLength="18"
+                    value={withdrawAmount}
+                    onChange={e => handleChangeAmount(e)}
+                    autoComplete="off"
+                    disabled={isLoading}
+                  />
+
+                  <span>DAI</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="popup__col">
+              <div className="popup__entry">
                 <p>
-                  <strong>
-                    Your facility has a {fromWei(data.collateralizationRequirement)}% collateralization requirement.
-                  </strong>{" "}
-                  You can withdraw collateral from your facility as long as you maintain this requirement.{" "}
+                  <strong>Facility collateralization</strong>
                 </p>
+
+                <ul>
+                  <li>
+                    <span>Current:</span>
+                    <span>{data.currentCollateralization}</span>
+                  </li>
+
+                  <li className={classNames({ highlight: allowedToProceed })}>
+                    <strong>New:</strong>
+                    <span>{data.newCollateralizationAmount}</span>
+                  </li>
+                </ul>
               </div>
             </div>
+          </div>
 
-            <div className="popup__body">
-              <div className="popup__col">
-                <div className="form-group">
-                  <label htmlFor="field-withdraw" className="form__label">
-                    Withdraw margin
-                  </label>
+          <div className="popup__actions">
+            <Link
+              to={"/ManagePositions/" + tokenAddress}
+              onClick={event => handleWithdrawClick(event)}
+              className={classNames(
+                "btn btn--size2 has-loading",
+                { disabled: !allowedToProceed },
+                { "is-loading": isLoading }
+              )}
+            >
+              <span>Withdraw</span>
 
-                  <div className="form__controls">
-                    <input
-                      type="text"
-                      className="field"
-                      id="field-withdraw"
-                      name="field-withdraw"
-                      maxLength="18"
-                      value={withdrawAmount}
-                      onChange={e => handleChangeAmount(e)}
-                      autoComplete="off"
-                      disabled={isLoading}
-                    />
+              <span className="loading-text">Processing</span>
 
-                    <span>DAI</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="popup__col">
-                <div className="popup__entry">
-                  <p>
-                    <strong>Facility collateralization</strong>
-                  </p>
-
-                  <ul>
-                    <li>
-                      <span>Current:</span>
-                      <span>{data.currentCollateralization}</span>
-                    </li>
-
-                    <li className={classNames({ highlight: allowedToProceed })}>
-                      <strong>New:</strong>
-                      <span>{data.newCollateralizationAmount}</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div className="popup__actions">
-              <Link
-                to={"/ManagePositions/" + tokenAddress}
-                onClick={event => handleWithdrawClick(event)}
-                className={classNames(
-                  "btn btn--size2 has-loading",
-                  { disabled: !allowedToProceed },
-                  { "is-loading": isLoading }
-                )}
-              >
-                <span>Withdraw</span>
-
-                <span className="loading-text">Processing</span>
-
-                <strong className="dot-pulse" />
-              </Link>
-            </div>
+              <strong className="dot-pulse" />
+            </Link>
           </div>
         </div>
       </div>
-    );
-  };
-  return render();
+    </div>
+  );
 }
 
 export default withAddedContract(TokenizedDerivative.abi, props => props.match.params.tokenAddress)(Withdraw);
