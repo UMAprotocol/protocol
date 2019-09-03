d9e3748b33e475b7a233aaa724aa537c69c3c80e
Matthew Rice <matthewcrice32@gmail.com>
2019-09-03 12:26:47 -0400

    Add rinkeby deployment and remove outdated kovan deployment
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- common/PublicNetworks.js
+++ common/PublicNetworks.js
@@ -2,21 +2,25 @@ module.exports = {
   1: {
     name: "mainnet",
     ethFaucet: null,
-    etherscan: "https://etherscan.io/"
+    etherscan: "https://etherscan.io/",
+    daiAddress: "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359"
   },
   3: {
     name: "ropsten",
     ethFaucet: "https://faucet.metamask.io/",
-    etherscan: "https://ropsten.etherscan.io/"
+    etherscan: "https://ropsten.etherscan.io/",
+    daiAddress: "0xB5E5D0F8C0cbA267CD3D7035d6AdC8eBA7Df7Cdd"
   },
   4: {
     name: "rinkeby",
     ethFaucet: "https://faucet.rinkeby.io/",
-    etherscan: "https://rinkeby.etherscan.io/"
+    etherscan: "https://rinkeby.etherscan.io/",
+    daiAddress: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa"
   },
   42: {
     name: "kovan",
     ethFaucet: "https://faucet.kovan.network/",
-    etherscan: "https://kovan.etherscan.io/"
+    etherscan: "https://kovan.etherscan.io/",
+    daiAddress: "0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99"
   }
 };
