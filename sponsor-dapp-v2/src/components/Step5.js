eb62095cda4a689f7c2f8a70441f5879be2d2390
Matt Rice <matthewcrice32@gmail.com>
2019-09-04 11:04:56 -0400

    Remove render fxns (#661)
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- sponsor-dapp-v2/src/components/Step5.js
+++ sponsor-dapp-v2/src/components/Step5.js
@@ -75,115 +75,111 @@ function Step5(props) {
 
   const allowedToProceed = dai !== "" && tokens !== "";
 
-  const render = () => {
-    return (
-      <div className="step step-5-enter-done">
-        <div className="form-borrow">
-          <form action="#" method="post">
-            <div className="form__body">
-              <div className="form__row">
-                <div className="form__col">
-                  <div className="form-group">
-                    <label htmlFor="field-dai" className="form__label">
-                      How much Dai would you like to collateralize?
-                    </label>
+  return (
+    <div className="step step-5-enter-done">
+      <div className="form-borrow">
+        <form action="#" method="post">
+          <div className="form__body">
+            <div className="form__row">
+              <div className="form__col">
+                <div className="form-group">
+                  <label htmlFor="field-dai" className="form__label">
+                    How much Dai would you like to collateralize?
+                  </label>
 
-                    <div className="form__controls">
-                      <input
-                        type="text"
-                        className="field"
-                        id="field-dai"
-                        name="field-dai"
-                        value={dai}
-                        maxLength="18"
-                        autoComplete="off"
-                        disabled={status === "pending"}
-                        onChange={e => handleChangeDai(e)}
-                      />
+                  <div className="form__controls">
+                    <input
+                      type="text"
+                      className="field"
+                      id="field-dai"
+                      name="field-dai"
+                      value={dai}
+                      maxLength="18"
+                      autoComplete="off"
+                      disabled={status === "pending"}
+                      onChange={e => handleChangeDai(e)}
+                    />
 
-                      <span>DAI</span>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="form__col">
-                  <div className="form-group">
-                    <label htmlFor="field-tokes" className="form__label">
-                      How many synthetic tokens do you want to borrow?
-                    </label>
-
-                    <div className="form__controls">
-                      <input
-                        type="text"
-                        id="field-tokes"
-                        name="field-tokes"
-                        className="field"
-                        maxLength="18"
-                        autoComplete="off"
-                        disabled={status === "pending"}
-                        value={tokens}
-                        onChange={e => handleChangeTokens(e)}
-                      />
-                      <span>Tokens</span>
-                    </div>
-
-                    {tokens !== "" && dai !== "" && ready && (
-                      <div className="form-hint">
-                        <p>(Max {format(maxTokens)})</p>
-                      </div>
-                    )}
+                    <span>DAI</span>
                   </div>
                 </div>
               </div>
+
+              <div className="form__col">
+                <div className="form-group">
+                  <label htmlFor="field-tokes" className="form__label">
+                    How many synthetic tokens do you want to borrow?
+                  </label>
+
+                  <div className="form__controls">
+                    <input
+                      type="text"
+                      id="field-tokes"
+                      name="field-tokes"
+                      className="field"
+                      maxLength="18"
+                      autoComplete="off"
+                      disabled={status === "pending"}
+                      value={tokens}
+                      onChange={e => handleChangeTokens(e)}
+                    />
+                    <span>Tokens</span>
+                  </div>
+
+                  {tokens !== "" && dai !== "" && ready && (
+                    <div className="form-hint">
+                      <p>(Max {format(maxTokens)})</p>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-
-            <div className="form__actions">
-              <input type="submit" value="Submit" className="form__btn hidden" />
-            </div>
-          </form>
-        </div>
-
-        <div className="step__inner">
-          <div className="step__content">
-            <dl className="step__description">
-              <dt>
-                Liquidation price [{identifier}]: {liquidationPrice ? format(liquidationPrice) : "--"}
-              </dt>
-              <dd>
-                Current price [{identifier}]: ${currentPrice ? format(currentPrice.underlyingPrice) : " --"}
-              </dd>
-            </dl>
-
-            <dl className="step__description">
-              <dt>Collateralization ratio: {currentCollateralization || "-- %"}</dt>
-              <dd>Minimum ratio: {collateralizationRequirement ? format(collateralizationRequirement) : "-- %"}%</dd>
-            </dl>
           </div>
 
-          <div className="step__aside">
-            <div className="step__actions">
-              <a
-                href="test"
-                onClick={e => handleClick(e)}
-                className={classNames("btn has-loading", {
-                  disabled: !allowedToProceed,
-                  "is-loading": status === "pending"
-                })}
-              >
-                <span>Borrow tokens</span>
+          <div className="form__actions">
+            <input type="submit" value="Submit" className="form__btn hidden" />
+          </div>
+        </form>
+      </div>
 
-                <span className="loading-text">Processing</span>
+      <div className="step__inner">
+        <div className="step__content">
+          <dl className="step__description">
+            <dt>
+              Liquidation price [{identifier}]: {liquidationPrice ? format(liquidationPrice) : "--"}
+            </dt>
+            <dd>
+              Current price [{identifier}]: ${currentPrice ? format(currentPrice.underlyingPrice) : " --"}
+            </dd>
+          </dl>
 
-                <strong className="dot-pulse" />
-              </a>
-            </div>
+          <dl className="step__description">
+            <dt>Collateralization ratio: {currentCollateralization || "-- %"}</dt>
+            <dd>Minimum ratio: {collateralizationRequirement ? format(collateralizationRequirement) : "-- %"}%</dd>
+          </dl>
+        </div>
+
+        <div className="step__aside">
+          <div className="step__actions">
+            <a
+              href="test"
+              onClick={e => handleClick(e)}
+              className={classNames("btn has-loading", {
+                disabled: !allowedToProceed,
+                "is-loading": status === "pending"
+              })}
+            >
+              <span>Borrow tokens</span>
+
+              <span className="loading-text">Processing</span>
+
+              <strong className="dot-pulse" />
+            </a>
           </div>
         </div>
       </div>
-    );
-  };
-
-  return render();
+    </div>
+  );
 }
 
 export default withAddedContract(TokenizedDerivative.abi, props => props.userSelectionsRef.current.contractAddress)(
