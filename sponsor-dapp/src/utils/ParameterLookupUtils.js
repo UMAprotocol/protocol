04449108824f1fbb84a57e6ed0256c479793e218
Matthew Rice <matthewcrice32@gmail.com>
2019-10-17 09:32:23 -0400

    Remove original dead sponsor dapp code
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- sponsor-dapp/src/utils/ParameterLookupUtils.js
+++
@@ -1,5 +0,0 @@
-// Returns the currency name if its name is listed in the params.
-// Otherwise, returns undefined.
-export function currencyAddressToName(params, address) {
-  return params.currencies[address];
-}
