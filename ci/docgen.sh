7c7a8214305cd819631c8b324e3081bcb1f2f499
Matthew Rice <matthewcrice32@gmail.com>
2019-10-29 16:30:46 -0400

    WIP
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- ci/docgen.sh
+++ ci/docgen.sh
@@ -3,6 +3,4 @@
 # Note: because we've forked the solidity-docgen library, providing the path alias doesn't have an effect. However,
 # once external libraries are supported in v2 and we deprecate our fork, this will allow the docgen to find the
 # openzeppelin directory. See https://github.com/OpenZeppelin/solidity-docgen/issues/24 for progress on that front.
-# OPEN_ZEPPELIN_DIR=$(pwd)/node_modules/openzeppelin-solidity
-# SOLC_ARGS="openzeppelin-solidity=$OPEN_ZEPPELIN_DIR" \
 $(npm bin)/solidity-docgen -i ./core/contracts -t documentation --contract-pages -x adoc -e core/contracts/test,core/contracts/echidna_tests
