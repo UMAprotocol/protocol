1865d9165689ad351e3619f2c3dc2725a4a8f9ca
Matthew Rice <matthewcrice32@gmail.com>
2019-10-30 22:15:00 -0400

    WIP
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- scripts/build_docs_site.sh
+++ scripts/build_docs_site.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 set -e
 
+md_to_adoc() {
+    # Input filename
+    local infile=$1
+
+    # Output filename
+    local outfile=$2
+
+    # Create temporary file for intermediate step.
+    local tmpfile=$(mktemp)
+
+    # Convert headings to the correct level before converting.
+    cp $infile $tmpfile
+    
+    # Ensures that markdown headers are correctly converted to the right level in asciidoc.
+    sed -i '' 's/^#[[:space:]]/= /' $tmpfile
+    sed -i '' 's/^##/#/' $tmpfile
+
+    # Changes markdown file interlink extensions to html so interlinks continue to work when the site is rendered.
+    sed -i '' 's/\.md)/.html)/' $tmpfile
+
+    # Use pandoc to do the remainder of the conversion.
+    pandoc --atx-headers --verbose --wrap=none --toc --reference-links -f gfm -s -o $outfile -t asciidoc $tmpfile
+}
+
 START_DIR=$(pwd)
 
 rm -rf $START_DIR/build/site
@@ -26,11 +50,11 @@ shopt -s nullglob
 for FNAME in $START_DIR/documentation/tutorials/*.md
 do
     BNAME=$(basename "$FNAME" .md)
-    pandoc --atx-headers --verbose --wrap=none --toc --reference-links -f gfm -s -o $START_DIR/modules/ROOT/pages/$BNAME.adoc -t asciidoc $FNAME
+    md_to_adoc $FNAME $START_DIR/modules/ROOT/pages/$BNAME.adoc
 done
 
 node $START_DIR/scripts/gen-nav.js $START_DIR/modules/ROOT/pages Tutorials > $START_DIR/modules/ROOT/nav.adoc
 
-pandoc --atx-headers --verbose --wrap=none --toc --reference-links -f gfm -s -o $START_DIR/modules/ROOT/pages/index.adoc -t asciidoc $START_DIR/documentation/intro.md
+md_to_adoc $START_DIR/documentation/intro.md $START_DIR/modules/ROOT/pages/index.adoc
 $(npm bin)/antora playbook.yml
 
