1421aa39dd8e4f072d667f67a7c8334261efe93c
Matthew Rice <matthewcrice32@gmail.com>
2019-11-01 11:54:33 -0400

    Rework site directory structure to make doc linking easier
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- scripts/build_docs_site.sh
+++ scripts/build_docs_site.sh
@@ -41,6 +41,7 @@ npm run bundle
 
 cd $START_DIR
 mkdir -p $START_DIR/modules/ROOT/pages
+mkdir -p $START_DIR/modules/tutorials/pages
 mkdir -p $START_DIR/modules/explainers/pages
 mkdir -p $START_DIR/modules/explainers/assets/images
 mkdir -p $START_DIR/modules/contracts/pages
@@ -55,7 +56,7 @@ shopt -s nullglob
 for FNAME in $START_DIR/documentation/tutorials/*.md
 do
     BNAME=$(basename "$FNAME" .md)
-    md_to_adoc $FNAME $START_DIR/modules/ROOT/pages/$BNAME.adoc
+    md_to_adoc $FNAME $START_DIR/modules/tutorials/pages/$BNAME.adoc
 done
 
 for FNAME in $START_DIR/documentation/explainers/*.md
@@ -64,9 +65,10 @@ do
     md_to_adoc $FNAME $START_DIR/modules/explainers/pages/$BNAME.adoc
 done
 
-node $START_DIR/scripts/gen-nav.js $START_DIR/modules/ROOT/pages Tutorials > $START_DIR/modules/ROOT/nav.adoc
+touch $START_DIR/modules/ROOT/nav.adoc
+node $START_DIR/scripts/gen-nav.js $START_DIR/modules/tutorials/pages Tutorials > $START_DIR/modules/tutorials/nav.adoc
 node $START_DIR/scripts/gen-nav.js $START_DIR/modules/explainers/pages Explainers > $START_DIR/modules/explainers/nav.adoc
 
-md_to_adoc $START_DIR/documentation/intro.md $START_DIR/modules/ROOT/pages/index.adoc
+md_to_adoc $START_DIR/documentation/index.md $START_DIR/modules/ROOT/pages/index.adoc
 $(npm bin)/antora playbook.yml
 
