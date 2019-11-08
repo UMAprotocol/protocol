86d2a33355377550509b05d30c0c56dde0c31372
Matthew Rice <matthewcrice32@gmail.com>
2019-11-08 08:12:47 -0500

    lint
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- core/migrations/14_deploy_return_calculators.js
+++ core/migrations/14_deploy_return_calculators.js
@@ -8,12 +8,18 @@ module.exports = async function(deployer, network, accounts) {
 
   // Deploy return calculators for 1x and 2x cases. Note: must leave the 1x for last so it's the default return
   // calculator contract in truffle.
-  const { contract: levered2x } = await deploy(deployer, network, LeveragedReturnCalculator, 2, { from: keys.deployer });
-  const { contract: levered1x } = await deploy(deployer, network, LeveragedReturnCalculator, 1, { from: keys.deployer });
+  const { contract: levered2x } = await deploy(deployer, network, LeveragedReturnCalculator, 2, {
+    from: keys.deployer
+  });
+  const { contract: levered1x } = await deploy(deployer, network, LeveragedReturnCalculator, 1, {
+    from: keys.deployer
+  });
 
   // Grab the whitelist.
   const tokenizedDerivativeCreator = await TokenizedDerivativeCreator.deployed();
-  const returnCalculatorWhitelist = await AddressWhitelist.at(await tokenizedDerivativeCreator.returnCalculatorWhitelist());
+  const returnCalculatorWhitelist = await AddressWhitelist.at(
+    await tokenizedDerivativeCreator.returnCalculatorWhitelist()
+  );
 
   // Approve the new return calculators on the whitelists.
   await returnCalculatorWhitelist.addToWhitelist(levered2x.address, { from: keys.returnCalculatorWhitelist });
