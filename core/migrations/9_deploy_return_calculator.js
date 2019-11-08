08b45b77fb48d885b5ee815a3742213f114caa1c
Matt Rice <matthewcrice32@gmail.com>
2019-11-08 08:39:53 -0500

    Replace testnet contracts (#743)
    Signed-off-by: Matthew Rice <matthewcrice32@gmail.com>

--- core/migrations/9_deploy_return_calculator.js
+++
@@ -1,8 +0,0 @@
-const LeveragedReturnCalculator = artifacts.require("LeveragedReturnCalculator");
-const { getKeysForNetwork, deploy, addToTdr } = require("../../common/MigrationUtils.js");
-
-module.exports = async function(deployer, network, accounts) {
-  const keys = getKeysForNetwork(network, accounts);
-
-  await deploy(deployer, network, LeveragedReturnCalculator, 1, { from: keys.deployer });
-};
