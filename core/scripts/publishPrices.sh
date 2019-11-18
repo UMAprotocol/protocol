93fefa3a78cc5c45dbad08f88584db2bf5b0b7ce
Matt Rice <matthewcrice32@gmail.com>
2019-11-18 15:55:48 -0500

    Set 1 hour delay on price feed updates
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/publishPrices.sh
+++ core/scripts/publishPrices.sh
@@ -3,4 +3,4 @@
 # Example: ./scripts/publishPrices.sh --network mainnet --keys priceFeed
 # Note: this script must be run from the core/ directory (not core/scripts/).
 
-while sleep 600; do $(npm bin)/truffle exec ./scripts/PublishPrices.js "$@" >>out.log 2>&1; done
+while true; do $(npm bin)/truffle exec ./scripts/PublishPrices.js "$@" >>out.log 2>&1; sleep 3600; done
