85b8fb81783b8606e7dad1da33f3ec965e27478f
Matt Rice <matthewcrice32@gmail.com>
2019-11-20 18:35:20 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -8,4 +8,4 @@ PROTOCOL_DIR=$(pwd)
 
 # $1 is the truffle directory over which we want to run the coverage tool.
 cd $TRUFFLE_DIR
-$(npm bin)/truffle run coverage --network coverage
+$(npm bin)/truffle run coverage --network ci
--- common/globalSolcoverConfig.js
+++ common/globalSolcoverConfig.js
@@ -1,3 +1,6 @@
 module.exports = {
+  providerOptions: {
+    network_id: 1234
+  },
   skipFiles: ["Migrations.sol", "echidna_tests", "test"]
 };
