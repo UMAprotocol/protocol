a9ecb5e6a66e2d497b1f155b0f19ee356eae7ead
Matt Rice <matthewcrice32@gmail.com>
2019-12-12 16:25:06 -0500

    lint
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/contracts/Voting.sol
+++ core/contracts/Voting.sol
@@ -17,7 +17,6 @@ import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
-
 /**
  * @title Voting system for Oracle.
  * @dev Handles receiving and resolving price requests via a commit-reveal voting scheme.
