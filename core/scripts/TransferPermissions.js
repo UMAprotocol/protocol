1e1ec49017a76d3ef33271f9615e973f4c084536
Matt Rice <matthewcrice32@gmail.com>
2019-12-16 16:01:15 -0500

    prettier
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/TransferPermissions.js
+++ core/scripts/TransferPermissions.js
@@ -9,36 +9,36 @@ const Governor = artifacts.require("Governor");
 const argv = require("minimist")(process.argv.slice(), { string: ["multisig"] });
 
 async function transferPermissions(multisig) {
-    // The owner role is generally the 0th role (this is true for all the contracts in this script).
-    const ownerRole = "0";
+  // The owner role is generally the 0th role (this is true for all the contracts in this script).
+  const ownerRole = "0";
 
-    // Governor is owned by the multisig and the hot wallet/deployer should remain the proposer.
-    const governor = await Governor.deployed();
-    await governor.resetMember(ownerRole, multisig);
+  // Governor is owned by the multisig and the hot wallet/deployer should remain the proposer.
+  const governor = await Governor.deployed();
+  await governor.resetMember(ownerRole, multisig);
 
-    // Registry should be owned by the governor.
-    const registry = await Registry.deployed();
-    await registry.resetMember(ownerRole, governor.address);
+  // Registry should be owned by the governor.
+  const registry = await Registry.deployed();
+  await registry.resetMember(ownerRole, governor.address);
 
-    // Finder should be owned by the governor.
-    const finder = await Finder.deployed();
-    await finder.transferOwnership(governor.address);
+  // Finder should be owned by the governor.
+  const finder = await Finder.deployed();
+  await finder.transferOwnership(governor.address);
 
-    // FinancialContractsAdmin should be owned by the governor.
-    const financialContractsAdmin = await FinancialContractsAdmin.deployed();
-    await financialContractsAdmin.transferOwnership(governor.address);
+  // FinancialContractsAdmin should be owned by the governor.
+  const financialContractsAdmin = await FinancialContractsAdmin.deployed();
+  await financialContractsAdmin.transferOwnership(governor.address);
 
-    // Store should be owned by the governor and the hot wallet/deployer should remain the withdrawer.
-    const store = await Store.deployed();
-    await store.resetMember(ownerRole, governor.address);
+  // Store should be owned by the governor and the hot wallet/deployer should remain the withdrawer.
+  const store = await Store.deployed();
+  await store.resetMember(ownerRole, governor.address);
 
-    // Voting should be owned by the governor.
-    const voting = await Voting.deployed();
-    await voting.transferOwnership(governor.address);
+  // Voting should be owned by the governor.
+  const voting = await Voting.deployed();
+  await voting.transferOwnership(governor.address);
 
-    // VotingToken should be owned by the governor.
-    const votingToken = await VotingToken.deployed();
-    await votingToken.resetMember(ownerRole, governor.address);
+  // VotingToken should be owned by the governor.
+  const votingToken = await VotingToken.deployed();
+  await votingToken.resetMember(ownerRole, governor.address);
 }
 
 // This script moves certain permissions from the truffle in-memory hot key to the governor and foundation multisig.
@@ -52,7 +52,7 @@ async function wrapper(callback) {
   }
 
   callback();
-};
+}
 
 wrapper.transferPermissions = transferPermissions;
 module.exports = wrapper;
