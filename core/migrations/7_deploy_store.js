93ca42abc73b3301cc39770b1c3e3b9237b6bf29
Matt Rice <matthewcrice32@gmail.com>
2020-01-09 16:11:02 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/migrations/7_deploy_store.js
+++ core/migrations/7_deploy_store.js
@@ -7,19 +7,10 @@ const { interfaceName } = require("../utils/Constants.js");
 module.exports = async function(deployer, network, accounts) {
   const keys = getKeysForNetwork(network, accounts);
 
-  const { contract: store, didDeploy } = await deploy(deployer, network, Store, { from: keys.deployer });
+  const { contract: store } = await deploy(deployer, network, Store, { from: keys.deployer });
 
   const finder = await Finder.deployed();
   await finder.changeImplementationAddress(web3.utils.utf8ToHex(interfaceName.Store), store.address, {
     from: keys.deployer
   });
-
-  // Only update the fees if this contract was newly deployed during this migration.
-  if (didDeploy) {
-    // Set oracle fees to 0.5% per year.
-    const annualFee = web3.utils.toWei("0");
-    const secondsPerYear = 31536000;
-    const feePerSecond = web3.utils.toBN(annualFee).divn(secondsPerYear);
-    await store.setFixedOracleFeePerSecond({ rawValue: feePerSecond.toString() }, { from: keys.deployer });
-  }
 };
