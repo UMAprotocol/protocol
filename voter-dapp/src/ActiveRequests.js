699168c7e4f28af096523929578de752e53bfee3
Prasad Tare <ptare@users.noreply.github.com>
2020-01-10 12:00:58 -0500

    Make salt generation in dApp more consistent with unit tests (#811)
    The dApp was generating its random salt in a different way from the
    Truffle unit tests. We should do the same thing so that the dApp's
    behavior is verified by the unit tests (the dApp code doesn't currently
    have any automated testing).
    
    Truffle test:
    https://github.com/UMAprotocol/protocol/blob/f3a2ce6a0f9036c58dcc9f91d062556862df30c8/core/test/Voting.js#L1023
    
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- voter-dapp/src/ActiveRequests.js
+++ voter-dapp/src/ActiveRequests.js
@@ -15,6 +15,7 @@ import { VotePhasesEnum } from "./common/Enums.js";
 import { decryptMessage, deriveKeyPairFromSignatureMetamask, encryptMessage } from "./common/Crypto.js";
 import { useTableStyles } from "./Styles.js";
 const { getKeyGenMessage } = require("./common/EncryptionHelper.js");
+const { getRandomUnsignedInt } = require("./common/Random.js");
 
 const editStateReducer = (state, action) => {
   switch (action.type) {
@@ -159,7 +160,7 @@ function ActiveRequests({ votingAccount, votingGateway }) {
           identifier: pendingRequests[index].identifier,
           time: pendingRequests[index].time,
           price: decryptedCommits[index].price.toString(),
-          salt: web3.utils.hexToNumberString(decryptedCommits[index].salt)
+          salt: decryptedCommits[index].salt
         });
       }
     }
@@ -178,7 +179,7 @@ function ActiveRequests({ votingAccount, votingGateway }) {
         continue;
       }
       const price = web3.utils.toWei(editState[index]);
-      const salt = web3.utils.toBN(web3.utils.randomHex(32));
+      const salt = getRandomUnsignedInt().toString();
       const encryptedVote = await encryptMessage(
         decryptionKeys[account][currentRoundId].publicKey,
         JSON.stringify({ price, salt })
