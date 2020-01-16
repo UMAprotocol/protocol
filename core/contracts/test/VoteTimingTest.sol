4d682889134250dabde4fb2b2b17eb84350b5f7a
Christopher Maree <christopher.maree@gmail.com>
2020-01-16 13:55:26 -0500

    Updated for linter
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/contracts/test/VoteTimingTest.sol
+++ core/contracts/test/VoteTimingTest.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 import "../VoteTiming.sol";
 
+
 // Wraps the library VoteTiming for testing purposes.
 contract VoteTimingTest {
     using VoteTiming for VoteTiming.Data;
@@ -19,6 +20,7 @@ contract VoteTimingTest {
     function wrapComputeCurrentPhase(uint currentTime) external view returns (VoteTiming.Phase) {
         return voteTiming.computeCurrentPhase(currentTime);
     }
+    
     function wrapInit(uint phaseLength) public {
         voteTiming.init(phaseLength);
     }
