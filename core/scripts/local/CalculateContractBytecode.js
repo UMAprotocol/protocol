b2388e149aecb3d2f79bb9c85371bf8785a68aa6
Christopher Maree <christopher.maree@gmail.com>
2020-01-23 10:15:52 -0500

    Added bytecode calculator tool
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

---
+++ core/scripts/local/CalculateContractBytecode.js
@@ -0,0 +1,18 @@
+// This simple script tells you how big your contract byte code is and how much you have until you exceed
+// the current block limit as defined by EIP170.
+
+const argv = require("minimist")(process.argv.slice(), { string: ["contract"] });
+const contractName = argv.contract;
+
+if (!contractName) {
+  console.log("Please enter the contract name as a parameter as `--contract <name>`.");
+  return;
+}
+
+console.log("loading", contractName + ".json");
+let obj = require("./../../build/contracts/" + contractName + ".json");
+
+const byteCodeSize = (obj.bytecode.length - 2) / 2;
+const remainingSize = 2 ** 14 + 2 ** 13 - (obj.bytecode.length - 2) / 2;
+console.log("Contract is", byteCodeSize, "bytes in size.");
+console.log("This leaves a total of", remainingSize, " within EIP170");
