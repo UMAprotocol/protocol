f1c40f6e9e9efaa11b97a4bc50abc8dc0dfaffc1
Nick Pai <npai.nyc@gmail.com>
2020-01-27 15:29:08 -0500

    Contracts compile, unit test skeleton created
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/contracts/financial_templates/ExpiringMultiParty.sol
+++ core/contracts/financial_templates/ExpiringMultiParty.sol
@@ -1,9 +1,8 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
-import "./Liquidation.sol";
 import "./Position.sol";
 
-contract ExpiringMultiParty is Position, Liquidation {
+contract ExpiringMultiParty is Position {
     constructor(uint _expirationTimestamp, bool _isTest) public Position(_expirationTimestamp, _isTest) {}
 }
