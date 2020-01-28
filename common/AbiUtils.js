2fe7d071fc9615493057dc7074ca79fb27bdc3e8
Matt Rice <matthewcrice32@gmail.com>
2020-01-27 22:22:26 -0500

    Add transaction decoding script and library
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

---
+++ common/AbiUtils.js
@@ -0,0 +1,49 @@
+const abiDecoder = require("abi-decoder");
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+function getAllContracts() {
+  let importedObjects;
+
+  // Note: we use a try here because we don't want to install the require-context package in node.js contexts where
+  // it won't work.
+  try {
+    // This only works in webpack.
+    const requireContext = require("require-context");
+
+    // Note: all arguments must be hardcoded here for webpack to bundle the files correctly.
+    // This line also generates a few build warnings that should be ignored.
+    const contractContext = require.context("../core/build/contracts/", true, /\.json$/);
+
+    importedObjects = importAll(contractContext);
+  } catch (e) {
+    // This only works in node.js.
+    const fs = require("fs");
+    const path = require("path");
+    const contractsPath = "../core/build/contracts/";
+
+    const fileList = fs.readdirSync(contractsPath).filter(name => name.match(/\.json$/));
+    importedObjects = fileList.map(filename => {
+      const fileContents = fs.readFileSync(path.join(contractsPath, filename));
+      return JSON.parse(fileContents);
+    });
+  }
+
+  return importedObjects;
+}
+
+function getAbiDecoder() {
+  const contracts = getAllContracts();
+  for (const contract of contracts) {
+    abiDecoder.addABI(contract.abi);
+  }
+
+  return abiDecoder;
+}
+
+module.exports = {
+  getAllContracts,
+  getAbiDecoder
+};
