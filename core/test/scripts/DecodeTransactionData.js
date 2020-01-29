6f6620a2fb873b6f98f05a1c91940b5558dbfbe5
Matt Rice <matthewcrice32@gmail.com>
2020-01-29 17:16:34 -0500

    Add transaction decoding script and library (#841)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

---
+++ core/test/scripts/DecodeTransactionData.js
@@ -0,0 +1,55 @@
+const DecodeTransactionData = require("../../scripts/DecodeTransactionData");
+const { getRandomSignedInt, getRandomUnsignedInt } = require("../../../common/Random.js");
+
+const Registry = artifacts.require("Registry");
+const Voting = artifacts.require("Voting");
+
+contract("scripts/DecodeTransactionData.js", function(accounts) {
+  it("Decode registerDerivative", async function() {
+    const derivativeAddress = web3.utils.randomHex(20);
+
+    const registry = await Registry.deployed();
+    const txnData = registry.contract.methods.registerDerivative([], derivativeAddress).encodeABI();
+
+    const expectedObject = {
+      name: "registerDerivative",
+      params: {
+        counterparties: [],
+        derivativeAddress: derivativeAddress
+      }
+    };
+
+    assert.equal(
+      JSON.stringify(DecodeTransactionData.run(txnData)).toLowerCase(),
+      JSON.stringify(expectedObject).toLowerCase()
+    );
+  });
+
+  it("Decode batchReveal", async function() {
+    const voting = await Voting.deployed();
+
+    // Generate 5 random reveals to test.
+    const revealArray = [];
+    for (let i = 0; i < 5; i++) {
+      revealArray.push({
+        identifier: web3.utils.randomHex(32),
+        time: getRandomUnsignedInt().toString(),
+        price: getRandomSignedInt().toString(),
+        salt: getRandomSignedInt().toString()
+      });
+    }
+
+    const txnData = voting.contract.methods.batchReveal(revealArray).encodeABI();
+    const expectedObject = {
+      name: "batchReveal",
+      params: {
+        reveals: revealArray
+      }
+    };
+
+    assert.equal(
+      JSON.stringify(DecodeTransactionData.run(txnData)).toLowerCase(),
+      JSON.stringify(expectedObject).toLowerCase()
+    );
+  });
+});
