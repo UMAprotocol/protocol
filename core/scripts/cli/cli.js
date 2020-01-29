13ae095ffb727d8c2a73b78e8a3db92388af0cf4
Matt Rice <matthewcrice32@gmail.com>
2020-01-29 18:44:49 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/cli/cli.js
+++ core/scripts/cli/cli.js
@@ -2,7 +2,7 @@ const cli = async function(callback) {
   try {
     console.log("You have started the UMA CLI!");
   } catch (e) {
-    console.log("ERROR: " + e);
+    callback(e);
   }
 
   callback();
