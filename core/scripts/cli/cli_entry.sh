13091d05f3ee427c6e33235e43ab350fa617d0f5
Matt Rice <matthewcrice32@gmail.com>
2020-01-30 10:37:01 -0500

    Add better error handling in the CLI tool (#849)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/cli/cli_entry.sh
+++ core/scripts/cli/cli_entry.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Any errors in this script should immediately terminate execution.
+set -e
+
 # This script should be installed with the UMA project and symlinked to the "uma" file in the npm binary directory.
 # Usage wherever the UMA package is installed:
 # $(npm bin)/uma --network mainnet_ledger
 
 # Platform independent way of evaluating symlinks to find the real location of the script.
-SCRIPT_FILE=$(python2 -c "import os; print(os.path.realpath(\"$0\"))")
+SCRIPT_FILE=$(/usr/bin/env python2 -c "import os; print(os.path.realpath(\"$0\"))")
 SCRIPT_DIR=$(dirname $SCRIPT_FILE)
 
 # cd into the directory containing the cli file.
@@ -15,6 +18,9 @@ cd $SCRIPT_DIR/../..
 # Execute the script from within the scripts directory so all project artifacts/scripts are available.
 # Note: if the user needs to pass in a file path, we will need to pass the original working directory to the script so
 # we can correctly resolve the file location.
-$(npm bin)/truffle compile
-$(npm bin)/apply-registry
-$(npm bin)/truffle exec -c ./scripts/cli/cli.js "$@"
+echo "Setting up UMA contracts..."
+$(npm bin)/truffle compile > /dev/null || (echo "Contract compilation failed! Please check your @umaprotocol/protocol installation."; exit 1)
+$(npm bin)/apply-registry > /dev/null || (echo "Could not read contract addresses! Please check your @umaprotocol/protocol installation."; exit 1)
+
+echo "Starting Truffle..."
+$(npm bin)/truffle exec ./scripts/cli/cli.js "$@"
