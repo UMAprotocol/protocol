e0625ec1f2eaee8057001e3e4f0ebd2c3aa7099e
Matt Rice <matthewcrice32@gmail.com>
2020-01-30 12:37:38 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/cli/cli.js
+++ core/scripts/cli/cli.js
@@ -3,8 +3,6 @@ const { decodeGovernorProposal, decodeAllActiveGovernorProposals } = require("./
 const cli = async function(callback) {
   try {
     console.log("You have started the UMA CLI!");
-
-    await decodeAllActiveGovernorProposals(artifacts, web3);
   } catch (e) {
     callback(e);
   }
