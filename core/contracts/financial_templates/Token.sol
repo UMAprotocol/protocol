7ed24a8ef22a4e7079b3ba47ec053db197f4aa4b
Christopher Maree <christopher.maree@gmail.com>
2020-02-06 17:56:10 -0500

    reverted token contract for slither
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/contracts/financial_templates/Token.sol
+++ core/contracts/financial_templates/Token.sol
@@ -1,16 +1,20 @@
 pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "../ExpandedIERC20.sol";
 
 /**
  * @notice A burnable and mintable ERC20.
  */
-contract Token is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
-    constructor(string memory name, string memory symbol, uint8 decimals)
-        public
-        ERC20Detailed(name, symbol, decimals)
-    {}
+contract Token is ExpandedIERC20, ERC20 {
+    function burn(uint value) external {
+        _burn(msg.sender, value);
+    }
+
+    // TODO: Inherit from ERC20Mintable instead, which will require redefining ExpandedIERC20 to have this method be
+    // public.
+    function mint(address to, uint value) external returns (bool) {
+        _mint(to, value);
+        return true;
+    }
 }
