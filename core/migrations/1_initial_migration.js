a550b7ebcbe7bee09a8ac7378bbd26363208d73d
Christopher Maree <christopher.maree@gmail.com>
2020-02-06 11:17:24 -0500

    removed migration flag
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/migrations/1_initial_migration.js
+++ core/migrations/1_initial_migration.js
@@ -3,10 +3,6 @@ const Migrations = artifacts.require("./Migrations.sol");
 const { getKeysForNetwork, deploy, addToTdr } = require("../../common/MigrationUtils.js");
 
 module.exports = async function(deployer, network, accounts) {
-  if (argv["skip-migration"] === "t" || argv["s"]) {
-    console.log("Running tests with skipped migrations...");
-    return;
-  }
   const keys = getKeysForNetwork(network, accounts);
   await deploy(deployer, network, Migrations, { from: keys.deployer });
 };
