e4751c6f2cda5d5518c0bfaaee402970ee415b5e
Christopher Maree <christopher.maree@gmail.com>
2020-02-07 10:39:36 -0500

    Fix for slither
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/contracts/financial_templates/Token.sol
+++ core/contracts/financial_templates/Token.sol
@@ -1,6 +1,4 @@
 pragma solidity ^0.5.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
@@ -8,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 /**
  * @notice A burnable and mintable ERC20.
  */
-contract Token is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
+contract Token is ERC20Detailed, ERC20Mintable, ERC20Burnable {
     constructor(string memory name, string memory symbol, uint8 decimals)
         public
         ERC20Detailed(name, symbol, decimals)
