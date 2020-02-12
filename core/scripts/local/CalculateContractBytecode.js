06bbe12cb3625f1648f99cd2eff13bc0e392755d
Matt Rice <matthewcrice32@gmail.com>
2020-02-12 15:37:29 -0500

    Modify bytecode script to use external compilation (#910)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/local/CalculateContractBytecode.js
+++ core/scripts/local/CalculateContractBytecode.js
@@ -1,7 +1,7 @@
 // This simple script tells you how big your contract byte code is and how much you have until you exceed
 // the current block limit as defined by EIP170. This script should be run from the /core directory.
 // To run the script navigate to /core and then run:
-// truffle exec ./scripts/local/CalculateContractBytecode.js --contract Voting --network test
+// truffle exec -c ./scripts/local/CalculateContractBytecode.js --contract Voting --network test
 // where voting is the name of the contract you want to check.
 
 const argv = require("minimist")(process.argv.slice(), { string: ["contract"] });
@@ -12,17 +12,6 @@ module.exports = async function(callback) {
     console.log("Please enter the contract name as a parameter as `--contract <name>`.");
     callback();
   }
-  // Compile contracts with truffle.
-  await truffle.contracts
-    .compile({
-      contracts_directory: "./contracts",
-      contracts_build_directory: "./build/contracts"
-    })
-    .then(() => console.log("Compilation complete!"))
-    .catch(e => {
-      console.error(e);
-      callback();
-    });
 
   // Load contracts into script and output info.
   console.log("loading", argv.contract + ".json");
