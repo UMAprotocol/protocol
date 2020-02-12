debba8dbf7d43b430527b71121c2e2670fc6458d
Nick Pai <npai.nyc@gmail.com>
2020-02-12 15:42:18 -0500

    tests
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/test/financial_templates/TokenFactory.js
+++ core/test/financial_templates/TokenFactory.js
@@ -3,11 +3,13 @@ const TokenFactory = artifacts.require("TokenFactory");
 
 // Helper Contracts
 const Token = artifacts.require("Token");
-const TokenInterface = artifacts.require("TokenInterface");
+
+const { toWei, toBN } = web3.utils;
 
 contract("TokenFactory", function(accounts) {
   const contractDeployer = accounts[0];
   const tokenCreator = accounts[1];
+  const rando = accounts[2];
 
   let tokenFactory;
 
@@ -21,10 +23,56 @@ contract("TokenFactory", function(accounts) {
     tokenFactory = await TokenFactory.deployed();
   });
   it("Can create new tokens and transfers minter role successfully", async () => {
-    const token = await tokenFactory.createToken(tokenDetails.name, tokenDetails.symbol, tokenDetails.decimals, {
+    const tokenAddress = await tokenFactory.createToken.call(tokenDetails.name, tokenDetails.symbol, tokenDetails.decimals, {
       from: tokenCreator
     });
-    console.log(token);
+    await tokenFactory.createToken(tokenDetails.name, tokenDetails.symbol, tokenDetails.decimals, {
+      from: tokenCreator
+    })
+    const token = await Token.at(tokenAddress)
+    assert(await token.isMinter(tokenCreator))
   });
-  it("Token can execute expected methods", async () => {});
+  it("Token can execute expected methods", async () => {
+    const tokenAddress = await tokenFactory.createToken.call(tokenDetails.name, tokenDetails.symbol, tokenDetails.decimals, {
+      from: tokenCreator
+    });
+    await tokenFactory.createToken(tokenDetails.name, tokenDetails.symbol, tokenDetails.decimals, {
+      from: tokenCreator
+    })
+    const token = await Token.at(tokenAddress)
+
+    // Check ERC20Detailed methods
+    assert.equal(await token.name(), tokenDetails.name)
+    assert.equal(await token.symbol(), tokenDetails.symbol)
+    assert.equal((await token.decimals()).toString(), tokenDetails.decimals)
+
+    // Mint rando some tokens
+    const amountToMint = toWei('10.5').toString()
+    await token.mint(rando, amountToMint, { from: tokenCreator })
+    assert.equal((await token.balanceOf(rando)).toString(), amountToMint);
+    assert.equal((await token.totalSupply()).toString(), amountToMint)
+
+    // Transfer some tokens to another account
+    const amountToTransfer = toWei('1').toString()
+    await token.transfer(contractDeployer, amountToTransfer, { from: rando })
+    assert.equal((await token.balanceOf(rando)).toString(), toBN(amountToMint).sub(toBN(amountToTransfer)).toString());
+    assert.equal((await token.balanceOf(contractDeployer)).toString(), amountToTransfer);
+
+    // Burn transferred tokens
+    await token.burn(amountToTransfer, { from: contractDeployer })
+    assert.equal((await token.balanceOf(contractDeployer)).toString(), '0');
+
+    // Increase allowance for a spender, have spender transferFrom tokens away, and decrease allowance
+    await token.increaseAllowance(tokenCreator, amountToTransfer, { from: rando })
+    await token.increaseAllowance(tokenCreator, amountToTransfer, { from: rando })
+    await token.transferFrom(rando, tokenCreator, amountToTransfer, { from: tokenCreator })
+    await token.decreaseAllowance(tokenCreator, amountToTransfer, { from: rando })
+    assert.equal((await token.allowance(rando, tokenCreator)).toString(), '0')
+
+    // Burn remaining tokens
+    await token.burn(amountToTransfer, { from: tokenCreator })
+    await token.burn(toWei('8.5'.toString()), { from: rando })
+    assert.equal((await token.balanceOf(rando)).toString(), '0');
+    assert.equal((await token.totalSupply()).toString(), '0')
+  });
 });
