f9cf5357e73df9de068c01a6519a50f6f038d99b
Nick Pai <npai.nyc@gmail.com>
2020-02-13 19:49:59 -0500

    Token is TokenInterface
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/contracts/financial_templates/implementation/Token.sol
+++ core/contracts/financial_templates/implementation/Token.sol
@@ -3,10 +3,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
+import "../interfaces/TokenInterface.sol";
+
 /**
  * @notice A burnable and mintable ERC20.
  */
-contract Token is ERC20Detailed, ERC20Mintable, ERC20Burnable {
+contract Token is TokenInterface, ERC20Detailed, ERC20Mintable, ERC20Burnable {
     constructor(string memory tokenName, string memory tokenSymbol, uint8 tokenDecimals)
         public
         ERC20Detailed(tokenName, tokenSymbol, tokenDecimals)
