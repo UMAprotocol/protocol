6c085a1857ea541bef1b296926602bb6597a933a
Nick Pai <npai.nyc@gmail.com>
2020-02-24 16:48:28 -0500

    fix
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/contracts/oracle/implementation/VotingToken.sol
+++ core/contracts/oracle/implementation/VotingToken.sol
@@ -9,9 +9,9 @@ import "../../common/implementation/PermissionedExpandedERC20.sol";
  */
 contract VotingToken is PermissionedExpandedERC20, ERC20Snapshot {
     // Standard ERC20 metadata.
-    string public _name = "UMA Voting Token v1"; // solhint-disable-line const-name-snakecase
-    string public _symbol = "UMA"; // solhint-disable-line const-name-snakecase
-    uint8 public _decimals = 18; // solhint-disable-line const-name-snakecase
+    string private constant tokenName = "UMA Voting Token v1"; // solhint-disable-line const-name-snakecase
+    string private constant tokenSymbol = "UMA"; // solhint-disable-line const-name-snakecase
+    uint8 private constant tokenDecimals = 18; // solhint-disable-line const-name-snakecase
 
-    constructor() public PermissionedExpandedERC20(_name, _symbol, _decimals) {}
+    constructor() public PermissionedExpandedERC20(tokenName, tokenSymbol, tokenDecimals) {}
 }
