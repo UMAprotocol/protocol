9aa0c314231b90e4410b98bc4ae895f5d86179ee
Christopher Maree <christopher.maree@gmail.com>
2020-02-24 09:04:21 -0500

    further refine tests
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/test/financial_templates/ExpiringMultiPartyCreator.js
+++ core/test/financial_templates/ExpiringMultiPartyCreator.js
@@ -18,24 +18,29 @@ const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
 ERC20Mintable.setProvider(web3.currentProvider);
 
 contract("ExpiringMultiParty", function(accounts) {
+  let contractCreator = accounts[0];
+
+  // Contract variables
   let collateralToken;
   let finder;
   let expiringMultiPartyCreator;
+  let registry;
+
+  // Re-used variables
+  let constructorParams;
 
   beforeEach(async () => {
-    collateralToken = await ERC20Mintable.new({ from: accounts[0] });
+    collateralToken = await ERC20Mintable.new({ from: contractCreator });
     finder = await Finder.deployed();
     registry = await Registry.deployed();
 
-    expiringMultiPartyCreator = await ExpiringMultiPartyCreator.new(true, finder.address, { from: accounts[0] });
+    expiringMultiPartyCreator = await ExpiringMultiPartyCreator.new(true, finder.address, { from: contractCreator });
 
     await registry.addMember(RegistryRolesEnum.DERIVATIVE_CREATOR, expiringMultiPartyCreator.address, {
-      from: accounts[0]
+      from: contractCreator
     });
-  });
 
-  it("Can create new instances of ExpiringMultiParty", async function() {
-    const constructorParams = {
+    constructorParams = {
       isTest: true,
       expirationTimestamp: "1234567890",
       withdrawalLiveness: "1000",
@@ -51,16 +56,18 @@ contract("ExpiringMultiParty", function(accounts) {
       sponsorDisputeRewardPct: { rawValue: toWei("0.1") },
       disputerDisputeRewardPct: { rawValue: toWei("0.1") }
     };
+  });
 
+  it("Can create new instances of ExpiringMultiParty", async function() {
     let createdAddressResult = await expiringMultiPartyCreator.createExpiringMultiParty(constructorParams, {
-      from: accounts[0]
+      from: contractCreator
     });
 
     // Catch the address of the new contract from the event. Ensure that the assigned party member is correct.
     let expiringMultiPartyAddress;
     truffleAssert.eventEmitted(createdAddressResult, "CreatedExpiringMultiParty", ev => {
       expiringMultiPartyAddress = ev.expiringMultiPartyAddress;
-      return ev.expiringMultiPartyAddress != 0 && ev.partyMemberAddress == accounts[0];
+      return ev.expiringMultiPartyAddress != 0 && ev.partyMemberAddress == contractCreator;
     });
 
     // Instantiate an instance of the expiringMultiParty and check a few constants that should hold true
@@ -68,5 +75,19 @@ contract("ExpiringMultiParty", function(accounts) {
     assert(await expiringMultiParty.expirationTimestamp(), constructorParams.expirationTimestamp);
     assert(await expiringMultiParty.withdrawalLiveness(), constructorParams.withdrawalLiveness);
     assert(await expiringMultiParty.priceIdentifer(), constructorParams.priceFeedIdentifier);
+  });
+
+  it("Creation correctly registers ExpiringMultiParty within the registry", async function() {
+    let createdAddressResult = await expiringMultiPartyCreator.createExpiringMultiParty(constructorParams, {
+      from: contractCreator
+    });
+
+    let expiringMultiPartyAddress;
+    truffleAssert.eventEmitted(createdAddressResult, "CreatedExpiringMultiParty", ev => {
+      expiringMultiPartyAddress = ev.expiringMultiPartyAddress;
+      return ev.expiringMultiPartyAddress != 0 && ev.partyMemberAddress == contractCreator;
+    });
+    assert.isTrue(await registry.isDerivativeRegistered(expiringMultiPartyAddress));
+    assert.isTrue(await registry.isPartyMemberOfDerivative(contractCreator, expiringMultiPartyAddress));
   });
 });
