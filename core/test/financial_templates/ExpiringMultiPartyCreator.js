044c63edf8bf5ebdcafeef2344837b54351903a3
Chris Maree <christopher.maree@gmail.com>
2020-02-25 13:42:23 -0500

    Add Expiring Multi Party Factory (#905)
    * initial contract and test implementation
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Added additional code
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Added additional comment
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Added function comments
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * further refine tests
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Refactored constructor parms to use a seperate struct for the create function
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * cleaned up contracts
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Small typo fix
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Fixed assert error
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

---
+++ core/test/financial_templates/ExpiringMultiPartyCreator.js
@@ -0,0 +1,101 @@
+const { toWei, hexToUtf8 } = web3.utils;
+
+const truffleAssert = require("truffle-assertions");
+const { RegistryRolesEnum } = require("../../../common/Enums.js");
+
+// Tested Contract
+const ExpiringMultiPartyCreator = artifacts.require("ExpiringMultiPartyCreator");
+
+// Helper Contracts
+const Finder = artifacts.require("Finder");
+const ERC20Mintable = artifacts.require("Token");
+const TokenFactory = artifacts.require("TokenFactory");
+const Registry = artifacts.require("Registry");
+const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
+
+ERC20Mintable.setProvider(web3.currentProvider);
+
+contract("ExpiringMultiParty", function(accounts) {
+  let contractCreator = accounts[0];
+
+  // Contract variables
+  let collateralToken;
+  let finder;
+  let expiringMultiPartyCreator;
+  let registry;
+
+  // Re-used variables
+  let constructorParams;
+
+  beforeEach(async () => {
+    collateralToken = await ERC20Mintable.new("COLLATERAL_TOKEN", "COL", 18, { from: contractCreator });
+    finder = await Finder.deployed();
+    registry = await Registry.deployed();
+
+    expiringMultiPartyCreator = await ExpiringMultiPartyCreator.new(true, finder.address, { from: contractCreator });
+
+    await registry.addMember(RegistryRolesEnum.DERIVATIVE_CREATOR, expiringMultiPartyCreator.address, {
+      from: contractCreator
+    });
+
+    constructorParams = {
+      expirationTimestamp: "1234567890",
+      withdrawalLiveness: "1000",
+      collateralAddress: collateralToken.address,
+      tokenFactoryAddress: TokenFactory.address,
+      priceFeedIdentifier: web3.utils.utf8ToHex("UMATEST"),
+      syntheticName: "Test UMA Token",
+      syntheticSymbol: "UMATEST",
+      liquidationLiveness: "1000",
+      collateralRequirement: { rawValue: toWei("1.5") },
+      disputeBondPct: { rawValue: toWei("0.1") },
+      sponsorDisputeRewardPct: { rawValue: toWei("0.1") },
+      disputerDisputeRewardPct: { rawValue: toWei("0.1") }
+    };
+  });
+
+  it("Can create new instances of ExpiringMultiParty", async function() {
+    // Use `.call` to get the returned value from the function.
+    let functionReturnedAddress = await expiringMultiPartyCreator.createExpiringMultiParty.call(constructorParams, {
+      from: contractCreator
+    });
+
+    // Execute without the `.call` to perform state change. catch the result to query the event.
+    let createdAddressResult = await expiringMultiPartyCreator.createExpiringMultiParty(constructorParams, {
+      from: contractCreator
+    });
+
+    // Catch the address of the new contract from the event. Ensure that the assigned party member is correct.
+    let expiringMultiPartyAddress;
+    truffleAssert.eventEmitted(createdAddressResult, "CreatedExpiringMultiParty", ev => {
+      expiringMultiPartyAddress = ev.expiringMultiPartyAddress;
+      return ev.expiringMultiPartyAddress != 0 && ev.partyMemberAddress == contractCreator;
+    });
+
+    // Ensure value returned from the event is the same as returned from the function
+    assert.equal(functionReturnedAddress, expiringMultiPartyAddress);
+
+    // Instantiate an instance of the expiringMultiParty and check a few constants that should hold true
+    let expiringMultiParty = await ExpiringMultiParty.at(expiringMultiPartyAddress);
+    assert.equal(await expiringMultiParty.expirationTimestamp(), constructorParams.expirationTimestamp);
+    assert.equal(await expiringMultiParty.withdrawalLiveness(), constructorParams.withdrawalLiveness);
+    assert.equal(
+      hexToUtf8(await expiringMultiParty.priceIdentifer()),
+      hexToUtf8(constructorParams.priceFeedIdentifier)
+    );
+  });
+
+  it("Creation correctly registers ExpiringMultiParty within the registry", async function() {
+    let createdAddressResult = await expiringMultiPartyCreator.createExpiringMultiParty(constructorParams, {
+      from: contractCreator
+    });
+
+    let expiringMultiPartyAddress;
+    truffleAssert.eventEmitted(createdAddressResult, "CreatedExpiringMultiParty", ev => {
+      expiringMultiPartyAddress = ev.expiringMultiPartyAddress;
+      return ev.expiringMultiPartyAddress != 0 && ev.partyMemberAddress == contractCreator;
+    });
+    assert.isTrue(await registry.isDerivativeRegistered(expiringMultiPartyAddress));
+    assert.isTrue(await registry.isPartyMemberOfDerivative(contractCreator, expiringMultiPartyAddress));
+  });
+});
