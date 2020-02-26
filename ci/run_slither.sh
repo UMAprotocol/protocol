3020793a5ccdab4e206b64013c6ee4878c435756
Nick Pai <npai.nyc@gmail.com>
2020-02-26 17:07:03 -0500

    slither config
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -3,15 +3,13 @@ set -e
 
 PROTOCOL_DIR=$(pwd)
 sudo chmod -R a+rwx /usr/local/lib/node_modules
-$(npm bin)/truffle version
 
 
 run_slither() {
     cd $1
-    $(npm bin)/truffle version
-    $(npm bin)/truffle compile 
     mkdir -p node_modules/
     cp -r ../node_modules/@openzeppelin ./node_modules/@openzeppelin
+    truffle compile 
 
     cd $PROTOCOL_DIR
     slither --exclude=naming-convention,solc-version,pragma,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,locked-ether,reentrancy-eth,uninitialized-state-variables,incorrect-equality,reentrancy-events,assembly,shadowing-local,low-level-calls,constant-function-state $1
