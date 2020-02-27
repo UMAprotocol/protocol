831399e5627a1f3bc57ddcc4f01e2c5d46dd0bb3
Nick Pai <npai.nyc@gmail.com>
2020-02-27 11:24:20 -0500

    add pragma
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -10,7 +10,7 @@ run_slither() {
     cp -r ../node_modules/@openzeppelin ./node_modules/@openzeppelin
 
     cd $PROTOCOL_DIR
-    slither --exclude=naming-convention,solc-version,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,incorrect-equality,reentrancy-events,assembly --filter-paths=@openzeppelin $1
+    slither --exclude=naming-convention,pragma,solc-version,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,incorrect-equality,reentrancy-events,assembly --filter-paths=@openzeppelin $1
 }
 
 run_slither $PROTOCOL_DIR/core
