0035a41cd20a4c9c2aaa6f3c96c4a881344e60fd
Chris Maree <christopher.maree@gmail.com>
2020-02-27 14:45:43 -0500

    Update core/contracts/oracle/implementation/Registry.sol
    Co-Authored-By: Matt Rice <matthewcrice32@gmail.com>

--- core/contracts/oracle/implementation/Registry.sol
+++ core/contracts/oracle/implementation/Registry.sol
@@ -183,7 +183,7 @@ contract Registry is RegistryInterface, MultiRole {
     /**
      * @notice checks if a party member is part of a derivative.
      * @param party party to check.
-     * @param derivativeAddress address to check against the party member.
+     * @param derivativeAddress address to check against the party.
      * @return bool indicating if the address member is party of the derivative party.
      */
     function isPartyMemberOfDerivative(address party, address derivativeAddress) public view returns (bool) {
