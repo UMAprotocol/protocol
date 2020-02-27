702fa38b2361e69c1a58b436d2ba55e8cc4edd3e
Chris Maree <christopher.maree@gmail.com>
2020-02-27 14:47:12 -0500

    Update core/contracts/oracle/implementation/DesignatedVotingFactory.sol
    Co-Authored-By: Matt Rice <matthewcrice32@gmail.com>

--- core/contracts/oracle/implementation/DesignatedVotingFactory.sol
+++ core/contracts/oracle/implementation/DesignatedVotingFactory.sol
@@ -6,7 +6,7 @@ import "../../common/implementation/Withdrawable.sol";
 import "./DesignatedVoting.sol";
 
 /**
- * @title Factory to create new instances of DesignatedVoting and to looking up deployed instances.
+ * @title Factory to deploy new instances of DesignatedVoting and look up previously deployed instances.
  * @dev Allows off-chain infrastructure to look up a hot wallet's deployed DesignatedVoting contract.
  */
 contract DesignatedVotingFactory is Withdrawable {
