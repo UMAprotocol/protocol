207982d41bfdbdc5d40291e3d038d3e0e966d12f
nicholaspai <npai.nyc@gmail.com>
2020-03-06 15:52:39 -0500

    Add EMP deployer constraints (#1023)
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/contracts/financial-templates/implementation/ExpiringMultiPartyCreator.sol
+++ core/contracts/financial-templates/implementation/ExpiringMultiPartyCreator.sol
@@ -10,26 +10,46 @@ import "./ExpiringMultiParty.sol";
 
 /**
 @title Expiring Multi Party Contract creator
-@notice Factory contract to create and register new instances of expiring multiparty contracts
+@notice Factory contract to create and register new instances of expiring multiparty contracts. Responsible for
+constraining the parameters used to construct a new EMP.
 */
 contract ExpiringMultiPartyCreator is ContractCreator, Testable {
+    using FixedPoint for FixedPoint.Unsigned;
+
     struct Params {
         uint expirationTimestamp;
-        uint withdrawalLiveness;
         uint siphonDelay;
         address collateralAddress;
         address tokenFactoryAddress;
         bytes32 priceFeedIdentifier;
         string syntheticName;
         string syntheticSymbol;
-        uint liquidationLiveness;
         FixedPoint.Unsigned collateralRequirement;
         FixedPoint.Unsigned disputeBondPct;
         FixedPoint.Unsigned sponsorDisputeRewardPct;
         FixedPoint.Unsigned disputerDisputeRewardPct;
     }
 
+    // @dev: These constraints can evolve over time and are initially constrained to conservative values
+    // in this first iteration of an EMP creator:
+    // - Whitelist allowed collateral currencies.
     AddressWhitelist public collateralTokenWhitelist;
+    // - Last expiration date: 2021-06-30T0:00:00.000Z.
+    uint public constant LATEST_EXPIRATION_TIMESTAMP = 1625011200;
+    // - Time for pending withdrawal to be disputed: 60 minutes. Lower liveness increases sponsor usability. However, this parameter is a reflection of how long we expect it to take for
+    // liquidators to identify that a sponsor is undercollateralized and acquire the tokens needed to liquidate them.
+    // This is also a reflection of how long a malicious sponsor would need to maintain a lower-price manipulation to get their withdrawal
+    // processed maliciously (if we set it too low, it’s quite easy for malicious sponsors to request a withdrawal and spend gas to prevent
+    // other transactions from processing until the withdrawal gets approved). Ultimately, we think liveness is a friction to be minimized,
+    // but not critical to the system functioning.
+    uint public constant STRICT_WITHDRAWAL_LIVENESS = 3600;
+    // - Time for liquidation to be disputed: 60 minutes. Similar reasoning to withdrawal liveness. Lower liveness is more usable for liquidators.
+    // However, the parameter is a reflection of how long we expect it to take disputers to notice bad liquidations.
+    // Malicious liquidators would also need to attack the base chain for this long to prevent dispute transactions from processing.
+    uint public constant STRICT_LIQUIDATION_LIVENESS = 3600;
+    // - Minimum dispute bond: 0%. We think this should be positive so that every dispute has some cost so that disputers are disincentivized from
+    // wrongly disputing sponsors.
+    uint public constant MIN_DISPUTE_BOND_PCT = 0;
 
     event CreatedExpiringMultiParty(address expiringMultiPartyAddress, address partyMemberAddress);
 
@@ -69,23 +89,27 @@ contract ExpiringMultiPartyCreator is ContractCreator, Testable {
         constructorParams.isTest = isTest;
         constructorParams.finderAddress = finderAddress;
 
+        // Enforce configuration constrainments
         // @dev: Technically there is nothing in the ExpiringMultiParty contract
-        // requiring the collateral token to be whitelisted. However, because "createExpiringMultiParty()"
-        // is supposed to be the only way to create valid financial contracts that are **registered** with the DVM (via "_registerContract()"),
-        // we can enforce whitelisting of collateral currencies here in practice.
+        // requiring these constraints. However, because "createExpiringMultiParty()"
+        // is intended to be the only way to create valid financial contracts that are **registered** with the DVM (via "_registerContract()"),
+        // we can enforce deployment configurations here.
+        require(params.expirationTimestamp <= LATEST_EXPIRATION_TIMESTAMP);
+        require(params.disputeBondPct.isGreaterThan(MIN_DISPUTE_BOND_PCT));
+        require(bytes(params.syntheticName).length != 0);
+        require(bytes(params.syntheticSymbol).length != 0);
+        constructorParams.withdrawalLiveness = STRICT_WITHDRAWAL_LIVENESS;
+        constructorParams.liquidationLiveness = STRICT_LIQUIDATION_LIVENESS;
         require(collateralTokenWhitelist.isOnWhitelist(params.collateralAddress));
-        constructorParams.collateralAddress = params.collateralAddress;
 
         // Input from function call
         constructorParams.expirationTimestamp = params.expirationTimestamp;
-        constructorParams.withdrawalLiveness = params.withdrawalLiveness;
         constructorParams.siphonDelay = params.siphonDelay;
         constructorParams.collateralAddress = params.collateralAddress;
         constructorParams.tokenFactoryAddress = params.tokenFactoryAddress;
         constructorParams.priceFeedIdentifier = params.priceFeedIdentifier;
         constructorParams.syntheticName = params.syntheticName;
         constructorParams.syntheticSymbol = params.syntheticSymbol;
-        constructorParams.liquidationLiveness = params.liquidationLiveness;
         constructorParams.collateralRequirement = params.collateralRequirement;
         constructorParams.disputeBondPct = params.disputeBondPct;
         constructorParams.sponsorDisputeRewardPct = params.sponsorDisputeRewardPct;
