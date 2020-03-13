4fb27c176e97569b73ec6c2871e58b7385fea083
Christopher Maree <christopher.maree@gmail.com>
2020-03-13 19:20:41 +0200

    Updated script
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- financial-templates-lib/GasEstimator.js
+++ financial-templates-lib/GasEstimator.js
@@ -1,4 +1,5 @@
-const https = require("https");
+const fetch = require("node-fetch");
+const url = "https://ethgasstation.info/json/ethgasAPI.json";
 
 class GasEstimator {
   constructor(updateThreshold = 60) {
@@ -12,6 +13,26 @@ class GasEstimator {
     if (currentTime > this.lastUpdateTimestamp + this.updateThreshold) {
       return;
     } else {
+      let response = await this.getData(url);
+      this.lastFastPrice = response.fast;
+    }
+  };
+
+  getCurrentFastPrice = () => {
+    return this.lastFastPrice;
+  };
+
+  getData = async url => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      console.log(error);
     }
   };
 }
+
+module.exports = {
+  GasEstimator
+};
