568d5eccf446861657da4e0ccaf00592fdc075d6
Christopher Maree <christopher.maree@gmail.com>
2020-03-17 09:55:23 +0200

    Updated how default case is handeled
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- financial-templates-lib/GasEstimator.js
+++ financial-templates-lib/GasEstimator.js
@@ -3,23 +3,41 @@
 
 const fetch = require("node-fetch");
 const url = "https://ethgasstation.info/json/ethgasAPI.json";
+const { Logger } = require("./Logger");
 
 // If no updateThreshold is specified then default to updating every 60 seconds.
 class GasEstimator {
   constructor(updateThreshold = 60) {
     this.updateThreshold = updateThreshold;
     this.lastUpdateTimestamp;
-    this.lastFastPriceGwei = 20; // default until updated by the `_update` function.
+    this.lastFastPriceGwei;
+
+    // If the script fails or the API resonce fails default to this value
+    this.defaultFastPriceGwei = 40;
   }
 
   _update = async () => {
     const currentTime = Math.floor(Date.now() / 1000);
-    if (currentTime > this.lastUpdateTimestamp + this.updateThreshold) {
+    if (currentTime < this.lastUpdateTimestamp + this.updateThreshold) {
+      Logger.debug({
+        at: "GasEstimator",
+        message: "Gas Estimator update skipped due to update threshold",
+        currentTime: currentTime,
+        lastUpdateTimestamp: this.lastUpdateTimestamp,
+        lastFastPriceGwei: this.lastFastPriceGwei,
+        timeRemainingUntilUpdate: this.lastUpdateTimestamp + this.updateThreshold - currentTime
+      });
       return;
     } else {
-      let response = await this.getData(url);
-      // The number returned by EthGasStation is a Gwei amount, scaled by 10.
-      this.lastFastPriceGwei = response.fast / 10;
+      let returnedPrice = await this.getPrice(url);
+      this.lastFastPriceGwei = returnedPrice;
+      this.lastUpdateTimestamp = currentTime;
+      Logger.debug({
+        at: "GasEstimator",
+        message: "Gas Estimator updated",
+        gasPrice: returnedPrice,
+        lastUpdateTimestamp: this.lastUpdateTimestamp
+      });
     }
   };
 
@@ -28,13 +46,22 @@ class GasEstimator {
     return this.lastFastPriceGwei * 1e9;
   };
 
-  getData = async url => {
+  getPrice = async url => {
     try {
       const response = await fetch(url);
       const json = await response.json();
-      return json;
+      // The number returned by EthGasStation is a Gwei amount, scaled by 10.
+      let price = json.fast / 10;
+      return price;
     } catch (error) {
-      console.log(error);
+      Logger.error({
+        at: "GasEstimator",
+        message: "client polling error",
+        error: error
+      });
+
+      // In the failure mode return the fast default price.
+      return this.defaultFastPriceGwei;
     }
   };
 }
