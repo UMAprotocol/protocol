607258d1987ad661dd69c33a565cd2eda59de798
Prasad Tare <ptare@users.noreply.github.com>
2020-03-17 12:31:03 -0400

    Refactor code for actions in showMarketDetails (#1074)
    Based on a suggestion from @nicholaspai on PR #1073.
    
    Issue #1040
    
    Signed-off-by: Prasad Tare prasad.tare@gmail.com

--- core/scripts/cli/sponsor/showMarketDetails.js
+++ core/scripts/cli/sponsor/showMarketDetails.js
@@ -9,18 +9,14 @@ const showMarketDetails = async (web3, artifacts, emp) => {
   const collateral = await emp.getCollateral(sponsorAddress);
 
   const backAction = "Back";
+  let actions;
   if (collateral.toString() === "0") {
     // Sponsor doesn't have a position.
-    const prompt = {
-      type: "list",
-      name: "choice",
-      message: "You are not currently a sponsor",
-      choices: [{ name: "Sponsor new position" }, { name: backAction }]
+    console.log("You are not currently a sponsor");
+    actions = {
+      back: "Back",
+      create: "Sponsor new position"
     };
-    const input = await inquirer.prompt(prompt);
-    if (input["choice"] !== backAction) {
-      await create(web3, artifacts, emp);
-    }
   } else {
     const position = await emp.positions(sponsorAddress);
     console.log(
@@ -30,30 +26,30 @@ const showMarketDetails = async (web3, artifacts, emp) => {
       fromWei(collateral.toString())
     );
 
-    const actions = {
+    actions = {
       back: "Back",
       create: "Borrow more tokens",
       redeem: "Repay tokens"
     };
-    const prompt = {
-      type: "list",
-      name: "choice",
-      message: "What would you like to do?",
-      choices: Object.values(actions)
-    };
-    const input = (await inquirer.prompt(prompt))["choice"];
-    switch (input) {
-      case actions.create:
-        await create(web3, artifacts, emp);
-        break;
-      case actions.redeem:
-        await redeem(web3, artifacts, emp);
-        break;
-      case actions.back:
-        return;
-      default:
-        console.log("unimplemented state");
-    }
+  }
+  const prompt = {
+    type: "list",
+    name: "choice",
+    message: "What would you like to do?",
+    choices: Object.values(actions)
+  };
+  const input = (await inquirer.prompt(prompt))["choice"];
+  switch (input) {
+    case actions.create:
+      await create(web3, artifacts, emp);
+      break;
+    case actions.redeem:
+      await redeem(web3, artifacts, emp);
+      break;
+    case actions.back:
+      return;
+    default:
+      console.log("unimplemented state");
   }
 };
 
