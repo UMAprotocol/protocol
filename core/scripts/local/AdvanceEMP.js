c98faa34195845412d1200b418d5665fc11be610
Nick Pai <npai.nyc@gmail.com>
2020-03-18 11:08:11 -0400

    add DVM to contract, improve comments
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/scripts/local/AdvanceEMP.js
+++ core/scripts/local/AdvanceEMP.js
@@ -1,37 +1,35 @@
 /**
- * This script advances time in the EMP forward by some specified amount of seconds (or one hour and one second by default).
+ * This script advances time in all EMP's and the Voting contract forward by some specified amount of seconds (or one hour and one second by default).
  *
- * `truffle exec ./scripts/local/AdvanceEMPTime --time 10000`
- * Advances time by 10 seconds
+ * `truffle exec ./scripts/local/AdvanceEMPTime --time 10`
+ * Advances time for all contracts by 10 seconds
  */
 const { toBN } = web3.utils;
 const argv = require("minimist")(process.argv.slice(), { string: ["time"] });
+const { interfaceName } = require("../../utils/Constants.js");
 
 // Deployed contract ABI's and addresses we need to fetch.
 const Governor = artifacts.require("Governor");
 const Registry = artifacts.require("Registry");
 const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
+const Finder = artifacts.require("Finder");
+const Voting = artifacts.require("Voting");
 
-// Contracts we need to interact with.
-let registry;
-let emp;
-
-/** ***************************************************
- * Main Script
- /*****************************************************/
 const advanceTime = async callback => {
   try {
-    registry = await Registry.deployed();
+    let registry = await Registry.deployed();
     const contractAddresses = await registry.getAllRegisteredContracts();
+
+    let leapForward = argv.time ? argv.time : 3601;
+    console.log(`Advancing contract time forward by ${leapForward} seconds`);
 
     // Query all registered EMP's.
     for (const address of contractAddresses) {
       // The governor is always registered as a contract, but it isn't an ExpiringMultiParty.
       if (address !== Governor.address) {
-        emp = await ExpiringMultiParty.at(address);
+        let emp = await ExpiringMultiParty.at(address);
 
-        // Advance time
-        let leapForward = argv.time ? argv.time : 3601;
+        // Advance time in the EMP.
         let currentTime = await emp.getCurrentTime();
         let newTime = toBN(currentTime.toString()).add(toBN(leapForward.toString()));
         await emp.setCurrentTime(newTime.toString());
@@ -40,6 +38,18 @@ const advanceTime = async callback => {
         console.log(`Set time to ${currentTimeReadable} for the EMP @ ${emp.address}`);
       }
     }
+
+    // Advance time in the registered Oracle.
+    const finder = await Finder.deployed();
+    const deployedVoting = await Voting.at(
+      await finder.getImplementationAddress(web3.utils.utf8ToHex(interfaceName.Oracle))
+    );
+    let currentTime = await deployedVoting.getCurrentTime();
+    let newTime = toBN(currentTime.toString()).add(toBN(leapForward.toString()));
+    await deployedVoting.setCurrentTime(newTime.toString());
+    currentTime = await deployedVoting.getCurrentTime();
+    let currentTimeReadable = new Date(Number(currentTime.toString()) * 1000);
+    console.log(`Set time to ${currentTimeReadable} for the DVM @ ${deployedVoting.address}`);
   } catch (err) {
     console.error(err);
   }
