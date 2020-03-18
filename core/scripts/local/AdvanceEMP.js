150d5c7801b508c897c55f36959c4826b0b18b55
Nick Pai <npai.nyc@gmail.com>
2020-03-18 11:13:10 -0400

    change let to const
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/scripts/local/AdvanceEMP.js
+++ core/scripts/local/AdvanceEMP.js
@@ -17,24 +17,24 @@ const Voting = artifacts.require("Voting");
 
 const advanceTime = async callback => {
   try {
-    let registry = await Registry.deployed();
+    const registry = await Registry.deployed();
     const contractAddresses = await registry.getAllRegisteredContracts();
 
-    let leapForward = argv.time ? argv.time : 3601;
+    const leapForward = argv.time ? argv.time : 3601;
     console.log(`Advancing contract time forward by ${leapForward} seconds`);
 
     // Query all registered EMP's.
     for (const address of contractAddresses) {
       // The governor is always registered as a contract, but it isn't an ExpiringMultiParty.
       if (address !== Governor.address) {
-        let emp = await ExpiringMultiParty.at(address);
+        const emp = await ExpiringMultiParty.at(address);
 
         // Advance time in the EMP.
         let currentTime = await emp.getCurrentTime();
-        let newTime = toBN(currentTime).add(toBN(leapForward));
+        const newTime = toBN(currentTime).add(toBN(leapForward));
         await emp.setCurrentTime(newTime);
         currentTime = await emp.getCurrentTime();
-        let currentTimeReadable = new Date(Number(currentTime) * 1000);
+        const currentTimeReadable = new Date(Number(currentTime) * 1000);
         console.log(`Set time to ${currentTimeReadable} for the EMP @ ${emp.address}`);
       }
     }
@@ -45,10 +45,10 @@ const advanceTime = async callback => {
       await finder.getImplementationAddress(web3.utils.utf8ToHex(interfaceName.Oracle))
     );
     let currentTime = await deployedVoting.getCurrentTime();
-    let newTime = toBN(currentTime).add(toBN(leapForward));
+    const newTime = toBN(currentTime).add(toBN(leapForward));
     await deployedVoting.setCurrentTime(newTime);
     currentTime = await deployedVoting.getCurrentTime();
-    let currentTimeReadable = new Date(Number(currentTime) * 1000);
+    const currentTimeReadable = new Date(Number(currentTime) * 1000);
     console.log(`Set time to ${currentTimeReadable} for the DVM @ ${deployedVoting.address}`);
   } catch (err) {
     callback(err);
