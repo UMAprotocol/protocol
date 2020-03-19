be7ab19d60e4883083f7737648547121d2666f23
Prasad Tare <ptare@users.noreply.github.com>
2020-03-19 15:12:23 -0400

    Add copy of WETH9 and local migration for it (#1098)
    Issue #1040
    
    Signed-off-by: Prasad Tare prasad.tare@gmail.com
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- common/PublicNetworks.js
+++ common/PublicNetworks.js
@@ -3,24 +3,28 @@ module.exports = {
     name: "mainnet",
     ethFaucet: null,
     etherscan: "https://etherscan.io/",
-    daiAddress: "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359"
+    daiAddress: "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359",
+    wethAddress: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
   },
   3: {
     name: "ropsten",
     ethFaucet: "https://faucet.metamask.io/",
     etherscan: "https://ropsten.etherscan.io/",
-    daiAddress: "0xB5E5D0F8C0cbA267CD3D7035d6AdC8eBA7Df7Cdd"
+    daiAddress: "0xB5E5D0F8C0cbA267CD3D7035d6AdC8eBA7Df7Cdd",
+    wethAddress: "0xc778417e063141139fce010982780140aa0cd5ab"
   },
   4: {
     name: "rinkeby",
     ethFaucet: "https://faucet.rinkeby.io/",
     etherscan: "https://rinkeby.etherscan.io/",
-    daiAddress: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa"
+    daiAddress: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
+    wethAddress: "0xc778417e063141139fce010982780140aa0cd5ab"
   },
   42: {
     name: "kovan",
     ethFaucet: "https://faucet.kovan.network/",
     etherscan: "https://kovan.etherscan.io/",
-    daiAddress: "0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99"
+    daiAddress: "0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99",
+    wethAddress: "0xd0a1e359811322d97991e03f863a0c30c2cf029c"
   }
 };
