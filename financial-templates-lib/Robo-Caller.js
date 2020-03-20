c0e04bfb6753f5f663efa9cbe31335592b075c02
Christopher Maree <christopher.maree@gmail.com>
2020-03-20 17:07:12 +0200

    merge fix
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- financial-templates-lib/Robo-Caller.js
+++ financial-templates-lib/Robo-Caller.js
@@ -1,30 +1,14 @@
 require("dotenv").config();
-<<<<<<< HEAD
-const { Logger } = require("./Logger");
-=======
->>>>>>> master
 
 class RoboCaller {
   constructor() {
     if (
-<<<<<<< HEAD
-      process.env.TWILIO_SID == "" || // require the account SID
-      process.env.TWILIO_AUTH == "" || // require the account authentication key
-      process.env.DRI_NUMBER1 == "" || // require at least 1 number to call
-      process.env.TWILIO_FROM_NUMBER == "" // require the number to originate the call from
-    ) {
-      Logger.debug({
-        at: "RoboCaller",
-        message: "Missing config variable. RoboCaller Disabled"
-      });
-=======
       !process.env.TWILIO_SID || // require the account SID
       !process.env.TWILIO_AUTH || // require the account authentication key
       !process.env.DRI_NUMBER1 || // require at least 1 number to call
       !process.env.TWILIO_FROM_NUMBER // require the number to originate the call from
     ) {
       console.error("Missing config variable. RoboCaller Disabled");
->>>>>>> master
       return;
     }
     this.client = require("twilio")(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
@@ -35,39 +19,16 @@ class RoboCaller {
         this.numbersToCall.push(process.env[envVariable]);
       }
     }
-<<<<<<< HEAD
-    Logger.debug({
-      at: "RoboCaller",
-      message: "Client configured",
-      numbersToCall: this.numbersToCall,
-      fromNumber: process.env.TWILIO_FROM_NUMBER
-    });
-=======
->>>>>>> master
   }
   canPlaceCall = () => {
     return this.client != undefined;
   };
   placeCall = async message => {
     if (!this.canPlaceCall) {
-<<<<<<< HEAD
-      Logger.debug({
-        at: "RoboCaller",
-        message: "Incorrectly configured! Cant place call"
-      });
-      return;
-    }
-    Logger.debug({
-      at: "RoboCaller",
-      message: "Placing calls",
-      numbersToCall: this.numbersToCall
-    });
-=======
       console.log("cant place call");
       return;
     }
 
->>>>>>> master
     for (const number of this.numbersToCall) {
       try {
         const callResponse = await this.client.calls.create({
@@ -75,27 +36,8 @@ class RoboCaller {
           to: number,
           from: process.env.TWILIO_FROM_NUMBER
         });
-<<<<<<< HEAD
-        Logger.debug({
-          at: "RoboCaller",
-          message: "call placed",
-          accountSid: callResponse.accountSid,
-          sid: callResponse.sid,
-          from: callResponse.fromFormatted,
-          to: callResponse.toFormatted
-        });
-      } catch (error) {
-        Logger.debug({
-          at: "RoboCaller",
-          message: "Failed to place a call",
-          to: number,
-          from: process.env.TWILIO_FROM_NUMBER,
-          error: error
-        });
-=======
       } catch (error) {
         console.error("something went wrong", error);
->>>>>>> master
       }
     }
   };
