caca150eb9afcb9fe7d8bade6cacade45aff47c6
Chris Maree <christopher.maree@gmail.com>
2020-03-20 17:01:57 +0200

    Initial twilio robocall bot (#1099)
    * Added initial twillio robo caller bot
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * removed comment
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Lint
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    Removed else and updated sample env
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    Removed else and updated sample env
    
    * Refactored bot
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Updated env sample
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * refactored if statement
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

---
+++ financial-templates-lib/Robo-Caller.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+class RoboCaller {
+  constructor() {
+    if (
+      !process.env.TWILIO_SID || // require the account SID
+      !process.env.TWILIO_AUTH || // require the account authentication key
+      !process.env.DRI_NUMBER1 || // require at least 1 number to call
+      !process.env.TWILIO_FROM_NUMBER // require the number to originate the call from
+    ) {
+      console.error("Missing config variable. RoboCaller Disabled");
+      return;
+    }
+    this.client = require("twilio")(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
+
+    this.numbersToCall = [];
+    for (const envVariable in process.env) {
+      if (envVariable.startsWith("DRI_NUMBER")) {
+        this.numbersToCall.push(process.env[envVariable]);
+      }
+    }
+  }
+  canPlaceCall = () => {
+    return this.client != undefined;
+  };
+  placeCall = async message => {
+    if (!this.canPlaceCall) {
+      console.log("cant place call");
+      return;
+    }
+
+    for (const number of this.numbersToCall) {
+      try {
+        const callResponse = await this.client.calls.create({
+          twiml: this.generateTwiML(message),
+          to: number,
+          from: process.env.TWILIO_FROM_NUMBER
+        });
+      } catch (error) {
+        console.error("something went wrong", error);
+      }
+    }
+  };
+  generateTwiML = message => {
+    return `<Response>
+            <Say voice="alice">${message}</Say>
+        </Response>`;
+  };
+}
+
+module.exports = { RoboCaller };
