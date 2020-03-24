585ec83f41532df8347d7fe443a817e3b50a493e
nicholaspai <npai.nyc@gmail.com>
2020-03-24 14:39:12 -0400

    Can dry run index.js for Liquidator bot (#1116)
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- liquidator/index.js
+++ liquidator/index.js
@@ -6,7 +6,7 @@ const { delay } = require("../financial-templates-lib/delay");
 const { Logger } = require("../financial-templates-lib/Logger");
 
 // JS libs
-const { Liquidator } = require("./Liquidator");
+const { Liquidator } = require("./liquidator");
 const { GasEstimator } = require("../financial-templates-lib/GasEstimator");
 const { ExpiringMultiPartyClient } = require("../financial-templates-lib/ExpiringMultiPartyClient");
 
@@ -14,29 +14,26 @@ const { ExpiringMultiPartyClient } = require("../financial-templates-lib/Expirin
 const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
 
 // TODO: Figure out a good way to run this script, maybe with a wrapper shell script.
-// Currently, you can run it with `truffle exec ../liquidator/liquidator.js --address=<address> --price=<price>` *from the core
+// Currently, you can run it with `truffle exec ../liquidator/index.js --address=<address> --price=<price>` *from the core
 // directory*.
 
-async function run() {
-  if (!argv.address) {
-    console.log("Bad input arg! Specify an `address` for the location of the expiring Multi Party.");
-    return;
-  }
-  // TODO: Remove this price flag once we have built the pricefeed module.
-  if (!argv.price) {
-    console.log("Bad input arg! Specify a `price` as the pricefeed.");
-    return;
-  }
+/**
+ * @notice Continuously attempts to liquidate positions in the EMP contract.å
+ * @param {Number} price Price used to determine undercollateralized positions to liquidate.
+ * @param {String} address Contract address of the EMP.
+ * @return None or throws an Error.
+ */
+async function run(price, address, shouldPoll) {
   Logger.info({
     at: "liquidator#index",
     message: "liquidator started 🕵️‍♂️",
-    empAddress: argv.address,
-    currentPrice: argv.price
+    empAddress: address,
+    currentPrice: price
   });
 
   // Setup web3 accounts an contract instance
   const accounts = await web3.eth.getAccounts();
-  const emp = await ExpiringMultiParty.at(argv.address);
+  const emp = await ExpiringMultiParty.at(address);
 
   // Client and liquidator bot
   const empClient = new ExpiringMultiPartyClient(ExpiringMultiParty.abi, web3, emp.address);
@@ -51,7 +48,7 @@ async function run() {
       // Acquire synthetic tokens somehow. v0: assume the bot holds on to them.
       // Liquidate any undercollateralized positions!
       // Withdraw money from any liquidations that are expired or DisputeFailed.
-      await liquidator.queryAndLiquidate(toWei(argv.price.toString()));
+      await liquidator.queryAndLiquidate(toWei(price.toString()));
       await liquidator.queryAndWithdrawRewards();
     } catch (error) {
       Logger.error({
@@ -61,15 +58,31 @@ async function run() {
       });
     }
     await delay(Number(10_000));
+
+    if (!shouldPoll) {
+      break;
+    }
   }
 }
 
-module.exports = async function(callback) {
+const Poll = async function(callback) {
   try {
-    await run();
+    if (!argv.address) {
+      throw new Error("Bad input arg! Specify an `address` for the location of the expiring Multi Party.");
+    }
+    // TODO: Remove this price flag once we have built the pricefeed module.
+    if (!argv.price) {
+      throw new Error("Bad input arg! Specify a `price` as the pricefeed.");
+    }
+
+    await run(argv.address, argv.price, true);
   } catch (err) {
-    console.error(err);
     callback(err);
   }
   callback();
 };
+
+// Attach this function to the exported function
+// in order to allow the script to be executed through both truffle and a test runner.
+Poll.run = run;
+module.exports = Poll;
