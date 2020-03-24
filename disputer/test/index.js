67091d60e1234e53b3d4eb73d72d92d5bc16095a
Nick Pai <npai.nyc@gmail.com>
2020-03-24 16:36:49 -0400

    remove extra code
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- disputer/test/index.js
+++ disputer/test/index.js
@@ -12,34 +12,27 @@ const { GasEstimator } = require("../../financial-templates-lib/GasEstimator");
 const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
 const Finder = artifacts.require("Finder");
 const IdentifierWhitelist = artifacts.require("IdentifierWhitelist");
-const MockOracle = artifacts.require("MockOracle");
-const TokenFactory = artifacts.require("TokenFactory");
 const Token = artifacts.require("ExpandedERC20");
+const TokenFactory = artifacts.require("TokenFactory");
 
 contract("Disputer.js", function(accounts) {
   const disputeBot = accounts[0];
-  const contractCreator = accounts[1];
+  const contractCreator = accounts[0];
 
-  let collateralToken;
   let emp;
-  let mockOracle;
+  let collateralToken;
 
   before(async function() {
     collateralToken = await Token.new({ from: contractCreator });
-    await collateralToken.addMember(1, contractCreator, {
-      from: contractCreator
-    });
 
-    // Create a mockOracle and finder. Register the mockMoracle with the finder.
-    mockOracle = await MockOracle.new(IdentifierWhitelist.address, {
-      from: contractCreator
-    });
-    finder = await Finder.deployed();
-    const mockOracleInterfaceName = utf8ToHex("Oracle");
-    await finder.changeImplementationAddress(mockOracleInterfaceName, mockOracle.address);
+    // Create identifier whitelist and register the price tracking ticker with it.
+    identifierWhitelist = await IdentifierWhitelist.deployed();
+    await identifierWhitelist.addSupportedIdentifier(utf8ToHex("UMATEST"));
   });
 
   beforeEach(async function() {
+    collateralToken = await Token.new({ from: contractCreator });
+
     const constructorParams = {
       isTest: true,
       expirationTimestamp: "12345678900",
@@ -56,11 +49,6 @@ contract("Disputer.js", function(accounts) {
       sponsorDisputeRewardPct: { rawValue: toWei("0.1") },
       disputerDisputeRewardPct: { rawValue: toWei("0.1") }
     };
-
-    identifierWhitelist = await IdentifierWhitelist.deployed();
-    await identifierWhitelist.addSupportedIdentifier(constructorParams.priceFeedIdentifier, {
-      from: accounts[0]
-    });
 
     // Deploy a new expiring multi party
     emp = await ExpiringMultiParty.new(constructorParams);
--- liquidator/test/index.js
+++ liquidator/test/index.js
@@ -12,37 +12,24 @@ const { GasEstimator } = require("../../financial-templates-lib/GasEstimator");
 const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
 const Finder = artifacts.require("Finder");
 const IdentifierWhitelist = artifacts.require("IdentifierWhitelist");
-const MockOracle = artifacts.require("MockOracle");
 const TokenFactory = artifacts.require("TokenFactory");
 const Token = artifacts.require("ExpandedERC20");
 
 contract("Liquidator.js", function(accounts) {
   // implementation uses the 0th address by default as the bot runs using the default truffle wallet accounts[0]
   const liquidatorBot = accounts[0];
-  const contractCreator = accounts[1];
+  const contractCreator = accounts[0];
 
   let collateralToken;
   let emp;
   let liquidator;
-  let mockOracle;
 
   before(async function() {
     collateralToken = await Token.new({ from: contractCreator });
-    await collateralToken.addMember(1, contractCreator, {
-      from: contractCreator
-    });
 
     // Create identifier whitelist and register the price tracking ticker with it.
     identifierWhitelist = await IdentifierWhitelist.deployed();
     await identifierWhitelist.addSupportedIdentifier(utf8ToHex("UMATEST"));
-
-    // Create a mockOracle and finder. Register the mockOracle with the finder.
-    mockOracle = await MockOracle.new(identifierWhitelist.address, {
-      from: contractCreator
-    });
-    finder = await Finder.deployed();
-    const mockOracleInterfaceName = utf8ToHex("Oracle");
-    await finder.changeImplementationAddress(mockOracleInterfaceName, mockOracle.address);
   });
 
   beforeEach(async function() {
