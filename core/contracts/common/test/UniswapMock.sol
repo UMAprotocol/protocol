4406e623bbd22a8b76b09b0df3e6fd7be1c64a98
Matt Rice <matthewcrice32@gmail.com>
2020-03-25 12:59:18 -0400

    Remove event declaration from impl
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/contracts/common/test/UniswapMock.sol
+++ core/contracts/common/test/UniswapMock.sol
@@ -7,9 +7,6 @@ import "../interfaces/Uniswap.sol";
  * @title Uniswap v2 Mock that allows manual price injection.
  */
 contract UniswapMock is Uniswap {
-    // Called after every swap showing the new uniswap "price" for this token pair.
-    event Sync(uint112 reserve0, uint112 reserve1);
-
     function setPrice(uint112 reserve0, uint112 reserve1) external {
         emit Sync(reserve0, reserve1);
     }
