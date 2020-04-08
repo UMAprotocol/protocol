eb1d5431d9f651e225562e625864122b65c7a1d2
Chris Maree <christopher.maree@gmail.com>
2020-04-08 15:33:57 +0200

    Revert FormattingUtils.js

--- common/FormattingUtils.js
+++ common/FormattingUtils.js
@@ -1,6 +1,6 @@
 const BigNumber = require("bignumber.js");
 
-const formatDate = (timestampInSeconds, web3) => {
+export function formatDate(timestampInSeconds, web3) {
   return new Date(
     parseInt(
       web3.utils
@@ -10,18 +10,18 @@ const formatDate = (timestampInSeconds, web3) => {
       10
     )
   ).toString();
-};
+}
 
 // formatWei converts a string or BN instance from Wei to Ether, e.g., 1e19 -> 10.
-const formatWei = (num, web3) => {
+export function formatWei(num, web3) {
   // Web3's `fromWei` function doesn't work on BN objects in minified mode (e.g.,
   // `web3.utils.isBN(web3.utils.fromBN("5"))` is false), so we use a workaround where we always pass in strings.
   // See https://github.com/ethereum/web3.js/issues/1777.
   return web3.utils.fromWei(num.toString());
-};
+}
 
 // Formats the input to round to decimalPlaces number of decimals.
-const formatWithMaxDecimals = (num, decimalPlaces, roundUp) => {
+export function formatWithMaxDecimals(num, decimalPlaces, roundUp) {
   if (roundUp) {
     BigNumber.set({ ROUNDING_MODE: BigNumber.ROUND_UP });
   } else {
@@ -31,56 +31,10 @@ const formatWithMaxDecimals = (num, decimalPlaces, roundUp) => {
   const fullPrecisionFloat = BigNumber(num);
 
   // Convert back to BN to truncate any trailing 0s that the toFixed() output would print.
-  const fixedPrecisionFloat = BigNumber(fullPrecisionFloat).toFixed(decimalPlaces);
-  return fixedPrecisionFloat.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
+  const fixedPrecisionFloat = BigNumber(fullPrecisionFloat.toFixed(decimalPlaces));
+  return fixedPrecisionFloat.toString();
+}
 
-const createFormatFunction = (web3, numDisplayedDecimals) => {
+export function createFormatFunction(web3, numDisplayedDecimals) {
   return valInWei => formatWithMaxDecimals(formatWei(valInWei, web3), numDisplayedDecimals, false);
-};
-
-// generate a etherscan link prefix
-function createEtherscanLinkFromtx(web3, networkUtils) {
-  // Construct etherscan link based on network
-  // const networkId = web3.networkId;
-  const networkId = 1;
-  let url;
-  if (networkUtils[networkId]) {
-    url = `${networkUtils[networkId].etherscan}`;
-  } else {
-    // No URL for localhost, just show transaction ID
-    url = "";
-  }
-  return url;
 }
-
-// Convert either an address or transaction to a shorter version.
-// 0x772871a444c6e4e9903d8533a5a13101b74037158123e6709470f0afbf6e7d94 -> 0x7787...7d94
-function createShortHexString(hex) {
-  return hex.substring(0, 5) + "..." + hex.substring(hex.length - 6, hex.length - 1);
-}
-
-// Take in either a transaction or an account and generate an etherscan link for the corresponding
-// network formatted in markdown.
-function createEtherscanLinkMarkdown(web3, networkUtils, hex) {
-  let shortURLString = createShortHexString(hex);
-
-  // Transaction hash
-  if (hex.length == 66) {
-    return `<${createEtherscanLinkFromtx(web3, networkUtils)}tx/${hex}|${shortURLString}>`;
-  }
-  // Account
-  else if (hex.length == 42) {
-    return `<${createEtherscanLinkFromtx(web3, networkUtils)}address/${hex}|${shortURLString}>`;
-  }
-}
-
-module.exports = {
-  formatDate,
-  formatWei,
-  formatWithMaxDecimals,
-  createFormatFunction,
-  createEtherscanLinkFromtx,
-  createShortHexString,
-  createEtherscanLinkMarkdown
-};
