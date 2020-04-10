5d2a68c91d76e163284ecc952092766d70a6f941
Christopher Maree <christopher.maree@gmail.com>
2020-04-10 09:56:22 +0200

    final test
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,13 +17,6 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double", { avoidEscape: true }],
     semi: ["error", "always"],
-    "spaced-comment": ["error", "always", { exceptions: ["-", "+"] }],
-    "no-undef": [0],
-    "no-unused-vars": [0],
-    "no-case-declarations": [0],
-    "no-constant-condition": [0],
-    "no-redeclare": [0],
-    "no-console": [0],
-    "no-irregular-whitespace": [0]
+    "spaced-comment": ["error", "always", { exceptions: ["-", "+"] }]
   }
 };
