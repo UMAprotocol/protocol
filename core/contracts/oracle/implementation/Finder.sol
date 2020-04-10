96ec0c02375b7fe371b7c43d5f6f811e2ab96ed1
Christopher Maree <christopher.maree@gmail.com>
2020-04-10 12:27:59 +0200

    Update
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/contracts/oracle/implementation/Finder.sol
+++ core/contracts/oracle/implementation/Finder.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 import "../interfaces/FinderInterface.sol";
 
@@ -10,6 +11,8 @@ import "../interfaces/FinderInterface.sol";
  * @dev Examples of interfaces with implementations that Finder locates are the Oracle and Store interfaces.
  */
 contract Finder is FinderInterface, Ownable {
+    using Address for address;
+
     mapping(bytes32 => address) public interfacesImplemented;
 
     event InterfaceImplementationChanged(bytes32 indexed interfaceName, address indexed newImplementationAddress);
@@ -22,6 +25,7 @@ contract Finder is FinderInterface, Ownable {
     // TODO(#969) Remove once prettier-plugin-solidity can handle the "override" keyword
     // prettier-ignore
     function changeImplementationAddress(bytes32 interfaceName, address implementationAddress) external override onlyOwner {
+        require(implementationAddress.isContract(),"Can only change implementation to a contract");
         interfacesImplemented[interfaceName] = implementationAddress;
 
         emit InterfaceImplementationChanged(interfaceName, implementationAddress);
