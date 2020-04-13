ad7fe88168267e0b610b789233d78d10b2f21bdd
Matt Rice <matthewcrice32@gmail.com>
2020-04-13 15:50:38 -0400

    remove other line
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- ci/deploy_to_staging.sh
+++ ci/deploy_to_staging.sh
@@ -21,9 +21,6 @@ gcloud auth activate-service-account --key-file=$GCLOUD_FNAME
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
 
-# Copy the staging config into the voter-dapp dir
-gsutil cp gs://staging-deployment-configuration/voter-app.yaml voter-dapp/app.yaml
-
 # Deploy docs
 ./scripts/deploy_docs.sh documentation/gae_app.yaml -q
 
