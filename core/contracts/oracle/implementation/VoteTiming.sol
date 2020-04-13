142896751d0c118cf3d8c4a0d66605b9dbf3e83c
Chris Maree <christopher.maree@gmail.com>
2020-04-13 08:40:13 +0200

    Update VoteTiming.sol

--- core/contracts/oracle/implementation/VoteTiming.sol
+++ core/contracts/oracle/implementation/VoteTiming.sol
@@ -18,7 +18,7 @@ library VoteTiming {
      * @notice Initializes the data object. Sets the phase length based on the input.
      */
     function init(Data storage data, uint phaseLength) internal {
-        require(phaseLength > 0, "Phase length can not be zero");
+        require(phaseLength > 0);
         data.phaseLength = phaseLength;
     }
 
