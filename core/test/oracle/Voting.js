b7832f19884d1b4339ae5978e746dc2c19bab605
Christopher Maree <christopher.maree@gmail.com>
2020-04-14 14:14:21 +0200

    lint
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/test/oracle/Voting.js
+++ core/test/oracle/Voting.js
@@ -691,7 +691,7 @@ contract("Voting", function(accounts) {
     // The snapshotId of the current round should be zero before initiating the snapshot
     assert.equal((await voting.rounds(await voting.getCurrentRoundId())).snapshotId.toString(), "0");
 
-  // Directly snapshot the current round to lock in token balances.
+    // Directly snapshot the current round to lock in token balances.
     await voting.snapshotCurrentRound();
 
     // The snapshotId of the current round should be non zero after the snapshot is taken
