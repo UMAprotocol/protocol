1a7f0e29fcac8d975106c767b56b22ff0117b243
Christopher Maree <christopher.maree@gmail.com>
2020-04-14 17:12:09 +0200

    test
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- liquidator/liquidator.js
+++ liquidator/liquidator.js
@@ -1,4 +1,4 @@
-// When running this script it assumed that the account has enough tokens and allowance from the unlocked truffle
+// When running this script it assumed that the account has enough tokens and allowance from the unlocked Truffle
 // wallet to run the liquidations. Future versions will deal with generating additional synthetic tokens from EMPs as the bot needs.
 
 const { Logger } = require("../financial-templates-lib/logger/Logger");
