ff5842ad81db39149ad772ad21836ae044817fde
Christopher Maree <christopher.maree@gmail.com>
2020-04-14 09:09:05 +0200

    further lint
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- financial-templates-lib/logger/SpyTransport.js
+++ financial-templates-lib/logger/SpyTransport.js
@@ -1,20 +1,19 @@
-const util = require('util');
-const winston = require('winston');
+const util = require("util");
+const winston = require("winston");
 
 spyLogger = function(options) {
   options = options || {};
-  this.level = options.level || 'info';
+  this.level = options.level || "info";
   this.spy = options.spy;
 };
 
 util.inherits(spyLogger, winston.Transport);
 
-spyLogger.prototype.name = 'spyLogger';
+spyLogger.prototype.name = "spyLogger";
 
 spyLogger.prototype.log = function(level, msg, meta, callback) {
   this.spy(level, msg, meta);
   callback(null, true);
 };
-
 
 module.exports = winston.transports.SpyLogger = spyLogger;
