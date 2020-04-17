edc9e7748c36ae0b0826666c8fefb80d13cf707e
Chris Maree <christopher.maree@gmail.com>
2020-04-17 11:06:05 +0200

    Contract event monitor (#1168)
    * Squashed commits
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * removed redundant files
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * updated event client address
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * lint
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Revert ExpiringMultiPartyClient.js
    
    * updated symbol for bond
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * added extra fix merge
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Revert ExpiringMultiPartyEventClient.js
    
    * Updated bot implementation
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Updated monitor
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * cleaned up frormatter imports
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * refactored
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * added catcher function for block length
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- liquidator/test/Liquidator.js
+++ liquidator/test/Liquidator.js
@@ -308,4 +308,17 @@ contract("Liquidator.js", function(accounts) {
     // The liquidation should have gone through.
     assert.equal((await emp.getLiquidations(sponsor1)).length, 1);
   });
+
+  it.only("set up positions", async function() {
+    console.log("seeding emp @", emp.address);
+    for (let i = 1; i < 6; i++) {
+      console.log("Creating position for account", accounts[i]);
+      await collateralToken.mint(accounts[i], toWei("100000"), { from: contractCreator });
+      await collateralToken.approve(emp.address, toWei("1000000"), { from: accounts[i] });
+      await emp.create({ rawValue: toWei("150") }, { rawValue: toWei("100") }, { from: accounts[i] });
+    }
+
+    // liquidatorBot creates a position to have synthetic tokens to pay off debt upon liquidation.
+    await emp.create({ rawValue: toWei("100000") }, { rawValue: toWei("50000") }, { from: liquidatorBot });
+  });
 });
