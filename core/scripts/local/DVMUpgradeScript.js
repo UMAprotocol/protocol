f3fa77f81926f676c1dbfb7b85c4275bdcb26fbe
Christopher Maree <christopher.maree@gmail.com>
2020-04-20 15:42:27 +0200

    updated script
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/scripts/local/DVMUpgradeScript.js
+++ core/scripts/local/DVMUpgradeScript.js
@@ -1,6 +1,7 @@
 const Finder = artifacts.require("Finder");
 const Registry = artifacts.require("Registry");
 const Voting = artifacts.require("Voting");
+const Store = artifacts.require("Store");
 const IdentifierWhitelist = artifacts.require("IdentifierWhitelist");
 const VotingToken = artifacts.require("VotingToken");
 const VotingTest = artifacts.require("VotingTest");
@@ -16,6 +17,13 @@ async function runExport() {
   console.log("Connected to network id", await web3.eth.net.getId());
   const accounts = await web3.eth.getAccounts();
 
+  // Get the previously deployed VotingToken and Finder.
+  console.log("VotingToken.address", VotingToken.address);
+  console.log("Finder.address", Finder.address);
+
+  const votingToken = new web3.eth.Contract(VotingToken.abi, VotingToken.address);
+  const finder = new web3.eth.Contract(Finder.abi, Finder.address);
+
   /** ***********************
    * 1) upgrade Voting.sol *
    *************************/
@@ -28,13 +36,6 @@ async function runExport() {
 
   // Set the rewards expiration timeout.
   const rewardsExpirationTimeout = 60 * 60 * 24 * 14; // Two weeks.
-
-  // Get the previously deployed VotingToken and Finder.
-  console.log("VotingToken.address", VotingToken.address);
-  console.log("Finder.address", Finder.address);
-
-  const votingToken = new web3.eth.Contract(VotingToken.abi, VotingToken.address);
-  const finder = new web3.eth.Contract(Finder.abi, Finder.address);
 
   // Set phase length to one day.
   const secondsPerDay = "86400";
@@ -52,17 +53,35 @@ async function runExport() {
 
   console.log("voting upgraded @", voting.address);
 
-  const upgradeTx = finder.methods
+  const upgradeVotingTx = finder.methods
     .changeImplementationAddress(web3.utils.utf8ToHex(interfaceName.Oracle), voting.address)
     .encodeABI();
 
-  console.log("upgradeTx", upgradeTx);
+  console.log("upgradeVotingTx", upgradeVotingTx);
 
   /** *************************
-   * 1) upgrade Registry.sol *
+   * 2) upgrade Registry.sol *
    ***************************/
 
-  //   const registry = await Registry.new({ from: accounts[0] });
+  const registry = await Registry.new({ from: accounts[0] });
+
+  const upgradeRegistryTx = finder.methods
+    .changeImplementationAddress(web3.utils.utf8ToHex(interfaceName.Registry), registry.address)
+    .encodeABI();
+
+  console.log("upgradeRegistryTx", upgradeRegistryTx);
+
+  /** **********************
+   * 3) upgrade Store.sol *
+   *************************/
+
+  const store = await Store.new("0x0000000000000000000000000000000000000000", { from: accounts[0] });
+
+  const upgradeStoreTx = finder.methods
+    .changeImplementationAddress(web3.utils.utf8ToHex(interfaceName.Store), store.address)
+    .encodeABI();
+
+  console.log("upgradeStoreTx", upgradeStoreTx);
 }
 
 run = async function(callback) {
