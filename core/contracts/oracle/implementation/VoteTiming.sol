0f74f58b52a83686a44ef801fa61c9ca34722b05
Nick Pai <npai.nyc@gmail.com>
2020-04-22 12:58:07 -0400

    clarify initial roundID not starting at 0/1
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/contracts/oracle/implementation/VoteTiming.sol
+++ core/contracts/oracle/implementation/VoteTiming.sol
@@ -25,6 +25,8 @@ library VoteTiming {
 
     /**
      * @notice Computes the roundID based off the current time as floor(timestamp/phaseLength).
+     * @dev The round ID depends on the global timestamp but not on the lifetime of the system.
+     * The consequence is that the initial round ID starts at an arbitrary number (that increments, as expected, for subsequent rounds) instead of zero or one.
      * @param data input data object.
      * @param currentTime input unix timestamp used to compute the current roundId.
      * @return roundId defined as a function of the currentTime and `phaseLength` from `data`.
