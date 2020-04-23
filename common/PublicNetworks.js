788f2820fe1ccb242cc11cd3488809ee2467ce22
Christopher Maree <christopher.maree@gmail.com>
2020-04-23 13:59:30 +0200

    Added other addresses
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- common/PublicNetworks.js
+++ common/PublicNetworks.js
@@ -3,28 +3,28 @@ module.exports = {
     name: "mainnet",
     ethFaucet: null,
     etherscan: "https://etherscan.io/",
-    daiAddress: "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359",
-    wethAddress: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    daiAddress: "0xC94a6e7776BaDE5dA316CF6FD8c751Fb0D5c3C5E",
+    wethAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
   },
   3: {
     name: "ropsten",
     ethFaucet: "https://faucet.metamask.io/",
     etherscan: "https://ropsten.etherscan.io/",
     daiAddress: "0xB5E5D0F8C0cbA267CD3D7035d6AdC8eBA7Df7Cdd",
-    wethAddress: "0xc778417e063141139fce010982780140aa0cd5ab"
+    wethAddress: "0xc778417E063141139Fce010982780140Aa0cD5Ab"
   },
   4: {
     name: "rinkeby",
     ethFaucet: "https://faucet.rinkeby.io/",
     etherscan: "https://rinkeby.etherscan.io/",
     daiAddress: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
-    wethAddress: "0xc778417e063141139fce010982780140aa0cd5ab"
+    wethAddress: "0xc778417E063141139Fce010982780140Aa0cD5Ab"
   },
   42: {
     name: "kovan",
     ethFaucet: "https://faucet.kovan.network/",
     etherscan: "https://kovan.etherscan.io/",
     daiAddress: "0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99",
-    wethAddress: "0xd0a1e359811322d97991e03f863a0c30c2cf029c"
+    wethAddress: "0xd0A1E359811322d97991E03f863a0C30C2cF029C"
   }
 };
