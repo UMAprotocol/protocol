878ea7f2621ae27c9203a1836ce43009bb77af1d
Matt Rice <matthewcrice32@gmail.com>
2020-04-26 12:42:30 -0400

    Remove TD
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/publishPrices.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Usage: ./scripts/publishPrices.sh <args to pass to PublishPrices>
-# Example: ./scripts/publishPrices.sh --network mainnet --keys priceFeed
-# Note: this script must be run from the core/ directory (not core/scripts/).
-
-while true; do $(npm bin)/truffle exec ./scripts/PublishPrices.js "$@" >>out.log 2>&1; sleep 3600; done
