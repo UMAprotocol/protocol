e811e197d0e953d53ea8025042cc8ab48e65fd4c
Nick Pai <npai.nyc@gmail.com>
2020-04-27 10:00:23 -0400

    change node version to LTS
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- ci/install_node_npm.sh
+++ ci/install_node_npm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -sSL "https://nodejs.org/dist/v13.8.0/node-v13.8.0-linux-x64.tar.xz" | sudo tar --strip-components=2 -xJ -C /usr/local/bin/ node-v13.8.0-linux-x64/bin/node
+curl -sSL "https://nodejs.org/dist/v12.16.2/node-v12.16.2-linux-x64.tar.xz" | sudo tar --strip-components=2 -xJ -C /usr/local/bin/ node-v12.16.2-linux-x64/bin/node
 curl https://www.npmjs.com/install.sh | sudo bash
