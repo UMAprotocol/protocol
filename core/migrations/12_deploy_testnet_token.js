eea3f07079a7eafd8fd0722f74ea61ab7d3f7545
Matt Rice <matthewcrice32@gmail.com>
2020-04-28 15:37:27 -0400

    Remove TD (#1295)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/migrations/12_deploy_testnet_token.js
+++ core/migrations/12_deploy_testnet_token.js
@@ -1,6 +1,4 @@
 const TestnetERC20 = artifacts.require("TestnetERC20");
-const TokenizedDerivativeCreator = artifacts.require("TokenizedDerivativeCreator");
-const AddressWhitelist = artifacts.require("AddressWhitelist");
 const { deploy, setToExistingAddress, getKeysForNetwork } = require("../../common/MigrationUtils.js");
 const publicNetworks = require("../../common/PublicNetworks.js");
 
@@ -23,10 +21,4 @@ module.exports = async function(deployer, network, accounts) {
       contract: { address: testnetERC20Address }
     } = await deploy(deployer, network, TestnetERC20, "Dai Stable Coin", "DAI", 18, { from: keys.deployer }));
   }
-
-  // Add testnetERC20 to the margin currency whitelist.
-  const tokenizedDerivativeCreator = await TokenizedDerivativeCreator.deployed();
-  const marginCurrencyWhitelistAddress = await tokenizedDerivativeCreator.marginCurrencyWhitelist();
-  const marginCurrencyWhitelist = await AddressWhitelist.at(marginCurrencyWhitelistAddress);
-  await marginCurrencyWhitelist.addToWhitelist(testnetERC20Address, { from: keys.deployer });
 };
