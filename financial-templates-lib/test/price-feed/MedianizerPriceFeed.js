38c0cdeecd64ee93d0da1c70630c77e4f121fe6c
Matt Rice <matthewcrice32@gmail.com>
2020-05-04 16:22:27 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- financial-templates-lib/test/price-feed/MedianizerPriceFeed.js
+++ financial-templates-lib/test/price-feed/MedianizerPriceFeed.js
@@ -6,6 +6,7 @@ const { PriceFeedMock } = require("./PriceFeedMock.js");
 contract("MedianizerPriceFeed.js", function(accounts) {
   it("Basic medians", async function() {
     const priceFeeds = [
+      //                currentPrice      historicalPrice    lastUpdatedTime
       new PriceFeedMock(toBN(toWei("1")), toBN(toWei("25")), 100),
       new PriceFeedMock(toBN(toWei("2")), toBN(toWei("57")), 50000),
       new PriceFeedMock(toBN(toWei("9")), toBN(toWei("10")), 25)
