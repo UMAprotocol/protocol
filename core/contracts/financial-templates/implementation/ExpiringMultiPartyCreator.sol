3eba0cfb6f9d8f12a3462d1e888ae8c7ac1238df
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2020-05-05 08:31:18 -0400

    Gas limit tweaks and EMP bytecode optimization (#1356)
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/contracts/financial-templates/implementation/ExpiringMultiPartyCreator.sol
+++ core/contracts/financial-templates/implementation/ExpiringMultiPartyCreator.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 import "../../oracle/implementation/ContractCreator.sol";
 import "../../common/implementation/Testable.sol";
 import "../../common/implementation/AddressWhitelist.sol";
-import "./ExpiringMultiParty.sol";
+import "./ExpiringMultiPartyLib.sol";
 
 
 /**
@@ -114,7 +114,7 @@ contract ExpiringMultiPartyCreator is ContractCreator, Testable {
      * @return address of the deployed ExpiringMultiParty contract
      */
     function createExpiringMultiParty(Params memory params) public returns (address) {
-        ExpiringMultiParty derivative = new ExpiringMultiParty(_convertParams(params));
+        address derivative = ExpiringMultiPartyLib.deploy(_convertParams(params));
 
         _registerContract(new address[](0), address(derivative));
 
--- core/migrations/14_deploy_expiring_multi_party_creator.js
+++ core/migrations/14_deploy_expiring_multi_party_creator.js
@@ -1,5 +1,6 @@
 const Finder = artifacts.require("Finder");
 const ExpiringMultiPartyCreator = artifacts.require("ExpiringMultiPartyCreator");
+const ExpiringMultiPartyLib = artifacts.require("ExpiringMultiPartyLib");
 const AddressWhitelist = artifacts.require("AddressWhitelist");
 const TokenFactory = artifacts.require("TokenFactory");
 const { getKeysForNetwork, deploy, enableControllableTiming } = require("../../common/MigrationUtils.js");
@@ -16,6 +17,10 @@ module.exports = async function(deployer, network, accounts) {
 
   const finder = await Finder.deployed();
   const tokenFactory = await TokenFactory.deployed();
+
+  // Deploy EMPLib and link to EMPCreator.
+  await deploy(deployer, network, ExpiringMultiPartyLib);
+  await deployer.link(ExpiringMultiPartyLib, ExpiringMultiPartyCreator);
 
   await deploy(
     deployer,
