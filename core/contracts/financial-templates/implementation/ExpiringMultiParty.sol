24aa9cbd61fb1ff61b627c3abafa0dec82473fad
Matt Rice <matthewcrice32@gmail.com>
2020-05-08 16:33:55 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/contracts/financial-templates/implementation/ExpiringMultiParty.sol
+++ core/contracts/financial-templates/implementation/ExpiringMultiParty.sol
@@ -7,6 +7,9 @@ import "./Liquidatable.sol";
 contract ExpiringMultiParty is Liquidatable {
     constructor(ConstructorParams memory params)
         public
-        Liquidatable(params) // Note: since there is no logic here, there is no need to add a re-entrancy guard.
-    {}
+        Liquidatable(params)
+    // Note: since there is no logic here, there is no need to add a re-entrancy guard.
+    {
+
+    }
 }
