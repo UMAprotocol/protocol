7d7f6bf5cdcef2c4ae9999467873eec13256a0dc
Christopher Maree <christopher.maree@gmail.com>
2020-05-12 10:53:01 +0200

    Added scripts
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

---
+++ scripts/bot-deployment/UpdateBotGCP.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "Incorrect number of arguments supplied! First argument is bot's name. Second argument is path to config file."
+    echo "example: ./DeployBotGCP.sh ethbtc-monitor-bot"
+    exit 1
+fi
+
+echo "Updating" $1
+echo "Using ENV file:" $2
+gcloud compute instances update-container $1 \
+    --container-image docker.io/chrismaree/monitorbot:v0.0.4 \
+    --container-env-file $2 \
+    --zone us-central1-a \
+    --container-restart-policy on-failure \
+    --container-stdin
