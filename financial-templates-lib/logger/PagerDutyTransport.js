8bb2342df46da42c1e8d977ba4ee9e2278d15c74
Chris Maree <christopher.maree@gmail.com>
2020-05-13 15:30:54 +0200

    Better pager duty error handling (#1419)

--- financial-templates-lib/logger/PagerDutyTransport.js
+++ financial-templates-lib/logger/PagerDutyTransport.js
@@ -16,22 +16,27 @@ module.exports = class PagerDutyTransport extends Transport {
   }
 
   async log(info, callback) {
-    // TODO: refactor this processing to better parse mrkdwn and complex data structure from winston.
-    await this.pd.incidents.createIncident(this.fromEmail, {
-      incident: {
-        type: "incident",
-        title: `${info.level}: ${info.at} ⭢ ${info.message}`,
-        service: {
-          id: this.serviceId,
-          type: "service_reference"
-        },
-        urgency: info.level == "warn" ? "low" : "high", // If level is warn then urgency is low. If level is error then urgency is high.
-        body: {
-          type: "incident_body",
-          details: info.mrkdwn ? info.mrkdwn : info // If the message has markdown then add it. Else put the whole info object.
+    try {
+      await this.pd.incidents.createIncident(this.fromEmail, {
+        incident: {
+          type: "incident",
+          title: `${info.level}: ${info.at} ⭢ ${info.message}`,
+          service: {
+            id: this.serviceId,
+            type: "service_reference"
+          },
+          urgency: info.level == "warn" ? "low" : "high", // If level is warn then urgency is low. If level is error then urgency is high.
+          body: {
+            type: "incident_body",
+            details: info.mrkdwn ? info.mrkdwn : info // If the message has markdown then add it. Else put the whole info object.
+          }
         }
-      }
-    });
+      });
+      callback();
+    } catch (error) {
+      console.error("PagerDuty error", error);
+    }
+
     callback();
   }
 };
