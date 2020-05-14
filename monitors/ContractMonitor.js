a2a140e492999f451828fc3e1734a55e26a92c60
Matt Rice <matthewcrice32@gmail.com>
2020-05-14 08:09:31 -0400

    Integrate price feed with monitor bot (#1420)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- monitors/ContractMonitor.js
+++ monitors/ContractMonitor.js
@@ -1,12 +1,15 @@
 const { createFormatFunction, createEtherscanLinkMarkdown } = require("../common/FormattingUtils");
 
 class ContractMonitor {
-  constructor(logger, expiringMultiPartyEventClient, monitoredLiquidators, monitoredDisputers) {
+  constructor(logger, expiringMultiPartyEventClient, monitoredLiquidators, monitoredDisputers, priceFeed) {
     this.logger = logger;
 
     // Bot and ecosystem accounts to monitor. Will inform the console logs when events are detected from these accounts.
     this.monitoredLiquidators = monitoredLiquidators;
     this.monitoredDisputers = monitoredDisputers;
+
+    // Offchain price feed to get the price for liquidations.
+    this.priceFeed = priceFeed;
 
     // EMP event client to read latest contract events.
     this.empEventClient = expiringMultiPartyEventClient;
@@ -29,7 +32,7 @@ class ContractMonitor {
 
   // Calculate the collateralization Ratio from the collateral, token amount and token price
   // This is cr = [collateral / (tokensOutstanding * price)] * 100
-  calculatePositionCRPercent = (collateral, tokensOutstanding, tokenPrice) => {
+  calculatePositionCRPercent = (collateral, tokensOutstanding, tokenPrice, price) => {
     return this.web3.utils
       .toBN(collateral)
       .mul(this.web3.utils.toBN(this.web3.utils.toWei("1")))
@@ -46,14 +49,10 @@ class ContractMonitor {
   }
 
   // Queries disputable liquidations and disputes any that were incorrectly liquidated.
-  checkForNewLiquidations = async priceFunction => {
-    const contractTime = this.empEventClient.getLastUpdateTime();
-    const priceFeed = priceFunction(contractTime);
-
+  checkForNewLiquidations = async () => {
     this.logger.debug({
       at: "ContractMonitor",
       message: "Checking for new liquidation events",
-      price: priceFeed,
       lastLiquidationBlockNumber: this.lastLiquidationBlockNumber
     });
 
@@ -66,6 +65,26 @@ class ContractMonitor {
     );
 
     for (let event of newLiquidationEvents) {
+      const { liquidationTime } = await this.empContract.methods
+        .liquidations(event.sponsor, event.liquidationId)
+        .call();
+      const price = this.priceFeed.getHistoricalPrice(parseInt(liquidationTime.toString()));
+
+      let collateralizationString;
+      if (price) {
+        collateralizationString = this.formatDecimalString(
+          this.calculatePositionCRPercent(event.liquidatedCollateral, event.tokensOutstanding, price)
+        );
+      } else {
+        this.logger.warn({
+          at: "ContractMonitor",
+          message: "Could not get historical price for liquidation",
+          price,
+          liquidationTime: liquidationTime.toString()
+        });
+        collateralizationString = "[Invalid]";
+      }
+
       // Sample message:
       // Liquidation alert: [ethereum address if third party, or “UMA” if it’s our bot]
       // initiated liquidation for for [x][collateral currency]of sponsor collateral
@@ -84,9 +103,7 @@ class ContractMonitor {
         " " +
         this.syntheticCurrencySymbol +
         " tokens. Sponsor collateralization was " +
-        this.formatDecimalString(
-          this.calculatePositionCRPercent(event.liquidatedCollateral, event.tokensOutstanding, priceFeed)
-        ) +
+        collateralizationString +
         "%. tx: " +
         createEtherscanLinkMarkdown(this.web3, event.transactionHash);
 
@@ -99,14 +116,10 @@ class ContractMonitor {
     this.lastLiquidationBlockNumber = this.getLastSeenBlockNumber(latestLiquidationEvents);
   };
 
-  checkForNewDisputeEvents = async priceFunction => {
-    const contractTime = this.empEventClient.getLastUpdateTime();
-    const priceFeed = priceFunction(contractTime);
-
+  checkForNewDisputeEvents = async () => {
     this.logger.debug({
       at: "ContractMonitor",
       message: "Checking for new dispute events",
-      price: priceFeed,
       lastDisputeBlockNumber: this.lastDisputeBlockNumber
     });
 
@@ -141,14 +154,10 @@ class ContractMonitor {
     this.lastDisputeBlockNumber = this.getLastSeenBlockNumber(latestDisputeEvents);
   };
 
-  checkForNewDisputeSettlementEvents = async priceFunction => {
-    const contractTime = this.empEventClient.getLastUpdateTime();
-    const priceFeed = priceFunction(contractTime);
-
+  checkForNewDisputeSettlementEvents = async () => {
     this.logger.debug({
       at: "ContractMonitor",
       message: "Checking for new dispute settlement events",
-      price: priceFeed,
       lastDisputeSettlementBlockNumber: this.lastDisputeSettlementBlockNumber
     });
 
