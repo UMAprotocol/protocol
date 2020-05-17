8dabc267846dd40f34fff3ad6adba9a902748b9d
Chris Maree <christopher.maree@gmail.com>
2020-05-17 21:30:15 +0200

    Revert coverage.sh

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -11,4 +11,4 @@ cd $TRUFFLE_DIR
 # Truffle compile can take a lot of memory, which I've experienced with the solc-0.6 compatible versions,
 # so I explicitly increase the javascript heap size.
 # More details here: https://github.com/trufflesuite/truffle/issues/957
-node --max-old-space-size=4096 $(npm bin)/truffle run coverage --temp build --network coverage && cat coverage/lcov.info | $(npm bin)/coveralls --verbose
+node --max-old-space-size=4096 $(npm bin)/truffle run coverage --temp build --network coverage
