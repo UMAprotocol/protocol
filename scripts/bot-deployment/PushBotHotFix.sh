b76b09ec15c4082648034bd44deb0c237b253f72
Chris Maree <christopher.maree@gmail.com>
2020-05-18 11:04:48 +0200

    Improve gcp scripts to make config file optional (#1452)

--- scripts/bot-deployment/PushBotHotFix.sh
+++ scripts/bot-deployment/PushBotHotFix.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ $# -ne 1 ]; then
-    echo "Incorrect number of arguments supplied! First and only argument is bot's name."
-    echo "example: ./PushBotHotFix.sh ethbtc-monitor-bot"
+if [ $# -eq 0 ] || [ $# -gt 2 ]; then
+    echo "Incorrect number of arguments supplied! First argument is the bot's name. Second argument (optional) is a config file with environment variables to supply to the bot."
+    echo "example: ./scripts/bot-deployment/UpdateBotGCP.sh ethbtc-monitor-bot [keep env variables]"
+    echo "example: ./scripts/bot-deployment/UpdateBotGCP.sh ethbtc-monitor-bot ./scripts/bot-deployment/ETHBTC-monitor-bot-env.txt [update env variables]"
     exit 1
 fi
 
-echo "Pushing hot fix to" $1
 echo "Building docker container..."
 docker build -t umaprotocol/protocol:hotfix .
 
@@ -15,8 +15,23 @@ echo "Pushing docker container to docker hub..."
 docker push umaprotocol/protocol:hotfix
 
 echo "Pushing docker container to docker hub..."
-gcloud compute instances update-container $1 \
-    --container-image docker.io/umaprotocol/protocol:hotfix \
-    --zone us-central1-a \
-    --container-restart-policy on-failure \
-    --container-stdin
+if [ $# -eq 1 ]; then
+    echo "Pushing hot fix to" $1
+    echo "No configuration file provided. Keeping current configuration"
+    gcloud compute instances update-container $1 \
+        --container-image docker.io/umaprotocol/protocol:hotfix \
+        --zone us-central1-a \
+        --container-restart-policy on-failure \
+        --container-stdin
+fi
+
+if [ $# -eq 2 ]; then
+    echo "Pushing hot fix to" $1
+    echo "Using enviroment variable config file" $2
+    gcloud compute instances update-container $1 \
+        --container-image docker.io/umaprotocol/protocol:hotfix \
+        --container-env-file $2 \
+        --zone us-central1-a \
+        --container-restart-policy on-failure \
+        --container-stdin
+fi
