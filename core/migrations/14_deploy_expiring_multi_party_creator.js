3853e62d0487241937f9cdc63f5cb1faeb89f949
Matt Rice <matthewcrice32@gmail.com>
2020-05-19 14:39:27 -0400

    Streamline EMP Creation process (#1480)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/migrations/14_deploy_expiring_multi_party_creator.js
+++ core/migrations/14_deploy_expiring_multi_party_creator.js
@@ -5,6 +5,8 @@ const AddressWhitelist = artifacts.require("AddressWhitelist");
 const TokenFactory = artifacts.require("TokenFactory");
 const { getKeysForNetwork, deploy, enableControllableTiming } = require("../../common/MigrationUtils.js");
 const Timer = artifacts.require("Timer");
+const Registry = artifacts.require("Registry");
+const { RegistryRolesEnum } = require("../../common/Enums.js");
 
 module.exports = async function(deployer, network, accounts) {
   const keys = getKeysForNetwork(network, accounts);
@@ -17,12 +19,13 @@ module.exports = async function(deployer, network, accounts) {
 
   const finder = await Finder.deployed();
   const tokenFactory = await TokenFactory.deployed();
+  const registry = await Registry.deployed();
 
   // Deploy EMPLib and link to EMPCreator.
   await deploy(deployer, network, ExpiringMultiPartyLib);
   await deployer.link(ExpiringMultiPartyLib, ExpiringMultiPartyCreator);
 
-  await deploy(
+  const { contract: expiringMultiPartyCreator } = await deploy(
     deployer,
     network,
     ExpiringMultiPartyCreator,
@@ -32,4 +35,6 @@ module.exports = async function(deployer, network, accounts) {
     controllableTiming ? Timer.address : "0x0000000000000000000000000000000000000000",
     { from: keys.deployer }
   );
+
+  await registry.addMember(RegistryRolesEnum.CONTRACT_CREATOR, expiringMultiPartyCreator.address);
 };
