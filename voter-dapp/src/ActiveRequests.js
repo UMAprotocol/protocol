dc93b6c21a9b94b6bcc7fcb6fed18ef8d3f49cc4
Nick Pai <npai.nyc@gmail.com>
2020-05-19 12:05:19 -0400

    remove leftover console.log in active requests
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- voter-dapp/src/ActiveRequests.js
+++ voter-dapp/src/ActiveRequests.js
@@ -80,8 +80,6 @@ function ActiveRequests({ votingAccount, votingGateway }) {
     ])
   );
 
-  console.log(encryptedVoteEvents);
-
   const closedDialogIndex = -1;
   const [dialogContentIndex, setDialogContentIndex] = useState(closedDialogIndex);
   const handleClickExplain = index => {
