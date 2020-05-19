934f8045a1de971e50df5a8ca49103ac47be8038
Matt Rice <matthewcrice32@gmail.com>
2020-05-19 13:22:40 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/migrations/14_deploy_expiring_multi_party_creator.js
+++ core/migrations/14_deploy_expiring_multi_party_creator.js
@@ -5,6 +5,8 @@ const AddressWhitelist = artifacts.require("AddressWhitelist");
 const TokenFactory = artifacts.require("TokenFactory");
 const { getKeysForNetwork, deploy, enableControllableTiming } = require("../../common/MigrationUtils.js");
 const Timer = artifacts.require("Timer");
+const Registry = artifacts.require("Registry");
+const { RegistryRolesEnum } = require("../../common/Enums.js");
 
 module.exports = async function(deployer, network, accounts) {
   const keys = getKeysForNetwork(network, accounts);
@@ -22,7 +24,7 @@ module.exports = async function(deployer, network, accounts) {
   await deploy(deployer, network, ExpiringMultiPartyLib);
   await deployer.link(ExpiringMultiPartyLib, ExpiringMultiPartyCreator);
 
-  await deploy(
+  const { contract: expiringMultiPartyCreator } = await deploy(
     deployer,
     network,
     ExpiringMultiPartyCreator,
@@ -32,4 +34,6 @@ module.exports = async function(deployer, network, accounts) {
     controllableTiming ? Timer.address : "0x0000000000000000000000000000000000000000",
     { from: keys.deployer }
   );
+
+  await registry.addMember(RegistryRolesEnum.CONTRACT_CREATOR, expiringMultiPartyCreator.address);
 };
