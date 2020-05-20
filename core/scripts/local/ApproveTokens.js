49185d145ffe2b7828de64a7db362cda08db9d76
Matt Rice <matthewcrice32@gmail.com>
2020-05-20 08:45:39 -0400

    Add/Modify convenience scripts to approve and mint tokens (#1482)
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- core/scripts/local/ApproveTokens.js
+++ core/scripts/local/ApproveTokens.js
@@ -3,20 +3,22 @@ const argv = require("minimist")(process.argv.slice(), { string: ["to", "token",
 // Note: this interface also contains a `burn` method, but it isn't used in this script, so it's safe to pass in
 // addresses that do not have the `burn` method.
 const TestnetERC20 = artifacts.require("TestnetERC20");
+const { MAX_UINT_VAL } = require("../../../common/Constants.js");
 
-const mintNewTokens = async function(callback) {
+const approveTokens = async function(callback) {
   try {
     const deployer = (await web3.eth.getAccounts())[0];
 
     // Initialize the token contract from the address.
     const marginToken = await TestnetERC20.at(argv.token);
 
-    // Mint new tokens.
-    await marginToken.allocateTo(argv.to, web3.utils.toWei(argv.amount, "ether"), { from: deployer });
+    const amount = argv.amount ? web3.utils.toWei(argv.amount) : MAX_UINT_VAL;
 
-    console.log(`Added ${argv.amount} token(s) at ${marginToken.address} to account ${argv.to}`);
+    // Mint new tokens.
+    await marginToken.approve(argv.to, amount, { from: deployer });
+
+    console.log(`Approved ${amount} token(s) at ${marginToken.address} to be spent by account ${argv.to}`);
   } catch (e) {
-    console.log(`ERROR: ${e}`);
     callback(e);
     return;
   }
@@ -24,4 +26,4 @@ const mintNewTokens = async function(callback) {
   callback();
 };
 
-module.exports = mintNewTokens;
+module.exports = approveTokens;
