036ee6badd5f0d260fc9669da3d03d37675f2ff1
Christopher Maree <christopher.maree@gmail.com>
2020-06-03 18:50:40 +0200

    Initial implementation
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- scripts/bot-deployment/DeployBotGCP.sh
+++ scripts/bot-deployment/DeployBotGCP.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 set -e
 
-if [ $# -ne 3 ]; then
-    echo "Incorrect number of arguments supplied! First argument is bot's name. Second argument is path to config file. Third is the service account the bot should run as."
-    echo "example: ./DeployBotGCP.sh ethbtc-monitor-bot ./ethbtc-monitor-bot-env.txt ethbtc-account@project-name.iam.gserviceaccount.com"
+if [ $# -ne 1 ]; then
+    echo "Incorrect number of arguments supplied! First and only argument is bot's name.From this the bot's config and service account will be inferred."
+    echo "example: ./DeployBotGCP.sh ethbtc-monitor-bot"
     exit 1
 fi
 
 echo "Deploying" $1
-echo "Using ENV file:" $2
-echo "Running as Service Account:" $3
-gcloud compute instances create-with-container $1 \
-    --container-image docker.io/umaprotocol/protocol:latest \
-    --container-env-file $2 \
-    --zone northamerica-northeast1-b \
-    --container-restart-policy on-failure \
-    --container-stdin \
-    --service-account $3 \
-    --scopes cloud-platform
+
+paramToServiceAccount=$(echo $1 | cut -d'-' -f 1,3)
+serviceAccountEmail=""
+
+# Loop through all service accounts in GCP and validate that the provided bot name matches to a service account email.
+for serviceAccount in $(gcloud iam service-accounts list --format="value(EMAIL)"); do
+    if [ $paramToServiceAccount == $(echo $serviceAccount | cut -d'@' -f 1) ]; then
+        serviceAccountEmail=$serviceAccount
+    fi
+done
+
+if [ $serviceAccountEmail == "" ]; then
+    echo "Bot name provided does not match to a service account."
+    exit 1
+fi
+
+gsutil cp gs://bot-configs/$1.env ./.tempConfig
+
+echo $serviceAccountEmail
