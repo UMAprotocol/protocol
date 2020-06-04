955e9e96db7be601dc1e2f7e19fa52a53bc0ad5a
Christopher Maree <christopher.maree@gmail.com>
2020-06-04 15:40:08 +0200

    small comment
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- reporters/SendSlackReport.sh
+++ reporters/SendSlackReport.sh
@@ -2,20 +2,19 @@
 set -e
 
 # This Script executes the UMA reporter and pipes the output into a text file which is sent as an attachment to slack.
-
 if [ -z "${SLACK_TOKEN}" ] || [ -z "${SLACK_CHANNEL}" ]; then
     echo "SLACK_TOKEN or SLACK_CHANNEL env variables are not set!"
     exit 1
 fi
 
-# Run the daily reporter script and built the output string
+# Run the daily reporter script and built the output string.
 echo "Generating daily report..."
-OUTPUT=$(truffle exec ../reporters/index.js --network mainnet_mnemonic)
+OUTPUT=$(npx truffle exec ../reporters/index.js --network mainnet_mnemonic)
 
 # The output from the node process contains single quotes. These need to be stripped out
 OUTPUT="${OUTPUT//\'/$' '}"
 
-# Configure the name of the file. Sample output: 2020-06-04-daily-report.txt
+# Configure the name of the file. Sample output: 2020-06-04-daily-report.txt.
 date=$(date +%F)
 fileName=$date"-daily-report.txt"
 echo "Saving daily report to file" $fileName
@@ -27,7 +26,7 @@ message_title=$date" Daily Report"
 filename=$fileName
 path_to_file="./"$fileName
 
-# Send a curl command to upload the file along with a message to the channel
+# Send a curl command to upload the file along with a message to the channel.
 echo "Sending file as slack message"
 curl https://slack.com/api/files.upload -F token="${SLACK_TOKEN}" -F channels="${SLACK_CHANNEL}" -F title="${message_title}" -F filename="${filename}" -F file=@"${path_to_file}"
 
