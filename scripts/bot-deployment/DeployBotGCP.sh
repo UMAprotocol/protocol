7e3bcdd531eb67c5c1644f6b59180aa6142c9ebd
Christopher Maree <christopher.maree@gmail.com>
2020-06-04 15:19:57 +0200

    Updated script
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- scripts/bot-deployment/DeployBotGCP.sh
+++ scripts/bot-deployment/DeployBotGCP.sh
@@ -2,13 +2,15 @@
 set -e
 
 if [ $# -ne 1 ]; then
-    echo "Incorrect number of arguments supplied! First and only argument is bot's name.From this the bot's config and service account will be inferred."
+    echo "Incorrect number of arguments supplied! First and only argument is bot's name. From this the bot's config and service account will be inferred."
     echo "example: ./DeployBotGCP.sh ethbtc-monitor-bot"
     exit 1
 fi
 
-echo "Deploying" $1
+echo "🔥Starting deployment script for bot" $1
 
+# Bot names are <identifer>-<network>-<bot-type>. EG: ethbtc-liquidator-mainnet.
+# This cut removes the network as this is not included in a service account. ethbtc-liquidator-mainnet becomes ethbtc-liquidator
 paramToServiceAccount=$(echo $1 | cut -d'-' -f 1,3)
 serviceAccountEmail=""
 
@@ -24,6 +26,23 @@ if [ $serviceAccountEmail == "" ]; then
     exit 1
 fi
 
-gsutil cp gs://bot-configs/$1.env ./.tempConfig
+echo "Bot service account found @ " $serviceAccountEmail
+echo "Pulling bot config from GCP bucket"
 
-echo $serviceAccountEmail
+gsutil cp gs://bot-configs/$1.env ~/.tempUMAConfig
+
+echo "Config has been pulled and placed in your home directory."
+
+echo "🚀Deploying bot to GCP"
+gcloud compute instances create-with-container $1 \
+    --container-image docker.io/umaprotocol/protocol:latest \
+    --container-env-file ~/.tempUMAConfig \
+    --zone northamerica-northeast1-b \
+    --container-restart-policy on-failure \
+    --container-stdin \
+    --service-account $serviceAccountEmail \
+    --scopes cloud-platform
+
+echo "🎉Bot deployed! Removing local config file"
+
+rm ~/.tempUMAConfig
