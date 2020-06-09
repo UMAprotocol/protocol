1934f8263de246d003b350e176bfa86127e27716
Chris Maree <christopher.maree@gmail.com>
2020-06-09 07:42:33 +0200

    Daily report GCP cloud scheduler script (#1581)

---
+++ reporters/cloud-run-scripts/CloudResponse.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const { exec } = require("child_process");
+
+app.get("/", (req, res) => {
+  console.log("Daily reporter received a request");
+
+  let reporterScript = exec("/bin/bash ../reporters/cloud-run-scripts/SendSlackReport.sh", (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+
+    if (error !== null) {
+      console.error(`exec error: ${error}`);
+      res.status(400).send({
+        message: error
+      });
+    } else {
+      console.log("Daily report generated");
+      res.send("Daily Report sent!");
+    }
+  });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
