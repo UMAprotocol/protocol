47ea4fadb423940032bd35048a9296a652feaba6
Christopher Maree <christopher.maree@gmail.com>
2020-06-09 15:49:27 +0200

    WIP
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

---
+++ scripts/bot-deployment/DeployBotMonitorConfig.sh
@@ -0,0 +1,28 @@
+''#!/bin/bash
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Incorrect number of arguments supplied! First and only argument is bot's name. From this the monitor config will be inferred."
+    echo "example: ./DeployBotMonitorConfig.sh ethbtc-mainnet-monitor"
+    exit 1
+fi
+
+echo "🔥 Starting deployment script for bot" $1
+
+config = '
+---
+combiner: OR
+conditions:
+- conditionAbsent:
+    aggregations:
+    - alignmentPeriod: 60s
+      perSeriesAligner: ALIGN_RATE
+    duration: 600s
+    filter: metric.type="logging.googleapis.com/user/ethbtc-mainnet-liquidator"
+    trigger:
+      percent: 100.0
+  displayName: ethbtc-mainnet-liquidator-winston-logger
+displayName: ethbtc-mainnet-liquidator
+enabled: true
+notificationChannels:
+- projects/uma-protocol/notificationChannels/380379380929930537'
