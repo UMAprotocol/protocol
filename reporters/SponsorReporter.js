de4cab429eaa131557261dc5b88507c346d18cee
Chris Maree <christopher.maree@gmail.com>
2020-06-09 07:41:30 +0200

    Improve daily reporter by joining sponsor tables by columns (#1582)

--- reporters/SponsorReporter.js
+++ reporters/SponsorReporter.js
@@ -1,4 +1,4 @@
-const { createFormatFunction, createEtherscanLinkMarkdown } = require("../common/FormattingUtils");
+const { createFormatFunction, createShortHexString } = require("../common/FormattingUtils");
 const chalkPipe = require("chalk-pipe");
 const bold = chalkPipe("bold");
 const italic = chalkPipe("italic");
@@ -33,22 +33,42 @@ class SponsorReporter {
       italic("- Each monitored wallet within the configuration object has their position and token balances printed")
     );
 
+    // Place holder object to store all table information.
+    let tableInfo = {
+      "Token debt": {},
+      "Backing collateral": {},
+      "Position CR %": {},
+      "Synthetic balance": {},
+      "Collateral balance": {},
+      "ETH balance": {}
+    };
+
     // For each wallet monitored run through the checks and log information.
     for (let wallet of this.walletsToMonitor) {
-      console.group();
-      console.log(bold(wallet.name));
-      // 1. Print information about wallets borrowed tokens, collateral and what’s the CR ratio.
-      console.group();
-      this._generatePositionTable(wallet.address);
-      console.groupEnd();
+      const position = this.empClient.getAllPositions().filter(position => position.sponsor == wallet.address);
+      const currentPrice = this.priceFeed.getCurrentPrice();
+      const balanceInformation = await this.tokenBalanceClient.getDirectTokenBalances(wallet.address);
 
-      // 2. Print balance information about wallets including synthetic, collateral and ether balances.
-      console.group();
-      await this._generateTokenBalanceTable(wallet.address);
-      console.groupEnd();
-      // end of main group
-      console.groupEnd();
+      if (position.length == 0) {
+        tableInfo["Token debt"][wallet.name] = "";
+        tableInfo["Backing collateral"][wallet.name] = "";
+        tableInfo["Position CR %"][wallet.name] = "";
+      } else {
+        tableInfo["Token debt"][wallet.name] = this.formatDecimalString(position[0].numTokens) + this.syntheticSymbol;
+        tableInfo["Backing collateral"][wallet.name] =
+          this.formatDecimalString(position[0].numTokens) + this.syntheticSymbol;
+        tableInfo["Position CR %"][wallet.name] =
+          this.formatDecimalString(
+            this._calculatePositionCRPercent(position[0].amountCollateral, position[0].numTokens, currentPrice)
+          ) + "%";
+      }
+      tableInfo["Synthetic balance"][wallet.name] =
+        this.formatDecimalString(balanceInformation.syntheticBalance) + this.syntheticSymbol;
+      tableInfo["Collateral balance"][wallet.name] =
+        this.formatDecimalString(balanceInformation.collateralBalance) + this.collateralSymbol;
+      tableInfo["ETH balance"][wallet.name] = this.formatDecimalString(balanceInformation.etherBalance) + "ETH";
     }
+    console.table(tableInfo);
   }
 
   async generateSponsorsTable() {
@@ -65,41 +85,11 @@ class SponsorReporter {
         "Collateral(DAI)": this.formatDecimalString(position.amountCollateral),
         "Tokens borrowed(ETHBTC)": this.formatDecimalString(position.numTokens),
         "Position CR %": this.formatDecimalString(
-          this._calculatePositionCRPercent(position.amountCollateral, position.numTokens, currentPrice).muln(100)
+          this._calculatePositionCRPercent(position.amountCollateral, position.numTokens, currentPrice)
         )
       };
     }
     console.table(allSponsorTable);
-  }
-
-  _generatePositionTable(address) {
-    const position = this.empClient.getAllPositions().filter(position => position.sponsor == address);
-    const currentPrice = this.priceFeed.getCurrentPrice();
-    console.log(italic("Position information:"));
-    if (position.length == 0) {
-      console.log(dim("\tWallet does not have an open position."));
-    } else {
-      console.table({
-        "Token debt": this.formatDecimalString(position[0].numTokens) + this.syntheticSymbol,
-        "Backing collateral": this.formatDecimalString(position[0].amountCollateral) + this.collateralSymbol,
-        "Position CR %":
-          this.formatDecimalString(
-            this._calculatePositionCRPercent(position[0].amountCollateral, position[0].numTokens, currentPrice).muln(
-              100
-            )
-          ) + "%"
-      });
-    }
-  }
-
-  async _generateTokenBalanceTable(address) {
-    console.log("Token balance information:");
-    const balanceInformation = await this.tokenBalanceClient.getDirectTokenBalances(address);
-    console.table({
-      "Synthetic balance": this.formatDecimalString(balanceInformation.syntheticBalance) + this.syntheticSymbol,
-      "Collateral balance": this.formatDecimalString(balanceInformation.collateralBalance) + this.collateralSymbol,
-      "ETH balance": this.formatDecimalString(balanceInformation.etherBalance) + "Ether"
-    });
   }
 
   _calculatePositionCRPercent(collateral, tokensOutstanding, tokenPrice) {
@@ -113,7 +103,8 @@ class SponsorReporter {
       .toBN(collateral)
       .mul(this.web3.utils.toBN(this.web3.utils.toWei("1")))
       .mul(this.web3.utils.toBN(this.web3.utils.toWei("1")))
-      .div(this.web3.utils.toBN(tokensOutstanding).mul(this.web3.utils.toBN(tokenPrice)));
+      .div(this.web3.utils.toBN(tokensOutstanding).mul(this.web3.utils.toBN(tokenPrice)))
+      .muln(100);
   }
 }
 module.exports = {
