e04cd037c7059435270344a7526cd1616bc84ac6
Christopher Maree <christopher.maree@gmail.com>
2020-06-09 15:54:27 +0200

    updated string
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- scripts/bot-deployment/DeployReporterCR.sh
+++ scripts/bot-deployment/DeployReporterCR.sh
@@ -15,26 +15,25 @@ echo "🤖 Pulling bot config from GCP bucket"
 # Create a temp file to store the config. This will be cleaned up by the OS.
 tempFile=$(mktemp -t UMA)
 
-# Copy config files from GCP to the temp file
+# Copy config files from GCP to the temp file.
 gsutil cp gs://bot-configs/$1.yml $tempFile
 
 echo "✍️  Config has been pulled and placed in a temp directory" $tempFile
 
 echo "🚀 Deploying cloud run instance to GCP"
 
-# Deploy The bot to GCP using the config file
+# Deploy The bot to GCP using the config file.
 gcloud beta run services replace $tempFile \
     --platform managed \
     --region=us-central1
-
 echo "🎉 Bot deployed!"
 
 echo "🧭 Fetching cloud run instance URL"
-# Fetch the cloud run URL created for the service
+
+# Fetch the cloud run URL created for the service.
 cloudRunURL=$(gcloud run services describe $1 --platform managed --region us-central1 --format 'value(status.url)')
 
 echo "🛣  Cloud run URL has been pulled as" $cloudRunURL
-
 # Fetch the project ID and use this to construct the default service account.
 # Note1: the default service account email is always <project_number>--compute@developer.gserviceaccount.com
 # Note2: this command assumes that the GCP account account only has 1 project associated with it.
@@ -44,6 +43,7 @@ echo "📄 Using default service account for project @" $serviceAccountEmail
 
 echo "⏱  Creating cloud scedular to run the cloud run instance"
 
+# Lastly, creat the scheduler job.
 gcloud scheduler jobs create http $1 \
     --schedule="0 8 * * *" \
     --uri=$cloudRunURL \
