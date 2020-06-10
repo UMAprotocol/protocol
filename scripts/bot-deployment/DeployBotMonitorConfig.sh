417fd4e45220c1f63978e10a983a181196e76491
Christopher Maree <christopher.maree@gmail.com>
2020-06-10 12:16:07 +0200

    implementation
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- scripts/bot-deployment/DeployBotMonitorConfig.sh
+++ scripts/bot-deployment/DeployBotMonitorConfig.sh
@@ -9,10 +9,26 @@ fi
 
 echo "🔥 Creating monitor config for bot" $1
 
+# Create the logging metric. This uses the friendly name based on the `bot-identifier` field.
+# This will capture all log messages that come from a bot and put them into a metric with the same
+# name as the bot. EG all ethbtc-mainnet-monitor will be aggregated to a ethbtc-mainnet-monitor metric
 gcloud logging metrics create $1 --description "$1 winston logging metric." --log-filter "jsonPayload.metadata.\"bot-identifier\"=\"$1\""
 
-config = '
----
+echo "🔎 Logging metric created!"
+
+echo "📣 Fetching notification channel config from GCP"
+
+# Fetch the notification channel to use with the monitoring policy.
+# Note this command is relativly fradgile as it assumes that there is only one notification channel.
+# Our current setup only has one notification channel: Pagerduty. if more were added (email, web, mobile)
+# then this would need to be refactored to pull the apropriate channel name.
+notificationChannel=$(gcloud alpha monitoring channels list --format='value(name)')
+
+echo " Pulled notification channel from GCP @" $notificationChannel
+
+# This YAML config below creates a monitor that trackes the logging metric created before.
+# Tt sends an alert to the `notificationChannel` if the `filter` has not been seen for `duration`.
+config="---
 combiner: OR
 conditions:
 - conditionAbsent:
@@ -20,11 +36,18 @@ conditions:
     - alignmentPeriod: 60s
       perSeriesAligner: ALIGN_RATE
     duration: 600s
-    filter: metric.type="logging.googleapis.com/user/ethbtc-mainnet-liquidator"
+    filter: metric.type=\"logging.googleapis.com/user/$1\" AND resource.type=\"gce_instance\"
     trigger:
       percent: 100.0
-  displayName: ethbtc-mainnet-liquidator-winston-logger
-displayName: ethbtc-mainnet-liquidator
-enabled: true
+  displayName: $1-winston-logger
+displayName: $1
 notificationChannels:
-- projects/uma-protocol/notificationChannels/380379380929930537'
+- $notificationChannel"
+
+echo "👷‍♂️ Built monitor configuration object. Creating monitoring policy:" 
+echo "$config"
+
+# Finnally, create the monitoring policy to alert on the metric created.
+gcloud alpha monitoring policies create --policy="$config"
+
+echo "🎉 Logging metric has been creased and monitoring policy has been deployed"
