e170c3264c86b20a84e1aa357c2bdf486c81e4e0
Christopher Maree <christopher.maree@gmail.com>
2020-06-16 11:53:03 +0200

    fixed delay and uniswap price feed tests
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- liquidator/test/index.js
+++ liquidator/test/index.js
@@ -96,7 +96,7 @@ contract("index.js", function(accounts) {
     assert.equal(syntheticAllowance.toString(), MAX_UINT_VAL);
   });
   // TODO: remove this test when we remove the black box testing URL.
-  it.skip("Responds to incoming monitor requests while bot is alive", async function() {
+  it("Responds to incoming monitor requests while bot is alive", async function() {
     const address = emp.address;
 
     const priceFeedConfig = defaultPricefeedConfig;
@@ -117,7 +117,7 @@ contract("index.js", function(accounts) {
     // Start bot synchronously so that we can attempt to send a request to the monitor server without
     // having to wait for the `run` method to return. Wait a little bit for bot to start before sending the request.
     Poll.run(address, 0, priceFeedConfig, monitorPort);
-    await delay(1000); // Empirically, anything > 100 ms seems to be sufficient delay.
+    await delay(1); // Empirically, anything > 100 ms seems to be sufficient delay.
 
     // Pinging server while bot is alive should succeed.
     const response = await fetch(`${url}${route}`);
