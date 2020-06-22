548bf288f8ee3173a6b814e9bd9dbff8d0b78f41
Nick Pai <npai.nyc@gmail.com>
2020-06-22 12:57:04 -0400

    init
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- core/scripts/local/DepositCollateral.js
+++ core/scripts/local/DepositCollateral.js
@@ -1,7 +1,7 @@
 /**
- * @notice Creates a new token position with --tokens synthetic tokens backed by --collateral of collateral.
+ * @notice Deposits collateral into existing token position with --collateral of collateral.
  *
- * Example: `$(npm bin)/truffle exec ./scripts/local/CreateTokens.js --network test --tokens 1000 --collateral 25 --emp 0x6E2F1B57AF5C6237B7512b4DdC1FFDE2Fb7F90B9 --collateralToken 0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99`
+ * Example: `$(npm bin)/truffle exec ./scripts/local/DepositCollateral.js --network test --collateral 25 --emp 0x6E2F1B57AF5C6237B7512b4DdC1FFDE2Fb7F90B9 --collateralToken 0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99`
  */
 const { toWei, toBN } = web3.utils;
 const { MAX_UINT_VAL } = require("../../../common/Constants");
@@ -10,21 +10,19 @@ const { MAX_UINT_VAL } = require("../../../common/Constants");
 const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
 const TestnetERC20 = artifacts.require("TestnetERC20");
 const ExpandedERC20 = artifacts.require("ExpandedERC20");
-const argv = require("minimist")(process.argv.slice(), { string: ["emp", "tokens", "collateral", "collateralToken"] });
+const argv = require("minimist")(process.argv.slice(), { string: ["emp", "collateral", "collateralToken"] });
 
 async function createPosition(callback) {
   try {
-    if (!argv.emp || !argv.tokens || !argv.collateral) {
+    if (!argv.emp || !argv.collateral) {
       throw new Error(`
       required: --emp must be the emp address.
-      required: --tokens must be the number of synthetic tokens to create.
       required: --collateral must be the amount of collateral to supply to back the tokens.
       optional: --collateralToken must be the address of the deployed collateral currency
       `);
     }
 
     const emp = await ExpiringMultiParty.at(argv.emp);
-    // Create tokens for liquidator to liquidate with.
     collateralToken = argv.collateralToken
       ? await ExpandedERC20.at(argv.collateralToken)
       : await TestnetERC20.deployed();
@@ -40,8 +38,8 @@ async function createPosition(callback) {
     }
 
     await collateralToken.approve(emp.address, MAX_UINT_VAL);
-    await emp.create({ rawValue: collateral.toString() }, { rawValue: toWei(argv.tokens) });
-    console.log(`Created ${argv.tokens} tokens (backed by ${argv.collateral} collateral)`);
+    await emp.deposit({ rawValue: collateral.toString() });
+    console.log(`Deposited ${argv.collateral} collateral`);
   } catch (err) {
     callback(err);
     return;
