fd4836f5457dbcf05a8b83b6a0bfac23f8c71303
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2020-06-22 10:08:57 -0400

    GCP deploy script: delete instance before deploying (#1624)
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- scripts/bot-deployment/DeployBotGCP.sh
+++ scripts/bot-deployment/DeployBotGCP.sh
@@ -38,6 +38,18 @@ gsutil cp gs://bot-configs/$1.env $tempFile
 
 echo "✍️  Config has been pulled and placed in a temp directory" $tempFile
 
+# Check if there is an existing instance. If there is, then we will delete it to clear space for the new one.
+existingImages=$(gcloud compute instances list --filter $1 --limit 1)
+if [[ "$existingImages" == *"$1"* ]]; then
+    echo "♻️  Deleting old bot.  ⚠️  Deleting an instance can take some time, please be patient.  ⚠️"
+    
+    gcloud compute instances delete $1 \
+        --zone northamerica-northeast1-b \
+        --quiet
+
+    echo "🎇  Old bot has been deleted!"
+fi
+
 # Deploy The bot to GCP using the config file and the service account
 echo "🚀 Deploying bot to GCP"
 gcloud compute instances create-with-container $1 \
