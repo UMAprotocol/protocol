1f82dba28820655ca2f8186616c1dcf925116a12
Christopher Maree <christopher.maree@gmail.com>
2020-06-25 13:55:30 +0200

    further generalize
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- scripts/bot-deployment/DeployJsonScheduler.sh
+++ scripts/bot-deployment/DeployJsonScheduler.sh
@@ -1,26 +1,28 @@
 if [ $# -ne 5 ]; then
     echo "Incorrect number of arguments supplied! arguments are:"
     echo "1) Scheduler name"
-    echo "2) Target HTTP URL"
-    echo "3) Scheduler service account"
-    echo "4) HTTP JSON body"
-    echo "5) Scheduler description"
+    echo "2) Schedular cron time"
+    echo "3) Target HTTP URL"
+    echo "4) Scheduler service account"
+    echo "5) HTTP JSON body"
+    echo "6) Scheduler description"
     exit 1
 fi
 
 echo "🔥 Creating scheduler with the following params:"
 echo "Scheduler Name:" $1
-echo "Target HTTP URL:" $2
-echo "Service Account:" $3
-echo "HTTP JSON body:" $4
-echo "Scheduler description:" $5
+echo "Schedular cron time" $2
+echo "Target HTTP URL:" $3
+echo "Service Account:" $4
+echo "HTTP JSON body:" $5
+echo "Scheduler description:" $6
 
 gcloud scheduler jobs create http $1 \
-    --schedule="0 12 * * *" \
-    --uri=$2 \
-    --oidc-service-account-email=$3 \
+    --schedule="$2" \
+    --uri=$3 \
+    --oidc-service-account-email=$4 \
     --http-method=post \
-    --message-body=$4 \
+    --message-body=$5 \
     --headers="Authorization=key=AUTHKEY, Content-Type=application/json" \
-    --description="$5"
+    --description="$6"
 echo "🎊 Scheduler created!"
