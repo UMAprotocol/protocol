f6673988168d5ed9e5f9ff5edc3c93c8a57970f8
Nick Pai <npai.nyc@gmail.com>
2020-06-30 13:57:01 -0400

    inject logger, enhance tests
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- monitors/test/index.js
+++ monitors/test/index.js
@@ -1,5 +1,4 @@
 const { toWei, utf8ToHex } = web3.utils;
-const { MAX_UINT_VAL } = require("../../common/Constants");
 
 // Script to test
 const Poll = require("../index.js");
@@ -12,6 +11,11 @@ const TokenFactory = artifacts.require("TokenFactory");
 const Token = artifacts.require("ExpandedERC20");
 const Timer = artifacts.require("Timer");
 const UniswapMock = artifacts.require("UniswapMock");
+
+// Custom winston transport module to monitor winston log outputs
+const winston = require("winston");
+const sinon = require("sinon");
+const { SpyTransport, spyLogLevel } = require("../../financial-templates-lib/logger/SpyTransport");
 
 contract("index.js", function(accounts) {
   const contractCreator = accounts[0];
@@ -28,6 +32,9 @@ contract("index.js", function(accounts) {
   let defaultContractMonitorConfig;
   let defaultSyntheticPegMonitorConfig;
 
+  let spy;
+  let spyLogger;
+
   before(async function() {
     collateralToken = await Token.new("UMA", "UMA", 18, { from: contractCreator });
 
@@ -37,6 +44,13 @@ contract("index.js", function(accounts) {
   });
 
   beforeEach(async function() {
+    // Create a sinon spy and give it to the SpyTransport as the winston logger. Use this to check all winston logs.
+    spy = sinon.spy(); // Create a new spy for each test.
+    spyLogger = winston.createLogger({
+      level: "info",
+      transports: [new SpyTransport({ level: "info" }, { spy: spy })]
+    });
+
     const constructorParams = {
       expirationTimestamp: "12345678900",
       withdrawalLiveness: "1000",
@@ -97,25 +111,24 @@ contract("index.js", function(accounts) {
     await uniswap.setPrice(toWei("1"), toWei("1"));
   });
 
-  it("Completes one iteration without throwing an error", async function() {
+  it("Completes one iteration without logging any errors", async function() {
     const address = emp.address;
 
-    let errorThrown = false;
-    try {
-      await Poll.run(
-        address,
-        10,
-        0,
-        defaultBotMonitorConfig,
-        defaultWalletMonitorConfig,
-        defaultContractMonitorConfig,
-        defaultSyntheticPegMonitorConfig,
-        defaultUniswapPricefeedConfig,
-        defaultMedianizerPricefeedConfig
-      );
-    } catch (err) {
-      errorThrown = true;
+    await Poll.run(
+      spyLogger,
+      address,
+      0,
+      0,
+      defaultBotMonitorConfig,
+      defaultWalletMonitorConfig,
+      defaultContractMonitorConfig,
+      defaultSyntheticPegMonitorConfig,
+      defaultUniswapPricefeedConfig,
+      defaultMedianizerPricefeedConfig
+    );
+
+    for (let i = 0; i < spy.callCount; i++) {
+      assert.notEqual(spyLogLevel(spy, i), "error");
     }
-    assert.isFalse(errorThrown);
   });
 });
