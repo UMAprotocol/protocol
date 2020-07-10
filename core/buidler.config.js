00d4c10c13acbf0212997532909c2485587a922b
Kendrick Tan <kendricktan0814@gmail.com>
2020-07-10 15:47:40 +1000

    timeout
    Signed-off-by: Kendrick Tan <kendricktan0814@gmail.com>

--- core/buidler.config.js
+++ core/buidler.config.js
@@ -13,7 +13,8 @@ module.exports = {
     buidlerevm: {
       gas: 9000000,
       blockGasLimit: 0x1fffffffffffff,
-      allowUnlimitedContractSize: true
+      allowUnlimitedContractSize: true,
+      timeout: 1800000
     },
     localhost: {
       url: "http://127.0.0.1:8545"
