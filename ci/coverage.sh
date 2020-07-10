d012923f8331c3d89f89bd59606c2232546bb96a
Kendrick Tan <kendricktan0814@gmail.com>
2020-07-10 16:32:58 +1000

    truffle compile
    Signed-off-by: Kendrick Tan <kendricktan0814@gmail.com>

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -11,4 +11,6 @@ cd $TRUFFLE_DIR
 # Truffle compile can take a lot of memory, which I've experienced with the solc-0.6 compatible versions,
 # so I explicitly increase the javascript heap size.
 # More details here: https://github.com/trufflesuite/truffle/issues/957
+# Truffle compile for DecodeTransactionData
+node --max-old-space-size=4096 $(npm bin)/truffle compile
 node --max-old-space-size=4096 $(npm bin)/buidler coverage && cat coverage/lcov.info | $(npm bin)/coveralls --verbose
