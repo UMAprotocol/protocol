3b506f1a0e9e31daffc3b32f043aba42b77c0ea1
Matt Rice <matthewcrice32@gmail.com>
2020-07-22 11:30:24 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

---
+++ core/migrations/2_deploy_finder.js
@@ -0,0 +1,8 @@
+const Finder = artifacts.require("Finder");
+const { getKeysForNetwork, deploy } = require("@umaprotocol/common");
+
+module.exports = async function(deployer, network, accounts) {
+  const keys = getKeysForNetwork(network, accounts);
+
+  await deploy(deployer, network, Finder, { from: keys.deployer });
+};
