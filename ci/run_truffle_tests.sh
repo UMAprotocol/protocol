77c492ab0798b5d9c1457e38495fb02dc15740dd
Kendrick Tan <kendricktan0814@gmail.com>
2020-07-23 10:45:13 +1000

    yam 199
    Signed-off-by: Kendrick Tan <kendricktan0814@gmail.com>

--- ci/run_truffle_tests.sh
+++ ci/run_truffle_tests.sh
@@ -54,4 +54,4 @@ $(npm bin)/truffle test $(find ../disputer/test -name '*.js') --network ci
 $(npm bin)/truffle test $(find ../monitors/test -name '*.js') --network ci
 
 # Only run tests that needs the mainnet forking here
-$(npm bin)/truffle test ../liquidator/test-fork/
+$(npm bin)/truffle test ../liquidator/test-fork/ --network mainnet-fork
--- core/contracts/common/interfaces/OneSplit.sol
+++ core/contracts/common/interfaces/OneSplit.sol
@@ -1,20 +1,18 @@
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 
 abstract contract OneSplit {
     function getExpectedReturn(
-        IERC20 fromToken,
-        IERC20 destToken,
+        address fromToken,
+        address destToken,
         uint256 amount,
         uint256 parts,
         uint256 flags // See constants in IOneSplit.sol
     ) public virtual view returns (uint256 returnAmount, uint256[] memory distribution);
 
     function swap(
-        IERC20 fromToken,
-        IERC20 destToken,
+        address fromToken,
+        address destToken,
         uint256 amount,
         uint256 minReturn,
         uint256[] memory distribution,
