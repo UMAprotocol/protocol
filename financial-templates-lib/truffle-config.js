752cb4099ec96f328aa7dd13daf98313520deece
Matt Rice <matthewcrice32@gmail.com>
2020-08-02 14:20:37 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- financial-templates-lib/truffle-config.js
+++ financial-templates-lib/truffle-config.js
@@ -1,9 +1,4 @@
 const path = require("path");
 const wkdir = path.dirname(require.resolve("@umaprotocol/core/package.json"));
 
-module.exports = {
-  ...require("@umaprotocol/common").TruffleConfig,
-  migrations_directory: path.join(wkdir, "migrations"),
-  contracts_directory: path.join(wkdir, "contracts"),
-  contracts_build_directory: path.join(wkdir, "build/contracts")
-};
+module.exports = require("@umaprotocol/common").getTruffleConfig(wkdir);
