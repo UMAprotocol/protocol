917197c669c381d9c871a6a7c345292da58b2d9f
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2020-08-10 12:44:28 -0400

    Update common/BlacklistedPriceRequests.js
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>

--- common/BlacklistedPriceRequests.js
+++ common/BlacklistedPriceRequests.js
@@ -1,4 +1,4 @@
-// Whitelisted price requests that will display on voter clients.
+// Blacklisted price requests that will display on voter clients.
 const REQUEST_BLACKLIST = {
   SOME_IDENTIFIER: ["1596666977"]
 };
