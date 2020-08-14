b5fdac3005f52faf60abcc8808646ca920f1e1f8
Christopher Maree <christopher.maree@gmail.com>
2020-08-14 12:46:56 +0200

    fixed nits
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- core/scripts/IntegrationTests.js
+++ core/scripts/IntegrationTests.js
@@ -89,6 +89,7 @@ contract("IntegrationTest", function(accounts) {
       liquidationLiveness: "3600",
       collateralRequirement: { rawValue: toWei("1.2") },
       disputeBondPct: { rawValue: toWei("0.1") },
+      sponsorDisputeRewardPct: { rawValue: toWei("0.1") },
       disputerDisputeRewardPct: { rawValue: toWei("0.1") },
       minSponsorTokens: { rawValue: toWei("1") },
       gcrWithdrawalScalingFactor: { rawValue: toWei("0") },
