7e0539fa0c59bb5709255bdf388e0a809143f213
Matt Rice <matthewcrice32@gmail.com>
2020-08-18 19:00:52 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- scripts/run_release.sh
+++ scripts/run_release.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+# Inject the npm token into the npm config.
+npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
+
+# Publish any packages whose versions are not present in the registry.
 yarn lerna publish from-package --yes
 
+# Create git tags for any tags that don't exist in the repo.
+# TODO: this is not explicitly synced with the step above.
+# For now, if this causes a difference, the git tags will need to be manually replaced.
 for TAG in $(yarn --silent lerna ls -p --long -a | cut -d: -f2-3 | tr : @)
 do
     echo "Attempting to publish tag: $TAG"
