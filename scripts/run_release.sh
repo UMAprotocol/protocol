e8d84cfd909ca7fe0b50d2e5ca0b79c6c4e87896
Matt Rice <matthewcrice32@gmail.com>
2020-08-18 18:20:01 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

---
+++ scripts/run_release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+for TAG in $(yarn --silent lerna ls -p --long -a | cut -d: -f2-3 | tr : @)
+do
+    git tag -a "$TAG" -m "$TAG" || echo "Not publishing $TAG since it already exists"
+done
+
+git push origin --tags
+
+yarn run publish-release-npm
