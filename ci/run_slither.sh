44beea65e3540a304bfd52e17adf6edcd021f2b0
Matt Rice <matthewcrice32@gmail.com>
2020-08-18 23:31:02 -0400

    refactor(lerna): move all packages to packages/
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -8,7 +8,7 @@ sudo chmod -R a+rwx /usr/local/lib/node_modules
 run_slither() {
     cd $1
     mkdir -p node_modules/
-    cp -r ../node_modules/@openzeppelin ./node_modules/@openzeppelin
+    cp -r ../../node_modules/@openzeppelin ./node_modules/@openzeppelin
 
     cd $PROTOCOL_DIR
 
@@ -17,4 +17,4 @@ run_slither() {
     slither --exclude=divide-before-multiply,locked-ether,unused-return,timestamp,naming-convention,pragma,solc-version,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,incorrect-equality,reentrancy-events,assembly --filter-paths="@openzeppelin|WETH9.sol|ReentrancyAttack.sol|ReentrancyMock.sol" $1
 }
 
-run_slither $PROTOCOL_DIR/core
+run_slither $PROTOCOL_DIR/packages/core
