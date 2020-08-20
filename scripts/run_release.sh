a843ed1ef0ca41ab9dfbc33c1a95d7702f58fd4e
Matt Rice <matthewcrice32@gmail.com>
2020-08-20 09:41:53 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- scripts/run_release.sh
+++ scripts/run_release.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Inject the npm token into the npm config.
-echo "//registry.npmjs.org/:_authToken=$npm_TOKEN" > ~/.npmrc
+# Inject the npm token and registry into the npm config.
+cat > ~/.npmrc << EOF
+@umaprotocol:registry=https://registry.npmjs.org/
+@uma:registry=https://registry.npmjs.org/
+//registry.npmjs.org/:_authToken=${npm_TOKEN}
+EOF
 
 # Publish any packages whose versions are not present in the registry.
 yarn lerna publish from-package --yes
