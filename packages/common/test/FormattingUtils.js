bf69cd17d9d76abf2adbee983ff96925a5ba01e3
David Adams <david@adams.software>
2020-08-21 15:49:26 +0000

    fix(common): use mocha instead of tape for test, add test script to package file
    Signed-off-by: David Adams <david@adams.software>

--- packages/common/test/FormattingUtils.js
+++ packages/common/test/FormattingUtils.js
@@ -1,38 +1,34 @@
-const test = require("tape");
 const utils = require("../src/FormattingUtils");
 const BigNumber = require("bignumber.js");
+const assert = require("assert");
 
-// Please recommend a better place/way to do this. Technically this
-// does not depend on web3 or truffle, but can include that for consistency.
-test("FormattingUtils", t => {
-  t.test("ConvertDecimals", t => {
-    t.test("init", t => {
+describe("FormattingUtils", () => {
+  describe("ConvertDecimals", () => {
+    it("init", () => {
       const result = utils.ConvertDecimals(6, 18);
-      t.ok(result);
-      t.end();
+      assert(result);
     });
-    t.test("conversions", t => {
+    it("conversions", () => {
       let convertDecimals = utils.ConvertDecimals(6, 18);
       let result = convertDecimals(1);
-      t.equal(result.toString(), (10n ** 12n).toString());
+      assert.equal(result.toString(), (10n ** 12n).toString());
       result = convertDecimals(100);
-      t.equal(result.toString(), (10n ** 14n).toString());
+      assert.equal(result.toString(), (10n ** 14n).toString());
 
       convertDecimals = utils.ConvertDecimals(18, 6);
       result = convertDecimals(1);
-      t.equal(result.toString(), "0");
+      assert.equal(result.toString(), "0");
       result = convertDecimals(10n ** 18n);
-      t.equal(result.toString(), (10n ** 6n).toString());
+      assert.equal(result.toString(), (10n ** 6n).toString());
 
       result = convertDecimals(0);
-      t.equal(result.toString(), "0");
+      assert.equal(result.toString(), "0");
 
       convertDecimals = utils.ConvertDecimals(0, 6);
       result = convertDecimals(1);
-      t.equal(result.toString(), (10n ** 6n).toString());
+      assert.equal(result.toString(), (10n ** 6n).toString());
       result = convertDecimals(1000);
-      t.equal(result.toString(), (10n ** 9n).toString());
-      t.end();
+      assert.equal(result.toString(), (10n ** 9n).toString());
     });
   });
 });
