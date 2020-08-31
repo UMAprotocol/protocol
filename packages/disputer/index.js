dca4cf98e81b78834778cd92ae1f84ee44410098
Kendrick Tan <kendricktan0814@gmail.com>
2020-08-31 16:40:10 +1000

    disputer index.js
    Signed-off-by: Kendrick Tan <kendricktan0814@gmail.com>

--- packages/disputer/index.js
+++ packages/disputer/index.js
@@ -30,7 +30,7 @@ const { getAbi, getAddress } = require("@umaprotocol/core");
  * @param {String} [disputerOverridePrice] Optional String representing a Wei number to override the disputer price feed.
  * @return None or throws an Error.
  */
-async function run(
+async function run({
   logger,
   web3,
   empAddress,
@@ -40,7 +40,7 @@ async function run(
   priceFeedConfig,
   disputerConfig,
   disputerOverridePrice
-) {
+}) {
   try {
     const { toBN } = web3.utils;
 
@@ -201,11 +201,11 @@ async function Poll(callback) {
       // Create a web3 instance. This has built in re-try on error and loads in a provided mnemonic or private key.
       const { web3 } = require("@umaprotocol/financial-templates-lib/src/clients/Web3WebsocketClient");
       if (!web3) throw new Error("Could not create web3 object from websocket");
-      await run(Logger, web3, ...Object.values(executionParameters));
+      await run({ logger: Logger, web3, ...executionParameters });
 
       // Else, if the web3 instance is not undefined, then the script is being run from Truffle. Use present web3 instance.
     } else {
-      await run(Logger, web3, ...Object.values(executionParameters));
+      await run({ logger: Logger, web3, ...executionParameters });
     }
   } catch (error) {
     Logger.error({
