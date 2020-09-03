2bfa6f306788406a7a8137b0e209de0fff58c2b8
Matt Rice <matthewcrice32@gmail.com>
2020-09-03 19:54:07 -0400

    Update TokenBalanceClient.js

--- packages/financial-templates-lib/test/clients/TokenBalanceClient.js
+++ packages/financial-templates-lib/test/clients/TokenBalanceClient.js
@@ -18,7 +18,7 @@ const Convert = decimals => number => parseFixed(number.toString(), decimals).to
 
 contract("TokenBalanceClient.js", function(accounts) {
   for (let tokenConfig of configs) {
-    describe(`${tokenConfigs.collateralDecimals} decimals`, function() {
+    describe(`${tokenConfig.collateralDecimals} decimals`, function() {
       const tokenCreator = accounts[0];
       const sponsor1 = accounts[1];
       const sponsor2 = accounts[2];
@@ -31,17 +31,17 @@ contract("TokenBalanceClient.js", function(accounts) {
       let identifier;
       let convert;
       before(async function() {
-        identifier = `${tokenConfigs.tokenName}TEST`;
-        convert = Convert(tokenConfigs.collateralDecimals);
+        identifier = `${tokenConfig.tokenName}TEST`;
+        convert = Convert(tokenConfig.collateralDecimals);
         // The TokenBalance Client is independent of the EMP and simply needs two tokens to monitor.
         collateralToken = await Token.new(
-          tokenConfigs.tokenName,
-          tokenConfigs.tokenName,
-          tokenConfigs.collateralDecimals,
+          tokenConfig.tokenName,
+          tokenConfig.tokenName,
+          tokenConfig.collateralDecimals,
           { from: tokenCreator }
         );
         await collateralToken.addMember(1, tokenCreator, { from: tokenCreator });
-        syntheticToken = await Token.new(tokenConfigs.tokenName, tokenConfigs.tokenName, 18, { from: tokenCreator });
+        syntheticToken = await Token.new(tokenConfig.tokenName, tokenConfig.tokenName, 18, { from: tokenCreator });
         await syntheticToken.addMember(1, tokenCreator, { from: tokenCreator });
       });
 
