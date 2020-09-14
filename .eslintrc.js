8f7d94da82b0ce51cccf0ce45b46e2c1b4c66468
Matt Rice <matthewcrice32@gmail.com>
2020-09-14 14:38:21 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
   env: {
-    browser: true
+    node: true,
+    mocha: true
   },
-  extends: ["plugin:prettier/recommended"],
+  extends: ["plugin:prettier/recommended", "eslint:recommended"],
   plugins: ["prettier", "mocha"],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-      modules: true
-    }
+    ecmaVersion: 10
   },
-  parser: "babel-eslint",
   rules: {
     "prettier/prettier": ["error"],
     indent: ["error", 2, { SwitchCase: 1 }],
@@ -18,9 +15,16 @@ module.exports = {
     quotes: ["error", "double", { avoidEscape: true }],
     semi: ["error", "always"],
     "spaced-comment": ["error", "always", { exceptions: ["-", "+"] }],
-    "mocha/no-exclusive-tests": "error"
+    "mocha/no-exclusive-tests": "error",
+    "no-console": 0
   },
   settings: {
     "mocha/additionalTestFunctions": ["describeModule"]
+  },
+  globals: {
+    web3: "readonly",
+    artifacts: "readonly",
+    assert: "readonly",
+    contract: "readonly"
   }
 };
