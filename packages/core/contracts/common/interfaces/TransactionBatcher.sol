ca8b82a5fbfe0965c626fbf8079733a908014e20
Matt Rice <matthewcrice32@gmail.com>
2020-09-22 21:05:33 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/core/contracts/common/interfaces/TransactionBatcher.sol
+++ packages/core/contracts/common/interfaces/TransactionBatcher.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 
+// See deployed implementation here: https://etherscan.io/address/0x82458d1c812d7c930bb3229c9e159cbabd9aa8cb.
 abstract contract TransactionBatcher {
     function batchSend(
         address[] memory targets,
