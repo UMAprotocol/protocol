b130c327ccc14196d767c3a4e4e0f1e689e0534d
Matt Rice <matthewcrice32@gmail.com>
2020-09-23 09:49:32 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/core/contracts/common/interfaces/TransactionBatcher.sol
+++ packages/core/contracts/common/interfaces/TransactionBatcher.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 
+// This is an interface to interact with a deployed implementation by https://github.com/kleros/action-callback-bots for
+// batching on-chain transactions.
 // See deployed implementation here: https://etherscan.io/address/0x82458d1c812d7c930bb3229c9e159cbabd9aa8cb.
 abstract contract TransactionBatcher {
     function batchSend(
