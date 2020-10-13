fc9a52f60195b65163f7f74545ba152c11dc6ba9
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2020-10-13 17:15:54 -0400

    feat(perpetual) add Perpetual and PerpetualLib (#2081)
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

---
+++ packages/core/contracts/financial-templates/perpetual-multiparty/Perpetual.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "./PerpetualLiquidatable.sol";
+
+
+/**
+ * @title Perpetual Multiparty Contract.
+ * @notice Convenient wrapper for Liquidatable.
+ */
+contract Perpetual is PerpetualLiquidatable {
+    /**
+     * @notice Constructs the Perpetual contract.
+     * @param params struct to define input parameters for construction of Liquidatable. Some params
+     * are fed directly into the PositionManager's constructor within the inheritance tree.
+     */
+    constructor(ConstructorParams memory params)
+        public
+        PerpetualLiquidatable(params)
+    // Note: since there is no logic here, there is no need to add a re-entrancy guard.
+    {
+
+    }
+}
