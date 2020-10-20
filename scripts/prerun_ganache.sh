f374c246bc324839306055e47f5db21bfd19bf33
Matt Rice <matthewcrice32@gmail.com>
2020-10-20 10:11:25 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- scripts/prerun_ganache.sh
+++ scripts/prerun_ganache.sh
@@ -6,5 +6,7 @@ set -e
 TEMP_FILE=$(mktemp)
 echo "Starting Ganache. Logs being written to $TEMP_FILE"
 
+
+trap "pkill -f '/ganache-cli'" EXIT
 yarn ganache-cli -e 1000000000 -l 9000000 -d -p 9545 $> $TEMP_FILE & 
 $1
