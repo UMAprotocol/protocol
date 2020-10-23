892042c0f941252b95bb8f8aa6aa5e6735ffc46a
Matt Rice <matthewcrice32@gmail.com>
2020-10-23 13:33:23 -0400

    feat(pmp): integrate signed fixed point into funding rate applier (#2107)

--- packages/core/contracts/financial-templates/test/FundingRateApplierTest.sol
+++ packages/core/contracts/financial-templates/test/FundingRateApplierTest.sol
@@ -11,7 +11,7 @@ contract FundingRateApplierTest is FundingRateApplier {
         address _fpFinderAddress,
         bytes32 _fundingRateIdentifier,
         address _timerAddress
-    ) public FundingRateApplier(_fpFinderAddress, _fundingRateIdentifier, _timerAddress) {}
+    ) public Testable(_timerAddress) FundingRateApplier(_fpFinderAddress, _fundingRateIdentifier) {}
 
     function applyFundingRate() public {
         _applyEffectiveFundingRate();
@@ -19,9 +19,14 @@ contract FundingRateApplierTest is FundingRateApplier {
 
     function calculateEffectiveFundingRate(
         uint256 paymentPeriodSeconds,
-        FixedPoint.Unsigned memory fundingRatePerSecond,
-        FixedPoint.Unsigned memory feeMultiplier
-    ) public pure returns (FixedPoint.Unsigned memory, FixedPoint.Unsigned memory) {
-        return _calculateEffectiveFundingRate(paymentPeriodSeconds, fundingRatePerSecond, feeMultiplier);
+        FixedPoint.Signed memory fundingRatePerSecond,
+        FixedPoint.Unsigned memory currentCumulativeFundingRateMultiplier
+    ) public pure returns (FixedPoint.Unsigned memory, FixedPoint.Signed memory) {
+        return
+            _calculateEffectiveFundingRate(
+                paymentPeriodSeconds,
+                fundingRatePerSecond,
+                currentCumulativeFundingRateMultiplier
+            );
     }
 }
