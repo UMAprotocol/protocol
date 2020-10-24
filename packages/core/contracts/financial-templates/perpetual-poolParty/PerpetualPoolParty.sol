f93a47a9c7ca9f5234cb20a2d08a990b652ef361
Alessandro Bergamaschi <alessandro@jarvis.network>
2020-10-24 02:08:22 +0200

    [core]: Add PerpetualPoolPart contract
    Signed-off-by: Alessandro Bergamaschi <alessandro@jarvis.network>
    Signed-off-by: Alessandro Bergamaschi <bergamaschi400@gmail.com>
    Signed-off-by: Alessandro Bergamaschi <alessandro@jarvis.network>
    Signed-off-by: Alessandro Bergamaschi <bergamaschi400@gmail.com>

---
+++ packages/core/contracts/financial-templates/perpetual-poolParty/PerpetualPoolParty.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "./PerpetualLiquidatablePoolParty.sol";
+
+
+/**
+ * @title Perpetual Multiparty Contract.
+ * @notice Convenient wrapper for Liquidatable.
+ */
+contract PerpetualPoolParty is PerpetualLiquidatablePoolParty {
+    /**
+     * @notice Constructs the Perpetual contract.
+     * @param params struct to define input parameters for construction of Liquidatable. Some params
+     * are fed directly into the PositionManager's constructor within the inheritance tree.
+     */
+    constructor(ConstructorParams memory params)
+        public
+        PerpetualLiquidatablePoolParty(params)
+    // Note: since there is no logic here, there is no need to add a re-entrancy guard.
+    {
+
+    }
+}
