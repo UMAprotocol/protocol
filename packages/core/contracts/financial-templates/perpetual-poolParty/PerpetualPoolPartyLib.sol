5cf18b8e5ec8fe34ec36a9222865e6831e2b8ee6
Alessandro Bergamaschi <alessandro@jarvis.network>
2020-10-24 02:09:09 +0200

    [core]: Add PerpetualPoolPartyLib library and PerpetutalPoolPartyCreator contract
    Signed-off-by: Alessandro Bergamaschi <alessandro@jarvis.network>
    Signed-off-by: Alessandro Bergamaschi <bergamaschi400@gmail.com>
    Signed-off-by: Alessandro Bergamaschi <alessandro@jarvis.network>
    Signed-off-by: Alessandro Bergamaschi <bergamaschi400@gmail.com>

---
+++ packages/core/contracts/financial-templates/perpetual-poolParty/PerpetualPoolPartyLib.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "./PerpetualPoolParty.sol";
+
+
+/**
+ * @title Provides convenient Perpetual Multi Party contract utilities.
+ * @dev Using this library to deploy Perpetuals allows calling contracts to avoid importing the full bytecode.
+ */
+library PerpetualPoolPartyLib {
+    /**
+     * @notice Returns address of new Perpetual deployed with given `params` configuration.
+     * @dev Caller will need to register new Perpetual with the Registry to begin requesting prices. Caller is also
+     * responsible for enforcing constraints on `params`.
+     * @param params is a `ConstructorParams` object from Perpetual.
+     * @return address of the deployed Perpetual contract
+     */
+    function deploy(PerpetualPoolParty.ConstructorParams memory params) public returns (address) {
+        PerpetualPoolParty derivative = new PerpetualPoolParty(params);
+        return address(derivative);
+    }
+}
