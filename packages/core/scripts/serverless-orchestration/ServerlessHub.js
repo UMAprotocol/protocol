975ccf10568bd04fe9ba291456b1457fa1a3f59f
Chris Maree <christopher.maree@gmail.com>
2020-10-27 12:20:16 +0200

    Restore ServerlessHub.js

--- packages/core/scripts/serverless-orchestration/ServerlessHub.js
+++ packages/core/scripts/serverless-orchestration/ServerlessHub.js
@@ -60,9 +60,7 @@ hub.post("/", async (req, res) => {
     // Fetch the last block number this given config file queried the blockchain at if running in production. Else, pull from env.
     const lastQueriedBlockNumber = await _getLastQueriedBlockNumber(req.body.configFile);
     if (!configObject || !lastQueriedBlockNumber)
-      throw new Error(
-        `Serverless hub requires a config object and a last updated block number! configObject:${configObject} lastQueriedBlockNumber: ${lastQueriedBlockNumber}`
-      );
+      throw new Error("Serverless hub requires a config object and a last updated block number!");
 
     // Get the latest block number. The query will run from the last queried block number to the latest block number.
     const latestBlockNumber = await _getLatestBlockNumber();
@@ -263,7 +261,7 @@ async function _saveQueriedBlockNumber(configIdentifier, blockNumber) {
 // recorded by the bot to inform where searches should start from.
 async function _getLastQueriedBlockNumber(configIdentifier) {
   // sometimes the GCP datastore can be flaky and return errors when saving data. Use re-try logic to re-run on error.
-  return await retry(
+  await retry(
     async () => {
       if (hubConfig.saveQueriedBlock == "gcp") {
         const key = datastore.key(["BlockNumberLog", configIdentifier]);
@@ -362,4 +360,4 @@ if (require.main === module) {
 }
 
 hub.Poll = Poll;
-module.exports = { hub, _getLastQueriedBlockNumber };
+module.exports = hub;
