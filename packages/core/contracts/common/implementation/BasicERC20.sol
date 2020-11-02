1b337c54379074ae01fd07399de24b49b31a2919
Nick Pai <npai.nyc@gmail.com>
2020-11-02 13:09:01 -0500

    comment length
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/contracts/common/implementation/BasicERC20.sol
+++ packages/core/contracts/common/implementation/BasicERC20.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title Implements only the required ERC20 methods, for example not `decimals()`.
- * @dev Mostly copied from Consensys EIP-20 implementation: https://github.com/ConsenSys/Tokens/blob/fdf687c69d998266a95f15216b1955a4965a0a6d/contracts/eip20/EIP20.sol
+ * @dev Mostly copied from Consensys EIP-20 implementation:
+ * https://github.com/ConsenSys/Tokens/blob/fdf687c69d998266a95f15216b1955a4965a0a6d/contracts/eip20/EIP20.sol
  */
 contract BasicERC20 is IERC20 {
     uint256 private constant MAX_UINT256 = 2**256 - 1;
