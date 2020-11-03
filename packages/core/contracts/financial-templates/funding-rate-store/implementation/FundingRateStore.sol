b937d9f3c2ff38e54a7223dfdf7d7300672396ad
Nick Pai <npai.nyc@gmail.com>
2020-11-03 12:29:16 -0500

    testing for dispute done
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/contracts/financial-templates/funding-rate-store/implementation/FundingRateStore.sol
+++ packages/core/contracts/financial-templates/funding-rate-store/implementation/FundingRateStore.sol
@@ -1,15 +1,41 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import "../../../oracle/interfaces/StoreInterface.sol";
+import "../../../oracle/interfaces/OracleInterface.sol";
+import "../../../oracle/interfaces/FinderInterface.sol";
+import "../../../oracle/implementation/Constants.sol";
+
 import "../interfaces/FundingRateStoreInterface.sol";
 import "../../../common/implementation/Testable.sol";
+import "../../../common/implementation/FixedPoint.sol";
 
 
 contract FundingRateStore is FundingRateStoreInterface, Testable {
+    using SafeMath for int256;
+    using FixedPoint for FixedPoint.Unsigned;
+    using FixedPoint for FixedPoint.Signed;
+    using SafeERC20 for IERC20;
+
+    /****************************************
+     *      FEE PAYER DATA STRUCTURES       *
+     ****************************************/
+
+    // The collateral currency used to reward successful proposers.
+    IERC20 public collateralCurrency;
+
+    // Finder contract used to look up addresses for UMA system contracts.
+    FinderInterface public finder;
+
     struct Proposal {
         FixedPoint.Signed rate;
         uint256 time;
         address proposer;
+        FixedPoint.Unsigned finalFee;
+        address disputer;
     }
 
     struct FundingRateRecord {
@@ -20,12 +46,35 @@ contract FundingRateStore is FundingRateStoreInterface, Testable {
     enum ProposalState { None, Pending, Expired }
 
     mapping(bytes32 => FundingRateRecord) public fundingRateRecords;
+    mapping(bytes32 => mapping(uint256 => FundingRateRecord)) public fundingRateDisputes;
 
     uint256 public proposalLiveness;
 
-    constructor(uint256 _proposalLiveness, address _timerAddress) public Testable(_timerAddress) {
+    /****************************************
+     *                EVENTS                *
+     ****************************************/
+
+    event ProposedRate(bytes32 indexed identifier, int256 rate, uint256 proposalTime, address indexed proposer);
+    event DisputedRate(
+        bytes32 indexed identifier,
+        int256 rate,
+        uint256 proposalTime,
+        address indexed proposer,
+        address indexed disputer
+    );
+    event PublishedRate(bytes32 indexed identifier, int256 rate, uint256 proposalTime, address indexed proposer);
+    event FinalFeesPaid(uint256 indexed amount);
+
+    constructor(
+        uint256 _proposalLiveness,
+        address _collateralAddress,
+        address _finderAddress,
+        address _timerAddress
+    ) public Testable(_timerAddress) {
         require(_proposalLiveness > 0, "Proposal liveness is 0");
         proposalLiveness = _proposalLiveness;
+        collateralCurrency = IERC20(_collateralAddress);
+        finder = FinderInterface(_finderAddress);
     }
 
     function getFundingRateForIdentifier(bytes32 identifier) external override view returns (FixedPoint.Signed memory) {
@@ -41,7 +90,6 @@ contract FundingRateStore is FundingRateStoreInterface, Testable {
     function propose(bytes32 identifier, FixedPoint.Signed memory rate) external {
         // TODO: check the identifier whitelist to ensure the proposed identifier is approved by the DVM.
         FundingRateRecord storage fundingRateRecord = _getFundingRateRecord(identifier);
-
         ProposalState proposalState = _getProposalState(fundingRateRecord.proposal);
 
         // TODO: bond logic.
@@ -49,14 +97,134 @@ contract FundingRateStore is FundingRateStoreInterface, Testable {
         require(proposalState != ProposalState.Pending, "Existing proposal still pending.");
 
         if (proposalState == ProposalState.Expired) {
+            // Publish expired rate, reward proposer.
             fundingRateRecord.rate = fundingRateRecord.proposal.rate;
+
+            // TODO: Reward = proposal bond + final fee credit
+            emit PublishedRate(
+                identifier,
+                fundingRateRecord.rate.rawValue,
+                fundingRateRecord.proposal.time,
+                fundingRateRecord.proposal.proposer
+            );
         }
 
-        fundingRateRecord.proposal = Proposal({ rate: rate, time: getCurrentTime(), proposer: msg.sender });
+        // Compute final fee at time of proposal.
+        FixedPoint.Unsigned memory finalFeeBond = _computeFinalFees();
+
+        uint256 currentTime = getCurrentTime();
+        fundingRateRecord.proposal = Proposal({
+            rate: rate,
+            time: currentTime,
+            proposer: msg.sender,
+            finalFee: finalFeeBond,
+            disputer: address(0x0)
+        });
+        emit ProposedRate(identifier, rate.rawValue, currentTime, msg.sender);
+
+        // Pull final fee bond from proposer.
+        collateralCurrency.safeTransferFrom(msg.sender, address(this), finalFeeBond.rawValue);
+    }
+
+    function dispute(bytes32 identifier) external {
+        FundingRateRecord storage fundingRateRecord = _getFundingRateRecord(identifier);
+        ProposalState proposalState = _getProposalState(fundingRateRecord.proposal);
+
+        require(proposalState == ProposalState.Pending, "Existing proposal not pending.");
+
+        // TODO: Bond logic.
+
+        // Pull final fee bond from disputer and pay store.
+        _payFinalFees(msg.sender, fundingRateRecord.proposal.finalFee);
+
+        // Send price request
+        _requestOraclePrice(identifier, fundingRateRecord.proposal.time);
+
+        emit DisputedRate(
+            identifier,
+            fundingRateRecord.proposal.rate.rawValue,
+            fundingRateRecord.proposal.time,
+            fundingRateRecord.proposal.proposer,
+            msg.sender
+        );
+
+        // Delete pending proposal and copy into dispute records.
+        fundingRateRecord.proposal.disputer = msg.sender;
+        fundingRateDisputes[identifier][fundingRateRecord.proposal.time] = fundingRateRecord;
+        delete fundingRateRecords[identifier].proposal;
+    }
+
+    function withdrawDispute(bytes32 identifier, uint256 proposalTime) external {
+        FundingRateRecord storage fundingRateDispute = _getFundingRateDispute(identifier, proposalTime);
+
+        // Get the returned funding rate from the oracle. If this has not yet resolved will revert.
+        FixedPoint.Signed memory settlementRate = _getOraclePrice(identifier, proposalTime);
+
+        // If the position has more than the required collateral it is solvent and the dispute is valid(liquidation is invalid)
+        // Note that this check uses the liquidatedCollateral not the lockedCollateral as this considers withdrawals.
+        bool disputeSucceeded = settlementRate.isEqual(fundingRateDispute.proposal.rate.rawValue);
+        address proposer = disputeSucceeded
+            ? fundingRateDispute.proposal.disputer
+            : fundingRateDispute.proposal.proposer;
+
+        if (disputeSucceeded) {
+            // If dispute succeeds:
+            // - pay disputer the dispute reward + final fee rebate
+
+            collateralCurrency.safeTransfer(
+                fundingRateDispute.proposal.disputer,
+                fundingRateDispute.proposal.finalFee.rawValue
+            );
+        } else {
+            // If dispute fails:
+            // - pay proposer the proposal reward + final fee rebate
+
+            collateralCurrency.safeTransfer(
+                fundingRateDispute.proposal.proposer,
+                fundingRateDispute.proposal.finalFee.rawValue
+            );
+        }
+
+        // Update current rate to settlement rate.
+        fundingRateRecords[identifier].rate = settlementRate;
+        emit PublishedRate(identifier, settlementRate.rawValue, proposalTime, proposer);
+
+        // Delete dispute
+        delete fundingRateDisputes[identifier][proposalTime];
+    }
+
+    /****************************************
+     *         INTERNAL FUNCTIONS           *
+     ****************************************/
+
+    // Requests a price for `priceIdentifier` at `requestedTime` from the Oracle.
+    function _requestOraclePrice(bytes32 identifier, uint256 requestedTime) internal {
+        OracleInterface oracle = _getOracle();
+        oracle.requestPrice(identifier, requestedTime);
+    }
+
+    // Pays UMA Oracle final fees of `amount` in `collateralCurrency` to the Store contract. Final fee is a flat fee
+    // charged for each price request.
+    function _payFinalFees(address payer, FixedPoint.Unsigned memory amount) internal {
+        if (amount.isEqual(0)) {
+            return;
+        }
+
+        collateralCurrency.safeTransferFrom(payer, address(this), amount.rawValue);
+
+        emit FinalFeesPaid(amount.rawValue);
+
+        StoreInterface store = _getStore();
+        collateralCurrency.safeIncreaseAllowance(address(store), amount.rawValue);
+        store.payOracleFeesErc20(address(collateralCurrency), amount);
     }
 
     function _getFundingRateRecord(bytes32 identifier) private view returns (FundingRateRecord storage) {
         return fundingRateRecords[identifier];
+    }
+
+    function _getFundingRateDispute(bytes32 identifier, uint256 time) private view returns (FundingRateRecord storage) {
+        return fundingRateDisputes[identifier][time];
     }
 
     function _getProposalState(Proposal storage proposal) private view returns (ProposalState) {
@@ -68,5 +236,32 @@ contract FundingRateStore is FundingRateStoreInterface, Testable {
         } else {
             return ProposalState.Expired;
         }
+    }
+
+    // Fetches a resolved Oracle price from the Oracle. Reverts if the Oracle hasn't resolved for this request.
+    function _getOraclePrice(bytes32 identifier, uint256 requestedTime)
+        internal
+        view
+        returns (FixedPoint.Signed memory)
+    {
+        // Create an instance of the oracle and get the price. If the price is not resolved revert.
+        OracleInterface oracle = _getOracle();
+        require(oracle.hasPrice(identifier, requestedTime), "Unresolved oracle price");
+        int256 oraclePrice = oracle.getPrice(identifier, requestedTime);
+
+        return FixedPoint.Signed(oraclePrice);
+    }
+
+    function _computeFinalFees() internal view returns (FixedPoint.Unsigned memory finalFees) {
+        StoreInterface store = _getStore();
+        return store.computeFinalFee(address(collateralCurrency));
+    }
+
+    function _getOracle() internal view returns (OracleInterface) {
+        return OracleInterface(finder.getImplementationAddress(OracleInterfaces.Oracle));
+    }
+
+    function _getStore() internal view returns (StoreInterface) {
+        return StoreInterface(finder.getImplementationAddress(OracleInterfaces.Store));
     }
 }
--- packages/core/test/financial-templates/funding-rate-store/FundingRateStore.js
+++ packages/core/test/financial-templates/funding-rate-store/FundingRateStore.js
@@ -1,8 +1,9 @@
 // External libs
-const { toWei, utf8ToHex: toHex } = web3.utils;
+const { toWei, utf8ToHex: toHex, toBN, hexToUtf8: toUtf8 } = web3.utils;
+const truffleAssert = require("truffle-assertions");
 
 // Local libs
-const { didContractThrow } = require("@uma/common");
+const { didContractThrow, interfaceName, ZERO_ADDRESS } = require("@uma/common");
 const { assert } = require("chai");
 
 // Tested Contract
@@ -10,6 +11,11 @@ const FundingRateStore = artifacts.require("FundingRateStore");
 
 // Helper Contracts
 const Timer = artifacts.require("Timer");
+const Store = artifacts.require("Store");
+const Finder = artifacts.require("Finder");
+const IdentifierWhitelist = artifacts.require("IdentifierWhitelist");
+const MockOracle = artifacts.require("MockOracle");
+const ExpandedERC20 = artifacts.require("ExpandedERC20");
 
 // Helper functions.
 async function incrementTime(contract, amount) {
@@ -17,20 +23,55 @@ async function incrementTime(contract, amount) {
   await contract.setCurrentTime(Number(currentTime) + amount);
 }
 
+function isEmptyProposalStruct(proposalStruct) {
+  assert.equal(proposalStruct.rate, "0");
+  assert.equal(proposalStruct.time, "0");
+  assert.equal(proposalStruct.proposer, ZERO_ADDRESS);
+  assert.equal(proposalStruct.disputer, ZERO_ADDRESS);
+  assert.equal(proposalStruct.finalFee, "0");
+}
 contract("FundingRateStore", function(accounts) {
   let timer;
+  let store;
+  let finder;
+  let collateralCurrency;
   let fundingRateStore;
-  let account1 = accounts[0];
+  let mockOracle;
+  let identifierWhitelist;
+
+  let contractDeployer = accounts[0];
+  let proposer = accounts[1];
+  let disputer = accounts[2];
+  let rando = accounts[3];
 
   const liveness = 7200;
 
+  const defaultTestIdentifier = toHex("TEST-IDENTIFIER");
+
   beforeEach(async () => {
+    collateralCurrency = await ExpandedERC20.new("USDC", "USDC", 18);
+    await collateralCurrency.addMember(1, contractDeployer);
     timer = await Timer.deployed();
-    fundingRateStore = await FundingRateStore.new(liveness, timer.address);
+    store = await Store.deployed();
+    finder = await Finder.deployed();
+    identifierWhitelist = await IdentifierWhitelist.deployed();
+    fundingRateStore = await FundingRateStore.new(liveness, collateralCurrency.address, finder.address, timer.address);
+
+    // Set up Oracle
+    mockOracle = await MockOracle.new(finder.address, timer.address, {
+      from: contractDeployer
+    });
+    const mockOracleInterfaceName = toHex(interfaceName.Oracle);
+    await finder.changeImplementationAddress(mockOracleInterfaceName, mockOracle.address, {
+      from: contractDeployer
+    });
+
+    // Set up whitelist
+    await identifierWhitelist.addSupportedIdentifier(defaultTestIdentifier);
   });
 
   it("Liveness check", async function() {
-    assert(await didContractThrow(FundingRateStore.new(0, timer.address)));
+    assert(await didContractThrow(FundingRateStore.new(0, collateralCurrency.address, finder.address, timer.address)));
   });
 
   it("Initial Funding Rate of 0", async function() {
@@ -39,9 +80,11 @@ contract("FundingRateStore", function(accounts) {
   });
 
   describe("Unexpired Proposal", function() {
-    const identifier = toHex("unexpired-proposal");
+    const identifier = defaultTestIdentifier;
+    let proposalTxn, proposalTime;
     beforeEach(async () => {
-      await fundingRateStore.propose(identifier, { rawValue: toWei("0.01") }, { from: account1 });
+      proposalTime = await fundingRateStore.getCurrentTime();
+      proposalTxn = await fundingRateStore.propose(identifier, { rawValue: toWei("0.01") }, { from: proposer });
       await incrementTime(fundingRateStore, liveness - 1);
     });
 
@@ -49,17 +92,128 @@ contract("FundingRateStore", function(accounts) {
       assert.equal((await fundingRateStore.getFundingRateForIdentifier(identifier)).rawValue.toString(), "0");
     });
 
+    it("Event emitted", async function() {
+      truffleAssert.eventEmitted(proposalTxn, "ProposedRate", ev => {
+        return (
+          toUtf8(ev.identifier) === toUtf8(identifier) &&
+          ev.rate.toString() === toWei("0.01").toString() &&
+          ev.proposalTime.toString() === proposalTime.toString() &&
+          ev.proposer === proposer
+        );
+      });
+    });
+
+    it("Proposer pays final fee bond", async function() {
+      // Set a nonzero final fee.
+      const finalFeeAmount = toWei("1");
+      await store.setFinalFee(collateralCurrency.address, { rawValue: finalFeeAmount.toString() });
+
+      // Advance to liveness so we can propose a new rate.
+      await incrementTime(fundingRateStore, 1);
+
+      // No balance.
+      assert(
+        await didContractThrow(fundingRateStore.propose(identifier, { rawValue: toWei("-0.01") }, { from: proposer }))
+      );
+
+      // No allowance
+      await collateralCurrency.mint(proposer, finalFeeAmount);
+      assert(
+        await didContractThrow(fundingRateStore.propose(identifier, { rawValue: toWei("-0.01") }, { from: proposer }))
+      );
+
+      // Allowance and balance OK.
+      await collateralCurrency.increaseAllowance(fundingRateStore.address, finalFeeAmount, { from: proposer });
+      const preBalance = await collateralCurrency.balanceOf(proposer);
+      await fundingRateStore.propose(identifier, { rawValue: toWei("-0.01") }, { from: proposer });
+      const postBalance = await collateralCurrency.balanceOf(proposer);
+      assert.equal(finalFeeAmount.toString(), toBN(preBalance).sub(toBN(postBalance)));
+    });
+
     it("New proposal not allowed", async function() {
       assert(
-        await didContractThrow(fundingRateStore.propose(identifier, { rawValue: toWei("0.01") }, { from: account1 }))
+        await didContractThrow(fundingRateStore.propose(identifier, { rawValue: toWei("0.01") }, { from: proposer }))
       );
     });
   });
 
-  describe("Expired Proposal", function() {
-    const identifier = toHex("expired-proposal");
+  describe("Disputed Proposal", function() {
+    const identifier = defaultTestIdentifier;
+    let proposalTime;
+    const finalFeeAmount = toWei("1");
     beforeEach(async () => {
-      await fundingRateStore.propose(identifier, { rawValue: toWei("0.01") }, { from: account1 });
+      // Set a nonzero final fee.
+      await store.setFinalFee(collateralCurrency.address, { rawValue: finalFeeAmount.toString() });
+
+      // Mint proposer enough collateral to cover final fee.
+      await collateralCurrency.mint(proposer, finalFeeAmount);
+      await collateralCurrency.increaseAllowance(fundingRateStore.address, finalFeeAmount, { from: proposer });
+
+      // Propose new funding rate
+      proposalTime = await fundingRateStore.getCurrentTime();
+      await fundingRateStore.propose(identifier, { rawValue: toWei("0.01") }, { from: proposer });
+      await incrementTime(fundingRateStore, liveness - 1);
+
+      // Mint disputer collateral to cover final fee bond as well.
+      await collateralCurrency.mint(disputer, finalFeeAmount);
+      await collateralCurrency.increaseAllowance(fundingRateStore.address, finalFeeAmount, { from: disputer });
+    });
+
+    it("Disputing unexpired proposal", async function() {
+      // Can't dispute if proposal is not yet pending
+      assert(await didContractThrow(fundingRateStore.dispute(toHex("other-proposal"), { from: disputer })));
+
+      // Disputer who has no balance or allowance to pay final fee cannot dispute.
+      assert(await didContractThrow(fundingRateStore.dispute(identifier, { from: rando })));
+      await collateralCurrency.mint(rando, finalFeeAmount);
+      assert(await didContractThrow(fundingRateStore.dispute(identifier, { from: rando })));
+
+      // Disputer must stake a final fee bond.
+      const preBalance = await collateralCurrency.balanceOf(disputer);
+      const disputeTxn = await fundingRateStore.dispute(identifier, { from: disputer });
+      const postBalance = await collateralCurrency.balanceOf(disputer);
+      assert.equal(finalFeeAmount.toString(), toBN(preBalance).sub(toBN(postBalance)));
+
+      // Price request is enqueued.
+      const pendingRequests = await mockOracle.getPendingQueries();
+      assert.equal(toUtf8(pendingRequests[0].identifier), toUtf8(identifier));
+      assert.equal(pendingRequests[0].time, proposalTime);
+
+      // Pending proposal is deleted, disputed proposal record is created.
+      const pendingProposal = await fundingRateStore.fundingRateRecords(identifier);
+      const disputedProposal = await fundingRateStore.fundingRateDisputes(identifier, proposalTime);
+      isEmptyProposalStruct(pendingProposal.proposal);
+      assert.equal(disputedProposal.proposal.time, proposalTime);
+      assert.equal(disputedProposal.proposal.rate, toWei("0.01").toString());
+      assert.equal(disputedProposal.proposal.proposer, proposer);
+      assert.equal(disputedProposal.proposal.disputer, disputer);
+      assert.equal(disputedProposal.proposal.finalFee.toString(), finalFeeAmount.toString());
+
+      // Dispute event was emitted.
+      truffleAssert.eventEmitted(disputeTxn, "DisputedRate", ev => {
+        return (
+          toUtf8(ev.identifier) === toUtf8(identifier) &&
+          ev.rate.toString() === toWei("0.01").toString() &&
+          ev.proposalTime.toString() === proposalTime.toString() &&
+          ev.proposer === proposer &&
+          ev.disputer === disputer
+        );
+      });
+    });
+
+    // - Settle dispute:
+    //     - Check that it reverts if there is no price request, rewards the winner, updates the current rate (without affecting a pending rate), emits an event, and deletes a dispute record
+
+    it("Cannot dispute expired proposal", async function() {
+      await incrementTime(fundingRateStore, 1);
+      assert(await didContractThrow(fundingRateStore.dispute(identifier, { from: disputer })));
+    });
+  });
+
+  describe("Expired Proposal", function() {
+    const identifier = defaultTestIdentifier;
+    beforeEach(async () => {
+      await fundingRateStore.propose(identifier, { rawValue: toWei("0.01") }, { from: proposer });
       await incrementTime(fundingRateStore, liveness);
     });
 
@@ -68,7 +222,7 @@ contract("FundingRateStore", function(accounts) {
     });
 
     it("New proposal allowed", async function() {
-      await fundingRateStore.propose(identifier, { rawValue: toWei("-0.01") }, { from: account1 });
+      await fundingRateStore.propose(identifier, { rawValue: toWei("-0.01") }, { from: proposer });
 
       // Double check that existing value still persists even after a fresh proposal.
       assert.equal((await fundingRateStore.getFundingRateForIdentifier(identifier)).rawValue.toString(), toWei("0.01"));
