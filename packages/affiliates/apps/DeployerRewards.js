8d0731ad67fad521c5cfec1e58a77296bbc3356c
David Adams <david@umaproject.org>
2020-11-04 14:01:22 +0000

    improve(affiliates): change the way web3 and config gets injected
    Signed-off-by: David Adams <david@umaproject.org>

--- packages/affiliates/apps/DeployerRewards.js
+++ packages/affiliates/apps/DeployerRewards.js
@@ -1,19 +1,21 @@
 // This file is meant to be run in the command line. It takes in a configuration to generate
 // final deployer reward output.
-// example: node apps/DeployerRewards ./config.example.js >> output.json
+// example: node apps/DeployerRewards ./config.example.js --network=mainnet_mnemonic >> output.json
 const assert = require("assert");
 const { getAbi, getAddress } = require("@uma/core");
 const { BigQuery } = require("@google-cloud/bigquery");
 const Promise = require("bluebird");
 
-const Start = require("../libs/start");
+const Config = require("../libs/config");
 const { DeployerRewards } = require("../libs/affiliates");
 const { Emp } = require("../libs/contracts");
 const Queries = require("../libs/bigquery");
 const Coingecko = require("../libs/coingecko");
+const { getWeb3 } = require("@uma/common");
 
 // This is the main function which configures all data sources for the calculation.
-async function App(config, { web3 }) {
+async function App(config) {
+  const web3 = getWeb3();
   const { empWhitelist = [], startTime, endTime, totalRewards, network = 1 } = config;
   assert(empWhitelist, "requires whitelist");
   assert(startTime, "requires startTime");
@@ -66,8 +68,8 @@ async function App(config, { web3 }) {
   };
 }
 
-// Run the app and log output to console to allow user to pipe to file
-// This can be changed easily if need be.
-Start(App)
+const config = Config();
+
+App(config)
   .then(console.log)
   .catch(console.error);
