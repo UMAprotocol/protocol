4f7e46d0ac4ea84201d8c05a8fc4505e00f1b3a1
Christopher Maree <christopher.maree@gmail.com>
2020-11-09 12:06:26 +0200

    Added licenses to contracts
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/contracts/financial-templates/common/WETH9.sol
+++ packages/core/contracts/financial-templates/common/WETH9.sol
@@ -17,6 +17,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.6.0;
 
 
--- packages/core/contracts/oracle/implementation/VoteTiming.sol
+++ packages/core/contracts/oracle/implementation/VoteTiming.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
