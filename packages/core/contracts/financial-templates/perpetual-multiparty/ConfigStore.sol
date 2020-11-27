ec2195bb7c126eeafce364e71fed3394f90f622e
Nick Pai <npai.nyc@gmail.com>
2020-11-27 10:58:26 -0500

    improve(perpetual-position-manager) Remove redundant check for emergency shutdown timestamp
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/contracts/financial-templates/perpetual-multiparty/ConfigStore.sol
+++ packages/core/contracts/financial-templates/perpetual-multiparty/ConfigStore.sol
@@ -23,16 +23,6 @@ contract ConfigStore is ConfigStoreInterface, Testable, Lockable, Ownable {
      *        STORE DATA STRUCTURES         *
      ****************************************/
 
-    // All of the configuration settings available for querying by a perpetual.
-    struct ConfigSettings {
-        // Liveness period (in seconds) for an update to currentConfig to become official.
-        uint256 timelockLiveness;
-        // Reward rate paid to successful proposers. Percentage of 1 E.g., .1 is 10%.
-        FixedPoint.Unsigned rewardRatePerSecond;
-        // Bond % (of given contract's PfC) that must be staked by proposers. Percentage of 1, e.g. 0.0005 is 0.05%.
-        FixedPoint.Unsigned proposerBondPct;
-    }
-
     // Make currentConfig private to force user to call getCurrentConfig, which returns the pendingConfig
     // if its liveness has expired.
     ConfigSettings private currentConfig;
