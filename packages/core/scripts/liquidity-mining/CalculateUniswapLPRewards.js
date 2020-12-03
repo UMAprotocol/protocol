126ad61f2432bb25a78dce5f8755b3848b6bfa12
Christopher Maree <christopher.maree@gmail.com>
2020-12-03 14:18:40 +0200

    Updated script
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/scripts/liquidity-mining/CalculateUniswapLPRewards.js
+++ packages/core/scripts/liquidity-mining/CalculateUniswapLPRewards.js
@@ -2,9 +2,13 @@
 // -> Define the starting and ending blockheight of each week.
 // -> Define snapshot blocks (eg every 64 blocks ~15min). For each snapshot block, calculate the proportional liquidity
 // provided by for each liquidity provider to the single whitelisted pool.
-// -> For each snapshot block, calculate the $UMA rewards to be received by each liquidity provider based on the target weekly distribution.
-
-// Example usage from core: node ./scripts/liquidity-mining/CalculateUniswapLPRewards.js --network mainnet_mnemonic --poolAddress="0x0099447ef539718bba3c4d4d4b4491d307eedc53" --fromBlock 10725993 --toBlock 10752010 --week=1
+// -> For each snapshot block, calculate the effective balance that each LP added to the pool. The effective balance is
+// the minimum of their redeemable synths for their LP tokens and their sponsor position. This acts to enforce that the
+// recipient of the LP reward is also a token sponsor.
+// -> Using the effective balance, compute the pro-rata contribution of each sponsor.
+// Example usage from core: node ./scripts/liquidity-mining/CalculateUniswapLPRewards.js --fromBlock 11356429 \
+// --toBlock 11379497 --poolAddress "0x25fb29d865c1356f9e95d621f21366d3a5db6bb0" \
+// --empAddress "0x516f595978D87B67401DaB7AfD8555c3d28a3Af4" --umaPerWeek 1000 --tokenName "ugas" --week 1 --network mainnet_mnemonic
 
 // Set the archival node using: export CUSTOM_NODE_URL=<your node here>
 const cliProgress = require("cli-progress");
@@ -20,7 +24,7 @@ const web3 = getWeb3();
 const { toWei, toBN, fromWei } = web3.utils;
 
 const argv = require("minimist")(process.argv.slice(), {
-  string: ["poolAddress", "tokenName"],
+  string: ["poolAddress", "empAddress", "tokenName"],
   integer: ["fromBlock", "toBlock", "week", "umaPerWeek", "blocksPerSnapshot"]
 });
 
@@ -29,14 +33,24 @@ async function calculateUniswapLPRewards(
   toBlock,
   tokenName,
   poolAddress,
+  empAddress,
   week,
   umaPerWeek = 25000,
   blocksPerSnapshot = 256
 ) {
   // Create two moment objects from the input string. Convert to UTC time zone. As no time is provided in the input
   // will parse to 12:00am UTC.
-  if (!web3.utils.isAddress(poolAddress) || !fromBlock || !toBlock || !week || !tokenName) {
-    throw new Error("Missing or invalid parameter! Provide poolAddress, fromBlock, toBlock, week & tokenName");
+  if (
+    !web3.utils.isAddress(poolAddress) ||
+    !web3.utils.isAddress(empAddress) ||
+    !fromBlock ||
+    !toBlock ||
+    !week ||
+    !tokenName
+  ) {
+    throw new Error(
+      "Missing or invalid parameter! Provide poolAddress, empAddress fromBlock, toBlock, week & tokenName"
+    );
   }
 
   console.log(`🔥Starting $UMA Uniswap liquidity provider script for ${tokenName}🔥`);
@@ -63,10 +77,19 @@ async function calculateUniswapLPRewards(
   console.log("shareHolders", shareHolders);
   console.log("🏖  Number of historic liquidity providers:", shareHolders.length);
 
-  const bPool = new web3.eth.Contract(getAbi("ERC20"), poolAddress);
+  // Initialize the contract we'll need for computation.
+  const uniswapPool = new web3.eth.Contract(getAbi("ERC20"), poolAddress);
+
+  const empContract = new web3.eth.Contract(getAbi("ExpiringMultiParty"), empAddress);
+
+  const syntheticTokenAddress = await empContract.methods.tokenCurrency().call();
+
+  const syntheticToken = new web3.eth.Contract(getAbi("ERC20"), syntheticTokenAddress);
 
   const shareHolderPayout = await _calculatePayoutsBetweenBlocks(
-    bPool,
+    uniswapPool,
+    empContract,
+    syntheticToken,
     shareHolders,
     fromBlock,
     toBlock,
@@ -83,6 +106,8 @@ async function calculateUniswapLPRewards(
     toBlock,
     tokenName,
     poolAddress,
+    empAddress,
+    syntheticTokenAddress,
     blocksPerSnapshot,
     umaPerWeek
   );
@@ -91,7 +116,9 @@ async function calculateUniswapLPRewards(
 // Calculate the payout to a list of `shareHolders` between `fromBlock` and `toBlock`. Split the block window up into
 // chunks of `blockPerSnapshot` and at each chunk assign `umaPerSnapshot` at a prorata basis.
 async function _calculatePayoutsBetweenBlocks(
-  bPool,
+  uniswapPool,
+  empContract,
+  syntheticToken,
   shareHolders,
   fromBlock,
   toBlock,
@@ -116,7 +143,14 @@ async function _calculatePayoutsBetweenBlocks(
   );
   progressBar.start(snapshotsToTake, 0);
   for (let currentBlock = fromBlock; currentBlock < toBlock; currentBlock += blocksPerSnapshot) {
-    shareHolderPayout = await _updatePayoutAtBlock(bPool, currentBlock, shareHolderPayout, umaPerSnapshot);
+    shareHolderPayout = await _updatePayoutAtBlock(
+      uniswapPool,
+      empContract,
+      syntheticToken,
+      currentBlock,
+      shareHolderPayout,
+      umaPerSnapshot
+    );
     progressBar.update(Math.ceil((currentBlock - fromBlock) / blocksPerSnapshot) + 1);
   }
   progressBar.stop();
@@ -125,34 +159,91 @@ async function _calculatePayoutsBetweenBlocks(
 }
 
 // For a given `blockNumber` (snapshot in time), return an updated `shareHolderPayout` object that has appended
-// payouts for a given `bPool` at a rate of `umaPerSnapshot`.
-async function _updatePayoutAtBlock(bPool, blockNumber, shareHolderPayout, umaPerSnapshot) {
+// payouts for a given `uniswapPool` at a rate of `umaPerSnapshot`.
+async function _updatePayoutAtBlock(
+  uniswapPool,
+  empContract,
+  syntheticToken,
+  blockNumber,
+  shareHolderPayout,
+  umaPerSnapshot
+) {
   // Get the total supply of Uniswap Pool tokens at the given snapshot's block number.
-  const bptSupplyAtSnapshot = toBN(await bPool.methods.totalSupply().call(undefined, blockNumber));
+  const lpTokenSupplyAtSnapshot = toBN(await uniswapPool.methods.totalSupply().call(undefined, blockNumber));
+
+  // Get the total number of synthetics in the Uniswap pool at the snapshot's block number.
+  const syntheticsInPoolAtSnapshot = toBN(
+    await syntheticToken.methods.balanceOf(uniswapPool._address).call(undefined, blockNumber)
+  );
+
+  // Compute how many synthetics each LP token is redeemable for at the current pool weighting.
+  const lpTokensToSynthetics = syntheticsInPoolAtSnapshot.mul(toBN(toWei("1"))).div(lpTokenSupplyAtSnapshot);
 
   // Get the given holders balance at the given block. Generate an array of promises to resolve in parallel.
-  const uniswapResults = await Promise.map(
+  const uniswapBalanceResults = await Promise.map(
     Object.keys(shareHolderPayout),
-    shareHolder => bPool.methods.balanceOf(shareHolder).call(undefined, blockNumber),
+    shareHolder => uniswapPool.methods.balanceOf(shareHolder).call(undefined, blockNumber),
     {
       concurrency: 50 // Keep infura happy about the number of incoming requests.
     }
   );
-  // For each balance result, calculate their associated payment addition.
-  uniswapResults.forEach(function(uniswapResult, index) {
+
+  // Also, get the position information for all shareholders.
+  const tokenShareHolderPositionResults = await Promise.map(
+    Object.keys(shareHolderPayout),
+    shareHolder => empContract.methods.positions(shareHolder).call(undefined, blockNumber),
+    {
+      concurrency: 50 // Keep infura happy about the number of incoming requests.
+    }
+  );
+
+  // For each balance result, calculate their associated payment addition. The data structures below are used to store
+  // and compute the "effective" ballance. this is the minimum of the token sponsors sponsor position OR redeemable
+  // synths from their LP position.
+  let shareHolderEffectiveSnapshotBalance = {};
+  let cumulativeEffectiveSnapshotBalance = toBN("0");
+  uniswapBalanceResults.forEach(function(uniswapResult, index) {
     // If the given shareholder had no BLP tokens at the given block, skip them.
     if (uniswapResult === "0") return;
     // The holders fraction is the number of BPTs at the block divided by the total supply at that block.
-    const shareHolderBalanceAtSnapshot = toBN(uniswapResult);
+    const shareHolderLpBalanceAtSnapshot = toBN(uniswapResult);
+
+    // Calculate how many synths the sponsors LP tokens are redeemable for at this given snapshot.
+    const shareHolderRedeemableSynthsFromLpShareAtSnapshot = shareHolderLpBalanceAtSnapshot
+      .mul(lpTokensToSynthetics)
+      .div(toBN("1"));
+
+    // Calculate how many synths the sponsors has created at the current snapshot.
+    const shareHolderShareHolderSynthsOutstandingAtSnapshot = toBN(
+      tokenShareHolderPositionResults[index].tokensOutstanding.rawValue
+    );
+
+    // The sponsors "effective" balance is the min of these two numbers.
+    const minEffectiveSynthBalance = web3.utils.BN.min(
+      shareHolderRedeemableSynthsFromLpShareAtSnapshot,
+      shareHolderShareHolderSynthsOutstandingAtSnapshot
+    );
+
+    // Store this effective balance for computation.
+    const shareHolderAddress = Object.keys(shareHolderPayout)[index];
+    shareHolderEffectiveSnapshotBalance[shareHolderAddress] = minEffectiveSynthBalance;
+    // Also, store the cumulative effective balance across all sponsors for the current snapshot. This is used next to
+    // find the pro-rata distribution over this effective snapshot balance.
+    cumulativeEffectiveSnapshotBalance = cumulativeEffectiveSnapshotBalance.add(minEffectiveSynthBalance);
+  });
+
+  // At this point we know each sponsors effective balance and the overall cumulative effective balance at the current
+  // snapshot. Using this, we can compute how much each sponsor contributed to the overall effective balance and
+  // allocate rewards accordingly.
+  Object.keys(shareHolderEffectiveSnapshotBalance).forEach(shareHolderAddress => {
     const shareHolderFractionAtSnapshot = toBN(toWei("1"))
-      .mul(shareHolderBalanceAtSnapshot)
-      .div(bptSupplyAtSnapshot);
+      .mul(shareHolderEffectiveSnapshotBalance[shareHolderAddress])
+      .div(cumulativeEffectiveSnapshotBalance);
 
     // The payout at the snapshot for the holder is their pro-rata fraction of per-snapshot rewards.
     const shareHolderPayoutAtSnapshot = shareHolderFractionAtSnapshot.mul(toBN(umaPerSnapshot)).div(toBN(toWei("1")));
 
     // Lastly, update the payout object for the given shareholder. This is their previous payout value + their new payout.
-    const shareHolderAddress = Object.keys(shareHolderPayout)[index];
     shareHolderPayout[shareHolderAddress] = shareHolderPayout[shareHolderAddress].add(shareHolderPayoutAtSnapshot);
   });
   return shareHolderPayout;
@@ -166,6 +257,8 @@ function _saveShareHolderPayout(
   toBlock,
   tokenName,
   poolAddress,
+  empAddress,
+  syntheticTokenAddress,
   blocksPerSnapshot,
   umaPerWeek
 ) {
@@ -176,7 +269,17 @@ function _saveShareHolderPayout(
   }
 
   // Format output and save to file.
-  const outputObject = { week, fromBlock, toBlock, poolAddress, blocksPerSnapshot, umaPerWeek, shareHolderPayout };
+  const outputObject = {
+    week,
+    fromBlock,
+    toBlock,
+    poolAddress,
+    empAddress,
+    syntheticTokenAddress,
+    blocksPerSnapshot,
+    umaPerWeek,
+    shareHolderPayout
+  };
   const savePath = `${path.resolve(__dirname)}/${tokenName}-weekly-payouts/Week_${week}_Mining_Rewards.json`;
   fs.writeFileSync(savePath, JSON.stringify(outputObject));
   console.log("🗄  File successfully written to", savePath);
@@ -219,6 +322,7 @@ async function Main(callback) {
       argv.toBlock,
       argv.tokenName,
       argv.poolAddress,
+      argv.empAddress,
       argv.week,
       argv.umaPerWeek,
       argv.blocksPerSnapshot
