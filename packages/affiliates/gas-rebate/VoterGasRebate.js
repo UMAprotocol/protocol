aef9ecfa24bfcabfbbb31941bd6eb0090751dd61
Nick Pai <npai.nyc@gmail.com>
2020-12-08 14:24:35 -0500

    add rebate 4 and update script
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/affiliates/gas-rebate/VoterGasRebate.js
+++ packages/affiliates/gas-rebate/VoterGasRebate.js
@@ -7,18 +7,21 @@
  * This script therefore relies on accurate (1) average gas price data over the specified period (i.e. in gwei), (2) average
  * ETH price data over the period, and (3) average UMA price data over the period. It also needs the current UMA-ETH price.
  *
- * Run: (from repo root) `node ./packages/core/scripts/gas-rebate/VoterGasRebate.js  --start 1598572800 --end 1599055419` --network mainnet_mnemonic
+ * Run: (from repo root) node ./packages/affiliates/gas-rebate/VoterGasRebate.js  --rebateNumber 4 --start-block 11112533 --end-block 11413784 --network mainnet_mnemonic
  *
  * Config options (can be specified as CLI flags):
  * - start: {Number, optional} start timestamp to query Reveal and Claim events from, described in Unix time in seconds.
- *          Defaults to default end time - 3 days.
+ *          Defaults to default end time - 5 days.
  * - end: {Number, optional} end timestamp to query Reveal and Claim events up to, described in Unix time in seconds.
- *          Defaults to current time minus 5 minutes in case node has not processed a block at current time.
+ *          Defaults to current time minus 1 day.
+ * - start-block: {Number, optional} start block to query Reveal and Claim events from.
+ *          Defaults to default end time - 5 days.
+ * - end-block: {Number, optional} end block to query Reveal and Claim events up to.
+ *          Defaults to current time minus 1 day.
  * - reveal-only: {Boolean, optional} Only query and parse Reveal events. Skip Claim events
  *          Defaults to false.
  * - claim-only: {Boolean, optional} Only query and parse Claim events. Skip Reveal events
  *          Defaults to false.
- *
  */
 
 require("dotenv").config();
@@ -26,7 +29,7 @@ const moment = require("moment");
 const fetch = require("node-fetch");
 const cliProgress = require("cli-progress");
 const argv = require("minimist")(process.argv.slice(), {
-  string: ["start", "end", "rebateNumber"],
+  string: ["start", "end", "rebateNumber", "start-block", "end-block"],
   boolean: ["reveal-only", "claim-only"]
 });
 const fs = require("fs");
@@ -52,6 +55,9 @@ const multibar = new cliProgress.MultiBar(
   },
   cliProgress.Presets.shades_classic
 );
+// Keep track of useful metrics for this current script run, such as unique identifiers seen across all claim and reveal events:
+const metrics = {};
+metrics["identifiers"] = [];
 
 /** *****************************************
  *
@@ -94,6 +100,9 @@ async function parseRevealEvents({ committedVotes, revealedVotes, priceData, reb
       web3.eth.getBlock(reveal.blockNumber),
       web3.eth.getTransactionReceipt(reveal.transactionHash)
     ]);
+
+    // Metrics:
+    if (metrics.identifiers.indexOf(identifier) === -1) metrics.identifiers.push(identifier);
 
     const key = `${voter}-${roundId}-${identifier}-${requestTime}`;
     const val = {
@@ -247,6 +256,9 @@ async function parseClaimEvents({ claimedRewards, priceData, rebateOutput, debug
       const roundId = claim.returnValues.roundId;
       const identifier = web3.utils.hexToUtf8(claim.returnValues.identifier);
       const requestTime = claim.returnValues.time;
+
+      // Metrics.
+      if (metrics.identifiers.indexOf(identifier) === -1) metrics.identifiers.push(identifier);
 
       const gasUsed = parseInt(transactionReceipt.gasUsed);
       const txnTimestamp = transactionBlock.timestamp;
@@ -468,6 +480,9 @@ async function calculateRebate({
       console.log(
         `🎟 UMA to rebate: ${totalUMAToRebate} across ${Object.keys(rebateOutput.shareHolderPayout).length} voters`
       );
+      // Add useful data to output file:
+      rebateOutput.countVoters = Object.keys(rebateOutput.shareHolderPayout).length;
+      rebateOutput.uniqueIdentifiers = metrics.identifiers;
       // Format output and save to file.
       const savePath = `${path.resolve(__dirname)}/rebates/Rebate_${rebateNumber}.json`;
       fs.writeFileSync(savePath, JSON.stringify(rebateOutput, null, 4));
@@ -581,13 +596,21 @@ async function Main(callback) {
     console.log("*=======================================*");
 
     const rebateNumber = argv.rebateNumber ? argv.rebateNumber : "1";
-    const endDate = argv.end ? argv.end : Math.round(Date.now() / 1000 - 24 * 60 * 60); // Default: Current time minus 1 day.
-    const startDate = argv.start ? argv.start : endDate - 60 * 60 * 24 * 5; // Default: End time - 5 days
-    console.log(`- Using start date: ${moment.unix(startDate).toString()}`);
-    console.log(`- Using end date: ${moment.unix(endDate).toString()}`);
+    // First check if block numbers are passed in, then default to timetamps.
+    let startBlock;
+    let endBlock;
+    if (argv["start-block"] || argv["end-block"]) {
+      endBlock = argv["end-block"] ? argv["end-block"] : (await web3.eth.getBlockNumber()) - 5000; // Default: Current block minus ~1 day of blocks.
+      startBlock = argv["start-block"] ? argv["start-block"] : endBlock - 25000; // Default: End time - ~5 days of blocks.
+    } else {
+      const endDate = argv.end ? argv.end : Math.round(Date.now() / 1000 - 24 * 60 * 60); // Default: Current time minus 1 day.
+      const startDate = argv.start ? argv.start : endDate - 60 * 60 * 24 * 5; // Default: End time - 5 days
+      console.log(`- Using start date: ${moment.unix(startDate).toString()}`);
+      console.log(`- Using end date: ${moment.unix(endDate).toString()}`);
 
-    let endBlock = (await FindBlockAtTimestamp._findBlockNumberAtTimestamp(web3, Number(endDate))).blockNumber;
-    let startBlock = (await FindBlockAtTimestamp._findBlockNumberAtTimestamp(web3, Number(startDate))).blockNumber;
+      endBlock = (await FindBlockAtTimestamp._findBlockNumberAtTimestamp(web3, Number(endDate))).blockNumber;
+      startBlock = (await FindBlockAtTimestamp._findBlockNumberAtTimestamp(web3, Number(startDate))).blockNumber;
+    }
 
     // Fetch gas price data in parallel
     const pricePromises = [];
