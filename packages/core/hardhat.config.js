1af8f479ed3eed53778d6a736286acbed14d501b
Christopher Maree <christopher.maree@gmail.com>
2020-12-15 18:28:32 +0200

    modified core
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/hardhat.config.js
+++ packages/core/hardhat.config.js
@@ -1,3 +1,17 @@
 const { getHardhatConfig } = require("@uma/common");
 
-module.exports = getHardhatConfig();
+const path = require("path");
+const coreWkdir = path.dirname(require.resolve("@uma/core/package.json"));
+const packageWkdir = path.dirname(require.resolve("@uma/core/package.json"));
+
+const configOverride = {
+  paths: {
+    root: coreWkdir,
+    sources: `${coreWkdir}/contracts`,
+    artifacts: `${coreWkdir}/artifacts`,
+    cache: `${coreWkdir}/cache`,
+    tests: `${packageWkdir}/test`
+  }
+};
+
+module.exports = getHardhatConfig(configOverride);
