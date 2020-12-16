b1bd6095c7560a6dd6f9227f811740518505594d
Christopher Maree <christopher.maree@gmail.com>
2020-12-16 11:10:10 +0200

    clean
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/contracts/financial-templates/perpetual-multiparty/PerpetualLiquidatable.sol
+++ packages/core/contracts/financial-templates/perpetual-multiparty/PerpetualLiquidatable.sol
@@ -183,7 +183,7 @@ contract PerpetualLiquidatable is PerpetualPositionManager {
             params.timerAddress
         )
     {
-        require(params.collateralRequirement.isGreaterThan(1), "CR must be more than 100%z");
+        require(params.collateralRequirement.isGreaterThan(1), "CR must be more than 100%");
         require(
             params.sponsorDisputeRewardPct.add(params.disputerDisputeRewardPct).isLessThan(1),
             "Rewards are more than 100%"
