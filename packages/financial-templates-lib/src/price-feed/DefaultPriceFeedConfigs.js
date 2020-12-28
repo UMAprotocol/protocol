cdf5eb448a617878f4c7442d82936cb6ff874fb5
smb2796 <sean.brown2796@gmail.com>
2020-12-28 09:59:58 -0500

    adding default price feed configs for ugas jan feb march

--- packages/financial-templates-lib/src/price-feed/DefaultPriceFeedConfigs.js
+++ packages/financial-templates-lib/src/price-feed/DefaultPriceFeedConfigs.js
@@ -71,6 +71,24 @@ const defaultConfigs = {
       { type: "cryptowatch", exchange: "binance", pair: "BCHBTC" },
       { type: "cryptowatch", exchange: "huobi", pair: "BCHBTC" }
     ]
+  },
+  "GASETH-TWAP-1Mx1M": {
+    type: "uniswap",
+    uniswapAddress: "0x25fb29D865C1356F9e95D621F21366d3a5DB6BB0",
+    twapLength: 7200,
+    lookback: 7200
+  },
+  "GASETH-FEB21": {
+    type: "uniswap",
+    uniswapAddress: "0x4a8a2ea3718964ed0551a3191c30e49ea38a5ade",
+    twapLength: 7200,
+    lookback: 7200
+  },
+  "GASETH-MAR21": {
+    type: "uniswap",
+    uniswapAddress: "0x683ea972ffa19b7bad6d6be0440e0a8465dba71c",
+    twapLength: 7200,
+    lookback: 7200
   }
 };
 
