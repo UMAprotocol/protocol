de5629a8d16e64c3879d1d3b4c3472c64f4d9089
Chris Maree <christopher.maree@gmail.com>
2020-12-30 08:41:22 +0200

    feat(serverless-hub): add retry hub logic for rejected calls (#2335)

--- packages/serverless-orchestration/test/ServerlessHub.js
+++ packages/serverless-orchestration/test/ServerlessHub.js
@@ -223,7 +223,7 @@ contract("ServerlessHub.js", function(accounts) {
     };
 
     const testHubPort = 8082; // create a separate port to run this specific test on.
-    // Create a hub instance with invalid spoke port
+    // Create a hub instance with invalid spoke port. This will force the spoke to reject
     await hub.Poll(
       hubSpyLogger, // injected spy logger
       testHubPort, // port to run the hub for this test on
@@ -234,6 +234,9 @@ contract("ServerlessHub.js", function(accounts) {
     // not a port the spoke is running on. will get rejected
     const rejectedResponse = await sendHubRequest(validBody, testHubPort);
 
+    assert.equal(JSON.parse(rejectedResponse.res.text).output.retriedOutputs.length, 1); // There should be 1 retried output.
+    assert.isTrue(spyLogIncludes(hubSpy, -3, "One or more spoke calls were rejected - Retrying"));
+    assert.isTrue(spyLogIncludes(hubSpy, -3, "retriedOutputs"));
     assert.equal(rejectedResponse.res.statusCode, 500); // error code
     assert.isTrue(rejectedResponse.res.text.includes("Some spoke calls returned errors"));
     assert.isTrue(rejectedResponse.res.text.includes("rejected"));
