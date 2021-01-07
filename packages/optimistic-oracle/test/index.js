03173e2a0abe2f2ee4adfc9e5879df1e5ac82cf7
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-01-07 15:02:04 -0500

    feat(optimistic-oracle-keeper): Add OptimisticOracleClient stub code (#2330)
    * add runner script for proposer package
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * rename package, change comments
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add comment about placeholder async looping method
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * initial release version to changelog
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * initial version 1.0.0
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * remove return true
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add stub for initial OO client
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * comment
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add todo
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * improve(financial-templates-lib): Update @uma/core version to latest and fix tests and clients to correct versions
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * revert lockfile
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add web3 to getTruffleContract
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * move OOClient to finTemplatesLib
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * bump to 1.2.2
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * update test command
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * import ABI's when web3 is available
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * update package.json
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * reverse order of imports
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * revert serverlessOrchestration package.json
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * change core version
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * pass in OO by address
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add todos to address
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * autodetect OptimisticOracle address from deployed network
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * remove comment from OOClient
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * change tests
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * rename ABI_VERSION
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * use dynamic fromBlock in OOClient; re-add logger test to OO/index.js
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add lookback parameter and test it
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/optimistic-oracle/test/index.js
+++ packages/optimistic-oracle/test/index.js
@@ -4,7 +4,16 @@ const Main = require("../index.js");
 // Custom winston transport module to monitor winston log outputs
 const winston = require("winston");
 const sinon = require("sinon");
+
 const { SpyTransport, spyLogLevel, spyLogIncludes } = require("@uma/financial-templates-lib");
+const { getTruffleContract } = require("@uma/core");
+const { addGlobalHardhatTestingAddress } = require("@uma/common");
+
+const CONTRACT_VERSION = "latest";
+
+const OptimisticOracle = getTruffleContract("OptimisticOracle", web3, CONTRACT_VERSION);
+const Finder = getTruffleContract("Finder", web3, CONTRACT_VERSION);
+const Timer = getTruffleContract("Timer", web3, CONTRACT_VERSION);
 
 contract("index.js", function() {
   let spy;
@@ -14,6 +23,18 @@ contract("index.js", function() {
   let errorRetries = 1;
   let errorRetriesTimeout = 0.1; // 100 milliseconds between preforming retries
 
+  let finder;
+  let timer;
+  let optimisticOracle;
+
+  before(async function() {
+    finder = await Finder.new();
+    timer = await Timer.new();
+
+    // Deploy a new OptimisticOracle.
+    optimisticOracle = await OptimisticOracle.new("120", finder.address, timer.address);
+    addGlobalHardhatTestingAddress("OptimisticOracle", optimisticOracle.address);
+  });
   beforeEach(async function() {
     // Create a sinon spy and give it to the SpyTransport as the winston logger. Use this to check all winston logs.
     spy = sinon.spy(); // Create a new spy for each test.
@@ -43,10 +64,10 @@ contract("index.js", function() {
       assert.notStrictEqual(spyLogLevel(spy, i), "error");
     }
 
-    // Logger should emit two logs: one indicating the start of the runner, and one about the early termination of the
-    // of the loop because the pollingDelay is set to 0.
-    assert.strictEqual(spy.getCalls().length, 2);
+    // The first log should indicate that the OO-Keeper runner started successfully
+    // and auto detected the OO's deployed address.
     assert.isTrue(spyLogIncludes(spy, 0, "OO keeper started"));
-    assert.isTrue(spyLogIncludes(spy, 1, "End of serverless execution loop - terminating process"));
+    assert.isTrue(spyLogIncludes(spy, 0, optimisticOracle.address));
+    assert.isTrue(spyLogIncludes(spy, spy.callCount - 1, "End of serverless execution loop - terminating process"));
   });
 });
