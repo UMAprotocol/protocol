7900d5fb98906549351767f4bf468a924e9f0b46
Nick Pai <npai.nyc@gmail.com>
2021-01-12 16:03:16 -0700

    Update ServerlessHub.js
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/serverless-orchestration/test/ServerlessHub.js
+++ packages/serverless-orchestration/test/ServerlessHub.js
@@ -124,8 +124,8 @@ contract("ServerlessHub.js", function(accounts) {
     defaultUniswapPricefeedConfig = {
       type: "uniswap",
       uniswapAddress: uniswap.address,
-      twapLength: 1,
-      lookback: 1
+      twapLength: 2,
+      lookback: 7200
     };
 
     // Set two uniswap prices to give it a little history.
