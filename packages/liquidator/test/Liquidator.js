013063fd452e12c9dc8acba82786d0efd79a70df
Christopher Maree <christopher.maree@gmail.com>
2021-01-21 13:01:44 +0200

    small nit
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/liquidator/test/Liquidator.js
+++ packages/liquidator/test/Liquidator.js
@@ -91,11 +91,11 @@ const versionedIt = function(supportedVersions, shouldBeItOnly = false) {
 
 const runTestForContractVersion = function(supportedVersions) {
   // Validate that the array of supportedVersions provided is in the SUPPORTED_CONTRACT_VERSIONS OR is any.
-  // if ([...SUPPORTED_CONTRACT_VERSIONS, "any"].filter(value => supportedVersions.includes(value)).length == 0) {
-  //   throw new Error(
-  //     `Contract versioned specified ${supportedVersions} is not part of the supported contracts for this test suit`
-  //   );
-  // }
+  if ([...SUPPORTED_CONTRACT_VERSIONS, "any"].filter(value => supportedVersions.includes(value)).length == 0) {
+    throw new Error(
+      `Contract versioned specified ${supportedVersions} is not part of the supported contracts for this test suit`
+    );
+  }
   // Return if the `currentTestIterationVersion` is part of the supported versions includes any. Returning true
   // means that test will be run. Else, if returned false, the test will be skipped.
   return supportedVersions.includes(currentTestIterationVersion) || supportedVersions.includes("any");
