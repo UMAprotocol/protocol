f434f13c9578a11a7aafa020bb3c808da6eeb0e2
John Shutt <pemulis@users.noreply.github.com>
2021-01-25 08:54:46 -0800

    improve(node-lts): update circleci and docker config to use the latest lts version of nodejs (#2455)
    * improve(node-v15): update circleci and docker config to use latest node version
    
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>
    
    * improve(node-lts): use lts instead of 15 in node version
    
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>
    
    * docs(node-lts): update node version in readme
    
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>
    
    * improve(node-lts): update node version in ci install shell script
    
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>

--- ci/install_node_npm.sh
+++ ci/install_node_npm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -sSL "https://nodejs.org/dist/v12.16.2/node-v12.16.2-linux-x64.tar.xz" | sudo tar --strip-components=2 -xJ -C /usr/local/bin/ node-v12.16.2-linux-x64/bin/node
+curl -sSL "https://nodejs.org/dist/v14.15.4/node-v14.15.4-linux-x64.tar.xz" | sudo tar --strip-components=2 -xJ -C /usr/local/bin/ node-v12.16.2-linux-x64/bin/node
 curl https://www.npmjs.com/install.sh | sudo bash
