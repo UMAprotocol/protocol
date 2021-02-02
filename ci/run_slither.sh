b07c904fe9202e3963d02b2fc3102655aa517629
Chris Maree <christopher.maree@gmail.com>
2021-02-02 09:44:05 +0200

    Restore run_slither.sh

--- ci/run_slither.sh
+++ ci/run_slither.sh
@@ -11,11 +11,10 @@ run_slither() {
     cp -r ../../node_modules/@openzeppelin ./node_modules/@openzeppelin
 
     cd $PROTOCOL_DIR
-    pwd
 
     # print out slither version for debugging
     slither --version
-    slither --exclude=divide-before-multiply,unused-return,timestamp,naming-convention,pragma,solc-version,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,incorrect-equality,reentrancy-events,assembly,uninitialized-local --filter-paths="@openzeppelin|WETH9.sol|test" .
+    slither --exclude=divide-before-multiply,unused-return,timestamp,naming-convention,pragma,solc-version,external-function,reentrancy-benign,reentrancy-no-eth,arbitrary-send,incorrect-equality,reentrancy-events,assembly,uninitialized-local --filter-paths="@openzeppelin|WETH9.sol|test" $1
 }
 
 run_slither $PROTOCOL_DIR/packages/core
