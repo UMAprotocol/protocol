d63e1c9a8ba820e4a886276d689cbd65df0af7d4
Matt Rice <matthewcrice32@gmail.com>
2021-02-08 02:05:31 -0500

    feat(price-feeds): add ExpressionPriceFeed
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

---
+++ packages/financial-templates-lib/src/price-feed/ExpressionPriceFeed.js
@@ -0,0 +1,117 @@
+const assert = require("assert");
+const { PriceFeedInterface } = require("./PriceFeedInterface");
+const Web3 = require("web3");
+const { create, all } = require("mathjs");
+const math = create(all, {
+  number: "BigNumber",
+  precision: 100
+});
+
+// Gets balancer spot and historical prices. This price feed assumes that it is returning
+// prices as 18 decimals of precision, so it will scale up the pool's price as reported by Balancer contracts
+// if the user specifies that the Balancer contract is returning non-18 decimal precision prices.
+class ExpressionPriceFeed extends PriceFeedInterface {
+  constructor(priceFeedMap, expression) {
+    super();
+    this.expressionCode = math.parse(expression).compile();
+    this.priceFeedMap = priceFeedMap;
+  }
+
+  async getHistoricalPrice(time) {
+    const historicalPrices = {};
+    const errors = [];
+    await Promise.all(
+      Object.entries(this.priceFeedMap).map(async ([name, pf]) => {
+        const price = await pf.getHistoricalPrice(time).catch(err => errors.push(err));
+        historicalPrices[name] = this._convertToDecimal(price);
+      })
+    );
+
+    if (errors.length > 0) {
+      throw errors;
+    }
+
+    return this._convertToFixed(this.expressionCode.evaluate(historicalPrices));
+  }
+
+  getLastUpdateTime() {
+    const lastUpdateTimes = Object.values(this.priceFeedMap).map(pf => pf.getLastUpdateTime());
+
+    // If any constituents returned an invalid value, bubble it up.
+    if (lastUpdateTimes.some(time => time === null || time === undefined)) {
+      return null;
+    }
+
+    // Take the max.
+    return Math.max(...lastUpdateTimes);
+  }
+
+  getLookback() {
+    const lookbacks = Object.values(this.priceFeedMap).map(priceFeed => priceFeed.getLookback());
+
+    // If any constituents returned an invalid value, bubble it up.
+    if (lookbacks.some(lookback => lookback === undefined || lookback === null)) {
+      return null;
+    }
+
+    // Take the min since the overall lookback will be the min.
+    return Math.min(...lookbacks);
+  }
+
+  getCurrentPrice() {
+    const prices = {};
+    const errors = [];
+    Object.entries(this.priceFeedMap).map(async ([name, pf]) => {
+      try {
+        const price = pf.getCurrentPrice();
+        assert(price !== undefined && price !== null, "Valid price must be returned");
+        prices[name] = this._convertToDecimal(price);
+      } catch (err) {
+        errors.push(err);
+      }
+    });
+
+    if (errors.length > 0) return null;
+
+    return this._convertToFixed(this.expressionCode.evaluate(prices));
+  }
+
+  getPriceFeedDecimals() {
+    const decimalArray = Object.values(this.priceFeedMap).map(pf => pf.getPriceFeedDecimals());
+    const decimalsValue = decimalArray[0];
+    assert(decimalsValue, "Invalid decimals value");
+    assert(
+      decimalArray.every(decimals => decimals === decimalArray[0]),
+      "Constituent price feeds do not have matching decimals"
+    );
+    return decimalsValue;
+  }
+
+  async update() {
+    // Update all constituent price feeds.
+    await Promise.all(Object.values(this.priceFeedMap).map(pf => pf.update()));
+  }
+
+  // Takes a BN fixed point number and converts it to a math.bignumber decimal number that the math library can handle.
+  _convertToDecimal(price) {
+    const decimals = math.bignumber(this.getPriceFeedDecimals());
+    const decimalsMultiplier = math.bignumber(10).pow(decimals);
+    return math.bignumber(price.toString()).div(decimalsMultiplier);
+  }
+
+  // Takes a math.bignumber number and converts it to a fixed point number that's expected outside this library.
+  _convertToFixed(price) {
+    const decimals = math.bignumber(this.getPriceFeedDecimals());
+    const decimalsMultiplier = math.bignumber(10).pow(decimals);
+    return Web3.utils.toBN(
+      price
+        .mul(decimalsMultiplier)
+        .round()
+        .toString()
+    );
+  }
+}
+
+module.exports = {
+  ExpressionPriceFeed
+};
---
+++ packages/financial-templates-lib/test/price-feed/ExpressionPriceFeed.js
@@ -0,0 +1,141 @@
+const { toWei, toBN } = web3.utils;
+
+const { ExpressionPriceFeed } = require("../../src/price-feed/ExpressionPriceFeed");
+const { PriceFeedMock } = require("../../src/price-feed/PriceFeedMock");
+
+contract("ExpressionPriceFeed.js", function() {
+  it("Update", async function() {
+    const priceFeedMap = {
+      ETHUSD: new PriceFeedMock(),
+      BTCUSD: new PriceFeedMock()
+    };
+
+    const expressionPriceFeed = new ExpressionPriceFeed(priceFeedMap, "ETHUSD + BTCUSD");
+    await expressionPriceFeed.update();
+    await expressionPriceFeed.update();
+
+    assert.equal(priceFeedMap.ETHUSD.updateCalled, 2);
+    assert.equal(priceFeedMap.BTCUSD.updateCalled, 2);
+  });
+
+  it("Basic expression", async function() {
+    const priceFeedMap = {
+      //                        currentPrice      historicalPrice    lastUpdatedTime   decimals   lookback
+      ETHUSD: new PriceFeedMock(toBN(toWei("1")), toBN(toWei("25")), 100, 18, 500),
+      BTCUSD: new PriceFeedMock(toBN(toWei("2")), toBN(toWei("57")), 50000, 18, 4000),
+      COMPUSD: new PriceFeedMock(toBN(toWei("9")), toBN(toWei("10")), 25, 18, 50000)
+    };
+
+    const expressionPriceFeed = new ExpressionPriceFeed(priceFeedMap, "ETHUSD + BTCUSD + COMPUSD");
+
+    // Should return the sum of the current prices.
+    assert.equal(expressionPriceFeed.getCurrentPrice(), toWei("12"));
+
+    // Should return the summed historical price (because we're using mocks, the timestamp doesn't matter).
+    const arbitraryHistoricalTimestamp = 1000;
+    assert.equal(await expressionPriceFeed.getHistoricalPrice(arbitraryHistoricalTimestamp), toWei("92"));
+
+    // Should return the *maximum* lastUpdatedTime.
+    assert.equal(expressionPriceFeed.getLastUpdateTime(), 50000);
+
+    // Should return the min lookback.
+    assert.equal(expressionPriceFeed.getLookback(), 500);
+  });
+
+  it("Complex expression", async function() {
+    const priceFeedMap = {
+      //                        currentPrice      historicalPrice    lastUpdatedTime   decimals
+      ETHUSD: new PriceFeedMock(toBN(toWei("1")), toBN(toWei("25")), 100, 18),
+      BTCUSD: new PriceFeedMock(toBN(toWei("2")), toBN(toWei("57")), 50000, 18),
+      COMPUSD: new PriceFeedMock(toBN(toWei("9")), toBN(toWei("10")), 25, 18)
+    };
+
+    const expressionPriceFeed = new ExpressionPriceFeed(
+      priceFeedMap,
+      "(ETHUSD + COMPUSD) * (BTCUSD + COMPUSD) / 7.321"
+    );
+
+    // Should return the sum of the current prices.
+    // (1 + 9) * (2 + 9) / 7.321 = 15.025269771889086190 (rounded to 18 decimals).
+    assert.equal(expressionPriceFeed.getCurrentPrice(), toWei("15.025269771889086190"));
+
+    // Should return the summed historical price (because we're using mocks, the timestamp doesn't matter).
+    const arbitraryHistoricalTimestamp = 1000;
+    // (25 + 10) * (57 + 10) / 7.321 = 320.311432864362791968 (rounded to 18 decimals).
+    assert.equal(
+      await expressionPriceFeed.getHistoricalPrice(arbitraryHistoricalTimestamp),
+      toWei("320.311432864362791968")
+    );
+  });
+
+  it("sub-pricefeeds fail to return price", async function() {
+    const priceFeeds = {
+      //                currentPrice      historicalPrice    lastUpdatedTime
+      ETHUSD: new PriceFeedMock(toBN(toWei("1")), toBN(toWei("17")), 100),
+      BTCUSD: new PriceFeedMock(null, null, null),
+      COMPUSD: new PriceFeedMock(null, null, null)
+    };
+
+    const expressionPriceFeed = new ExpressionPriceFeed(priceFeeds, "ETHUSD * BTCUSD * COMPUSD");
+
+    // Should return null since there was a null price output.
+    assert.equal(expressionPriceFeed.getCurrentPrice(), null);
+
+    // Should throw an error for each null price output.
+    const arbitraryHistoricalTimestamp = 1000;
+    await expressionPriceFeed.getHistoricalPrice(arbitraryHistoricalTimestamp).then(
+      () => assert.fail(),
+      err => {
+        assert.equal(err[0].message, "PriceFeedMock expected error thrown");
+        assert.equal(err[1].message, "PriceFeedMock expected error thrown");
+        assert.equal(err.length, 2);
+      }
+    );
+
+    // Should return null since there was a null input.
+    assert.equal(expressionPriceFeed.getLastUpdateTime(), null);
+  });
+
+  it("undefined inputs", async function() {
+    const priceFeeds = {
+      //                currentPrice      historicalPrice    lastUpdatedTime
+      ETHUSD: new PriceFeedMock(toBN(toWei("1")), toBN(toWei("17")), 100),
+      BTCUSD: new PriceFeedMock(undefined, undefined, undefined)
+    };
+
+    const expressionPriceFeed = new ExpressionPriceFeed(priceFeeds, "ETHUSD * BTCUSD * COMPUSD");
+
+    // Should return null since there was an undefined price output.
+    assert.equal(expressionPriceFeed.getCurrentPrice(), null);
+
+    // Should throw since there was an undefined price output.
+    const arbitraryHistoricalTimestamp = 1000;
+    assert.isTrue(await expressionPriceFeed.getHistoricalPrice(arbitraryHistoricalTimestamp).catch(() => true));
+
+    // Should return null since there was an undefined output.
+    assert.equal(expressionPriceFeed.getLastUpdateTime(), null);
+  });
+
+  it("Validates feeds decimals correctly", async function() {
+    const validDecimalsPriceFeeds = {
+      //                        currentPrice      historicalPrice    lastUpdatedTime   decimals
+      ETHUSD: new PriceFeedMock(toBN(toWei("1")), toBN(toWei("25")), 100, 18),
+      BTCUSD: new PriceFeedMock(toBN(toWei("2")), toBN(toWei("57")), 50000, 18, 4000),
+      COMPUSD: new PriceFeedMock(toBN(toWei("9")), toBN(toWei("10")), 25, 18, 50000)
+    };
+
+    const validExpressionPriceFeed = new ExpressionPriceFeed(validDecimalsPriceFeeds, "ETHUSD * BTCUSD * COMPUSD");
+    assert.equal(validExpressionPriceFeed.getPriceFeedDecimals(), 18);
+
+    // Create three feeds, one with a diffrent number of decimals. Medianizer should reject this when checking the decimals.
+    const inValidDecimalsPriceFeeds = {
+      //                        currentPrice      historicalPrice    lastUpdatedTime   decimals
+      ETHUSD: new PriceFeedMock(toBN(toWei("1")), toBN(toWei("25")), 100, 18),
+      BTCUSD: new PriceFeedMock(toBN(toWei("2")), toBN(toWei("57")), 50000, 18),
+      COMPUSD: new PriceFeedMock(toBN(toWei("9")), toBN(toWei("10")), 25, 17)
+    };
+
+    const invalidExpressionPriceFeed = new ExpressionPriceFeed(inValidDecimalsPriceFeeds, "ETHUSD * BTCUSD * COMPUSD");
+    assert.throws(() => invalidExpressionPriceFeed.getPriceFeedDecimals());
+  });
+});
