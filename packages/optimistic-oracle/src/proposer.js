51ef3069c22b75df97ca6bdcac02cc71dced5621
Nick Pai <npai.nyc@gmail.com>
2021-02-09 10:27:03 -0500

    respond to chris' comments
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/optimistic-oracle/src/proposer.js
+++ packages/optimistic-oracle/src/proposer.js
@@ -65,69 +65,6 @@ class OptimisticOracleProposer {
     await this._setAllowances();
   }
 
-  // Sets allowances for all collateral currencies used in unproposed price requests
-  async _setAllowances() {
-    // TODO: Should we do this also for currencies from undisputedProposals?
-    for (let priceRequest of this.optimisticOracleClient.getUnproposedPriceRequests()) {
-      // The OO requires approval to transfer the proposed price request's collateral currency in order to post a bond.
-      // We'll set this once to the max value and top up whenever the bot's allowance drops below MAX_INT / 2.
-      const collateralToken = new this.web3.eth.Contract(getAbi("ExpandedERC20"), priceRequest.currency);
-      const [currentCollateralAllowance] = await Promise.all([
-        collateralToken.methods.allowance(this.account, this.ooContract.options.address).call()
-      ]);
-      if (this.toBN(currentCollateralAllowance).lt(this.toBN(MAX_UINT_VAL).div(this.toBN("2")))) {
-        const collateralApprovalTx = await collateralToken.methods
-          .approve(this.ooContract.options.address, MAX_UINT_VAL)
-          .send({
-            from: this.account,
-            gasPrice: this.gasEstimator.getCurrentFastPrice()
-          });
-        this.logger.info({
-          at: "OptimisticOracle#Proposer",
-          message: "Approved OO to transfer unlimited collateral tokens 💰",
-          currency: collateralToken.options.address,
-          collateralApprovalTx: collateralApprovalTx.transactionHash
-        });
-      }
-    }
-  }
-
-  // Create the pricefeed for a specific identifier and save it to the state, or
-  // return the saved pricefeed if already constructed.
-  async _createOrGetCachedPriceFeed(identifier) {
-    // First check for cached pricefeed for this identifier and return it if exists:
-    let priceFeed = this.priceFeedCache[identifier];
-    if (priceFeed) return priceFeed;
-
-    // No cached pricefeed found for this identifier. Create a new one.
-    // First, construct the config for this identifier. We start with the `defaultPriceFeedConfig`
-    // properties and add custom properties for this specific identifier such as precision.
-    let priceFeedConfig = {
-      ...this.defaultPriceFeedConfig,
-      priceFeedDecimals: getPrecisionForIdentifier(identifier)
-    };
-    this.logger.debug({
-      at: "OptimisticOracleProposer",
-      message: "Created pricefeed configuration for identifier",
-      defaultPriceFeedConfig: this.priceFeedConfig,
-      identifier
-    });
-
-    // Create a new pricefeed for this identifier. We might consider caching these price requests
-    // for re-use if any requests use the same identifier.
-    let newPriceFeed = await createReferencePriceFeedForEmp(
-      this.logger,
-      this.web3,
-      new Networker(this.logger),
-      () => Math.round(new Date().getTime() / 1000),
-      null, // No EMP Address needed since we're passing identifier explicitly
-      priceFeedConfig,
-      identifier
-    );
-    this.priceFeedCache[identifier] = newPriceFeed;
-    return newPriceFeed;
-  }
-
   // Submit proposals to unproposed price requests.
   async sendProposals() {
     this.logger.debug({
@@ -136,7 +73,7 @@ class OptimisticOracleProposer {
     });
 
     for (let priceRequest of this.optimisticOracleClient.getUnproposedPriceRequests()) {
-      let priceFeed = await this._createOrGetCachedPriceFeed(priceRequest.identifier);
+      const priceFeed = await this._createOrGetCachedPriceFeed(priceRequest.identifier);
 
       // Pricefeed is either constructed correctly or is null.
       if (!priceFeed) {
@@ -172,7 +109,8 @@ class OptimisticOracleProposer {
         proposalPrice
       );
 
-      // Simple version of inventory management: simulate the transaction and assume that if it fails, the caller didn't have enough collateral.
+      // Simple version of inventory management: simulate the transaction and assume that if it fails,
+      // the caller didn't have enough collateral.
       let proposalBond, gasEstimation;
       try {
         [proposalBond, gasEstimation] = await Promise.all([
@@ -243,6 +181,75 @@ class OptimisticOracleProposer {
 
   async settleRequests() {
     // TODO
+  }
+
+  /** **********************************
+   *
+   * INTERNAL METHODS
+   *
+   ************************************/
+
+  // Sets allowances for all collateral currencies used in unproposed price requests
+  async _setAllowances() {
+    // TODO: Should we do this also for currencies from undisputedProposals?
+    for (let priceRequest of this.optimisticOracleClient.getUnproposedPriceRequests()) {
+      // The OO requires approval to transfer the proposed price request's collateral currency in order to post a bond.
+      // We'll set this once to the max value and top up whenever the bot's allowance drops below MAX_INT / 2.
+      const collateralToken = new this.web3.eth.Contract(getAbi("ExpandedERC20"), priceRequest.currency);
+      const [currentCollateralAllowance] = await Promise.all([
+        collateralToken.methods.allowance(this.account, this.ooContract.options.address).call()
+      ]);
+      if (this.toBN(currentCollateralAllowance).lt(this.toBN(MAX_UINT_VAL).div(this.toBN("2")))) {
+        const collateralApprovalTx = await collateralToken.methods
+          .approve(this.ooContract.options.address, MAX_UINT_VAL)
+          .send({
+            from: this.account,
+            gasPrice: this.gasEstimator.getCurrentFastPrice()
+          });
+        this.logger.info({
+          at: "OptimisticOracle#Proposer",
+          message: "Approved OO to transfer unlimited collateral tokens 💰",
+          currency: collateralToken.options.address,
+          collateralApprovalTx: collateralApprovalTx.transactionHash
+        });
+      }
+    }
+  }
+
+  // Create the pricefeed for a specific identifier and save it to the state, or
+  // return the saved pricefeed if already constructed.
+  async _createOrGetCachedPriceFeed(identifier) {
+    // First check for cached pricefeed for this identifier and return it if exists:
+    let priceFeed = this.priceFeedCache[identifier];
+    if (priceFeed) return priceFeed;
+
+    // No cached pricefeed found for this identifier. Create a new one.
+    // First, construct the config for this identifier. We start with the `defaultPriceFeedConfig`
+    // properties and add custom properties for this specific identifier such as precision.
+    let priceFeedConfig = {
+      ...this.defaultPriceFeedConfig,
+      priceFeedDecimals: getPrecisionForIdentifier(identifier)
+    };
+    this.logger.debug({
+      at: "OptimisticOracleProposer",
+      message: "Created pricefeed configuration for identifier",
+      defaultPriceFeedConfig: this.priceFeedConfig,
+      identifier
+    });
+
+    // Create a new pricefeed for this identifier. We might consider caching these price requests
+    // for re-use if any requests use the same identifier.
+    let newPriceFeed = await createReferencePriceFeedForEmp(
+      this.logger,
+      this.web3,
+      new Networker(this.logger),
+      () => Math.round(new Date().getTime() / 1000),
+      null, // No EMP Address needed since we're passing identifier explicitly
+      priceFeedConfig,
+      identifier
+    );
+    this.priceFeedCache[identifier] = newPriceFeed;
+    return newPriceFeed;
   }
 }
 
