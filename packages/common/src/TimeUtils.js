6afefb62598767eeec92a0249baf726f246239aa
Matt Rice <matthewcrice32@gmail.com>
2021-02-09 11:02:47 -0500

    feat(financial-templates-lib): add BlockFinder (#2512)

--- packages/common/src/TimeUtils.js
+++ packages/common/src/TimeUtils.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 /**
  * @notice Return average block-time for a period.
  */
-const averageBlockTimeSeconds = async (/* lookbackSeconds */) => {
+async function averageBlockTimeSeconds(/* lookbackSeconds */) {
   // TODO: Call an external API to get this data. Currently this value is a hard-coded estimate
   // based on the data from https://etherscan.io/chart/blocktime. ~13.5 seconds has been the average
   // since April 2016, although this value seems to spike periodically for a relatively short period of time.
@@ -15,7 +15,7 @@ const averageBlockTimeSeconds = async (/* lookbackSeconds */) => {
   } else {
     return defaultBlockTimeSeconds;
   }
-};
+}
 
 // Sets fromBlock to the value of an environment variable if one is set. This can be set to 0 to make tests work with Ganache, or any other value needed for a production script or bot.
 async function getFromBlock(web3) {
@@ -29,7 +29,19 @@ async function getFromBlock(web3) {
   }
 }
 
+/**
+ * @notice Estimates the blocks elapsed over a certain number of seconds.
+ * @param seconds the number of seconds.
+ * @param cushionPercentage the percentage to add to the number as a cushion.
+ */
+async function estimateBlocksElapsed(seconds, cushionPercentage = 0.0) {
+  const cushionMultiplier = cushionPercentage + 1.0;
+  const averageBlockTime = await averageBlockTimeSeconds();
+  return Math.floor((seconds * cushionMultiplier) / averageBlockTime);
+}
+
 module.exports = {
   averageBlockTimeSeconds,
-  getFromBlock
+  getFromBlock,
+  estimateBlocksElapsed
 };
