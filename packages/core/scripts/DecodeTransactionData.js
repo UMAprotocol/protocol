64eb6bc73d3f5ac30fe5f6f298d3f3559e863a17
Nick Pai <npai.nyc@gmail.com>
2021-02-11 12:45:50 -0500

    improve(decode-transaction-data): Recursively print txn data
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/scripts/DecodeTransactionData.js
+++ packages/core/scripts/DecodeTransactionData.js
@@ -1,17 +1,32 @@
 // This script allows you to decode a transaction data blob revealing the method that the transaction is calling and
 // the parameters.
 // Example:
-// $(npm bin)/truffle exec --network test ./scripts/DecodeTransactionData.js --data 0x10a7e2014554482f55534400000000000000000000000000000000000000000000000000
+// yarn truffle exec --network test ./scripts/DecodeTransactionData.js --data 0x10a7e2014554482f55534400000000000000000000000000000000000000000000000000
 
 const { getAbiDecoder } = require("@uma/common");
 
 const argv = require("minimist")(process.argv.slice(), { string: ["data"] });
 
-function run(data) {
+function _decodeData(data) {
   return getAbiDecoder().decodeMethod(data);
 }
 
-const decodeTransactionData = async function(callback) {
+const _printTransactionDataRecursive = function(txnObj) {
+  // If transaction is a proposal then recursively print out its transactions
+  if (txnObj.name === "propose" && txnObj.params.transactions.length > 0) {
+    console.group(`Transaction is a proposal containing ${txnObj.params.transactions.length} transactions:`);
+    txnObj.params.transactions.forEach(_txn => {
+      const decodedTxnData = _decodeData(_txn.data);
+      _printTransactionDataRecursive(decodedTxnData);
+    });
+    console.groupEnd();
+  } else {
+    // Pretty print:
+    console.log(`${JSON.stringify(txnObj, null, 4)}`);
+  }
+};
+
+const decodeTransactionData = function(callback) {
   try {
     if (!argv.data) {
       callback("You must provide the transaction data using the --data argument, e.g. --data 0x1234");
@@ -19,17 +34,16 @@ const decodeTransactionData = async function(callback) {
       callback("The --data argument must be a hex string starting with `0x`, e.g. --data 0x1234");
     }
 
-    const txnObj = run(argv.data);
+    const txnData = _decodeData(argv.data);
 
-    if (!txnObj) {
+    if (!txnData) {
       console.log(
         "Could not identify the method that this transaction is calling.",
         "Are you sure it corresponds to a contract in the UMAprotocol/protocol repository?"
       );
     } else {
-      // Pretty print.
       console.log("Your decoded transaction information:");
-      console.log(JSON.stringify(txnObj, null, 4));
+      _printTransactionDataRecursive(txnData);
     }
   } catch (e) {
     // Forces the script to return a nonzero error code so failure can be detected in bash.
@@ -40,5 +54,4 @@ const decodeTransactionData = async function(callback) {
   callback();
 };
 
-decodeTransactionData.run = run;
 module.exports = decodeTransactionData;
