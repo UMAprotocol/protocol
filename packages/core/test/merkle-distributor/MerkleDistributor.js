0dd346de82dc5f447afb1a5e0ea03ca3885b3900
Christopher Maree <christopher.maree@gmail.com>
2021-02-25 15:50:47 +0200

    Added more changes
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/test/merkle-distributor/MerkleDistributor.js
+++ packages/core/test/merkle-distributor/MerkleDistributor.js
@@ -1,15 +1,19 @@
 const { MerkleTree } = require("../../../merkle-distributor/src/merkleTree");
 
-const SampleMerklePayouts = require("./SampleMerklePayout.json");
+const SamplePayouts = require("./SamplePayout.json");
 
-const { toBN, toWei } = web3.utils;
-const { MAX_UINT_VAL } = require("@uma/common");
+const { toBN, toWei, utf8ToHex } = web3.utils;
+const { MAX_UINT_VAL, didContractThrow } = require("@uma/common");
 
 // Tested Contract
 const MerkleDistributor = artifacts.require("MerkleDistributor");
 const Timer = artifacts.require("Timer");
 const Token = artifacts.require("ExpandedERC20");
 
+let merkleDistributor, timer, rewardToken, rewardRecipients, merkleTree, rewardLeafs, leaf, claimerProof;
+
+// For a recipeint object, create the leaf to be part of the merkle tree. The leaf is simply a hash of the concatenation
+// of all fields within the payout object for the recipient.
 const createLeaf = recipient => {
   // The recipient must contain all the keys to correctly generate the leaf hash. If anything is undefined we'll have nonsensical problems.
   assert.isTrue(
@@ -29,9 +33,24 @@ const createLeaf = recipient => {
   );
 };
 
+// Generate payouts to be used in tests using the SamplePayouts file.
+const createRewardRecipientsFromSampleData = (SamplePayouts, windowIndex, windowStart, windowEnd) => {
+  return Object.keys(SamplePayouts.exampleRecipients).map(recipientAddress => {
+    return {
+      account: recipientAddress,
+      amount: SamplePayouts.exampleRecipients[recipientAddress].amount,
+      metaData: SamplePayouts.exampleRecipients[recipientAddress].metaData,
+      windowIndex,
+      windowStart,
+      windowEnd,
+      rewardToken: rewardToken.address
+    };
+  });
+};
+
 contract("ExpiringMultiParty", function(accounts) {
   let contractCreator = accounts[0];
-  let merkleDistributor, timer, rewardToken;
+  let rando = accounts[1];
 
   beforeEach(async () => {
     timer = await Timer.deployed();
@@ -47,8 +66,7 @@ contract("ExpiringMultiParty", function(accounts) {
       const rewardAmount = toBN(toWei("100"));
       // Create a an array of reward recipients. Each object within the array represents the payout for one account. The
       // metaData is an arbitrary string that can be appended to each recipient to add additional information about the payouts.
-
-      let rewardRecipients = [
+      rewardRecipients = [
         {
           account: accounts[3],
           amount: rewardAmount.muln(1).toString(),
@@ -82,26 +100,26 @@ contract("ExpiringMultiParty", function(accounts) {
       });
 
       // Generate leafs for each recipient. This is simply the hash of each component of the payout from above.
-      const rewardLeafs = rewardRecipients.map(item => ({ ...item, leaf: createLeaf(item) }));
+      rewardLeafs = rewardRecipients.map(item => ({ ...item, leaf: createLeaf(item) }));
 
       // Build the merkle tree from an array of hashes from each recipient.
-      const vestingMerkleTree = new MerkleTree(rewardLeafs.map(item => item.leaf));
+      merkleTree = new MerkleTree(rewardLeafs.map(item => item.leaf));
 
       // Seed the merkleDistributor with the root of the tree and additional information.
-      await rewardToken.approve(merkleDistributor.address, MAX_UINT_VAL);
+      await rewardToken.approve(merkleDistributor.address, MAX_UINT_VAL, { from: contractCreator });
       await merkleDistributor.setWindowMerkleRoot(
         windowIndex,
         toWei("600"),
         currentTime,
         currentTime,
         rewardToken.address,
-        vestingMerkleTree.getRoot()
+        merkleTree.getRoot()
       );
 
       // A member of the tree should now be able to claim rewards.
-      const leaf = rewardLeafs[0];
+      leaf = rewardLeafs[0];
       const claimerBalanceBefore = await rewardToken.balanceOf(leaf.account);
-      const claimerProof = vestingMerkleTree.getProof(leaf.leaf);
+      claimerProof = merkleTree.getProof(leaf.leaf);
 
       // Claim the rewards, providing the information needed to re-build the tree & verify the proof.
       await merkleDistributor.claimWindow(leaf.windowIndex, leaf.account, leaf.amount, leaf.metaData, claimerProof);
@@ -113,13 +131,78 @@ contract("ExpiringMultiParty", function(accounts) {
     });
   });
   describe("Single window", function() {
+    // For each test in the single window, load in the SampleMerlePayouts, generate a tree and set it in the distributor.
     beforeEach(async function() {
-      SampleMerklePayouts;
+      const windowIndex = 0;
+      const currentTime = await timer.getCurrentTime();
+
+      rewardRecipients = createRewardRecipientsFromSampleData(SamplePayouts, windowIndex, currentTime, currentTime);
+
+      // Generate leafs for each recipient. This is simply the hash of each component of the payout from above.
+      rewardLeafs = rewardRecipients.map(item => ({ ...item, leaf: createLeaf(item) }));
+      merkleTree = new MerkleTree(rewardLeafs.map(item => item.leaf));
+
+      // Seed the merkleDistributor with the root of the tree and additional information.
+      await rewardToken.approve(merkleDistributor.address, MAX_UINT_VAL, { from: contractCreator });
+      await merkleDistributor.setWindowMerkleRoot(
+        windowIndex,
+        SamplePayouts.totalRewardsDistributed,
+        currentTime,
+        currentTime,
+        rewardToken.address,
+        merkleTree.getRoot()
+      );
+
+      leaf = rewardLeafs[0];
+      claimerProof = merkleTree.getProof(leaf.leaf);
     });
-    it("Can claim to a another EOA", async function() {});
-    it("Can not double claim rewards", async function() {});
-    it("Can not claim rewards if not part of the tree", async function() {});
-    it("Can not claim rewards with invalid data", async function() {});
+    it("Can claim rewards on another EOA's behalf", async function() {
+      // Can correctly claim on the EOAs behalf.
+      const claimerBalanceBefore = await rewardToken.balanceOf(leaf.account);
+      await merkleDistributor.claimWindow(leaf.windowIndex, leaf.account, leaf.amount, leaf.metaData, claimerProof, {
+        from: rando
+      });
+      // The EOA balance should have increased by the amount of the reward.
+      assert.equal(
+        (await rewardToken.balanceOf(leaf.account)).toString(),
+        claimerBalanceBefore.add(toBN(leaf.amount)).toString()
+      );
+    });
+    it("Can not double claim rewards", async function() {
+      // Claim rewards for the EOA.
+      await merkleDistributor.claimWindow(leaf.windowIndex, leaf.account, leaf.amount, leaf.metaData, claimerProof, {
+        from: rando
+      });
+      // Can not re-claim rewards for the EOA.
+      assert(
+        await didContractThrow(
+          merkleDistributor.claimWindow(leaf.windowIndex, leaf.account, leaf.amount, leaf.metaData, claimerProof, {
+            from: rando
+          })
+        )
+      );
+    });
+    it("Can not claim rewards if not part of the tree", async function() {
+      // Can not claim the recipient rewards as your own. Set the account in the claim component to `rando`, using
+      // the rest of the valid proof.
+      assert(
+        await didContractThrow(
+          merkleDistributor.claimWindow(leaf.windowIndex, rando, leaf.amount, leaf.metaData, claimerProof, {
+            from: rando
+          })
+        )
+      );
+    });
+    it("Can not claim rewards with invalid data", async function() {
+      const invalidProof = [utf8ToHex("0x")];
+      assert(
+        await didContractThrow(
+          merkleDistributor.claimWindow(leaf.windowIndex, leaf.account, leaf.amount, leaf.metaData, invalidProof, {
+            from: rando
+          })
+        )
+      );
+    });
     it("Can not claim rewards with invalid proof", async function() {});
   });
 
