cceb966c949831ac549c4996045ebc463a6db0e6
John Shutt <john.d.shutt@gmail.com>
2021-03-02 11:46:51 -0800

    improve(script): simply encoding script
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>

--- packages/core/scripts/local/EncodeParams.js
+++ packages/core/scripts/local/EncodeParams.js
@@ -2,15 +2,16 @@
  * @notice The purpose of this script is to encode ExpiringMultiParty parameters.
  *
  * Spinning up mainnet fork: ganache-cli --fork https://mainnet.infura.io/v3/5f56f0a4c8844c96a430fbd3d7993e39 --unlock 0x2bAaA41d155ad8a4126184950B31F50A1513cE25 --unlock 0x7a3a1c2de64f20eb5e916f40d11b01c441b2a8dc --port 9545
- * Example: yarn truffle exec ./packages/core/scripts/local/EncodeParams.js --cversion latest --network mainnet-fork
+ * Example: yarn truffle exec ./packages/core/scripts/local/EncodeParams.js --cversion latest --address 0x5A7f8F8B0E912BBF8525bc3fb2ae46E70Db9516B --network mainnet-fork
  */
 const { toWei } = web3.utils;
 const { ZERO_ADDRESS } = require("@uma/common");
 const { getAbi, getTruffleContract } = require("../../index");
 const argv = require("minimist")(process.argv.slice(), {
-  string: ["cversion"]
+  string: ["cversion", "address"]
 });
 const abiVersion = argv.cversion || "latest"; // Default to most recent mainnet deployment, 1.2.2.
+const address = argv.address || "0x0";
 
 // Deployed contract ABI's and addresses we need to fetch.
 const ExpiringMultiPartyCreator = getTruffleContract("ExpiringMultiPartyCreator", web3, abiVersion);
@@ -27,6 +28,8 @@ const encodeParams = async callback => {
     const finder = await Finder.deployed();
     const tokenFactory = await TokenFactory.deployed();
 
+    const emp = await ExpiringMultiParty.at(address);
+
     // Replace these with the EMP constructor params you want to encode
     let constructorParams = {
       expirationTimestamp: "1619812800",
@@ -40,41 +43,28 @@ const encodeParams = async callback => {
       disputerDisputeRewardPercentage: { rawValue: toWei("0.2") },
       minSponsorTokens: { rawValue: toWei("1") },
       liquidationLiveness: 7200,
-      withdrawalLiveness: 7200
+      withdrawalLiveness: 7200,
+      finderAddress: finder.address,
+      tokenFactoryAddress: tokenFactory.address,
+      timerAddress: await expiringMultiPartyCreator.timerAddress()
     };
 
     // Inject constructor params neccessary for "latest" version of the EMPCreator:
     if (abiVersion === "latest") {
       constructorParams = {
         ...constructorParams,
-        financialProductLibraryAddress: ZERO_ADDRESS
-      };
-    }
-
-    const emp = await ExpiringMultiParty.at("0x5A7f8F8B0E912BBF8525bc3fb2ae46E70Db9516B");
-
-    let empConstructorParams = {
-      ...constructorParams,
-      finderAddress: finder.address,
-      tokenFactoryAddress: tokenFactory.address,
-      timerAddress: await expiringMultiPartyCreator.timerAddress()
-    };
-
-    // Grab `tokenAddress` from newly constructed EMP and add to `empConstructorParams` for new EMP's
-    if (abiVersion === "latest") {
-      empConstructorParams = {
-        ...empConstructorParams,
+        financialProductLibraryAddress: ZERO_ADDRESS,
         tokenAddress: await emp.tokenCurrency()
       };
     }
 
     const encodedParameters = web3.eth.abi.encodeParameters(getAbi("ExpiringMultiParty", abiVersion)[0].inputs, [
-      empConstructorParams
+      constructorParams
     ]);
 
     // Done!
     console.log("Encoded EMP Parameters", encodedParameters);
-    console.table(empConstructorParams);
+    console.table(constructorParams);
   } catch (err) {
     console.error(err);
     callback(err);
