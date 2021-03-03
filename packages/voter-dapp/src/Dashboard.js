f95baa2319986cfee12b9b1e01013cfec3c074d2
Matt Rice <matthewcrice32@gmail.com>
2021-03-03 15:26:08 -0500

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/voter-dapp/src/Dashboard.js
+++ packages/voter-dapp/src/Dashboard.js
@@ -8,12 +8,14 @@ import DesignatedVotingTransfer from "./DesignatedVotingTransfer.js";
 import DesignatedVoting from "@uma/core/build/contracts/DesignatedVoting.json";
 import RetrieveRewards from "./RetrieveRewards.js";
 import { drizzleReactHooks } from "@umaprotocol/react-plugin";
+import { formatWithMaxDecimals, formatWei } from "@uma/common";
 
 function Dashboard() {
   const { drizzle, useCacheCall } = drizzleReactHooks.useDrizzle();
   const { account } = drizzleReactHooks.useDrizzleState(drizzleState => ({
     account: drizzleState.accounts[0]
   }));
+  const { web3 } = drizzle;
 
   const addressZero = "0x0000000000000000000000000000000000000000";
 
@@ -21,6 +23,23 @@ function Dashboard() {
   const { designatedVoting } = drizzleReactHooks.useDrizzleState(drizzleState => ({
     designatedVoting: drizzleState.contracts[deployedDesignatedVotingAddress]
   }));
+
+  const { shouldShowBanner, oldDesignatedVotingAddress, balance } = useCacheCall(
+    ["OldDesignatedVotingFactory", "VotingToken"],
+    call => {
+      if (!account) return {};
+      const oldDesignatedVotingAddress = call("OldDesignatedVotingFactory", "designatedVotingContracts", account);
+      if (!oldDesignatedVotingAddress) return {};
+      const balance = call("VotingToken", "balanceOf", oldDesignatedVotingAddress);
+      if (!oldDesignatedVotingBalance) return {};
+
+      const balanceBN = web3.utils.toBN(oldDesignatedVotingBalance.toString());
+      const shouldShowBanner = !balanceBN.isZero();
+      const formattedBalance = formatWithMaxDecimals(formatWei(balance, web3), 2, 4, false);
+
+      return { shouldShowBanner, oldDesignatedVotingAddress, oldDesignatedVotingBalance: formattedbalance };
+    }
+  );
 
   // We only want to run `drizzle.addContract` once, even if a change deep inside the `drizzle` object retriggers this
   // `useEffect`.
