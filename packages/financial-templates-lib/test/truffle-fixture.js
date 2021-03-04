1afac5286d47a78642fec298938bb5abd93ca35f
Nick Pai <npai.nyc@gmail.com>
2021-03-04 08:57:55 -0500

    add truffle-fixture to financial-templates-lib, move FinancialContractsFactoryEventClient to hardhat tests
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/financial-templates-lib/test/truffle-fixture.js
+++ packages/financial-templates-lib/test/truffle-fixture.js
@@ -1,3 +1,2 @@
 // Export the truffle-fixture file from `/core/test` so that we can use the `.deployed()` syntax in our tests.
-const truffleFixture = require("@uma/core/test/truffle-fixture");
-module.exports = truffleFixture;
+module.exports = require("@uma/core/test/truffle-fixture");
