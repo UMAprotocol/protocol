22df51755a98fa81efdec043ab079ae4d093f0b5
Nick Pai <npai.nyc@gmail.com>
2021-03-04 09:32:37 -0500

    move funding-rate-proposer/index.js to hardhat
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/funding-rate-proposer/test/index.js
+++ packages/funding-rate-proposer/test/index.js
@@ -2,7 +2,7 @@
 // `Error: ExpiringMultiPartyCreator contains unresolved libraries. You must deploy and link the following libraries before
 //         you can deploy a new version of ExpiringMultiPartyCreator: $585a446ef18259666e65e81865270bd4dc$`
 // We should look more into library linking via hardhat within a script: https://hardhat.org/plugins/hardhat-deploy.html#handling-contract-using-libraries
-const Main = require("../../index.js");
+const Main = require("../index.js");
 
 const winston = require("winston");
 const sinon = require("sinon");
@@ -10,11 +10,10 @@ const sinon = require("sinon");
 const { toWei, utf8ToHex } = web3.utils;
 
 const { SpyTransport, spyLogIncludes, spyLogLevel } = require("@uma/financial-templates-lib");
-const { interfaceName, RegistryRolesEnum } = require("@uma/common");
+const { interfaceName, RegistryRolesEnum, addGlobalHardhatTestingAddress } = require("@uma/common");
 const { getTruffleContract } = require("@uma/core");
 
 const PerpetualCreator = getTruffleContract("PerpetualCreator", web3);
-const TokenFactory = getTruffleContract("TokenFactory", web3);
 const Finder = getTruffleContract("Finder", web3);
 const Store = getTruffleContract("Store", web3);
 const IdentifierWhitelist = getTruffleContract("IdentifierWhitelist", web3);
@@ -33,7 +32,6 @@ contract("index.js", function(accounts) {
   let timer;
   let identifierWhitelist;
   let collateralWhitelist;
-  let tokenFactory;
   let registry;
   let collateral;
   let perpsCreated = [];
@@ -66,32 +64,36 @@ contract("index.js", function(accounts) {
     timelockLiveness: 86400, // 1 day
     rewardRatePerSecond: { rawValue: toWei("0.000001") },
     proposerBondPercentage: { rawValue: toWei("0.0001") },
-    maxFundingRate: { rawValue: toWei("0.00001") },
-    minFundingRate: { rawValue: toWei("-0.00001") },
+    maxFundingRate: { rawValue: toWei("1") },
+    minFundingRate: { rawValue: toWei("-1") },
     proposalTimePastLimit: 1800
   };
+  // The TEST identifier will map to a PriceFeedMock, which requires the following
+  // config fields to be set to construct a price feed properly:
+  let commonPriceFeedConfig = { currentPrice: "1", historicalPrice: "1" };
 
   before(async function() {
-    finder = await Finder.new();
-    timer = await Timer.new();
-    tokenFactory = await TokenFactory.new();
-    perpFactory = await PerpetualCreator.new(finder.address, tokenFactory.address, timer.address);
+    finder = await Finder.deployed();
+    timer = await Timer.deployed();
+    perpFactory = await PerpetualCreator.deployed();
+    // Set the address in the global name space to enable proposer's index.js to access it.
+    addGlobalHardhatTestingAddress("PerpetualCreator", perpFactory.address);
 
     // Whitelist an initial identifier so we can deploy.
-    identifierWhitelist = await IdentifierWhitelist.new();
+    identifierWhitelist = await IdentifierWhitelist.deployed();
     await identifierWhitelist.addSupportedIdentifier(defaultCreationParams.priceFeedIdentifier);
     await identifierWhitelist.addSupportedIdentifier(defaultCreationParams.fundingRateIdentifier);
     await finder.changeImplementationAddress(utf8ToHex(interfaceName.IdentifierWhitelist), identifierWhitelist.address);
 
-    store = await Store.new({ rawValue: "0" }, { rawValue: "0" }, timer.address);
+    store = await Store.deployed();
     await finder.changeImplementationAddress(utf8ToHex(interfaceName.Store), store.address);
 
     // Add Registry to finder so factories can register contracts.
-    registry = await Registry.new();
+    registry = await Registry.deployed();
     await registry.addMember(RegistryRolesEnum.CONTRACT_CREATOR, perpFactory.address);
     await finder.changeImplementationAddress(utf8ToHex(interfaceName.Registry), registry.address);
 
-    collateralWhitelist = await AddressWhitelist.new();
+    collateralWhitelist = await AddressWhitelist.deployed();
     await finder.changeImplementationAddress(utf8ToHex(interfaceName.CollateralWhitelist), collateralWhitelist.address);
 
     // Use the same collateral for all perps.
@@ -110,6 +112,17 @@ contract("index.js", function(accounts) {
       from: deployer
     });
     perpsCreated.push({ transaction: perpCreation, address: perpAddress });
+    // This is the time that the funding rate applier's update time is initialized to:
+    let contractStartTime = await timer.getCurrentTime();
+
+    // Set the pricefeed's latestUpdateTime to be +1 second from the funding rate applier's
+    // initialized update time, otherwise any proposals will fail because the new proposal timestamp
+    // must always be > than the last update time.
+    let lastUpdateTime = contractStartTime.toNumber() + 1;
+    commonPriceFeedConfig = { ...commonPriceFeedConfig, lastUpdateTime };
+    // Advance the perpetual contract forward in time to match pricefeed's update time, otherwise
+    // the proposal will fail because it is "in the future".
+    await timer.setCurrentTime(lastUpdateTime);
   });
   it("Completes one iteration without logging any errors", async function() {
     // We will create a new spy logger, listening for debug events because success logs are tagged with the
@@ -126,7 +139,8 @@ contract("index.js", function(accounts) {
       perpetualAddress: perpsCreated[0].address,
       pollingDelay,
       errorRetries,
-      errorRetriesTimeout
+      errorRetriesTimeout,
+      commonPriceFeedConfig
     });
 
     for (let i = 0; i < spy.callCount; i++) {
---
+++ packages/funding-rate-proposer/test/truffle-fixture.js
@@ -0,0 +1,2 @@
+// Export the truffle-fixture file from `/core/test` so that we can use the `.deployed()` syntax in our tests.
+module.exports = require("@uma/core/test/truffle-fixture");
