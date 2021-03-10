66c7dc322d549452fde77636f40c653155be8d5b
Christopher Maree <christopher.maree@gmail.com>
2021-03-10 11:33:53 +0200

    fixed broken export
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/merkle-distributor/src/index.ts
+++ packages/merkle-distributor/src/index.ts
@@ -1,7 +1,5 @@
-// TODO(CM): can't export cloudflare helper from index.ts because anyone using any other module in this file would be
-// required to set ENV variables. MerkleDistributorHelper imports CloudflareKVHelper so it has a similar restriction.
-// export * from "./CloudflareKVHelper";
-// export * from "./MerkleDistributorHelper";
+export * from "./CloudflareKVHelper";
+export * from "./MerkleDistributorHelper";
 export * from "./IpfsHelper";
 import MerkleTree from "./MerkleTree";
 export { MerkleTree };
