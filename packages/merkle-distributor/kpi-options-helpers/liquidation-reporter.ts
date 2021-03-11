d880726599068af636771b916173baab51038ada
Christopher Maree <christopher.maree@gmail.com>
2021-03-11 21:52:50 +0200

    updated reporter
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/merkle-distributor/kpi-options-helpers/liquidation-reporter.ts
+++ packages/merkle-distributor/kpi-options-helpers/liquidation-reporter.ts
@@ -6,6 +6,8 @@ const { toWei, toBN, fromWei } = web3.utils;
 
 const { Networker, createReferencePriceFeedForFinancialContract } = require("@uma/financial-templates-lib");
 
+const fixedPointAdjustment = toBN(toWei("1"));
+
 import { getAllEmpsPositions } from "./emp-position-helper";
 
 import { fetchAllFinancialContractsData, evaluateFinancialContractCollateral } from "./calculate-uma-tvl";
@@ -17,6 +19,19 @@ export async function fetchUmaEcosystemData() {
 
   const allEmpPositions = await getAllEmpsPositions();
   console.log("allEmpPositions", allEmpPositions[2].positions);
+  const sponsorsSubset = allEmpPositions[2].positions;
+  console.log("subset", sponsorsSubset[0]);
+  console.log("c", sponsorsSubset[0].collateral);
+  console.log("z", toBN(toWei(sponsorsSubset[0].collateral.toString()).toString()));
+  console.log(
+    "liquidationPrice",
+    calculateLiquidationPrice(
+      toBN(toWei(sponsorsSubset[0].collateral)),
+      toBN(toWei(sponsorsSubset[0].tokensOutstanding)),
+      toBN(toWei("1.25")),
+      true
+    )
+  );
 
   const logger = winston.createLogger({
     level: "info",
@@ -32,11 +47,16 @@ export async function fetchUmaEcosystemData() {
     "0x1c3f1a342c8d9591d9759220d114c685fd1cf6b8"
   );
   await samplePriceFeed.update();
+  console.log("inverted keys", Object.keys(samplePriceFeed));
+  console.log("inverted", samplePriceFeed.priceFeeds[0].invertPrice);
   console.log("CURRENT", samplePriceFeed.getCurrentPrice().toString());
+  console.log("decimals", samplePriceFeed.getPriceFeedDecimals());
 }
 
-function calculateLiquidationPrice(collateral: BigNumber, debt: BigNumber, collateralRequirement: BigNumber) {
-  return collateral.dividedBy(debt.times(collateralRequirement));
+function calculateLiquidationPrice(collateral: any, debt: any, collateralRequirement: any, invertedPrice: boolean) {
+  console.log(collateral, debt, collateralRequirement, invertedPrice);
+  if (!invertedPrice) return collateral.mul(fixedPointAdjustment).div(debt.mul(collateralRequirement));
+  else return fixedPointAdjustment.mul(fixedPointAdjustment).div(collateral.div(debt.mul(collateralRequirement)));
 }
 
 fetchUmaEcosystemData().catch(e => {
