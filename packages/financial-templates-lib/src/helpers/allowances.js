a146220b4b6994e15841dc18f916019e19341f77
Nick Pai <npai.nyc@gmail.com>
2021-03-17 11:35:19 -0400

    fix per chris suggestion
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/financial-templates-lib/src/helpers/allowances.js
+++ packages/financial-templates-lib/src/helpers/allowances.js
@@ -1,8 +1,8 @@
-const { MAX_SAFE_ALLOWANCE } = require("@uma/common");
+const { MAX_SAFE_ALLOWANCE, MAX_UINT_VAL } = require("@uma/common");
 const { getAbi } = require("@uma/core");
 
-// Sets `owner` allowance for `spender` to MAX_ALLOWANCE, unless `spender` already has
-// an allowance > 1/2 of the MAX_ALLOWANCE. Return successful approval transaction data, or undefined
+// Sets `owner` allowance for `spender` to MAX_UINT_VAL, unless `spender` already has
+// an allowance > MAX_SAFE_ALLOWANCE. Return successful approval transaction data, or undefined
 // for skipped approvals.
 const setAllowance = async (web3, gasEstimator, ownerAddress, spenderAddress, currencyAddress) => {
   const { toBN } = web3.utils;
@@ -10,9 +10,9 @@ const setAllowance = async (web3, gasEstimator, ownerAddress, spenderAddress, cu
   // Increase `perpetualAddress` allowance to MAX for the collateral @ `currencyAddress`
   const collateralToken = new web3.eth.Contract(getAbi("ExpandedERC20"), currencyAddress);
   const currentCollateralAllowance = await collateralToken.methods.allowance(ownerAddress, spenderAddress).call();
-  if (toBN(currentCollateralAllowance).lt(toBN(MAX_SAFE_ALLOWANCE).div(toBN("2")))) {
+  if (toBN(currentCollateralAllowance).lt(toBN(MAX_SAFE_ALLOWANCE))) {
     return {
-      tx: await collateralToken.methods.approve(spenderAddress, MAX_SAFE_ALLOWANCE).send({
+      tx: await collateralToken.methods.approve(spenderAddress, MAX_UINT_VAL).send({
         from: ownerAddress,
         gasPrice: gasEstimator.getCurrentFastPrice()
       }),
