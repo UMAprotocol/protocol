37395a793bcf3ab9163dee229080657df8cf0007
Nick Pai <npai.nyc@gmail.com>
2021-03-17 11:28:07 -0400

    fix(allowances): Change default allowance value
    UNI token's `approve()` method constrains max allowance to 2^96-1 because their contract uses `uint96` for internal balance and allowance tracking. Presumably this is due to gas optimizations. This max value is about 1/3 less of the MAX_UINT_VAL value, but is large enough that it should work for almost all other collateral types.
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/financial-templates-lib/src/helpers/allowances.js
+++ packages/financial-templates-lib/src/helpers/allowances.js
@@ -1,17 +1,18 @@
-const { MAX_UINT_VAL } = require("@uma/common");
+const { MAX_SAFE_ALLOWANCE } = require("@uma/common");
 const { getAbi } = require("@uma/core");
 
-// Sets `owner` allowance for `spender` to MAX, unless `spender` already has an allowance > 1/2 of the MAX_UINT.
-// Return successful approval transaction data, or undefined for skipped approvals.
+// Sets `owner` allowance for `spender` to MAX_ALLOWANCE, unless `spender` already has
+// an allowance > 1/2 of the MAX_ALLOWANCE. Return successful approval transaction data, or undefined
+// for skipped approvals.
 const setAllowance = async (web3, gasEstimator, ownerAddress, spenderAddress, currencyAddress) => {
   const { toBN } = web3.utils;
 
   // Increase `perpetualAddress` allowance to MAX for the collateral @ `currencyAddress`
   const collateralToken = new web3.eth.Contract(getAbi("ExpandedERC20"), currencyAddress);
   const currentCollateralAllowance = await collateralToken.methods.allowance(ownerAddress, spenderAddress).call();
-  if (toBN(currentCollateralAllowance).lt(toBN(MAX_UINT_VAL).div(toBN("2")))) {
+  if (toBN(currentCollateralAllowance).lt(toBN(MAX_SAFE_ALLOWANCE).div(toBN("2")))) {
     return {
-      tx: await collateralToken.methods.approve(spenderAddress, MAX_UINT_VAL).send({
+      tx: await collateralToken.methods.approve(spenderAddress, MAX_SAFE_ALLOWANCE).send({
         from: ownerAddress,
         gasPrice: gasEstimator.getCurrentFastPrice()
       }),
