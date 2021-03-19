7ae9417be7aaf4d697b2946c9a7dc05a6a912b7d
Christopher Maree <christopher.maree@gmail.com>
2021-03-19 09:11:49 +0200

    Added implementation
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

---
+++ packages/reporters/liquidation-reporter/index.ts
@@ -0,0 +1,15 @@
+const { fetchUmaEcosystemData } = require("./liquidation-reporter");
+const { createExcelSheetFromLiquidationDrawDownData } = require("./excel-writer");
+
+async function fetchDataAndWriteToExcelFile() {
+  const ecosystemData = await fetchUmaEcosystemData();
+  createExcelSheetFromLiquidationDrawDownData(ecosystemData);
+}
+
+fetchDataAndWriteToExcelFile()
+  .then(() => {
+    setTimeout(function() {
+      process.exit(0);
+    }, 2000);
+  })
+  .catch(() => {});
