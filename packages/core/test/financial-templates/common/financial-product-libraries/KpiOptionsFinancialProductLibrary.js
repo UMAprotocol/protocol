9cb649ab97b5d688f1fd5b9bbb648c26b5c705c1
smb2796 <sean.brown2796@gmail.com>
2021-03-24 17:01:22 -0400

    Add price transformation logic and tests

--- packages/core/test/financial-templates/common/financial-product-libraries/KpiOptionsFinancialProductLibrary.js
+++ packages/core/test/financial-templates/common/financial-product-libraries/KpiOptionsFinancialProductLibrary.js
@@ -30,24 +30,65 @@ contract("KpiOptionsFinancialProductLibrary", function() {
       timer.address
     );
   });
+  describe("price transformation", () => {
+    it("Library returns 2 price if before expiration", async () => {
+      // Calling the transformation function through the emp mock.
+      assert.equal(
+        (
+          await expiringMultiParty.transformPrice(
+            { rawValue: toWei("1") },
+            (await expiringMultiParty.getCurrentTime()).toString()
+          )
+        ).toString(),
+        toWei("2")
+      );
+
+      // Calling the transformation function as a mocked emp caller should also work.
+      assert.equal(
+        (
+          await expiringMultiParty.transformPrice.call(
+            { rawValue: toWei("1") },
+            (await expiringMultiParty.getCurrentTime()).toString(),
+            { from: expiringMultiParty.address }
+          )
+        ).toString(),
+        toWei("2")
+      );
+    });
+
+    it("Library returns correctly transformed price after expiration", async () => {
+      await timer.setCurrentTime(expirationTime + 1);
+
+      // If transformPrice is called after expiration, no transformation should occur.
+      assert.equal(
+        (
+          await expiringMultiParty.transformPrice(
+            { rawValue: toWei("0.2") },
+            (await expiringMultiParty.getCurrentTime()).toString()
+          )
+        ).toString(),
+        "0.2"
+      );
+    });
+  });
 
   describe("Collateralization ratio transformation", () => {
     it("Library returns correctly transformed collateralization ratio", async () => {
-      // Under all scenarios, the collateral requirement of the contract should be 2.
+      // Under all scenarios, the collateral requirement of the contract should be 1.
 
       // Check pre-expiration
       assert.equal(
         (await expiringMultiParty.transformCollateralRequirement({ rawValue: toWei("0.1") })).toString(),
-        toWei("2")
+        toWei("1")
       );
 
-      // advance the timer after expiration and ensure the CR is still 2.
+      // advance the timer after expiration and ensure the CR is still 1.
       await timer.setCurrentTime(expirationTime + 1);
 
       // Check post-expiration
       assert.equal(
         (await expiringMultiParty.transformCollateralRequirement({ rawValue: toWei("0.2") })).toString(),
-        toWei("2")
+        toWei("1")
       );
     });
   });
