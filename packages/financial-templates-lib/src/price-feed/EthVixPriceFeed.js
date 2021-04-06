368f2b76ba4765502befb410d8f30a59a3d80e2d
Chris Cashwell <ccashwell@gmail.com>
2021-04-06 14:40:36 -0400

    Return price in wei units per DVM requirements
    Signed-off-by: Chris Cashwell <ccashwell@gmail.com>

--- packages/financial-templates-lib/src/price-feed/EthVixPriceFeed.js
+++ packages/financial-templates-lib/src/price-feed/EthVixPriceFeed.js
@@ -8,27 +8,39 @@ class ETHVIXPriceFeed extends PriceFeedInterface {
   /**
    * @notice Constructs price feeds for indexes listed on dVIX.io.
    * @param {Object} logger Winston module used to send logs.
+   * @param {Object} web3 Provider from truffle instance to connect to Ethereum network.
    * @param {Boolean} inverse Whether to return the short/inverse result.
    * @param {Object} networker Used to send the API requests.
    * @param {Function} getTime Returns the current time.
    * @param {Integer} minTimeBetweenUpdates Min number of seconds between updates. If update() is called again before
    *      this number of seconds has passed, it will be a no-op.
+   * @param {Number} priceFeedDecimals Number of priceFeedDecimals to use to convert price to wei.
    */
-  constructor(logger, inverse, networker, getTime, minTimeBetweenUpdates = 60) {
+  constructor(logger, web3, inverse, networker, getTime, minTimeBetweenUpdates = 60, priceFeedDecimals = 18) {
     super();
     this.logger = logger;
+    this.web3 = web3;
     this.inverse = inverse;
     this.networker = networker;
     this.getTime = getTime;
     this.minTimeBetweenUpdates = minTimeBetweenUpdates;
+    this.priceFeedDecimals = priceFeedDecimals;
 
     this.uuid = `dVIX.${inverse ? "iethVIX" : "ethVIX"}`;
     this.historicalPrices = [];
+
+    this.toBN = this.web3.utils.toBN;
+
+    this.convertPriceFeedDecimals = number => {
+      // Converts the decimal price result to a BigNumber integer scaled to wei units.
+      // Note: Must ensure that `number` has no more decimal places than `priceFeedDecimals`.
+      return this.toBN(parseFixed(number.toString().substring(0, priceFeedDecimals), priceFeedDecimals).toString());
+    };
   }
 
   getCurrentPrice() {
     assert(this.lastUpdateTime, `${this.uuid}: undefined lastUpdateTime. Update required.`);
-    return this.currentPrice;
+    return this.convertPriceFeedDecimals(this.currentPrice);
   }
 
   async getHistoricalPrice(time) {
@@ -48,7 +60,7 @@ class ETHVIXPriceFeed extends PriceFeedInterface {
     const result = this.historicalPrices.find(price => roundedTime.isSame(price.timestamp));
     assert(result, `${this.uuid}: No cached result found for timestamp: ${roundedTime.toISOString()}`);
 
-    return parseFixed(result.vix, 6);
+    return this.convertPriceFeedDecimals(result.vix);
   }
 
   getLastUpdateTime() {
@@ -92,8 +104,8 @@ class ETHVIXPriceFeed extends PriceFeedInterface {
     // [
     //   {
     //     "timestamp": "2021-03-24T15:00:00.000Z",
-    //     "iVix": 142.4470728085134,
-    //     "vix": 70.20151276427174,
+    //     "iVix": "142.44",
+    //     "vix": "70.20",
     //     ...
     //   },
     //   ...
@@ -105,7 +117,7 @@ class ETHVIXPriceFeed extends PriceFeedInterface {
     // 4. Store results.
     this.lastUpdateTime = currentTime;
     this.historicalPrices = [...this.historicalPrices, ...response];
-    this.currentPrice = parseFixed(this.inverse ? mostRecent.iVix : mostRecent.vix, 6);
+    this.currentPrice = this.inverse ? mostRecent.iVix : mostRecent.vix;
   }
 }
 
