f27096ea3adc32f5f6911c03c19616653cb10648
Nick Pai <npai.nyc@gmail.com>
2021-04-21 11:52:41 -0400

    simplify tests
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/test/chainbridge/voting.js
+++ packages/core/test/chainbridge/voting.js
@@ -6,7 +6,7 @@ const TruffleAssert = require("truffle-assertions");
 const Ethers = require("ethers");
 
 const Helpers = require("./helpers");
-const { interfaceName, didContractThrow } = require("@uma/common");
+const { didContractThrow } = require("@uma/common");
 
 // Chainbridge Contracts:
 const BridgeContract = artifacts.require("Bridge");
@@ -14,12 +14,9 @@ const GenericHandlerContract = artifacts.require("GenericHandler");
 
 // UMA DVM Contracts:
 const MockOracle = artifacts.require("MockOracleAncillary");
-const OptimisticOracle = artifacts.require("OptimisticOracle");
 const IdentifierWhitelist = artifacts.require("IdentifierWhitelist");
 const Finder = artifacts.require("Finder");
 const Timer = artifacts.require("Timer");
-const Token = artifacts.require("ExpandedERC20");
-const CollateralWhitelist = artifacts.require("AddressWhitelist");
 
 const { utf8ToHex, padRight, hexToUtf8, toWei } = web3.utils;
 
@@ -44,124 +41,86 @@ contract("GenericHandler - [UMA Cross-chain Voting]", async accounts => {
   // DVM contracts:
   let voting;
   let votingSidechain;
-  let optimisticOracle;
   let identifierWhitelist;
   let finder;
   let timer;
-  let collateralToken;
-  let collateralWhitelist;
 
   // Test variables
-  let requestTime; // Time that will be used as the price request timestamp
-  let ancillaryData;
+  const ancillaryData = utf8ToHex("Test Ancillary Data");
   const identifier = utf8ToHex("Test Identifier");
   const requestPrice = toWei("1");
+  const requestTime = Date.now();
 
   // These are the functions that the GenericHandler will be calling.
-  let votingGetPriceFuncSig;
   let votingPushPriceFuncSig;
   let votingRequestPriceFuncSig;
-  let optimisticOracleRequestPriceFuncSig;
 
   // Resource ID's are unique for each contract address.
   let votingResourceId;
   let votingResourceSidechainId;
-  let optimisticOracleResourceId;
 
   beforeEach(async () => {
     // Duplicate contracts on both chain (represented for convenience in this test as a singleton contract).
     finder = await Finder.deployed();
     timer = await Timer.deployed();
     identifierWhitelist = await IdentifierWhitelist.deployed();
-    collateralWhitelist = await CollateralWhitelist.deployed();
-    collateralToken = await Token.new("Wrapped Ether", "WETH", 18);
 
     // Mainnet bridge variables:
     bridgeMainnet = await BridgeContract.new(chainId, initialRelayers, relayerThreshold, 0, 100);
     voting = await MockOracle.new(finder.address, timer.address);
     votingResourceId = Helpers.createResourceID(voting.address, chainId);
-    votingGetPriceFuncSig = Helpers.getFunctionSignature(voting, "getPrice");
-    optimisticOracle = await OptimisticOracle.new(7200, finder.address, timer.address);
-    optimisticOracleRequestPriceFuncSig = Helpers.getFunctionSignature(optimisticOracle, "requestPrice");
-    optimisticOracleResourceId = Helpers.createResourceID(optimisticOracle.address, chainId);
+    votingRequestPriceFuncSig = Helpers.getFunctionSignature(voting, "requestPrice");
 
     // Sidechain bridge variables:
     bridgeSidechain = await BridgeContract.new(sidechainId, initialRelayers, relayerThreshold, 0, 100);
     votingSidechain = await MockOracle.new(finder.address, timer.address);
     votingResourceSidechainId = Helpers.createResourceID(votingSidechain.address, sidechainId);
     votingPushPriceFuncSig = Helpers.getFunctionSignature(votingSidechain, "pushPrice");
-    votingRequestPriceFuncSig = Helpers.getFunctionSignature(votingSidechain, "requestPrice");
 
     // Configure contracts such that price requests will succeed:
     await identifierWhitelist.addSupportedIdentifier(identifier);
-    await collateralWhitelist.addToWhitelist(collateralToken.address);
-    // Note: Need to point Finder to Voting for any network that the OptimisticOracle runs on.
-    await finder.changeImplementationAddress(utf8ToHex(interfaceName.Oracle), voting.address);
-    requestTime = (await optimisticOracle.getCurrentTime()).toNumber() - 10;
-    ancillaryData = collateralToken.address;
 
-    // Set up Handlers: Should specify a contract address and function to call for each resource ID:
-    // - Mainnet handler maps functions to Voting and OptimisticOracle contracts.
-    // - Sidechain handler maps functions to Voting contract.
+    // Set up Handlers: Should specify a contract address and function to call for each resource ID.
     genericHandlerMainnet = await GenericHandlerContract.new(
       bridgeMainnet.address,
-      [votingResourceId, optimisticOracleResourceId],
-      [voting.address, optimisticOracle.address],
-      [votingGetPriceFuncSig, Helpers.blankFunctionSig],
-      [Helpers.blankFunctionSig, optimisticOracleRequestPriceFuncSig]
+      [votingResourceId],
+      [voting.address],
+      [Helpers.blankFunctionSig],
+      [votingRequestPriceFuncSig]
     );
     genericHandlerSidechain = await GenericHandlerContract.new(
       bridgeSidechain.address,
       [votingResourceSidechainId],
       [votingSidechain.address],
-      [votingRequestPriceFuncSig],
+      [Helpers.blankFunctionSig],
       [votingPushPriceFuncSig]
     );
 
     // Sidechain resource ID 1: Voting Contract
-    // - Deposit: Should check if requestPrice() reverts.
+    // - Deposit: null
     // - ExecuteProposal: Should push price.
     await bridgeSidechain.adminSetGenericResource(
       genericHandlerSidechain.address,
       votingResourceSidechainId,
       votingSidechain.address,
-      // Note: Its conceivable that we don't even need to call `requestPrice` on the Voting contract for the sidechain
-      // deposit. Instead, we can just treat the sidechain's `deposit` method as a way to trigger the off-chain
-      // relayer to begin the process of requesting a price from the Mainnet Voting contract.
-      votingRequestPriceFuncSig,
+      Helpers.blankFunctionSig,
       votingPushPriceFuncSig
     );
-    // Mainnet resource ID 1: Optimistic Oracle Contract
-    // - Deposit: null
-    // - ExecuteProposal: Should requestPrice() to OptimisticOracle contract.
-    await bridgeMainnet.adminSetGenericResource(
-      genericHandlerMainnet.address,
-      optimisticOracleResourceId,
-      optimisticOracle.address,
-      Helpers.blankFunctionSig,
-      optimisticOracleRequestPriceFuncSig
-    );
-    // Mainnet resource ID 2: Voting Contract
-    // - Deposit: Should check if getPrice() reverts.
-    // - ExecuteProposal: null
+    // Mainnet resource ID 1: Voting Contract
+    // - Deposit: null.
+    // - ExecuteProposal: Should request price.
     await bridgeMainnet.adminSetGenericResource(
       genericHandlerMainnet.address,
       votingResourceId,
       voting.address,
-      votingGetPriceFuncSig,
-      Helpers.blankFunctionSig
+      Helpers.blankFunctionSig,
+      votingRequestPriceFuncSig
     );
   });
 
-  // Scenario: Sidechain contract needs a price from the sidechain oracle:
-  it("Sidechain deposit: Handler should check if Voting.requestPrice reverts", async function() {
-    // Deposit succeeds on Sidechain, meaning that the identifier is whitelisted.
-    // Note: Deposit data is needed to call requestPrice() on the Voting contract.
-    const encodedMetaDataDeposit = Helpers.abiEncode(
-      ["bytes32", "uint256", "bytes"],
-      [padRight(identifier, 64), requestTime, ancillaryData]
-    );
-    const depositData = Helpers.createGenericDepositData(encodedMetaDataDeposit);
+  // Scenario: Sidechain contract needs a price from the sidechain oracle.
+  it("Sidechain deposit: emits Deposit event", async function() {
+    const depositData = Helpers.createGenericDepositData(null);
     const depositTxn = await bridgeSidechain.deposit(sidechainId, votingResourceSidechainId, depositData, {
       from: depositerAddress
     });
@@ -175,34 +134,22 @@ contract("GenericHandler - [UMA Cross-chain Voting]", async accounts => {
         event.resourceID.toLowerCase() === votingResourceSidechainId.toLowerCase() &&
         event.depositNonce.toString() === expectedDepositNonce.toString()
     );
-
-    // Handler on sidechain correctly requested a price to the Voting contract.
-    const internalDepositTx = await TruffleAssert.createTransactionResult(votingSidechain, depositTxn.tx);
-    TruffleAssert.eventEmitted(
-      internalDepositTx,
-      "PriceRequestAdded",
-      event =>
-        event.requester.toLowerCase() === genericHandlerSidechain.address.toLowerCase() &&
-        hexToUtf8(event.identifier) === hexToUtf8(identifier) &&
-        event.time.toString() === requestTime.toString() &&
-        event.ancillaryData.toLowerCase() === ancillaryData.toLowerCase()
-    );
   });
 
   // Scenario: Off-chain relayer sees that sidechain Bridge emitted a Deposit event, relayer should now
-  // vote to execute a proposal on the mainnet Bridge, which should ultimately make a price request to the Optimistic
-  // Oracle.
-  it("Mainnet executeProposal: Handler should call OptimisticOracle.requestPrice", async function() {
-    // Note: Deposit proposal data is needed to call requestPrice() on the OptimisticOracle mainnet contract.
+  // vote to execute a proposal on the mainnet Bridge, which should ultimately make a price request to the Mainnet
+  // oracle.
+  it("Mainnet executeProposal: Handler should call Voting.requestPrice", async function() {
+    // Note: Deposit proposal data is needed to call requestPrice() on the Voting mainnet contract.
     const encodedMetaDataProposal = Helpers.abiEncode(
-      ["bytes32", "uint256", "bytes", "address", "uint256"],
-      [padRight(identifier, 64), requestTime, ancillaryData, collateralToken.address, 0]
+      ["bytes32", "uint256", "bytes"],
+      [padRight(identifier, 64), requestTime, ancillaryData]
     );
     const proposalData = Helpers.createGenericDepositData(encodedMetaDataProposal);
     // Datahash must be the hash of (handlerAddress, proposalData)
     let proposalDataHash = Ethers.utils.keccak256(genericHandlerMainnet.address + proposalData.substr(2));
     TruffleAssert.passes(
-      await bridgeMainnet.voteProposal(chainId, expectedDepositNonce, optimisticOracleResourceId, proposalDataHash, {
+      await bridgeMainnet.voteProposal(chainId, expectedDepositNonce, votingResourceId, proposalDataHash, {
         from: relayer1Address
       })
     );
@@ -210,7 +157,7 @@ contract("GenericHandler - [UMA Cross-chain Voting]", async accounts => {
     // relayer2 votes in favor of the deposit proposal because the relayerThreshold is 2, the deposit proposal will
     // go into a finalized state
     TruffleAssert.passes(
-      await bridgeMainnet.voteProposal(chainId, expectedDepositNonce, optimisticOracleResourceId, proposalDataHash, {
+      await bridgeMainnet.voteProposal(chainId, expectedDepositNonce, votingResourceId, proposalDataHash, {
         from: relayer2Address
       })
     );
@@ -220,46 +167,27 @@ contract("GenericHandler - [UMA Cross-chain Voting]", async accounts => {
       chainId,
       expectedDepositNonce,
       proposalData,
-      optimisticOracleResourceId,
+      votingResourceId,
       {
         from: relayer2Address
       }
     );
-    // Verifying price was requested on OptimisticOracle
-    const internalTx = await TruffleAssert.createTransactionResult(optimisticOracle, executeProposalTx.tx);
-    TruffleAssert.eventEmitted(internalTx, "RequestPrice", event => {
-      return (
+    // Verifying price was requested on Voting
+    const internalTx = await TruffleAssert.createTransactionResult(voting, executeProposalTx.tx);
+    TruffleAssert.eventEmitted(
+      internalTx,
+      "PriceRequestAdded",
+      event =>
         event.requester.toLowerCase() === genericHandlerMainnet.address.toLowerCase() &&
         hexToUtf8(event.identifier) === hexToUtf8(identifier) &&
-        event.timestamp.toString() === requestTime.toString() &&
-        event.ancillaryData.toLowerCase() === ancillaryData.toLowerCase() &&
-        event.currency.toLowerCase() === collateralToken.address.toLowerCase() &&
-        event.reward.toString() === "0" &&
-        event.finalFee.toString() === "0"
-      );
-    });
+        event.time.toString() === requestTime.toString() &&
+        event.ancillaryData.toLowerCase() === ancillaryData.toLowerCase()
+    );
   });
 
   // Scenario: Someone wants to make a price available from mainnet to sidechain.
-  it("Mainnet deposit: Should check if getPrice() reverts on Voting", async function() {
-    // Deposit succeeds on Mainnet, meaning that the price is available.
-    // Note: Deposit data is needed to call getPrice() on the Voting contract.
-    const encodedMetaDataDeposit = Helpers.abiEncode(
-      ["bytes32", "uint256", "bytes"],
-      [padRight(identifier, 64), requestTime, ancillaryData]
-    );
-    const depositData = Helpers.createGenericDepositData(encodedMetaDataDeposit);
-
-    // Will revert until a price is available on Voting.
-    assert(
-      await didContractThrow(
-        bridgeMainnet.deposit(chainId, votingResourceId, depositData, {
-          from: depositerAddress
-        })
-      )
-    );
-    await voting.requestPrice(identifier, requestTime, ancillaryData);
-    await voting.pushPrice(identifier, requestTime, ancillaryData, requestPrice);
+  it("Mainnet deposit: emits Deposit event", async function() {
+    const depositData = Helpers.createGenericDepositData(null);
     const depositTxn = await bridgeMainnet.deposit(chainId, votingResourceId, depositData, {
       from: depositerAddress
     });
