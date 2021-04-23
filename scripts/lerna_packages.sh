6b34e7d53cc5c03f3c077f91f5b2946477ffc69c
evaldofelipe <contato@evaldofelipe.com>
2021-04-23 02:13:59 -0300

    Add more packages jobs
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- scripts/lerna_packages.sh
+++ scripts/lerna_packages.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd ..
+mkdir truffle_workaround
+mv protocol truffle_workaround/
+cd truffle_workaround/protocol
 HASH=$(git merge-base HEAD master)
 yarn lerna ls --since ${HASH} --include-dependents > lerna_output
 cat lerna_output | grep @ > lerna_packages
