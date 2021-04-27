ad6bfa9af1ac9cb5380d05174889d70560a2e757
uma-mining <contato@evaldofelipe.com>
2021-04-27 19:46:30 -0300

    Debug current path
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- ci/check_lerna_packages.sh
+++ ci/check_lerna_packages.sh
@@ -6,4 +6,4 @@ set -o nounset
 HASH=$(git merge-base HEAD master)
 yarn lerna ls --since ${HASH} --include-dependents > lerna_output
 cat lerna_output | grep @ > lerna_packages
-echo "edited packages:" && cat lerna_packages
+echo "edited packages:" && cat lerna_packages && pwd
