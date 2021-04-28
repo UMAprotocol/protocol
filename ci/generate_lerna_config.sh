593eb37994ccb2961887bebcf6874fa264712ed8
uma-mining <contato@evaldofelipe.com>
2021-04-27 22:04:01 -0300

    Add checks for empty packages
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- ci/generate_lerna_config.sh
+++ ci/generate_lerna_config.sh
@@ -6,36 +6,43 @@ set -o nounset
 PACKAGES_ARRAY=($(cat /home/circleci/protocol/lerna_packages))
 CI_CONFIG_FILE="/home/circleci/protocol/.circleci/lerna_config.yml"
 
-printf "version: 2.1\n\njobs:\n" >> $CI_CONFIG_FILE
+if [ ${#PACKAGES_ARRAY[@]} -eq 0 ]; then
 
-for PACKAGE in "${PACKAGES_ARRAY[@]}"
-  do
-    cat <<EOF >> $CI_CONFIG_FILE
-  test-${PACKAGE:5}:
-    docker:
-      - image: circleci/node:lts
-      - image: trufflesuite/ganache-cli
-        command: ganache-cli -i 1234 -l 9000000 -p 9545
-    working_directory: ~/protocol
-    resource_class: large
-    steps:
-      - restore_cache:
-          key: protocol-completed-build-{{ .Environment.CIRCLE_SHA1 }}
-      - run:
-          name: Run tests
-          command: |
-            ./scripts/truffle_workaround.sh
-            export PACKAGES_CHANGES=$PACKAGE
-            yarn run test-concurrent-test;
+  printf "version: 2.1\n\njobs:\n" >> $CI_CONFIG_FILE
+
+  for PACKAGE in "${PACKAGES_ARRAY[@]}"
+    do
+      cat <<EOF >> $CI_CONFIG_FILE
+    test-${PACKAGE:5}:
+      docker:
+        - image: circleci/node:lts
+        - image: trufflesuite/ganache-cli
+          command: ganache-cli -i 1234 -l 9000000 -p 9545
+      working_directory: ~/protocol
+      resource_class: large
+      steps:
+        - restore_cache:
+            key: protocol-completed-build-{{ .Environment.CIRCLE_SHA1 }}
+        - run:
+            name: Run tests
+            command: |
+              ./scripts/truffle_workaround.sh
+              export PACKAGES_CHANGES=$PACKAGE
+              yarn run test-concurrent-test;
 EOF
-done
+  done
 
-printf "\n\nworkflows:\n  version: 2.1\n  build_and_test:\n    jobs:\n" >> $CI_CONFIG_FILE
+  printf "\n\nworkflows:\n  version: 2.1\n  build_and_test:\n    jobs:\n" >> $CI_CONFIG_FILE
 
-for PACKAGE in "${PACKAGES_ARRAY[@]}"
-  do
-    cat <<EOF >> $CI_CONFIG_FILE
-      test-${PACKAGE:5}:
-          context: api_keys
+  for PACKAGE in "${PACKAGES_ARRAY[@]}"
+    do
+      cat <<EOF >> $CI_CONFIG_FILE
+        - test-${PACKAGE:5}:
+            context: api_keys
 EOF
-done
+  done
+
+else
+    echo "No packages for testing."
+    circleci-agent step halt;
+fi
