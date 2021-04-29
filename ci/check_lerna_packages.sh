69936231e65a77a269f161e7d51666811f4dfec0
uma-mining <contato@evaldofelipe.com>
2021-04-29 18:33:06 -0300

    Add empty packages option
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- ci/check_lerna_packages.sh
+++ ci/check_lerna_packages.sh
@@ -5,16 +5,11 @@ set -o nounset
 
 HASH=$(git merge-base HEAD master)
 yarn lerna ls --since ${HASH} --include-dependents > lerna_output
-cat lerna_output | grep @ > lerna_packages
-
-if [ -s lerna_packages ]
-then
-
-  echo "edited packages:" && cat lerna_packages
-
+if [[ $(cat lerna_output | grep @) ]]; then
+  cat lerna_output | grep @ > lerna_packages
+  echo "Packages to test:"
+  cat lerna_packages
 else
-
   echo "No packages for testing."
   circleci-agent step halt;
-  
 fi
