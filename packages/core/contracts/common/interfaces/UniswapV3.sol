d87066cac46b72b3d1a5e4734d8a7536c6a93da8
Matt Rice <matthewcrice32@gmail.com>
2021-04-30 09:54:13 -0400

    feat(price-feeds): add uniswap v3 price feed (#2918)

---
+++ packages/core/contracts/common/interfaces/UniswapV3.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.6.0;
+
+/**
+ * @title Interface for Uniswap v3.
+ * @dev This only contains the methods/events that we use in our contracts or offchain infrastructure.
+ */
+abstract contract UniswapV3 {
+    // Called after every swap showing the new uniswap price for this token pair.
+    event Swap(
+        address indexed sender,
+        address indexed recipient,
+        int256 amount0,
+        int256 amount1,
+        uint160 sqrtPriceX96,
+        int24 tick
+    );
+    // Base currency.
+    address public token0;
+    // Quote currency.
+    address public token1;
+}
