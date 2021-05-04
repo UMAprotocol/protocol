0b1076771ea369ed03d6a900ab3afd6641c3e0bd
Christopher Maree <christopher.maree@gmail.com>
2021-05-04 17:03:47 +0200

    nits
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/financial-templates-lib/truffle-config.js
+++ packages/financial-templates-lib/truffle-config.js
@@ -1,5 +1,4 @@
 const path = require("path");
 const wkdir = path.dirname(require.resolve("@uma/core/package.json"));
-console.log("wkdir", wkdir);
 
 module.exports = require("@uma/common").getTruffleConfig(wkdir);
