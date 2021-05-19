42914169ef3b9693e3e75019c59cd1597b82ab7a
Fabrizio Audisio <fabrizio.audisio@dextf.com>
2021-05-19 14:47:24 +0800

    Amended TWAP
    Amended TWAP to 300 secs (5 mins), in line with UMIP
    
    Signed-off-by: Fabrizio Audisio <fabrizio.audisio@dextf.com>

--- packages/financial-templates-lib/src/price-feed/DefaultPriceFeedConfigs.js
+++ packages/financial-templates-lib/src/price-feed/DefaultPriceFeedConfigs.js
@@ -1013,7 +1013,7 @@ const defaultConfigs = {
     expression: "ETHDEXTF * USDETH",
     lookback: 7200,
     minTimeBetweenUpdates: 60,
-    twapLength: 3600,
+    twapLength: 300,
     customFeeds: {
       ETHDEXTF: {
         type: "uniswap",
