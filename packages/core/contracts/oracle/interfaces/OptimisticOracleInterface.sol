0ed905194394fdf0100e282e771e69495ff76736
John Shutt <john.d.shutt@gmail.com>
2021-05-25 17:24:05 -0700

    fix(oo): add comments on getState
    Signed-off-by: John Shutt <john.d.shutt@gmail.com>

--- packages/core/contracts/oracle/interfaces/OptimisticOracleInterface.sol
+++ packages/core/contracts/oracle/interfaces/OptimisticOracleInterface.sol
@@ -28,7 +28,7 @@ abstract contract OptimisticOracleInterface {
         bool refundOnDispute; // True if the requester should be refunded their reward on dispute.
         int256 proposedPrice; // Price that the proposer submitted.
         int256 resolvedPrice; // Price resolved once the request is settled.
-        uint256 expirationTime; // Time at which the request expires if no price was proposed.
+        uint256 expirationTime; // Time at which the request auto-settles without a dispute.
         uint256 reward; // Amount of the currency to pay to the proposer on settlement.
         uint256 finalFee; // Final fee to pay to the Store upon request to the DVM.
         uint256 bond; // Bond that the proposer and disputer must pay on top of the final fee.
@@ -225,6 +225,14 @@ abstract contract OptimisticOracleInterface {
         bytes memory ancillaryData
     ) public view virtual returns (Request memory);
 
+    /**
+     * @notice Returns the state of a price request.
+     * @param requester sender of the initial price request.
+     * @param identifier price identifier to identify the existing request.
+     * @param timestamp timestamp to identify the existing request.
+     * @param ancillaryData ancillary data of the price being requested.
+     * @return the State enum value.
+     */
     function getState(
         address requester,
         bytes32 identifier,
