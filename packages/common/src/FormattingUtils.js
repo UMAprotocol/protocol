9c0f7c0a8ec47a408bd980de9d10525bf7c768f0
Nick Pai <npai.nyc@gmail.com>
2021-05-26 10:08:34 -0400

    fix(common): Fix PublicNetworks

--- packages/common/src/FormattingUtils.js
+++ packages/common/src/FormattingUtils.js
@@ -1,4 +1,4 @@
-const networkUtils = require("./PublicNetworks");
+const { PublicNetworks: networkUtils } = require("./PublicNetworks");
 
 const BigNumber = require("bignumber.js");
 const moment = require("moment");
