648701c7227d41173ce13960c842130d6bd0381b
Matt Rice <matthewcrice32@gmail.com>
2021-05-27 13:14:14 -0400

    Update packages/common/src/hardhat/tasks/bridge.js
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>

--- packages/common/src/hardhat/tasks/bridge.js
+++ packages/common/src/hardhat/tasks/bridge.js
@@ -36,7 +36,7 @@ task("register-generic-resource", "Admin can set generic resource ID on Bridge")
     };
 
     if (rname || cid) {
-      if (id) throw new Error("Cannot provide --cid or --resouceName with --id");
+      if (id) throw new Error("Cannot provide --cid or --resourceName with --id");
       if (!cid || !rname) throw new Error("Must provide --cid and --resouceName when generating the resource id");
       id = _getResourceId(rname, cid);
     } else if (!id) {
