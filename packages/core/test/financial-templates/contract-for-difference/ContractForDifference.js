300e795863397e2ff42f9ff85a22aafb09731184
Matt Rice <matthewcrice32@gmail.com>
2021-06-03 17:03:35 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/core/test/financial-templates/contract-for-difference/ContractForDifference.js
+++ packages/core/test/financial-templates/contract-for-difference/ContractForDifference.js
@@ -29,7 +29,7 @@ let collateralWhitelist;
 let identifierWhitelist;
 let optimisticOracle;
 let finder;
-let ancillaryData = web3.utf8ToHex("some-address-field:0x1234");
+let ancillaryData = web3.utils.utf8ToHex("some-address-field:0x1234");
 let timer;
 let constructorParams;
 
