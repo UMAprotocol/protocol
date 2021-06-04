4e98402896fe50a5013c8decfaa0f261363ae33c
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-06-03 20:35:37 -0400

    feat: Add hardhat task to migrate identifier whitelist to new contract (#3046)

--- packages/common/src/hardhat/tasks/identifiers.js
+++ packages/common/src/hardhat/tasks/identifiers.js
@@ -24,3 +24,63 @@ task("whitelist-identifiers", "Whitelist identifiers from JSON file")
 
     await _whitelistIdentifier(web3, id, identifierWhitelist, deployer);
   });
+
+task("migrate-identifiers", "Adds all whitelisted identifiers on one IdentifierWhitelist to another")
+  .addParam("from", "The contract from which to query a whitelist of identifiers.", "", types.string)
+  .addOptionalParam("to", "The contract on which to whitelist new identifiers.", "", types.string)
+  .setAction(async function (taskArguments, hre) {
+    const { deployments, getNamedAccounts, web3 } = hre;
+    const { deployer } = await getNamedAccounts();
+    const { from, to } = taskArguments;
+
+    const IdentifierWhitelist = await deployments.get("IdentifierWhitelist");
+    const oldWhitelist = new web3.eth.Contract(IdentifierWhitelist.abi, from);
+    const addedIdentifierEvents = await oldWhitelist.getPastEvents("SupportedIdentifierAdded", { fromBlock: 0 });
+
+    // Filter out identifiers that are not currently whitelisted.
+    const isIdentifierSupported = await Promise.all(
+      addedIdentifierEvents.map((_event) =>
+        oldWhitelist.methods.isIdentifierSupported(_event.returnValues.identifier).call()
+      )
+    );
+    const identifiersToWhitelist = isIdentifierSupported
+      .map((isOnWhitelist, i) => {
+        if (isOnWhitelist) return addedIdentifierEvents[i].returnValues.identifier;
+        return null;
+      })
+      .filter((id) => id);
+
+    // Create table with results to display to user:
+    let resultsTable = identifiersToWhitelist.map((id) => {
+      return {
+        identifierToWhitelist: id,
+        utf8: web3.utils.hexToUtf8(id),
+      };
+    });
+
+    if (to) {
+      const newWhitelist = new web3.eth.Contract(IdentifierWhitelist.abi, to);
+      const isIdentifierSupportedOnNewWhitelist = await Promise.all(
+        identifiersToWhitelist.map((id) => newWhitelist.methods.isIdentifierSupported(id).call())
+      );
+
+      // Send transactions sequentially to avoid nonce collisions. Note that this might fail due to timeout if there
+      // are a lot of transactions to send or the gas price to send with is too low.
+      const addSupportedIdentifierReceipts = [];
+      for (let i = 0; i < isIdentifierSupportedOnNewWhitelist.length; i++) {
+        if (!isIdentifierSupportedOnNewWhitelist[i]) {
+          const receipt = await newWhitelist.methods.addSupportedIdentifier(identifiersToWhitelist[i]).send({
+            from: deployer,
+          });
+          addSupportedIdentifierReceipts.push(receipt);
+        }
+      }
+      addSupportedIdentifierReceipts.forEach((receipt, i) => {
+        resultsTable[i] = { ...resultsTable[i], txn: receipt.transactionHash };
+      });
+    }
+
+    console.group("Identifiers to Whitelist");
+    console.table(resultsTable);
+    console.groupEnd();
+  });
