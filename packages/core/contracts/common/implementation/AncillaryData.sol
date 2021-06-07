76731408c293a027a782bbd6ef93f56c2693f443
Nick Pai <npai.nyc@gmail.com>
2021-06-07 18:18:26 -0400

    lint fix
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/contracts/common/implementation/AncillaryData.sol
+++ packages/core/contracts/common/implementation/AncillaryData.sol
@@ -5,7 +5,13 @@ pragma solidity ^0.8.0;
  * @title Library for encoding and decoding ancillary data for DVM price requests.
  */
 library AncillaryData {
-    // Source: https://ethereum.stackexchange.com/questions/8346/convert-address-to-string/8447#8447
+    /**
+     * @notice Returns utf8-encoded address that can be read via web3.utils.hexToUtf8.
+     * Source: https://ethereum.stackexchange.com/questions/8346/convert-address-to-string/8447#8447
+     * @dev Will return address in all lower case characters and without the leading 0x.
+     * @param x address to encode.
+     * @return utf8 encoded address bytes.
+     */
     function toUtf8Bytes(address x) internal pure returns (bytes memory) {
         bytes memory s = new bytes(40);
         for (uint256 i = 0; i < 20; i++) {
