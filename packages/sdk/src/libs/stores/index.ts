66046deff48121a002648eaf4c966791397be925
David A <david@umaproject.org>
2021-06-08 08:51:21 -0400

    feat(sdk): add in memory sorted kv store  (#3065)
    * feat(sdk): add in memory sorted kv store
    
    Signed-off-by: David Adams <david@umaproject.org>
    
    * improve(sdk): add sorted store type as seperate from base store
    
    Signed-off-by: David Adams <david@umaproject.org>

--- packages/sdk/src/libs/stores/index.ts
+++ packages/sdk/src/libs/stores/index.ts
@@ -1,14 +1,25 @@
 export { default as JsMap } from "./js-map";
+export { default as SortedJsMap } from "./sorted-js-map";
 export { default as GoogleDatastore } from "./google-datastore";
 
-// types
-export interface Store<I, D> {
-  get: (id: I) => Promise<D | undefined>;
-  set: (id: I, data: D) => Promise<void>;
-  delete: (id: I) => Promise<void>;
-  has: (id: I) => Promise<boolean>;
-  entries: () => Promise<Array<[I, D]>>;
-  values: () => Promise<Array<D>>;
-  keys: () => Promise<Array<I>>;
+// Base store interface which is inspired by JS map
+export interface Store<Id, Data> {
+  get: (id: Id) => Promise<Data | undefined>;
+  set: (id: Id, data: Data) => Promise<void>;
+  delete: (id: Id) => Promise<void>;
+  has: (id: Id) => Promise<boolean>;
+  entries: () => Promise<Array<[Id, Data]>>;
+  values: () => Promise<Array<Data>>;
+  keys: () => Promise<Array<Id>>;
   size: () => Promise<number>;
+  clear: () => Promise<void>;
+  // use this to expose the underlying database driver
+  driver?: any;
+}
+
+// Sorted (ordered) key value with some extra queries
+export interface SortedStore<Id, Data> extends Store<Id, Data> {
+  // these fields are for stores with sorted data
+  between: (a: Id, b: Id) => Promise<Array<Data>>;
+  slice: (id: Id, length: number) => Promise<Array<Data>>;
 }
