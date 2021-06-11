0da8453e31a3a6f364804005127e57eea783585e
uma-mining <contato@evaldofelipe.com>
2021-06-11 11:25:30 -0300

    Fix lint
    Signed-off-by: uma-mining <contato@evaldofelipe.com>

--- ci/tests/test-not-required.sh
+++ ci/tests/test-not-required.sh
@@ -3,7 +3,7 @@
 cat << EOF
 version: 2.1
 jobs:
-  tests-required:
+  tests_required:
     docker:
       - image: circleci/node:lts
     steps:
@@ -13,9 +13,9 @@ jobs:
             echo "No packages for testing."
             circleci-agent step halt;
 
-  workflows:
+workflows:
   version: 2.1
   build_and_test:
-    jobs:
-      - tests-required
+      jobs:
+        - tests_required
 EOF
