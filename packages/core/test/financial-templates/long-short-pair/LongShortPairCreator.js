06b5c0c881364bd2b95c51f362cde9d0afb5f689
Matt Rice <matthewcrice32@gmail.com>
2021-06-17 13:09:22 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/core/test/financial-templates/long-short-pair/LongShortPairCreator.js
+++ packages/core/test/financial-templates/long-short-pair/LongShortPairCreator.js
@@ -148,7 +148,7 @@ contract("LongShortPairCreator", function (accounts) {
       ...Object.values({ ...constructorParams, prepaidProposerReward: customPrepaidProposerReward })
     );
 
-    const lspAddress = (await longShortPairCreator.getPastEvents("CreatedContractForDifference"))[0].returnValues
+    const lspAddress = (await longShortPairCreator.getPastEvents("CreatedLongShortPair"))[0].returnValues
       .contractForDifference;
 
     assert.equal((await collateralToken.balanceOf(lspAddress)).toString(), customPrepaidProposerReward);
