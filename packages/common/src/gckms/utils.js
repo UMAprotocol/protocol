25ddc935ead49b9ad0b499911af00001edf1ea0c
Matt Rice <matthewcrice32@gmail.com>
2021-06-18 18:10:50 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/common/src/gckms/utils.js
+++ packages/common/src/gckms/utils.js
@@ -1,6 +1,17 @@
 const kms = require("@google-cloud/kms");
 const { Storage } = require("@google-cloud/storage");
 
+// This function takes an array of GCKMS configs that are shaped as follows:
+// {
+//   projectId: "project-name",
+//   locationId: "asia-east2",
+//   keyRingId: "Keyring_Test",
+//   cryptoKeyId: "keyname",
+//   ciphertextBucket: "cipher_bucket",
+//   ciphertextFilename: "ciphertext_fname.enc",
+// }
+//
+// It returns an array of private keys that can be used to send transactions.
 async function retrieveGckmsKeys(gckmsConfigs) {
   return await Promise.all(
     gckmsConfigs.map(async (config) => {
