e968a8aa9323beea7613397971e05f9201cfa2af
Nick Pai <npai.nyc@gmail.com>
2021-06-21 13:26:45 -0400

    add comment about burner role
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/contracts/financial-templates/long-short-pair/LongShortPair.sol
+++ packages/core/contracts/financial-templates/long-short-pair/LongShortPair.sol
@@ -177,6 +177,7 @@ contract LongShortPair is Testable, Lockable {
     /**
      * @notice Return a pair of long and short tokens equal in number to tokensToCreate. Returns the commensurate amount
      * of collateral to the caller for the pair of tokens, defined by the collateralPerPair value.
+     * @dev This contract must have the `Burner` role for the `longToken` and `shortToken` in order to call `burnFrom`.
      * @dev The caller does not need to approve this contract to transfer any amount of `tokensToRedeem` since long
      * and short tokens are burned, rather than transferred, from the caller.
      * @param tokensToRedeem number of long and short synthetic tokens to redeem.
@@ -196,6 +197,7 @@ contract LongShortPair is Testable, Lockable {
     /**
      * @notice Settle long and/or short tokens in for collateral at a rate informed by the contract settlement.
      * @dev Uses financialProductLibrary to compute the redemption rate between long and short tokens.
+     * @dev This contract must have the `Burner` role for the `longToken` and `shortToken` in order to call `burnFrom`.
      * @dev The caller does not need to approve this contract to transfer any amount of `tokensToRedeem` since long
      * and short tokens are burned, rather than transferred, from the caller.
      * @param longTokensToRedeem number of long tokens to settle.
