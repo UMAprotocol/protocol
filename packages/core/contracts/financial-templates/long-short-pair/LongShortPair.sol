133fab1565022b8d88ad649fac00066f3fe55d80
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-06-22 08:19:21 -0400

    improve(lsp): Add contract dev comments (#3133)
    * improve(lsp): Add contract dev comments
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add comment about burner role
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>

--- packages/core/contracts/financial-templates/long-short-pair/LongShortPair.sol
+++ packages/core/contracts/financial-templates/long-short-pair/LongShortPair.sol
@@ -159,6 +159,7 @@ contract LongShortPair is Testable, Lockable {
     /**
      * @notice Creates a pair of long and short tokens equal in number to tokensToCreate. Pulls the required collateral
      * amount into this contract, defined by the collateralPerPair value.
+     * @dev The caller must approve this contract to transfer `tokensToCreate / collateralPerPair` amount of collateral.
      * @param tokensToCreate number of long and short synthetic tokens to create.
      * @return collateralUsed total collateral used to mint the synthetics.
      */
@@ -176,6 +177,9 @@ contract LongShortPair is Testable, Lockable {
     /**
      * @notice Return a pair of long and short tokens equal in number to tokensToCreate. Returns the commensurate amount
      * of collateral to the caller for the pair of tokens, defined by the collateralPerPair value.
+     * @dev This contract must have the `Burner` role for the `longToken` and `shortToken` in order to call `burnFrom`.
+     * @dev The caller does not need to approve this contract to transfer any amount of `tokensToRedeem` since long
+     * and short tokens are burned, rather than transferred, from the caller.
      * @param tokensToRedeem number of long and short synthetic tokens to redeem.
      * @return collateralReturned total collateral returned in exchange for the pair of synthetics.
      */
@@ -193,6 +197,9 @@ contract LongShortPair is Testable, Lockable {
     /**
      * @notice Settle long and/or short tokens in for collateral at a rate informed by the contract settlement.
      * @dev Uses financialProductLibrary to compute the redemption rate between long and short tokens.
+     * @dev This contract must have the `Burner` role for the `longToken` and `shortToken` in order to call `burnFrom`.
+     * @dev The caller does not need to approve this contract to transfer any amount of `tokensToRedeem` since long
+     * and short tokens are burned, rather than transferred, from the caller.
      * @param longTokensToRedeem number of long tokens to settle.
      * @param shortTokensToRedeem number of short tokens to settle.
      * @param collateralReturned number of collateral tokens returned in exchange for long and short tokens.
