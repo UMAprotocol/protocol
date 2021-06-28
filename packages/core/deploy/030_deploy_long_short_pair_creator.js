804925b9057fa48cda69901a5c8e174d21b95404
Chris Maree <christopher.maree@gmail.com>
2021-06-28 14:36:21 +0200

    feat(hardhat-deploy-scripts): add hardhat deploy scripts for LSP on test nets (#3120)

---
+++ packages/core/deploy/030_deploy_long_short_pair_creator.js
@@ -0,0 +1,20 @@
+const { ZERO_ADDRESS } = require("@uma/common");
+const func = async function (hre) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  const Finder = await deployments.get("Finder");
+  const TokenFactory = await deployments.get("TokenFactory");
+  const Timer = (await deployments.getOrNull("Timer")) || { address: ZERO_ADDRESS };
+
+  await deploy("LongShortPairCreator", {
+    from: deployer,
+    args: [Finder.address, TokenFactory.address, Timer.address],
+    log: true,
+  });
+};
+module.exports = func;
+func.tags = ["LongShortPairCreator", "lsp"];
+func.dependencies = ["Finder", "TokenFactory", "Timer"];
