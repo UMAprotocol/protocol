161c4ca90309055f92c51b3e37710c408ec16ef9
Chris Maree <christopher.maree@gmail.com>
2021-07-08 09:03:54 +0200

    liquidator(Proxy-Helper-Scripts): Add DSProxy script to settle an expired position (#3174)

--- packages/trader/scripts/SettleExpiredPositionWithDSProxy.ts
+++ packages/trader/scripts/SettleExpiredPositionWithDSProxy.ts
@@ -1,13 +1,16 @@
-// This script enables a DSProxy to redeem tokens from a contract. This should be used after a swap/mint/liquidate transaction
-// to close out a bots position. To execute the script, run:
-// truffle exec ./scripts/RedeemTokensWithDSProxy.ts--financialContractAddress 0x123 --numTokens 100000000000000000000 --dsProxyFactoryAddress 0x123 --network mainnet_mnemonic
-// Note if you do not provide the dsProxyAddress the script will try find one deployed at the unlocked wallet account.
+// TODO: the location of these scripts is a bit confusing. This script was put here to follow the Redeem and Withdraw
+// scripts but in future they should be moved somewhere more generic.
 
-async function RedeemTokensWithDSProxy() {
+// This script enables a DSProxy to settle expired positions from an EMP. This should be used after a DSProxy opens a
+// position due to liquidation and needs to settle the position at contract expiration. To execute the script, run:
+// truffle exec ./scripts/SettleExpiredPositionWithDSProxy.ts --financialContractAddress 0x123 --dsProxyAddress 0x456 --network mainnet_mnemonic
+// Note: if you do not provide the dsProxyAddress the script will try find one deployed at the unlocked wallet account.
+
+async function SettleExpiredPositionWithDSProxy() {
   const winston = require("winston");
   const assert = require("assert");
   const argv = require("minimist")(process.argv.slice(), {
-    string: ["financialContractAddress", "numTokens", "dsProxyAddress", "dsProxyFactoryAddress"],
+    string: ["financialContractAddress", "numTokens", "dsProxyAddress"],
   });
 
   const { getWeb3 } = require("@uma/common");
@@ -16,10 +19,8 @@ async function RedeemTokensWithDSProxy() {
   const { getAbi, getAddress, getTruffleContract } = require("@uma/core");
   const { DSProxyManager, GasEstimator } = require("@uma/financial-templates-lib");
 
-  assert(argv.numTokens, "Must provide the number of tokens to redeem");
-
   assert(web3.utils.isAddress(argv.financialContractAddress), "`financialContractAddress` needs to be a valid address");
-  console.log("Running Token withdrawal script 💰");
+  console.log("Running position settler script 💰");
 
   const [accounts, networkId] = await Promise.all([web3.eth.getAccounts(), web3.eth.net.getId()]);
   console.log("Connected to network id", await web3.eth.net.getId());
@@ -36,27 +37,25 @@ async function RedeemTokensWithDSProxy() {
     web3,
     gasEstimator,
     account: accounts[0],
-    dsProxyFactoryAddress: argv.dsProxyFactoryAddress || getAddress("DSProxyFactory", networkId),
+    dsProxyFactoryAddress: getAddress("DSProxyFactory", networkId),
     dsProxyFactoryAbi: getAbi("DSProxyFactory"),
     dsProxyAbi: getAbi("DSProxy"),
   });
 
   // Load in a DSProxy address. If you did not provide one in the args then the script will check against the factory.
   // False as the second param in this function prevents the DSProxyManager from deploying a DSProxy if you don't have one.
-  await dsProxyManager.initializeDSProxy(argv.dsProxyAddress, false);
+  await dsProxyManager.initializeDSProxy(argv.dsProxyAddress || null, false);
 
   const dsProxyAddress = dsProxyManager.getDSProxyAddress();
-  if (!dsProxyAddress) throw new Error("DSProxy Address was not found or parameterized");
+  if (!dsProxyAddress) throw new Error("DSProxy Address was not found or not parameterized");
 
-  // Create the callData as the encoded tx against the TokenRedeemer contract.
-  const TokenRedeemer = getTruffleContract("TokenRedeemer", web3);
-  const TokenRedeemerInstance = new web3.eth.Contract(TokenRedeemer.abi);
-  const callData = TokenRedeemerInstance.methods
-    .redeem(argv.financialContractAddress, { rawValue: argv.numTokens })
-    .encodeABI();
+  // Create the callData as the encoded tx against the PositionSettler contract.
+  const PositionSettler = getTruffleContract("PositionSettler", web3);
+  const PositionSettlerInstance = new web3.eth.Contract(PositionSettler.abi);
+  const callData = PositionSettlerInstance.methods.settleExpired(argv.financialContractAddress).encodeABI();
 
   // The library also needs the code of the contract to deploy.
-  const callCode = TokenRedeemer.bytecode;
+  const callCode = PositionSettler.bytecode;
 
   // Send the transaction against the DSProxy manager.
   const dsProxyCallReturn = await dsProxyManager.callFunctionOnNewlyDeployedLibrary(callCode, callData);
@@ -66,7 +65,7 @@ async function RedeemTokensWithDSProxy() {
 
 const run = async function (callback) {
   try {
-    await RedeemTokensWithDSProxy();
+    await SettleExpiredPositionWithDSProxy();
   } catch (err) {
     console.error(err);
     callback(err);
@@ -75,5 +74,5 @@ const run = async function (callback) {
   callback();
 };
 
-run.RedeemTokensWithDSProxy = RedeemTokensWithDSProxy;
+run.SettleExpiredPositionWithDSProxy = SettleExpiredPositionWithDSProxy;
 module.exports = run;
