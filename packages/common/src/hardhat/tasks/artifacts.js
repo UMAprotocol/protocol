813c869c7d52b5b73a54249996e2ccf8da767669
Matt Rice <matthewcrice32@gmail.com>
2021-07-14 13:01:45 -0400

    full generation functions
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/common/src/hardhat/tasks/artifacts.js
+++ packages/common/src/hardhat/tasks/artifacts.js
@@ -1,36 +1,53 @@
 const path = require("path");
 const fs = require("fs");
+const uniqBy = require("lodash.uniqby");
 
 const { task, types } = require("hardhat/config");
 
-task("dump-artifacts", "Admin can set generic resource ID on Bridge")
-  .addParam("out", "ts file to dump the output", undefined, types.string)
+function removeFileIfExists(filename) {
+  try {
+    fs.unlinkSync(filename);
+  } catch (e) {
+    // do nothing.
+  }
+}
+
+task("generate-ts", "Admin can set generic resource ID on Bridge")
+  .addParam("browser", "browser ts output file", undefined, types.string)
+  .addParam("nodejs", "nodejs ts output file", undefined, types.string)
   .setAction(async function (taskArguments, hre) {
-    const { out } = taskArguments;
+    const { browser: browserOutfile, nodejs: nodejsOutfile } = taskArguments;
+
+    removeFileIfExists(browserOutfile);
+    removeFileIfExists(nodejsOutfile);
 
     const artifactPaths = await hre.artifacts.getArtifactPaths();
 
-    const imports = {};
-    for (const artifactPath of artifactPaths) {
-      const contractName = path.basename(artifactPath).split(".")[0];
-      const relativeArtifactPath = `./${path.relative(path.dirname(out), artifactPath)}`;
-      imports[contractName] = {
-        importLine: `import ${contractName} from "${relativeArtifactPath}";\n`,
-        abiLine: `export function get${contractName}Abi(): string { return ${contractName}.abi; }\n`,
-        bytecodeLine: `export function get${contractName}Bytecode(): string { return ${contractName}.bytecode; }\n`,
-      };
-    }
+    // Generate a unique list of artifacts and paths to them. Unique is necessary because there are some redundantly
+    // named contracts.
+    const artifacts = uniqBy(
+      artifactPaths.map((artifactPath) => ({
+        contractName: path.basename(artifactPath).split(".")[0],
+        browserRelativePath: `./${path.relative(path.dirname(browserOutfile), artifactPath)}`,
+        nodejsRelativePath: `./${path.relative(path.dirname(nodejsOutfile), artifactPath)}`,
+      })),
+      "contractName"
+    );
+    // for (const artifactPath of artifactPaths) {
+    //   const contractName = path.basename(artifactPath).split(".")[0];
 
-    const values = Object.values(imports);
-    try {
-      fs.unlinkSync(out);
-    } catch (e) {
-      // do nothing.
-    }
-    values.forEach(({ importLine }) => fs.appendFileSync(out, importLine));
-    values.forEach(({ abiLine }) => fs.appendFileSync(out, abiLine));
-    values.forEach(({ bytecodeLine }) => fs.appendFileSync(out, bytecodeLine));
+    //   paths[contractName] = {
+    //     browserRelativePath: `./${path.relative(path.dirname(browserOutfile), artifactPath)}`,
+    //     nodejsRelativePath: `./${path.relative(path.dirname(nodejsOutfile), artifactPath)}`,
+    //   };
+    //   // imports[contractName] = {
+    //   //   importLine: `import ${contractName} from "${relativeArtifactPath}";\n`,
+    //   //   abiLine: `export function get${contractName}Abi(): string { return ${contractName}.abi; }\n`,
+    //   //   bytecodeLine: `export function get${contractName}Bytecode(): string { return ${contractName}.bytecode; }\n`,
+    //   // };
+    // }
 
+    // Generate a map of name => chain id => address.
     const dirs = fs.readdirSync("./networks");
     const addresses = {};
     for (const dir of dirs) {
@@ -48,14 +65,97 @@ task("dump-artifacts", "Admin can set generic resource ID on Bridge")
       }
     }
 
+    // Write abi and bytecode for the browser file.
+    // Note: the idea behind writing the functions this way is to make them as optimized as possible for tree-shaking
+    // to remove any unused json files. In modern versions of webpack, this should allow absolutely _no_ artifact
+    // information that isn't needed to be pulled in.
+    artifacts.forEach(({ contractName, browserRelativePath }) =>
+      fs.appendFileSync(
+        browserOutfile,
+        `import { abi as ${contractName}Abi, bytecode as ${contractName}Bytecode } from "${browserRelativePath}";\n`
+      )
+    );
+    artifacts.forEach(({ contractName }) =>
+      fs.appendFileSync(
+        browserOutfile,
+        `export function get${contractName}Abi(): any[] { return ${contractName}Abi; }\n`
+      )
+    );
+    artifacts.forEach(({ contractName }) =>
+      fs.appendFileSync(
+        browserOutfile,
+        `export function get${contractName}Bytecode(): string { return ${contractName}Bytecode; }\n`
+      )
+    );
+
+    // Completely disable general getAbi and getBytecode functions in the browser to avoid the possibility of bunding all artifacts.
+    fs.appendFileSync(
+      browserOutfile,
+      "export function getAbi(contractName: string): any[] { throw new Error(`Must call get${contractName}Abi() in browser.`); }\n"
+    );
+    fs.appendFileSync(
+      browserOutfile,
+      "export function getBytecode(contractName: string): string { throw new Error(`Must call get${contractName}Bytecode() in browser.`); }\n"
+    );
+
+    // Write abi and bytecode for the nodejs file.
+    fs.appendFileSync(nodejsOutfile, "const artifactPaths = {\n");
+    artifacts.forEach(({ contractName, nodejsRelativePath }) =>
+      fs.appendFileSync(nodejsOutfile, `  ${contractName}: "${nodejsRelativePath}",\n`)
+    );
+    fs.appendFileSync(nodejsOutfile, "};\n");
+    fs.appendFileSync(
+      nodejsOutfile,
+      "export function getAbi(contractName: string): any[] { return require(artifactPaths[contractName]).abi; }\n"
+    );
+    fs.appendFileSync(
+      nodejsOutfile,
+      "export function getBytecode(contractName: string): string { return require(artifactPaths[contractName]).bytecode; }\n"
+    );
+
+    artifacts.forEach(({ contractName }) =>
+      fs.appendFileSync(
+        nodejsOutfile,
+        `export function get${contractName}Abi(): string { return require(artifactPaths["${contractName}"]).abi; }\n`
+      )
+    );
+    artifacts.forEach(({ contractName }) =>
+      fs.appendFileSync(
+        nodejsOutfile,
+        `export function get${contractName}Bytecode(): string { return require(artifactPaths["${contractName}"]).bytecode; }\n`
+      )
+    );
+
+    // Creates a switch statement for each deployment.
     for (const [name, addressesByChain] of Object.entries(addresses)) {
       const declaration = `export function get${name}Address(chainId: number): string {\n  switch (chainId.toString()) {\n`;
       const cases = Object.entries(addressesByChain).map(([chainId, address]) => {
         return `    case "${chainId}":\n      return "${address}";\n`;
       });
       const endStatement = `    default:\n      throw new Error(\`No address found for deployment ${name} on chainId \${chainId}\`)\n  }\n}\n`;
-      fs.appendFileSync(out, declaration.concat(...cases, endStatement));
+      fs.appendFileSync(browserOutfile, declaration.concat(...cases, endStatement));
+      fs.appendFileSync(nodejsOutfile, declaration.concat(...cases, endStatement));
     }
+
+    fs.appendFileSync(
+      browserOutfile,
+      `export function getAddress(name: string, chainId: number): string {
+  throw new Error(\`getAddress not available in browser, please call get\${name}Address(\${chainId}) instead\`);
+}\n`
+    );
+
+    fs.appendFileSync(nodejsOutfile, "const addressFunctions = {\n");
+    Object.keys(addresses).forEach((name) => fs.appendFileSync(nodejsOutfile, `  ${name}: get${name}Address,\n`));
+    fs.appendFileSync(nodejsOutfile, "};\n");
+    fs.appendFileSync(
+      nodejsOutfile,
+      `export function getAddress(name: string, chainId: number): string {
+  const fn = addressFunctions[name];
+  if (!fn) throw new Error(\`No deployments for name: \${name}\`);
+  return fn(chainId);
+}
+`
+    );
   });
 
 task("load-addresses", "Load addresses from the networks folder into the hardhat deployments folder").setAction(
