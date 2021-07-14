82cab9f1a4df2600c248c1181a9c2474d47c03dd
Christopher Maree <christopher.maree@gmail.com>
2021-07-14 16:31:21 +0200

    nit
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/common/src/MultiDecimalTestHelper.js
+++ packages/common/src/MultiDecimalTestHelper.js
@@ -9,7 +9,13 @@ const TEST_DECIMAL_COMBOS = [
     syntheticDecimals: 18,
     priceFeedDecimals: 18,
   },
-  { tokenSymbol: "BTC", tokenName: "Wrapped BTC", collateralDecimals: 8, syntheticDecimals: 8, priceFeedDecimals: 18 },
+  {
+    tokenSymbol: "BTC",
+    tokenName: "Wrapped Bitcoin",
+    collateralDecimals: 8,
+    syntheticDecimals: 8,
+    priceFeedDecimals: 18,
+  },
 ];
 
 module.exports = { TEST_DECIMAL_COMBOS };
