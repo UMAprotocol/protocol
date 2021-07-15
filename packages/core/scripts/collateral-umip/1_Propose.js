9f5c7d5c68372c8cb47ae4c53af5d55f623c9d49
John Shutt <pemulis@users.noreply.github.com>
2021-07-14 19:04:57 -0700

    Update packages/core/scripts/collateral-umip/1_Propose.js
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>

--- packages/core/scripts/collateral-umip/1_Propose.js
+++ packages/core/scripts/collateral-umip/1_Propose.js
@@ -1,5 +1,5 @@
 // This script generates and submits a collateral-add upgrade transaction to the DVM. It can be run on a local ganache
-// fork of the main net or can be run directly on the main net to execute the upgrade transactions.
+// fork of the mainnet or can be run directly on the mainnet to execute the upgrade transactions.
 // To run this on the localhost first fork main net into Ganache with the proposerWallet unlocked as follows:
 // ganache-cli --fork https://mainnet.infura.io/v3/5f56f0a4c8844c96a430fbd3d7993e39 --unlock 0x2bAaA41d155ad8a4126184950B31F50A1513cE25 --unlock 0x7a3a1c2de64f20eb5e916f40d11b01c441b2a8dc --port 9545
 // Then execute the script as: yarn truffle exec ./scripts/collateral-umip/1_Propose.js --network mainnet-fork --collateral 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 --fee 0.1 --collateral 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 --fee 400 from core
