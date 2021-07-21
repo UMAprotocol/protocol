9e6eb078096c2c986ecfc3d014b7e45ee5e4be54
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-07-21 11:10:01 -0400

    feat(common): Initial PR adding Optimism plugin to Hardhat config (#3154)
    * Init: add `ovm` to hardhat.config
    
    * add demo test
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * can compile from whitelist
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * Update packages/common/src/hardhat/tasks/compile.js
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    
    * rename optimism to ovm; add optimism-node scripts
    
    * remove hardhat ethers and waffle
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * lockfile
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * Use hardhat artifacts to import contract artifact
    
    * don't test ovm tests on hardhat network
    
    * Move deps to devdeps
    
    * Add some files to compileWhitelist
    
    * can't get Finder.js to work
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * wip
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * Update Finder.js
    
    * Update Finder.js
    
    * Make Finder test work with optimism network
    
    * Update package.json
    
    * revert yarn.lock
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * revert yarn.lock
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * Update yarn.lock
    
    * Remove runDefaultFixture
    
    * Fix test task
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/common/src/hardhat/tasks/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ...require("./identifiers.js"),
-  ...require("./registry.js"),
-  ...require("./finder.js"),
-  ...require("./bridge.js"),
-  ...require("./fxTunnel.js"),
-  ...require("./oracle.js"),
-};
