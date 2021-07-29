190370f3a0d3f020103c09ea027da81eae413f49
Christopher Maree <christopher.maree@gmail.com>
2021-07-29 09:56:10 +0200

    updated config
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/.mocharc.js
+++ packages/core/.mocharc.js
@@ -1,24 +1,34 @@
 // End to end tests in this package are run using Mocha. This file fetches only those test files to run against. End to
 // end tests, that should be run with mocha, are notated with `*.e2e.js` at the end of the file.
 
-const path = require("path");
-const { getAllFilesInPath } = require("@uma/common");
+// There are other mocha tests in this package, such as those that test the index.js files. To make this config compatible
+// with the existing tests, you must append `-e2e` to your mocha test command to run these tests. Else, simply run the
+// `index.js` tests. This defaults these tests to be skipped in CI unless explicitly enabled with the flag.
+if (!process.argv.includes("--e2e")) {
+  console.log(
+    "Running mocha tests but skipping e2e tests. To run e2e tests with mocha, append `--e2e` to the mocha test command."
+  );
+  module.exports = {};
+} else {
+  const path = require("path");
+  const { getAllFilesInPath } = require("@uma/common");
 
-// exit test runner on unhandled rejections
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection during test execution:", promise, "reason:", reason);
-  process.exit(1);
-});
+  // exit test runner on unhandled rejections
+  process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled Rejection during test execution:", promise, "reason:", reason);
+    process.exit(1);
+  });
 
-// Fetch an array of all test files in this package.
-const allTestFiles = getAllFilesInPath(path.resolve(__dirname, "./test"));
-// Select only the ones that end with `e2e.js`.
-const endToEndTests = allTestFiles.filter((path) => path.endsWith(".e2e.js"));
+  // Fetch an array of all test files in this package.
+  const allTestFiles = getAllFilesInPath(path.resolve(__dirname, "./test"));
+  // Select only the ones that end with `e2e.js`.
+  const endToEndTests = allTestFiles.filter((path) => path.endsWith(".e2e.js"));
 
-module.exports = {
-  require: ["ts-node/register/transpile-only"],
-  extension: ["js"],
-  watchExtensions: ["js"],
-  spec: endToEndTests,
-  timeout: 80000,
-};
+  module.exports = {
+    require: ["ts-node/register/transpile-only"],
+    extension: ["js"],
+    watchExtensions: ["js"],
+    spec: endToEndTests, // mocha will run this sent of test files defined in this array.
+    timeout: 80000,
+  };
+}
