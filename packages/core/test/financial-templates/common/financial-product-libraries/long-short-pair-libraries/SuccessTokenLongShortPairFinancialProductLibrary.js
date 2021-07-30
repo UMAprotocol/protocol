204aa59fcb9dad8d0ec9ca9b57f60ff1a865dc5e
John Shutt <pemulis@users.noreply.github.com>
2021-07-29 21:53:10 -0700

    test(fpl): add test for different base percentage
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- packages/core/test/financial-templates/common/financial-product-libraries/long-short-pair-libraries/SuccessTokenLongShortPairFinancialProductLibrary.js
+++ packages/core/test/financial-templates/common/financial-product-libraries/long-short-pair-libraries/SuccessTokenLongShortPairFinancialProductLibrary.js
@@ -14,7 +14,7 @@ const LongShortPairMock = getContract("LongShortPairMock");
 const { toWei, toBN } = web3.utils;
 const strikePrice = toWei("400");
 const basePercentage1 = toBN(toWei("0.5"));
-// const basePercentage2 = toBN(toWei("0.2"));
+const basePercentage2 = toBN(toWei("0.2"));
 const collateralPerPair = toBN(toWei("1"));
 
 describe("SuccessTokenLongShortPairFinancialProductLibrary", function () {
@@ -94,6 +94,41 @@ describe("SuccessTokenLongShortPairFinancialProductLibrary", function () {
         .percentageLongCollateralAtExpiry(toWei("500"))
         .call({ from: lspMock.options.address });
       assert.equal(expiryTokensForCollateral.toString(), toWei("0.6"));
+    });
+    it("Should never return a value greater than 1", async () => {
+      // Create a massive expiry price. 1e18*1e18. Under all conditions should return less than 1.
+      const expiryTokensForCollateral = await successTokenLSPFPL.methods
+        .percentageLongCollateralAtExpiry(toWei(toWei("1")))
+        .call({ from: lspMock.options.address });
+      assert.isTrue(toBN(expiryTokensForCollateral).lt(toBN(toWei("1"))));
+    });
+  });
+  describe("Compute expiry tokens for collateral with base percentage of 0.2", () => {
+    beforeEach(async () => {
+      await successTokenLSPFPL.methods
+        // Set the strike price at 400.
+        .setLongShortPairParameters(lspMock.options.address, strikePrice, basePercentage2)
+        .send({ from: accounts[0] });
+    });
+    it("Lower than strike should return 0.2", async () => {
+      const expiryTokensForCollateral = await successTokenLSPFPL.methods
+        // If the expiry price is below the strike price, the long should be worth 20% of the
+        // collateralPerPair, or 0.2.
+        .percentageLongCollateralAtExpiry(toWei("300"))
+        .call({ from: lspMock.options.address });
+      assert.equal(expiryTokensForCollateral.toString(), toWei("0.2"));
+    });
+    it("Higher than strike correct value", async () => {
+      const expiryTokensForCollateral = await successTokenLSPFPL.methods
+        // If the expiry price is above the strike price, the long should be worth 20% of the
+        // collateralPerPair, plus 80% * ((expiry price - strike price) / expiry price)
+        // With a collateralPerPair of 1, this can be expressed as:
+        // 0.2 + (0.8 * (expiryPrice - strikePrice) / expiryPrice)
+        // With an expiry price of 500 and strike price of 400, this becomes:
+        // 0.2 + (0.8 * (500 - 400) / 500) = 0.36
+        .percentageLongCollateralAtExpiry(toWei("500"))
+        .call({ from: lspMock.options.address });
+      assert.equal(expiryTokensForCollateral.toString(), toWei("0.36"));
     });
     it("Should never return a value greater than 1", async () => {
       // Create a massive expiry price. 1e18*1e18. Under all conditions should return less than 1.
