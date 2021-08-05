a4d3015bf253dc047d76348697ac6af8e61822e6
Chris Maree <christopher.maree@gmail.com>
2021-08-05 18:09:19 +0200

    feat(insured-bridge): Additional admin e2e unit tests for remaining cross-domain admin logic (#3280)
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    Co-authored-by: nicholaspai <9457025+nicholaspai@users.noreply.github.com>
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>
    Co-authored-by: Nick Pai <npai.nyc@gmail.com>

---
+++ packages/core/test/insured-bridge/e2e/helpers/TestPreamble.js
@@ -0,0 +1,53 @@
+const { interfaceName } = require("@uma/common");
+
+const { ethers } = require("hardhat");
+
+const { OPTIMISM_GAS_OPTS } = require("./OptimismConstants");
+const { createLocalEthersFactory } = require("./ArtifactsHelper");
+
+// UMA ecosystem contract factories
+const factory__L1_Finder = createLocalEthersFactory("Finder");
+const factory__L1_Timer = createLocalEthersFactory("Timer");
+const factory__L1_AddressWhitelist = createLocalEthersFactory("AddressWhitelist");
+const factory__L1_IdentifierWhitelist = createLocalEthersFactory("IdentifierWhitelist");
+const factory__L1_Store = createLocalEthersFactory("Store");
+const factory__L2_Timer = createLocalEthersFactory("OVM_Timer", true);
+
+async function setUpUmaEcosystemContracts(l1Wallet, l2Wallet, l1Erc20, identifier) {
+  // TODO: setup OO when we have more implementation cross-chain.
+  // Set up required UMA L1 ecosystem contracts.
+  const l1Finder = await factory__L1_Finder.connect(l1Wallet).deploy();
+  await l1Finder.deployTransaction.wait();
+  const l1Timer = await factory__L1_Timer.connect(l1Wallet).deploy();
+  await l1Timer.deployTransaction.wait();
+
+  const l1CollateralWhitelist = await factory__L1_AddressWhitelist.connect(l1Wallet).deploy();
+  await l1CollateralWhitelist.deployTransaction.wait();
+  await l1Finder.changeImplementationAddress(
+    ethers.utils.formatBytes32String(interfaceName.CollateralWhitelist),
+    l1CollateralWhitelist.address
+  );
+  await l1CollateralWhitelist.addToWhitelist(l1Erc20.address);
+
+  const l1IdentifierWhitelist = await factory__L1_IdentifierWhitelist.connect(l1Wallet).deploy();
+  await l1IdentifierWhitelist.deployTransaction.wait();
+  await l1Finder.changeImplementationAddress(
+    ethers.utils.formatBytes32String(interfaceName.IdentifierWhitelist),
+    l1IdentifierWhitelist.address
+  );
+  await l1IdentifierWhitelist.addSupportedIdentifier(identifier);
+
+  const l1Store = await factory__L1_Store
+    .connect(l1Wallet)
+    .deploy({ rawValue: "0" }, { rawValue: "0" }, l1Timer.address);
+  await l1Store.deployTransaction.wait();
+  await l1Finder.changeImplementationAddress(ethers.utils.formatBytes32String(interfaceName.Store), l1Store.address);
+  await l1Store.setFinalFee(l1Erc20.address, { rawValue: ethers.utils.parseEther("1").toString() });
+
+  const l2Timer = await factory__L2_Timer.connect(l2Wallet).deploy(OPTIMISM_GAS_OPTS);
+  await l2Timer.deployTransaction.wait();
+
+  return { l1Timer, l1Finder, l1CollateralWhitelist, l1IdentifierWhitelist, l1Store, l2Timer };
+}
+
+module.exports = { setUpUmaEcosystemContracts };
