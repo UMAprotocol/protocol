f8483704c4193f56d1cbdcc1b58b0847cb8eb2fc
evaldofelipe <contato@evaldofelipe.com>
2021-08-12 10:21:24 -0300

    Add integration tests
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

---
+++ ci/tests/test-integration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat << EOF
+  test-integration:
+    docker:
+      - image: circleci/node:lts
+    working_directory: ~/protocol
+    resource_class: medium+
+    steps:
+      - restore_cache:
+          key: protocol-completed-build-{{ .Environment.CIRCLE_SHA1 }}
+      - run:
+          name: Run integration tests
+          command: |
+            yarn optimism-up
+            yarn --cwd packages/core test-e2e
+EOF
