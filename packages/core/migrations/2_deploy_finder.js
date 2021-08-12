3a7006069743fc07db0774dfa4492a71a370e80b
evaldofelipe <contato@evaldofelipe.com>
2021-08-12 10:21:13 -0300

    Simple edit for CI test packages
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- packages/core/migrations/2_deploy_finder.js
+++ packages/core/migrations/2_deploy_finder.js
@@ -4,5 +4,5 @@ const { getKeysForNetwork, deploy } = require("./MigrationUtils");
 module.exports = async function (deployer, network, accounts) {
   const keys = getKeysForNetwork(network, accounts);
 
-  await deploy(deployer, network, Finder, { from: keys.deployer });
+  await deploy(deployer, accounts, network, Finder, { from: keys.deployer });
 };
