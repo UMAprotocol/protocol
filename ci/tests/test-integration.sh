f257d7984fba820e3e45b08d01144915bfbda66d
evaldofelipe <contato@evaldofelipe.com>
2021-08-12 12:11:33 -0300

    Add docker-compose
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- ci/tests/test-integration.sh
+++ ci/tests/test-integration.sh
@@ -10,6 +10,12 @@ cat << EOF
       - restore_cache:
           key: protocol-completed-build-{{ .Environment.CIRCLE_SHA1 }}
       - run:
+          name: Install Docker Compose
+          command: |
+            set -x
+            curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+            chmod +x /usr/local/bin/docker-compose
+      - run:
           name: Run integration tests
           command: |
             yarn optimism-up
