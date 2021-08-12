5799bdf1c67fd446623d7f436f9438519c2dfc65
evaldofelipe <contato@evaldofelipe.com>
2021-08-12 14:26:44 -0300

    Remove sudo
    Signed-off-by: evaldofelipe <contato@evaldofelipe.com>

--- ci/tests/test-integration.sh
+++ ci/tests/test-integration.sh
@@ -10,9 +10,17 @@ cat << EOF
       - restore_cache:
           key: protocol-completed-build-{{ .Environment.CIRCLE_SHA1 }}
       - run:
+          name: Install Docker client
+          command: |
+            set -x
+            VER="17.03.0-ce"
+            curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
+            tar -xz -C /tmp -f /tmp/docker-$VER.tgz
+            mv /tmp/docker/* /usr/bin
+      - run:
           name: Install Docker Compose
           command: |
-            sudo set -x
+            set -x
             sudo curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
             sudo chmod +x /usr/local/bin/docker-compose
       - run:
