f67dbe563465d6251f72c12746c36a9b4c9cf548
Christopher Maree <christopher.maree@gmail.com>
2021-08-16 13:03:00 +0200

    Updated a comment
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/contracts-ovm/insured-bridge/implementation/BridgePool.sol
+++ packages/core/contracts-ovm/insured-bridge/implementation/BridgePool.sol
@@ -395,7 +395,7 @@ contract BridgePool is Testable, BridgePoolInterface, ExpandedERC20 {
         updateFeeCounters(0); // Accumulate all allocated fees from the last time this method was called.
         sync(); // Fetch any balance changes due to token bridging finalization and factor them in.
 
-        // ExchangeRate := (liquidReserves+utilizedReserves+cumulativeLpFeesEarned-undistributedLpFees)/lpTokenSupply
+        // ExchangeRate := (liquidReserves+utilizedReserves-undistributedLpFees)/lpTokenSupply
         // Note to accommodate negative utilizedReserves without using FixedPoint.Signed we need to do a bit of
         // branching logic. This is a gas optimization so we don't need to import this extra library logic.
         FixedPoint.Unsigned memory numerator =
