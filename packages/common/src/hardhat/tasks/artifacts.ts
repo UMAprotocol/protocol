ee89f2f14ff0d92ceba0da18d42ceb4c8fc5e915
Matt Rice <matthewcrice32@gmail.com>
2021-08-24 00:21:45 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/common/src/hardhat/tasks/artifacts.ts
+++ packages/common/src/hardhat/tasks/artifacts.ts
@@ -103,8 +103,8 @@ task("generate-contracts-frontend", "Generate typescipt for the contracts-fronte
           `export type { ${normalizeClassName(contractName)} as ${normalizeClassName(
             contractName
           )}Web3 } from "../typechain/web3/${contractName}";
-          import type * as ${normalizeClassName(contractName)}Web3Events from "../typechain/web3/${contractName}";
-          export type { ${normalizeClassName(contractName)}Web3Events };\n`
+import type * as ${normalizeClassName(contractName)}Web3Events from "../typechain/web3/${contractName}";
+export type { ${normalizeClassName(contractName)}Web3Events };\n`
         );
     });
 
@@ -176,8 +176,8 @@ task("generate-contracts-node", "Generate typescipt for the contracts-node packa
           `export type { ${normalizeClassName(contractName)} as ${normalizeClassName(
             contractName
           )}Web3 } from "../typechain/web3/${contractName}";
-          import type * as ${normalizeClassName(contractName)}Web3Events from "../typechain/web3/${contractName}";
-          export type { ${normalizeClassName(contractName)}Web3Events };\n`
+import type * as ${normalizeClassName(contractName)}Web3Events from "../typechain/web3/${contractName}";
+export type { ${normalizeClassName(contractName)}Web3Events };\n`
         );
     });
 
