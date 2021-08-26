a2d8568f493a60ba19c6d6fdc5bb646526d876fe
amateima <amatei@umaproject.org>
2021-08-27 01:30:43 +0300

    Make contract abstract
    Signed-off-by: amateima <amatei@umaproject.org>

--- packages/core/contracts/common/interfaces/Multicall2.sol
+++ packages/core/contracts/common/interfaces/Multicall2.sol
@@ -6,7 +6,7 @@ pragma solidity >=0.5.0;
  * @dev This adds method to allow calls within the batch to fail
  * @dev Full implementation can be found here: https://github.com/makerdao/multicall/blob/16ec5e2859b3a4829ceed4ee1ef609e6e9a744ee/src/Multicall2.sol
  */
-contract Multicall2 {
+abstract contract Multicall2 {
     struct Call {
         address target;
         bytes callData;
@@ -16,14 +16,14 @@ contract Multicall2 {
         bytes returnData;
     }
 
-    function aggregate(Call[] memory calls) public virtual returns (uint256 blockNumber, bytes[] memory returnData) {}
+    function aggregate(Call[] memory calls) public virtual returns (uint256 blockNumber, bytes[] memory returnData);
 
     function tryBlockAndAggregate(bool requireSuccess, Call[] memory calls)
         public
+        virtual
         returns (
             uint256 blockNumber,
             bytes32 blockHash,
             Result[] memory returnData
-        )
-    {}
+        );
 }
