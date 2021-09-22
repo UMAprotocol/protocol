06888e5b7caf82fc63fef588e38022da56f0b9e3
Matt Rice <matthewcrice32@gmail.com>
2021-09-21 21:12:31 -0400

    Update hardhat.config.js

--- packages/common/hardhat.config.js
+++ packages/common/hardhat.config.js
@@ -10,7 +10,7 @@ const configOverride = {
     sources: `${coreWkdir}/contracts`,
     artifacts: `${coreWkdir}/artifacts`,
     cache: `${coreWkdir}/cache`,
-    tests: `${packageWkdir}/test,
+    tests: `${packageWkdir}/test`,
   },
 };
 
