c0505da68b4bcf865a9d321d94915d039674c6bd
Christopher Maree <christopher.maree@gmail.com>
2021-09-22 14:06:31 +0200

    WIP
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- packages/core/contracts-ovm/insured-bridge/implementation/AVM_BridgeDepositBox.sol
+++ packages/core/contracts-ovm/insured-bridge/implementation/AVM_BridgeDepositBox.sol
@@ -3,10 +3,9 @@ pragma solidity >=0.7.6;
 
 import "./BridgeDepositBox.sol";
 
-import { OVM_CrossDomainEnabled } from "@eth-optimism/contracts/libraries/bridge/OVM_CrossDomainEnabled.sol";
-import { Lib_PredeployAddresses } from "@eth-optimism/contracts/libraries/constants/Lib_PredeployAddresses.sol";
+// TODO: Add Arbitrum specific cross-chain guards to limit who can call cross-chain methods to the L1 admin.
 
-contract OVM_BridgeDepositBox is BridgeDepositBox, OVM_CrossDomainEnabled {
+contract AVM_BridgeDepositBox is BridgeDepositBox {
     // Address of the L1 contract that acts as the owner of this Bridge deposit box.
     address public bridgeAdmin;
 
@@ -16,10 +15,7 @@ contract OVM_BridgeDepositBox is BridgeDepositBox, OVM_CrossDomainEnabled {
         address _bridgeAdmin,
         uint64 _minimumBridgingDelay,
         address timerAddress
-    )
-        OVM_CrossDomainEnabled(Lib_PredeployAddresses.L2_CROSS_DOMAIN_MESSENGER)
-        BridgeDepositBox(_minimumBridgingDelay, 10, timerAddress)
-    {
+    ) BridgeDepositBox(_minimumBridgingDelay, 10, timerAddress) {
         _setBridgeAdmin(_bridgeAdmin);
     }
 
@@ -32,7 +28,7 @@ contract OVM_BridgeDepositBox is BridgeDepositBox, OVM_CrossDomainEnabled {
      * @dev Only callable by the existing bridgeAdmin via the optimism cross domain messenger.
      * @param _bridgeAdmin address of the new L1 admin contract.
      */
-    function setBridgeAdmin(address _bridgeAdmin) public onlyFromCrossDomainAccount(bridgeAdmin) {
+    function setBridgeAdmin(address _bridgeAdmin) public {
         _setBridgeAdmin(_bridgeAdmin);
     }
 
@@ -41,7 +37,7 @@ contract OVM_BridgeDepositBox is BridgeDepositBox, OVM_CrossDomainEnabled {
      * @dev Only callable by the existing bridgeAdmin via the optimism cross domain messenger.
      * @param _minimumBridgingDelay the new minimum delay.
      */
-    function setMinimumBridgingDelay(uint64 _minimumBridgingDelay) public onlyFromCrossDomainAccount(bridgeAdmin) {
+    function setMinimumBridgingDelay(uint64 _minimumBridgingDelay) public {
         _setMinimumBridgingDelay(_minimumBridgingDelay);
     }
 
@@ -56,7 +52,7 @@ contract OVM_BridgeDepositBox is BridgeDepositBox, OVM_CrossDomainEnabled {
         address l1Token,
         address l2Token,
         address l1BridgePool
-    ) public onlyFromCrossDomainAccount(bridgeAdmin) {
+    ) public {
         _whitelistToken(l1Token, l2Token, l1BridgePool);
     }
 
@@ -66,7 +62,7 @@ contract OVM_BridgeDepositBox is BridgeDepositBox, OVM_CrossDomainEnabled {
      * @param _l2Token address of L2 token to enable/disable deposits for.
      * @param _depositsEnabled bool to set if the deposit box should accept/reject deposits.
      */
-    function setEnableDeposits(address _l2Token, bool _depositsEnabled) public onlyFromCrossDomainAccount(bridgeAdmin) {
+    function setEnableDeposits(address _l2Token, bool _depositsEnabled) public {
         _setEnableDeposits(_l2Token, _depositsEnabled);
     }
 
@@ -90,15 +86,7 @@ contract OVM_BridgeDepositBox is BridgeDepositBox, OVM_CrossDomainEnabled {
 
         whitelistedTokens[l2Token].lastBridgeTime = uint64(getCurrentTime());
 
-        StandardBridgeLike(Lib_PredeployAddresses.L2_STANDARD_BRIDGE).withdrawTo(
-            l2Token, // _l2Token. Address of the L2 token to bridge over.
-            whitelistedTokens[l2Token].l1BridgePool, // _to. Withdraw, over the bridge, to the l1 withdraw contract.
-            bridgeDepositBoxBalance, // _amount. Send the full balance of the deposit box to bridge.
-            l1Gas, // _l1Gas. Unused, but included for potential forward compatibility considerations
-            "0x" // _data. TODO: add additional info into this data prop this.
-        );
-
-        emit TokensBridged(l2Token, bridgeDepositBoxBalance, l1Gas, msg.sender);
+        // TODO: Add Arbitrum specific cross-chain token sending over the canonical bridge.
     }
 
     /**************************************
