95abd8d2d6e481a54e234bda6c7f8585babaa5eb
Chris Maree <christopher.maree@gmail.com>
2021-09-27 15:21:49 +0200

    feat(insured-bridge): Add realized LP fee pct computation (#3373)
    * Added additional logic
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Implementation
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * update
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Update
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * wip
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Updated comments
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Comment
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * emoji update
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * revert nit
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Restore InsuredBridgeL2Client.js
    
    * Added comment
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * clean
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * lint
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Delete settings.json
    
    * removed repeated code
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Delete script.js
    
    * nit
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * wip
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * nit
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Added more comments.
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * revert comment change
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * nit
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * nit
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * nit
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * nit
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Update
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * CI fixes
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Review nits
    
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>
    
    * Update packages/core/contracts-ovm/insured-bridge/implementation/BridgePool.sol
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/financial-templates-lib/test/price-feed/InsuredBridgePriceFeed.js
+++ packages/financial-templates-lib/test/price-feed/InsuredBridgePriceFeed.js
@@ -1,12 +1,20 @@
 const winston = require("winston");
 const { assert } = require("chai");
 const hre = require("hardhat");
-const { web3 } = require("hardhat");
-const { interfaceName, TokenRolesEnum, InsuredBridgeRelayStateEnum, ZERO_ADDRESS } = require("@uma/common");
+const { web3, network } = require("hardhat");
+
+const {
+  interfaceName,
+  TokenRolesEnum,
+  InsuredBridgeRelayStateEnum,
+  ZERO_ADDRESS,
+  mineTransactionsAtTimeHardhat,
+} = require("@uma/common");
 const { SpyTransport, lastSpyLogIncludes } = require("../../dist/logger/SpyTransport");
 const sinon = require("sinon");
 const { getContract } = hre;
-const { utf8ToHex, toWei, toBN, soliditySha3 } = web3.utils;
+const { utf8ToHex, toWei, toBN } = web3.utils;
+const toBNWei = (number) => toBN(toWei(number.toString()).toString());
 
 const chainId = 10;
 const Messenger = getContract("MessengerMock");
@@ -25,6 +33,7 @@ const Timer = getContract("Timer");
 const { InsuredBridgePriceFeed } = require("../../dist/price-feed/InsuredBridgePriceFeed");
 const { InsuredBridgeL1Client } = require("../../dist/clients/InsuredBridgeL1Client");
 const { InsuredBridgeL2Client } = require("../../dist/clients/InsuredBridgeL2Client");
+const { calculateRealizedLpFeePct } = require("../../dist/helpers/acrossFeesCalculator");
 
 // Contract objects
 let messenger, bridgeAdmin, bridgePool;
@@ -47,10 +56,8 @@ let finder,
   l2Token,
   depositData,
   relayData,
-  depositDataAbiEncoded,
-  depositHash,
   relayAncillaryData,
-  relayAncillaryDataHash;
+  rateModels;
 
 // Hard-coded test params:
 const defaultIdentifier = utf8ToHex("IS_CROSS_CHAIN_RELAY_VALID");
@@ -82,26 +89,44 @@ describe("InsuredBridgePriceFeed", function () {
     return await bridgePool.methods.getRelayAncillaryData(depositData, relayData).call();
   };
 
-  const generateRelayData = async (depositData, relayData, bridgePool) => {
-    // Save other reused values.
-    depositDataAbiEncoded = web3.eth.abi.encodeParameters(
-      ["uint8", "uint64", "address", "address", "address", "uint256", "uint64", "uint64", "uint64"],
-      [
-        depositData.chainId,
-        depositData.depositId,
-        depositData.l1Recipient,
-        depositData.l2Sender,
-        l1Token.options.address,
-        depositData.amount,
-        depositData.slowRelayFeePct,
-        depositData.instantRelayFeePct,
-        depositData.quoteTimestamp,
-      ]
-    );
-    depositHash = soliditySha3(depositDataAbiEncoded);
+  const generateRelayData = async (
+    bridgePool,
+    quoteTimestamp,
+    depositTimestamp,
+    depositDataOverride,
+    relayDataOverride
+  ) => {
+    depositData = {
+      chainId: 10,
+      depositId: 0,
+      l1Recipient: l1Recipient,
+      l2Sender: depositor,
+      l1Token: l1Token.options.address,
+      amount: relayAmount,
+      slowRelayFeePct: defaultSlowRelayFeePct,
+      instantRelayFeePct: defaultInstantRelayFeePct,
+      quoteTimestamp,
+      ...depositDataOverride,
+    };
+
+    relayData = {
+      relayId: 0,
+      relayState: InsuredBridgeRelayStateEnum.UNINITIALIZED,
+      priceRequestTime: depositTimestamp,
+      // This should match the realized fee % that the L1 client computes, otherwise the pricefeed will determine the
+      // relay to be invalid.
+      realizedLpFeePct: calculateRealizedLpFeePct(
+        rateModels[l1Token.options.address],
+        toBNWei("0"),
+        toBNWei("0.1")
+      ).toString(),
+      slowRelayer: relayer,
+      instantRelayer: ZERO_ADDRESS,
+      ...relayDataOverride,
+    };
+
     relayAncillaryData = await generateRelayAncillaryData(depositData, relayData, bridgePool);
-    relayAncillaryDataHash = soliditySha3(relayAncillaryData);
-    return { depositHash, relayAncillaryData, relayAncillaryDataHash };
+    return { depositData, relayAncillaryData, relayData };
   };
 
   before(async function () {
@@ -200,7 +225,16 @@ describe("InsuredBridgePriceFeed", function () {
     // Add some liquidity to the pool to facilitate bridging actions.
     await l1Token.methods.mint(liquidityProvider, initialPoolLiquidity).send({ from: owner });
     await l1Token.methods.approve(bridgePool.options.address, initialPoolLiquidity).send({ from: liquidityProvider });
-    await bridgePool.methods.addLiquidity(initialPoolLiquidity).send({ from: liquidityProvider });
+
+    const liquidityAdditionTime = Number(await bridgePool.methods.getCurrentTime().call()) + 100;
+    await mineTransactionsAtTimeHardhat(
+      network,
+      [bridgePool.methods.addLiquidity(initialPoolLiquidity)],
+      liquidityAdditionTime,
+      liquidityProvider
+    );
+
+    await bridgePool.methods.setCurrentTime(liquidityAdditionTime).send({ from: owner });
 
     // The InsuredBridgePriceFeed does not emit any info `level` events.  Therefore no need to test Winston outputs.
     // DummyLogger will not print anything to console as only capture `info` level events.
@@ -211,41 +245,21 @@ describe("InsuredBridgePriceFeed", function () {
     });
 
     // Construct L1 and L2 clients that we'll need to construct the pricefeed:
-    l1Client = new InsuredBridgeL1Client(spyLogger, web3, bridgeAdmin.options.address);
+    rateModels = {
+      [l1Token.options.address]: {
+        UBar: toBNWei("0.65"),
+        R0: toBNWei("0.00"),
+        R1: toBNWei("0.08"),
+        R2: toBNWei("1.00"),
+      },
+    };
+
+    l1Client = new InsuredBridgeL1Client(spyLogger, web3, bridgeAdmin.options.address, rateModels);
+
     l2Client = new InsuredBridgeL2Client(spyLogger, web3, depositBox.options.address);
 
     // Create the InsuredBridgePriceFeed to be tested:
     pricefeed = new InsuredBridgePriceFeed({ logger: spyLogger, l1Client, l2Client });
-
-    // Create some data for initial relay.
-
-    // Store expected relay data that we'll use to verify contract state:
-    const expectedDepositTimestamp = Number(await optimisticOracle.methods.getCurrentTime().call());
-    depositData = {
-      chainId: chainId,
-      depositId: 0,
-      l1Recipient: l1Recipient,
-      l2Sender: depositor,
-      amount: relayAmount,
-      slowRelayFeePct: defaultSlowRelayFeePct,
-      instantRelayFeePct: defaultInstantRelayFeePct,
-      quoteTimestamp: expectedDepositTimestamp + quoteTimestampOffset,
-    };
-    relayData = {
-      relayId: 0,
-      relayState: InsuredBridgeRelayStateEnum.UNINITIALIZED,
-      priceRequestTime: expectedDepositTimestamp,
-      // This should match the realized fee % that the L1 client computes, otherwise the pricefeed will determine the
-      // relay to be invalid.
-      realizedLpFeePct: (await l1Client.calculateRealizedLpFeePctForDeposit(depositData)).toString(),
-      slowRelayer: relayer,
-      instantRelayer: ZERO_ADDRESS,
-    };
-    ({ depositHash, relayAncillaryData, relayAncillaryDataHash } = await generateRelayData(
-      depositData,
-      relayData,
-      bridgePool
-    ));
   });
   it("Pricefeed initial setup", async function () {
     // Updating the pricefeed should also fetch state from updated clients.
@@ -259,47 +273,83 @@ describe("InsuredBridgePriceFeed", function () {
   describe("Lifecycle tests", function () {
     it("Pricefeed returns 1 if the relay ancillary data correctly matches a deposit", async function () {
       // Deposit some tokens.
+
       await l2Token.methods.mint(depositor, toWei("200")).send({ from: owner });
+
       await l2Token.methods.approve(depositBox.options.address, toWei("200")).send({ from: depositor });
-      const quoteTimestamp = Number(await timer.methods.getCurrentTime().call()) + quoteTimestampOffset;
-      await depositBox.methods
-        .deposit(
-          l1Recipient,
-          l2Token.options.address,
-          relayAmount,
-          defaultSlowRelayFeePct,
-          defaultInstantRelayFeePct,
-          quoteTimestamp
-        )
-        .send({ from: depositor });
+
+      // increment time for the deposit tx.
+      const depositTimestamp = Number(await bridgePool.methods.getCurrentTime().call()) + 300;
+      const quoteTimestamp = depositTimestamp - quoteTimestampOffset;
+
+      await mineTransactionsAtTimeHardhat(
+        network,
+        [
+          depositBox.methods.deposit(
+            l1Recipient,
+            l2Token.options.address,
+            relayAmount,
+            defaultSlowRelayFeePct,
+            defaultInstantRelayFeePct,
+            quoteTimestamp
+          ),
+        ],
+        depositTimestamp,
+        depositor
+      );
+      await bridgePool.methods.setCurrentTime(quoteTimestamp).send({ from: owner });
+
+      ({ depositData, relayAncillaryData, relayData } = await generateRelayData(
+        bridgePool,
+        quoteTimestamp,
+        depositTimestamp
+      ));
 
       // Relay the deposit to trigger a price request.
       const totalRelayBond = toBN(relayAmount).mul(toBN(defaultProposerBondPct));
       await l1Token.methods.mint(relayer, totalRelayBond).send({ from: owner });
       await l1Token.methods.approve(bridgePool.options.address, totalRelayBond).send({ from: relayer });
-      await bridgePool.methods.relayDeposit(...generateRelayParams()).send({ from: relayer });
+
+      await bridgePool.methods.relayDeposit(...generateRelayParams({ quoteTimestamp })).send({ from: relayer });
 
       // Update pricefeed and get price for relay request. Note that relay time doesn't actually matter for the
       // getHistoricalPrice method.
+
       await pricefeed.update();
+
       const price = await pricefeed.getHistoricalPrice(1, relayAncillaryData);
-      assert.equal(price, toWei("1"));
+
+      assert.equal(price.toString(), toWei("1"));
     });
     it("Pricefeed returns 0 if incorrect ancillary data or relay time is passed into getPrice method", async function () {
       // Deposit some tokens.
       await l2Token.methods.mint(depositor, toWei("200")).send({ from: owner });
       await l2Token.methods.approve(depositBox.options.address, toWei("200")).send({ from: depositor });
-      const quoteTimestamp = Number(await timer.methods.getCurrentTime().call()) + quoteTimestampOffset;
-      await depositBox.methods
-        .deposit(
-          l1Recipient,
-          l2Token.options.address,
-          relayAmount,
-          defaultSlowRelayFeePct,
-          defaultInstantRelayFeePct,
-          quoteTimestamp
-        )
-        .send({ from: depositor });
+
+      const depositTimestamp = Number(await bridgePool.methods.getCurrentTime().call()) + 300;
+      const quoteTimestamp = depositTimestamp - quoteTimestampOffset;
+      await mineTransactionsAtTimeHardhat(
+        network,
+        [
+          depositBox.methods.deposit(
+            l1Recipient,
+            l2Token.options.address,
+            relayAmount,
+            defaultSlowRelayFeePct,
+            defaultInstantRelayFeePct,
+            quoteTimestamp
+          ),
+        ],
+        depositTimestamp,
+        depositor
+      );
+      await bridgePool.methods.setCurrentTime(quoteTimestamp).send({ from: owner });
+
+      ({ depositData, relayAncillaryData, relayData } = await generateRelayData(
+        bridgePool,
+        quoteTimestamp,
+        depositTimestamp
+      ));
 
       // Relay the deposit to trigger a price request.
       const totalRelayBond = toBN(relayAmount).mul(toBN(defaultProposerBondPct));
@@ -310,6 +360,14 @@ describe("InsuredBridgePriceFeed", function () {
       // Update pricefeed and get price for relay request. Note that relay time doesn't actually matter for the
       // getHistoricalPrice method.
       await pricefeed.update();
+
+      // Validate the stock params (nothing modified) returns 1.
+      ({ depositData, relayAncillaryData, relayData } = await generateRelayData(
+        bridgePool,
+        quoteTimestamp,
+        depositTimestamp
+      ));
+      assert.equal(await pricefeed.getHistoricalPrice(1, relayAncillaryData), toWei("1"));
       // Modify every param of the ancillary data to verify that the feed is checking each param.
       assert.equal(
         await pricefeed.getHistoricalPrice(
@@ -378,20 +436,36 @@ describe("InsuredBridgePriceFeed", function () {
       assert.isTrue(lastSpyLogIncludes(spy, "No deposit event found matching relay request ancillary data and time"));
     });
     it("Pricefeed returns 0 if the relay realized fee % is invalid", async function () {
+      const invalidRealizedLpFeePct = toWei("0.49");
       // Deposit some tokens.
       await l2Token.methods.mint(depositor, toWei("200")).send({ from: owner });
       await l2Token.methods.approve(depositBox.options.address, toWei("200")).send({ from: depositor });
-      const quoteTimestamp = Number(await timer.methods.getCurrentTime().call()) + quoteTimestampOffset;
-      await depositBox.methods
-        .deposit(
-          l1Recipient,
-          l2Token.options.address,
-          relayAmount,
-          defaultSlowRelayFeePct,
-          defaultInstantRelayFeePct,
-          quoteTimestamp
-        )
-        .send({ from: depositor });
+
+      // increment time for the deposit tx.
+      const depositTimestamp = Number(await bridgePool.methods.getCurrentTime().call()) + 300;
+      const quoteTimestamp = depositTimestamp - quoteTimestampOffset;
+      await mineTransactionsAtTimeHardhat(
+        network,
+        [
+          depositBox.methods.deposit(
+            l1Recipient,
+            l2Token.options.address,
+            relayAmount,
+            defaultSlowRelayFeePct,
+            defaultInstantRelayFeePct,
+            quoteTimestamp
+          ),
+        ],
+        depositTimestamp,
+        depositor
+      );
+      await bridgePool.methods.setCurrentTime(quoteTimestamp).send({ from: owner });
+
+      ({ depositData, relayAncillaryData, relayData } = await generateRelayData(
+        bridgePool,
+        quoteTimestamp,
+        depositTimestamp
+      ));
 
       // Relay the deposit but modify the realized fee % to be incorrect.
       const totalRelayBond = toBN(relayAmount).mul(toBN(defaultProposerBondPct));
@@ -400,10 +474,10 @@ describe("InsuredBridgePriceFeed", function () {
       await bridgePool.methods
         .relayDeposit(
           // Note: Look at BridgePool contract for max allowable realizedLpFeePct, current max is 0.5e18
-          ...generateRelayParams({}, { realizedLpFeePct: toWei("0.49") })
+          ...generateRelayParams({}, { realizedLpFeePct: invalidRealizedLpFeePct })
         )
         .send({ from: relayer });
-      const modifiedRelayData = { ...relayData, realizedLpFeePct: toWei("0.49") };
+      const modifiedRelayData = { ...relayData, realizedLpFeePct: invalidRealizedLpFeePct };
       const modifiedRelayAncillaryData = await bridgePool.methods
         .getRelayAncillaryData(depositData, modifiedRelayData)
         .call();
