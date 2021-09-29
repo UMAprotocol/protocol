9641d663f19cc42727218b41483e30727f4d7a49
Matt Rice <matthewcrice32@gmail.com>
2021-09-29 01:22:23 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/scripts/src/local/CalculateContractBytecode.js
+++ packages/scripts/src/local/CalculateContractBytecode.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 // This simple script tells you how big your contract byte code is and how much you have until you exceed
 // the current block limit as defined by EIP170. This script should be run from the /core directory.
 // To run the script navigate to /core and then run:
@@ -5,31 +7,22 @@
 // where voting is the name of the contract you want to check.
 
 const argv = require("minimist")(process.argv.slice(), { string: ["contract"] });
+const { getContract } = require("hardhat");
 
-function printBytecodeInfo(path) {
-  // Load contracts into script and output info.
-  const obj = require(path);
-  const byteCodeSize = (obj.deployedBytecode.length - 2) / 2;
+function printBytecodeInfo(name) {
+  const byteCodeSize = (getContract(name).deployedBytecode.length - 2) / 2;
   const remainingSize = 2 ** 14 + 2 ** 13 - byteCodeSize;
   console.log("Contract is", byteCodeSize, "bytes in size.");
   console.log("This leaves a total of", remainingSize, "bytes within the EIP170 limit.");
 }
 
-module.exports = async function (callback) {
+function main() {
   if (!argv.contract) {
     console.log("Please enter the contract name as a parameter as `--contract <name>`.");
-    callback();
   }
 
-  // Truffle
-  console.group("Truffle Compilation Output");
-  printBytecodeInfo("./../../build/contracts/" + argv.contract + ".json");
+  printBytecodeInfo(argv.contract);
   console.groupEnd();
+}
 
-  // hardhat
-  console.group("hardhat Compilation Output");
-  printBytecodeInfo("./../../artifacts/" + argv.contract + ".json");
-  console.groupEnd();
-
-  callback();
-};
+main();
