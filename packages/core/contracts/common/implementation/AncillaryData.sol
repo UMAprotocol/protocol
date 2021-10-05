df7e3a981506752a401fa290c2c2c8b7556eacfa
Matt Rice <matthewcrice32@gmail.com>
2021-10-05 15:17:29 -0400

    WIP
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/core/contracts/common/implementation/AncillaryData.sol
+++ packages/core/contracts/common/implementation/AncillaryData.sol
@@ -12,17 +12,11 @@ import "hardhat/console.sol";
  * https://docs.google.com/document/d/1zhKKjgY1BupBGPPrY_WOJvui0B6DMcd-xDR8-9-SPDw/edit
  */
 library AncillaryData {
-    /**
-     * @notice Returns utf8-encoded bytes32 string that can be read via web3.utils.hexToUtf8.
-     * Source: brilliant implementation at https://gitter.im/ethereum/solidity?at=5840d23416207f7b0ed08c9b.
-     * @dev Will return bytes32 in all lower case hex characters and without the leading 0x.
-     * This has minor changes from the toUtf8BytesAddress to control for the size of the input.
-     * @param bytesIn bytes32 to encode.
-     * @return utf8 encoded bytes32.
-     */
-    function toUtf8Bytes(bytes32 bytesIn) internal pure returns (bytes memory) {
+    // This converts the bottom half of a bytes32 input to hex in a highly gas-optimized way.
+    // Source: the brilliant implementation at https://gitter.im/ethereum/solidity?at=5840d23416207f7b0ed08c9b.
+    function toUtf8Bytes32Bottom(bytes32 bytesIn) private pure returns (bytes32) {
         unchecked {
-            uint256 x = uint256(bytesIn) / 2**128;
+            uint256 x = uint256(bytesIn);
 
             // Nibble interleave
             x = x & 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff;
@@ -39,25 +33,19 @@ library AncillaryData {
             x = x + (h & (i | j)) * 0x27 + 0x3030303030303030303030303030303030303030303030303030303030303030;
 
             // Store and load next batch
-            bytes32 first = bytes32(x);
-            x = uint256(bytesIn);
-
-            // Nibble interleave
-            x = x & 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff;
-            x = (x | (x * 2**64)) & 0x0000000000000000ffffffffffffffff0000000000000000ffffffffffffffff;
-            x = (x | (x * 2**32)) & 0x00000000ffffffff00000000ffffffff00000000ffffffff00000000ffffffff;
-            x = (x | (x * 2**16)) & 0x0000ffff0000ffff0000ffff0000ffff0000ffff0000ffff0000ffff0000ffff;
-            x = (x | (x * 2**8)) & 0x00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff00ff;
-            x = (x | (x * 2**4)) & 0x0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f;
-
-            // Hex encode
-            h = (x & 0x0808080808080808080808080808080808080808080808080808080808080808) / 8;
-            i = (x & 0x0404040404040404040404040404040404040404040404040404040404040404) / 4;
-            j = (x & 0x0202020202020202020202020202020202020202020202020202020202020202) / 2;
-            x = x + (h & (i | j)) * 0x27 + 0x3030303030303030303030303030303030303030303030303030303030303030;
-
-            return abi.encodePacked(first, x);
+            return bytes32(x);
         }
+    }
+
+    /**
+     * @notice Returns utf8-encoded bytes32 string that can be read via web3.utils.hexToUtf8.
+     * @dev Will return bytes32 in all lower case hex characters and without the leading 0x.
+     * This has minor changes from the toUtf8BytesAddress to control for the size of the input.
+     * @param bytesIn bytes32 to encode.
+     * @return utf8 encoded bytes32.
+     */
+    function toUtf8Bytes(bytes32 bytesIn) internal pure returns (bytes memory) {
+        return abi.encodePacked(toUtf8Bytes32Bottom(bytesIn >> 128), toUtf8Bytes32Bottom(bytesIn));
     }
 
     /**
