64e593a1767e453edd2e136ffed386b355c973d5
Nick Pai <npai.nyc@gmail.com>
2021-10-07 15:28:16 -0400

    fix tests

--- packages/core/contracts/insured-bridge/test/Arbitrum_InboxMock.sol
+++ packages/core/contracts/insured-bridge/test/Arbitrum_InboxMock.sol
@@ -2,15 +2,17 @@ pragma solidity ^0.8.0;
 import "../../external/avm/interfaces/iArbitrum_Inbox.sol";
 
 contract Arbitrum_InboxMock is iArbitrum_Inbox {
+    // We leave these unused function parameters named because this contract is used with smockit and makes testing
+    // this function's call inputs easier.
     function createRetryableTicketNoRefundAliasRewrite(
-        address,
-        uint256,
-        uint256,
-        address,
-        address,
-        uint256,
-        uint256,
-        bytes calldata
+        address destAddr,
+        uint256 l2CallValue,
+        uint256 maxSubmissionCost,
+        address excessFeeRefundAddress,
+        address callValueRefundAddress,
+        uint256 maxGas,
+        uint256 gasPriceBid,
+        bytes calldata data
     ) external payable override returns (uint256) {
         return 1;
     }
