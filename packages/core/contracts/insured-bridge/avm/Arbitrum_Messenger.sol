34e786f0a4ee90132bf3d1fffc7733596117dad5
Nick Pai <npai.nyc@gmail.com>
2021-10-08 14:42:37 -0400

    Update Arbitrum_Messenger.sol

--- packages/core/contracts/insured-bridge/avm/Arbitrum_Messenger.sol
+++ packages/core/contracts/insured-bridge/avm/Arbitrum_Messenger.sol
@@ -19,11 +19,13 @@ contract Arbitrum_Messenger is Ownable, Arbitrum_CrossDomainEnabled, MessengerIn
     constructor(address _inbox) Arbitrum_CrossDomainEnabled(_inbox) {}
 
     /**
-     * @notice Sends a message to an account on L2.
+     * @notice Sends a message to an account on L2. If this message reverts on l2 for any reason it can either be
+     * resent on L1, or redeemed on L2 manually. To learn more see how "retryable tickets" work on Arbitrum
+     * https://developer.offchainlabs.com/docs/l1_l2_messages#parameters
      * @param target The intended recipient on L2.
      * @param userToRefund User on L2 to refund extra fees to.
      * @param l1CallValue Amount of ETH deposited to `target` contract on L2. Used to pay for L2 submission fee and
-     * l2CallValue.
+     * l2CallValue. This will usually be > 0.
      * @param gasLimit The gasLimit for the receipt of the message on L2.
      * @param gasPrice Gas price bid for L2 execution.
      * @param maxSubmissionCost: Max gas deducted from user's L2 balance to cover base submission fee.
