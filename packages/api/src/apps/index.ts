0be76650359dca84f58167466d59a9e3cfa16f9c
amateima <amatei@umaproject.org>
2021-10-20 14:11:03 +0300

    Add the ingestor app
    Signed-off-by: amateima <amatei@umaproject.org>

--- packages/api/src/apps/index.ts
+++ packages/api/src/apps/index.ts
@@ -1,3 +1,4 @@
 export { default as api } from "./api";
 export { default as lsp_api } from "./lsp_api";
 export { default as datastore_api } from "./datastore_api";
+export { default as ingestor_app } from "./ingestor_app";
