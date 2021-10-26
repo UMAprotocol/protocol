043f7c2083a2cff89c3dcee93dc51d8c8df018d6
David A <david@umaproject.org>
2021-10-26 13:16:26 -0400

    feat(sdk): add across bridge pool read only client (#3493)
    * feat(sdk): add across bridge pool read only client
    * improve(sdk): convert bridgepool clients to classes

--- packages/sdk/src/across/index.ts
+++ packages/sdk/src/across/index.ts
@@ -2,3 +2,4 @@ export * as feeCalculator from "./feeCalculator";
 export { default as gasFeeCalculator } from "./gasFeeCalculator";
 export * as utils from "./utils";
 export * as constants from "./constants";
+export * as clients from "./clients";
