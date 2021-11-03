2a61154a65ce20c116709e5c4a6d76dac89d3ba8
Nick Pai <npai.nyc@gmail.com>
2021-11-03 12:25:35 -0400

    optimize(insured-bridge-relayer): Hardcode deployment block height

--- packages/insured-bridge-relayer/src/RelayerConfig.ts
+++ packages/insured-bridge-relayer/src/RelayerConfig.ts
@@ -22,14 +22,26 @@ const supportedChainIds = [
 // because it cannot validate a relay's realizedLpFeePct properly. We need to therefore know which deposit quote times
 // are off-limits and need to either be ignored or auto-disputed. This is a hack because ideally the bridgePool sets
 // a timestamp on construction. Then we can easily query on-chain whether a deposit.quoteTime is before this timestamp.
-const bridgePoolDeployTimestamps = {
+// The alternative to using this hard-coded mapping is to call `web3.eth.getCode(bridgePoolAddress, blockNumber)` but
+// making a web3 call per relay is expensive. Therefore this mapping is also a runtime optimization where we can
+// eliminate web3 calls. This is also why we store block numbers for the deploy timestamp.
+const bridgePoolDeployData = {
   // Note: keyed by L1 token.
   // WETH:
-  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": 1635305400,
+  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": {
+    timestamp: 1635305400,
+    blockNumber: 13495915,
+  },
   // USDC:
-  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": 1635312600,
+  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": {
+    timestamp: 1635312600,
+    blockNumber: 13496441,
+  },
   // UMA:
-  "0x04fa0d235c4abf4bcf4787af4cf447de572ef828": 1635312600,
+  "0x04fa0d235c4abf4bcf4787af4cf447de572ef828": {
+    timestamp: 1635312600,
+    blockNumber: 13496464,
+  },
 };
 
 export interface ProcessEnv {
@@ -54,7 +66,7 @@ export class RelayerConfig {
   readonly activatedChainIds: number[];
   readonly l2BlockLookback: number;
   readonly botModes: BotModes;
-  readonly deployTimestamps: { [key: string]: number };
+  readonly deployTimestamps: { [key: string]: { timestamp: number; blockNumber: number } };
 
   constructor(env: ProcessEnv) {
     const {
@@ -90,7 +102,7 @@ export class RelayerConfig {
     this.errorRetries = ERROR_RETRIES ? Number(ERROR_RETRIES) : 3;
     this.errorRetriesTimeout = ERROR_RETRIES_TIMEOUT ? Number(ERROR_RETRIES_TIMEOUT) : 1;
 
-    this.deployTimestamps = DEPLOY_TIMESTAMPS ? JSON.parse(DEPLOY_TIMESTAMPS) : bridgePoolDeployTimestamps;
+    this.deployTimestamps = DEPLOY_TIMESTAMPS ? JSON.parse(DEPLOY_TIMESTAMPS) : bridgePoolDeployData;
 
     assert(RATE_MODELS, "RATE_MODELS required");
     const processingRateModels = JSON.parse(RATE_MODELS);
