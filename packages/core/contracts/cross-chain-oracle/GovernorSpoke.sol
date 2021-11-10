24b43c1f7425d34cdf077f025b34655ba5d97a04
Nick Pai <npai.nyc@gmail.com>
2021-11-10 08:46:21 -0500

    Remove inline assembly

--- packages/core/contracts/cross-chain-oracle/GovernorSpoke.sol
+++ packages/core/contracts/cross-chain-oracle/GovernorSpoke.sol
@@ -34,25 +34,7 @@ contract GovernorSpoke is Lockable {
      */
     function processMessageFromParent(bytes memory data) public nonReentrant() onlyMessenger() {
         (address to, bytes memory inputData) = abi.decode(data, (address, bytes));
-        require(_executeCall(to, inputData), "execute call failed");
+        require(to.call(data), "execute call failed");
         emit ExecutedGovernanceTransaction(to, inputData);
-    }
-
-    // Note: this snippet of code is copied from Governor.sol.
-    function _executeCall(address to, bytes memory data) private returns (bool) {
-        // Note: this snippet of code is copied from Governor.sol.
-        // solhint-disable-next-line max-line-length
-        // https://github.com/gnosis/safe-contracts/blob/59cfdaebcd8b87a0a32f87b50fead092c10d3a05/contracts/base/Executor.sol#L23-L31
-        // solhint-disable-next-line no-inline-assembly
-
-        bool success;
-        assembly {
-            let inputData := add(data, 0x20)
-            let inputDataSize := mload(data)
-            // Hardcode value to be 0 for relayed governance calls in order to avoid addressing complexity of bridging
-            // value cross-chain.
-            success := call(gas(), to, 0, inputData, inputDataSize, 0, 0)
-        }
-        return success;
     }
 }
