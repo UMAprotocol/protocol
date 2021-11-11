f865eb6abf762e43aef9e6dcee1c75873dd5016e
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-11-11 10:38:47 -0500

    feat(cross-chain-oracle): Generalized cross-chain oracle and governance contracts (#3586)
    * feat(cross-chain-oracle): Generalized cross-chain oracle and governance contracts
    
    * Clean up comments and add public requestPrice method to OracleHub
    
    * Update packages/core/contracts/cross-chain-oracle/GovernorHub.sol
    
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>
    
    * Add to interface
    
    * Add ancillaryBytesLimit to constants
    
    * Add comments
    
    * Remove inline assembly
    
    * Licenses
    
    * Update GovernorSpoke.sol
    
    * Re add inline assembly
    
    * Overload OracleSpoke functions for backwards compatibility
    
    * remove requirement that messenger address != 0x0
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * add todo about multicall
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * replace @title with @notice
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    * replace 0x0 with ''
    
    Signed-off-by: Nick Pai <npai.nyc@gmail.com>
    
    Co-authored-by: chrismaree <christopher.maree@gmail.com>

---
+++ packages/core/contracts/cross-chain-oracle/test/OracleBaseMock.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "../OracleBase.sol";
+
+/**
+ * @title Test implementation of OracleBase enabling unit tests on internal methods.
+ */
+contract OracleBaseMock is OracleBase {
+    constructor(address _finderAddress) OracleBase(_finderAddress) {}
+
+    function requestPrice(
+        bytes32 identifier,
+        uint256 time,
+        bytes memory ancillaryData
+    ) public returns (bool) {
+        return _requestPrice(identifier, time, ancillaryData);
+    }
+
+    function encodePriceRequest(
+        bytes32 identifier,
+        uint256 time,
+        bytes memory ancillaryData
+    ) public pure returns (bytes32) {
+        return _encodePriceRequest(identifier, time, ancillaryData);
+    }
+
+    function publishPrice(
+        bytes32 identifier,
+        uint256 time,
+        bytes memory ancillaryData,
+        int256 price
+    ) public {
+        _publishPrice(identifier, time, ancillaryData, price);
+    }
+}
