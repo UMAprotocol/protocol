732ff78ad38e2b17affb51195602b61864b87d40
amateima <89395931+amateima@users.noreply.github.com>
2021-11-24 19:37:32 +0200

    feat(sdk): add L1 -> L2 Optimism Bridge Client (#3623)
    * Add Optimism Bridge contracts
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Add deposit function
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Change Solidity version
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Add deposit eth
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Export optimism bridge
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Move client inside across dir
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Fix import error
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Revert package.json
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Update types
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Fix typo
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Rename contracts
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Rename hardcoded values
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Add chainId assertion
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Delete development network
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Add README instructions
    
    Signed-off-by: amateima <amatei@umaproject.org>
    
    * Add comment
    
    Signed-off-by: amateima <amatei@umaproject.org>

---
+++ packages/core/contracts/external/optimism-bridge/interfaces/OptimismL1StandardBridge.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/**
+ * @title L1StandardBridge
+ * @dev The L1 ETH and ERC20 Bridge is a contract which stores deposited L1 funds and standard
+ * tokens that are in use on L2. It synchronizes a corresponding L2 Bridge, informing it of deposits
+ * and listening to it for newly finalized withdrawals.
+ *
+ */
+interface OptimismL1StandardBridge {
+    function depositETH(uint32 _l2Gas, bytes calldata _data) external payable;
+
+    function depositERC20(
+        address _l1Token,
+        address _l2Token,
+        uint256 _amount,
+        uint32 _l2Gas,
+        bytes calldata _data
+    ) external;
+}
