5e6c2a622d70df4b232138ebfbc431d2f9ccd870
chrismaree <christopher.maree@gmail.com>
2021-12-02 17:52:00 +0200

    fixed comment
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/core/contracts/insured-bridge/non-bridge-helpers/BridgePoolSynchronizer.sol
+++ packages/core/contracts/insured-bridge/non-bridge-helpers/BridgePoolSynchronizer.sol
@@ -8,7 +8,7 @@ interface BridgePoolLike {
 
 /**
  * @notice Small helper contract to make calling sync methods on a set of bridge pools easier. When one pool needs to be
- * sync it is likely that all ecosystem pools also need to by sync. This contract lets the caller do this in a batch.
+ * sync it is likely that all ecosystem pools also need to be synced. This contract lets the caller do this in a batch.
  */
 contract BridgePoolSynchronizer {
     function syncUmaEcosystemParams(BridgePoolLike[] memory bridgePools) public {
