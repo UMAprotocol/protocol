94805aec794eafc826e29fc78d0af37b2628b39d
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-12-03 11:11:10 -0500

    improve(across): Improve L2 client provider redundancy (#3653)
    * WIP
    
    * Update Relayer.ts
    
    * refactor(across-relayer): Remove event querying from relayer code
    
    * fix
    
    * construct fallback l2 web3 array from RETRY_CONFIG env var
    
    * Update index.ts
    
    * Update ProviderUtils.ts
    
    * Update InsuredBridgeL2Client.ts
    
    * Update InsuredBridgeL2Client.ts
    
    * Update InsuredBridgeL2Client.ts
    
    * Update InsuredBridgeL2Client.ts
    
    * Return union of event objects
    
    * Update InsuredBridgeL2Client.ts
    
    * Add `filter` to event search config
    
    * Update InsuredBridgeL2Client.ts
    
    * Move getEventsForMultipleProviders to helpers file
    
    * Update contractEvents.ts
    
    * Update contractEvents.ts
    
    * Move to common/
    
    * make error log more detailed and add unit test for error log
    
    * Refactor
    
    * Update ContractUtils.ts
    
    * fix
    
    * Update InsuredBridgeL2Client.ts
    
    * shorten error message
    
    * Update InsuredBridgeL2Client.js
    
    * Update InsuredBridgeL2Client.js

--- packages/insured-bridge-relayer/src/index.ts
+++ packages/insured-bridge-relayer/src/index.ts
@@ -4,7 +4,7 @@ import retry from "async-retry";
 import { config } from "dotenv";
 import assert from "assert";
 
-import { getWeb3, getWeb3ByChainId } from "@uma/common";
+import { getWeb3, getWeb3ByChainId, getRetryWeb3sByChainId } from "@uma/common";
 import {
   GasEstimator,
   Logger,
@@ -53,13 +53,21 @@ export async function run(logger: winston.Logger, l1Web3: Web3): Promise<void> {
     const l2Web3 = getWeb3ByChainId(config.activatedChainIds[0]);
     const latestL2BlockNumber = await l2Web3.eth.getBlockNumber();
     const l2StartBlock = Math.max(0, latestL2BlockNumber - config.l2BlockLookback);
-
+    const fallbackL2Web3s = getRetryWeb3sByChainId(config.activatedChainIds[0]);
+    // Note: This will not construct duplicate Web3 objects for URL's in the Retry config that are the same as the
+    // one used to construct l2Web3.
+    logger.debug({
+      at: "AcrossRelayer#index",
+      message: `Constructed ${fallbackL2Web3s.length} fallback L2 web3 providers`,
+    });
     const l2Client = new InsuredBridgeL2Client(
       logger,
       l2Web3,
       await l1Client.getL2DepositBoxAddress(config.activatedChainIds[0]),
       config.activatedChainIds[0],
-      l2StartBlock
+      l2StartBlock,
+      null,
+      fallbackL2Web3s
     );
 
     // Update the L2 client and filter out tokens that are not whitelisted on the L2 from the whitelisted
