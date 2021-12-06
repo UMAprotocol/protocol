05c79904086a2f4ab635aa64ff28d8e63eb427c0
Nick Pai <npai.nyc@gmail.com>
2021-12-06 09:36:00 -0500

    feat(across): Add contract to map L1 tokens to rate models

--- packages/core/contracts/insured-bridge/RateModelStore.sol
+++ packages/core/contracts/insured-bridge/RateModelStore.sol
@@ -2,14 +2,17 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "../common/implementation/MultiCaller.sol";
 
 /**
  * @title Maps rate model objects to L1 token.
  * @dev This contract is designed to be queried by off-chain relayers that need to compute realized LP fee %'s before
  * submitting relay transactions to a BridgePool contract. Therefore, this contract does not perform any validation on
- * the shape of the rate model. The validation step is left as a responsibility to off-chain relayers.
+ * the shape of the rate model, which is stored as a string to enable arbitrary data encoding via a stringified JSON
+ * object. This leaves this contract unopionated on the parameters within the rate model, enabling governance to adjust
+ * the structure in the future.
  */
-contract RateModelStore is Ownable {
+contract RateModelStore is Ownable, MultiCaller {
     mapping(address => string) public l1TokenRateModels;
 
     event UpdatedRateModel(address indexed l1Token, string rateModel);
