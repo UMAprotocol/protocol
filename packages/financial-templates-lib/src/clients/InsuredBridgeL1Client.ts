a6dbe77badc75a2089c935d004f92a847dcdfeda
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2021-12-08 10:42:50 -0500

    improve(across-relayer): Additional logging when computing realized LP fee % (#3670)
    * improve(across-relayer): Additional logging when computing realized LP fee %
    
    * Update InsuredBridgeL1Client.ts
    
    * Revert "Update InsuredBridgeL1Client.ts"
    
    This reverts commit a31fd7c604936b3519182b958585f6446eb0ae96.

--- packages/financial-templates-lib/src/clients/InsuredBridgeL1Client.ts
+++ packages/financial-templates-lib/src/clients/InsuredBridgeL1Client.ts
@@ -184,6 +184,16 @@ export class InsuredBridgeL1Client {
       bridgePool.methods.liquidityUtilizationPostRelay(deposit.amount.toString()).call(undefined, quoteBlockNumber),
     ]);
 
+    this.logger.debug({
+      at: "InsuredBridgeL1Client",
+      message: "Computed realized LP fee % for deposit",
+      deposit,
+      quoteBlockNumber,
+      liquidityUtilizationCurrent: liquidityUtilizationCurrent.toString(),
+      liquidityUtilizationPostRelay: liquidityUtilizationPostRelay.toString(),
+      rateModel: this.rateModels[deposit.l1Token],
+    });
+
     return calculateRealizedLpFeePct(
       this.rateModels[deposit.l1Token],
       toBN(liquidityUtilizationCurrent),
