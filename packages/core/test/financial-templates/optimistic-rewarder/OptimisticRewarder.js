1bf696ef52073ee55afff8a2902982f86d78a582
chrismaree <christopher.maree@gmail.com>
2021-12-14 22:29:26 +0200

    removed comment
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/core/test/financial-templates/optimistic-rewarder/OptimisticRewarder.js
+++ packages/core/test/financial-templates/optimistic-rewarder/OptimisticRewarder.js
@@ -423,7 +423,6 @@ describe("OptimisticRewarder", () => {
     const { request, timestamp, ancillaryData } = (
       await findEvent(disputeReceipt, optimisticOracle, "DisputePrice")
     ).match.returnValues;
-    console.log("EXpected", tokenId, request.expirationTime.toString());
     await assertEventEmitted(
       disputeReceipt,
       optimisticRewarder,
