4eae69d9814a220987b3011dd9d45b88e3962fbb
David A <david@umaproject.org>
2021-12-15 08:52:37 -0500

    feat(sdk)!: add improved gas fee function (#3700)
    * feat(sdk)!: add improved gas fee function
    BREAKING CHANGE: calls to previous gas estimator must migrate to new interface
    * improve(sdk): improve gas fee type, docs and e2e test

--- packages/sdk/src/across/index.ts
+++ packages/sdk/src/across/index.ts
@@ -1,6 +1,6 @@
 export * as feeCalculator from "./feeCalculator";
 export { default as LpFeeCalculator } from "./lpFeeCalculator";
-export { default as gasFeeCalculator } from "./gasFeeCalculator";
+export * as gasFeeCalculator from "./gasFeeCalculator";
 export * as utils from "./utils";
 export * as constants from "./constants";
 export * as clients from "./clients";
