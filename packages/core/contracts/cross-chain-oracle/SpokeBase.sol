fd79c49a743ae885cddad1a52cf3383ee5a6761b
Nick Pai <npai.nyc@gmail.com>
2021-12-28 09:04:23 -0500

    Add HasFinder base contract

--- packages/core/contracts/cross-chain-oracle/SpokeBase.sol
+++ packages/core/contracts/cross-chain-oracle/SpokeBase.sol
@@ -1,26 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
-
-import "../oracle/interfaces/FinderInterface.sol";
-
 import "./interfaces/ChildMessengerInterface.sol";
 
 import "../oracle/implementation/Constants.sol";
+import "../common/implementation/HasFinder.sol";
 
 /**
  * @title Cross-chain Oracle L2 Spoke Base.
  * @notice Provides access control to Governance and Oracle spoke L2 contracts.
  */
 
-contract SpokeBase {
-    // Note: This is private because `OracleSpoke` inherits both `OracleBase` and this contract and there cannot be
-    // two public `finder` global variables.
-    FinderInterface private finder;
-
-    constructor(address _finderAddress) {
-        finder = FinderInterface(_finderAddress);
-    }
-
+abstract contract SpokeBase is HasFinder {
     modifier onlyMessenger() {
         require(msg.sender == address(getChildMessenger()), "Caller must be messenger");
         _;
