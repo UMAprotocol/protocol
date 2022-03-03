ad9b28c44e70ebdeeb6321645112b1217196c20c
chrismaree <christopher.maree@gmail.com>
2022-03-03 12:24:36 -0500

    nit
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/insured-bridge-relayer/src/CrossDomainFinalizer.ts
+++ packages/insured-bridge-relayer/src/CrossDomainFinalizer.ts
@@ -114,7 +114,7 @@ export class CrossDomainFinalizer {
   async checkForConfirmedL2ToL1RelaysAndFinalize() {
     // Fetch all whitelisted L2 tokens. Append the ETH address on Optimism to the whitelist to enable finalization of
     // Optimism -> Ethereum bridging actions. This is needed as we send ETH over the Optimism bridge, not WETH.
-    // Also, remove the DAI optimism address from the whitelist as we don't want to finalize DAI actions due to a bug.
+    // Also, remove the DAI Optimism address from the whitelist as we don't want to finalize DAI actions due to a bug.
     const whitelistedL2Tokens = [
       ...this.l1Client.getWhitelistedL2TokensForChainId(this.l2Client.chainId.toString()),
       "0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000", // Append L2ETH on Optimism address.
