fb456fd74e9cd3874889e6031d6cc9e206878339
chrismaree <christopher.maree@gmail.com>
2022-04-19 13:21:13 +0200

    feat: update dockerfile
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- scripts/runCommand.sh
+++ scripts/runCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Simple script that simply runs a command input as an environment variable. Should be run from the core/ directory.
-# Note: this is used to allow the docker image to run arbitrary commands by specifying them in the environment rather
-# than the `docker run` command.
-$COMMAND
+# Simple script that simply runs a command input as an environment variable. This is used to allow the docker image to
+# run arbitrary commands by specifying them in the environment rather than the docker run command. Optionally, specify
+#  UMA_PACKAGE as an ENV which lets you navigate to additional base packages installed within the container.
+if [ -z ${UMA_PACKAGE+x} ]; then $COMMAND; else cd ../${UMA_PACKAGE} && $COMMAND; fi
