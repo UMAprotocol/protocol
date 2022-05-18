5833fce724930ba27a01855dcbaf03bcfe7fa7a2
John Shutt <pemulis@users.noreply.github.com>
2022-05-17 18:32:02 -0700

    fix: N-11, Unnecessary imports
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- packages/core/contracts/zodiac/OptimisticGovernor.sol
+++ packages/core/contracts/zodiac/OptimisticGovernor.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../oracle/implementation/Constants.sol";
 import "../oracle/interfaces/FinderInterface.sol";
 import "../oracle/interfaces/OptimisticOracleInterface.sol";
-import "../oracle/implementation/OptimisticOracle.sol";
 import "../common/implementation/Lockable.sol";
 import "../common/interfaces/AddressWhitelistInterface.sol";
 import "../oracle/interfaces/IdentifierWhitelistInterface.sol";
