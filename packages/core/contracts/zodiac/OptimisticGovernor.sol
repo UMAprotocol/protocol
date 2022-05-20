cbb56486bb56fd1493e51849ba7fb934a927518d
John Shutt <pemulis@users.noreply.github.com>
2022-05-20 09:07:50 -0700

    fix: M-01 Change of collateral could result in unintended bond value (#3912)
    * fix: M-01
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    * improve: add events for SetBond and SetCollateral
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    * improve: inline comment on valid zero bonds
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    * improve: review notes
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    * improve: rename _bond to _bondAmount
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    * improve: rename bond to bondAmount
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    * fix: remove finder from initializeParams
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- packages/core/contracts/zodiac/OptimisticGovernor.sol
+++ packages/core/contracts/zodiac/OptimisticGovernor.sol
@@ -34,6 +34,8 @@ contract OptimisticGovernor is Module, Lockable {
 
     event ProposalDeleted(uint256 indexed proposalId, address indexed sender, bytes32 indexed status);
 
+    event SetBond(IERC20 indexed collateral, uint256 indexed bondAmount);
+
     event SetCollateral(IERC20 indexed collateral);
 
     event SetBond(uint256 indexed bond);
@@ -50,7 +52,7 @@ contract OptimisticGovernor is Module, Lockable {
     IERC20 public collateral;
     uint64 public liveness;
     // Extra bond in addition to the final fee for the collateral type.
-    uint256 public bond;
+    uint256 public bondAmount;
     string public rules;
     // This will usually be "ZODIAC" but a deployer may want to create a more specific identifier.
     bytes32 public identifier;
@@ -78,7 +80,7 @@ contract OptimisticGovernor is Module, Lockable {
      * @param _finder Finder address.
      * @param _owner Address of the owner.
      * @param _collateral Address of the ERC20 collateral used for bonds.
-     * @param _bond Additional bond required, beyond the final fee.
+     * @param _bondAmount Additional bond required, beyond the final fee.
      * @param _rules Reference to the rules for the Gnosis Safe (e.g., IPFS hash or URI).
      * @param _identifier The approved identifier to be used with the contract, usually "ZODIAC".
      * @param _liveness The period, in seconds, in which a proposal can be disputed.
@@ -88,12 +90,12 @@ contract OptimisticGovernor is Module, Lockable {
         address _finder,
         address _owner,
         address _collateral,
-        uint256 _bond,
+        uint256 _bondAmount,
         string memory _rules,
         bytes32 _identifier,
         uint64 _liveness
     ) {
-        bytes memory initializeParams = abi.encode(_owner, _collateral, _bond, _rules, _identifier, _liveness);
+        bytes memory initializeParams = abi.encode(_owner, _collateral, _bondAmount, _rules, _identifier, _liveness);
         require(_finder != address(0), "finder address can not be empty");
         finder = FinderInterface(_finder);
         setUp(initializeParams);
@@ -104,14 +106,14 @@ contract OptimisticGovernor is Module, Lockable {
         (
             address _owner,
             address _collateral,
-            uint256 _bond,
+            uint256 _bondAmount,
             string memory _rules,
             bytes32 _identifier,
             uint64 _liveness
         ) = abi.decode(initializeParams, (address, address, uint256, string, bytes32, uint64));
         require(_getCollateralWhitelist().isOnWhitelist(address(_collateral)), "bond token not supported");
         collateral = IERC20(_collateral);
-        bond = _bond;
+        bondAmount = _bondAmount;
         rules = _rules;
         require(_getIdentifierWhitelist().isIdentifierSupported(_identifier), "identifier not supported");
         identifier = _identifier;
@@ -126,24 +128,19 @@ contract OptimisticGovernor is Module, Lockable {
     }
 
     /**
-     * @notice Sets the bond amount for proposals.
-     * @param _bond amount of the bond token that will need to be paid for future proposals.
-     */
-    function setBond(uint256 _bond) public onlyOwner {
-        // Value of the bond required for proposals, in addition to the final fee.
-        bond = _bond;
-        emit SetBond(_bond);
-    }
-
-    /**
-     * @notice Sets the collateral token (and bond token) for future proposals.
+     * @notice Sets the collateral and bond amount for proposals.
      * @param _collateral token that will be used for all bonds for the contract.
+     * @param _bondAmount amount of the bond token that will need to be paid for future proposals.
      */
-    function setCollateral(IERC20 _collateral) public onlyOwner {
+    function setCollateralAndBond(IERC20 _collateral, uint256 _bondAmount) public onlyOwner {
         // ERC20 token to be used as collateral (must be approved by UMA Store contract).
         require(_getCollateralWhitelist().isOnWhitelist(address(_collateral)), "bond token not supported");
         collateral = _collateral;
-        emit SetCollateral(_collateral);
+
+        // Value of the bond required for proposals, in addition to the final fee. A bond of zero is
+        // acceptable, in which case the Optimistic Oracle will require the final fee as the bond.
+        bondAmount = _bondAmount;
+        emit SetBond(_collateral, _bondAmount);
     }
 
     /**
@@ -226,7 +223,7 @@ contract OptimisticGovernor is Module, Lockable {
         // Propose a set of transactions to the OO. If not disputed, they can be executed with executeProposal().
         // docs: https://github.com/UMAprotocol/protocol/blob/master/packages/core/contracts/oracle/interfaces/OptimisticOracleInterface.sol
         optimisticOracle.requestPrice(identifier, time, ancillaryData, collateral, 0);
-        uint256 totalBond = optimisticOracle.setBond(identifier, time, ancillaryData, bond);
+        uint256 totalBond = optimisticOracle.setBond(identifier, time, ancillaryData, bondAmount);
         optimisticOracle.setCustomLiveness(identifier, time, ancillaryData, liveness);
 
         // Get the bond from the proposer and approve the bond and final fee to be used by the oracle.
