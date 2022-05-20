e7ec38c27f9e597e7e03c6db2714f68a1cd336d1
John Shutt <pemulis@users.noreply.github.com>
2022-05-20 07:54:43 -0700

    fix: L-04, Proposals can be deleted repeatedly (#3918)
    * fix: L-04, Proposals can be deleted repeatedly
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    * Update packages/core/contracts/zodiac/OptimisticGovernor.sol
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    
    * Update packages/core/contracts/zodiac/OptimisticGovernor.sol
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    
    * chore: spacing change
    
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>
    
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/core/contracts/zodiac/OptimisticGovernor.sol
+++ packages/core/contracts/zodiac/OptimisticGovernor.sol
@@ -292,6 +292,8 @@ contract OptimisticGovernor is Module, Lockable {
      * @param _proposalId the id of the proposal being deleted.
      */
     function deleteProposal(uint256 _proposalId) external onlyOwner {
+        // Check that proposal exists and was not already deleted.
+        require(proposalHashes[_proposalId] != bytes32(0), "Proposal does not exist");
         delete proposalHashes[_proposalId];
         emit ProposalDeleted(_proposalId, msg.sender, "DeletedByAdmin");
     }
@@ -302,6 +304,9 @@ contract OptimisticGovernor is Module, Lockable {
      * @param _originalTime the time the proposal was made.
      */
     function deleteRejectedProposal(uint256 _proposalId, uint256 _originalTime) external {
+        // Check that proposal exists and was not already deleted.
+        require(proposalHashes[_proposalId] != bytes32(0), "Proposal does not exist");
+
         // Construct the ancillary data.
         bytes memory ancillaryData = AncillaryData.appendKeyValueUint("", "id", _proposalId);
 
