679fbd5eb8e737d637873047a57582a9ddb55ba5
chrismaree <christopher.maree@gmail.com>
2022-06-16 11:19:40 +0200

    wip
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/core/contracts/oracle/implementation/test/SkinnyOptimisticRequesterTest.sol
+++ packages/core/contracts/oracle/implementation/test/SkinnyOptimisticRequesterTest.sol
@@ -6,14 +6,14 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "../../interfaces/StoreInterface.sol";
 import "../../interfaces/FinderInterface.sol";
-import "../SkinnyOptimisticOracleV2.sol";
+import "../previous-versions/SkinnyOptimisticOracle.sol";
 import "../Constants.sol";
 
 // This is just a test contract to make requests to the optimistic oracle.
 contract SkinnyOptimisticRequesterTest {
     using SafeMath for uint256;
 
-    SkinnyOptimisticOracleV2 optimisticOracle;
+    SkinnyOptimisticOracle optimisticOracle;
     bool public shouldRevert = false;
 
     // Finder to provide addresses for DVM contracts.
@@ -23,43 +23,16 @@ contract SkinnyOptimisticRequesterTest {
     bytes32 public identifier;
     uint32 public timestamp;
     bytes public ancillaryData;
-    SkinnyOptimisticOracleV2.Request public request;
+    SkinnyOptimisticOracle.Request public request;
 
     // Manually set an expiration timestamp to simulate expiry price requests
     uint256 public expirationTimestamp;
 
-    constructor(SkinnyOptimisticOracleV2 _optimisticOracle, FinderInterface _finderAddress) {
+    constructor(SkinnyOptimisticOracle _optimisticOracle, FinderInterface _finderAddress) {
         optimisticOracle = _optimisticOracle;
         finder = _finderAddress;
     }
 
-    function requestAndProposePriceFor(
-        bytes32 _identifier,
-        uint32 _timestamp,
-        bytes memory _ancillaryData,
-        IERC20 currency,
-        uint256 reward,
-        SkinnyOptimisticOracleV2Interface.RequestSettings memory requestSettings,
-        address proposer,
-        int256 proposedPrice
-    ) public {
-        uint256 finalFee = _getStore().computeFinalFee(address(currency)).rawValue;
-
-        currency.approve(address(optimisticOracle), reward.add(requestSettings.bond).add(finalFee));
-        optimisticOracle.requestAndProposePriceFor(
-            _identifier,
-            _timestamp,
-            _ancillaryData,
-            currency,
-            reward,
-            requestSettings,
-            proposer,
-            proposedPrice
-        );
-    }
-
-    // Overload this method to enable backwards compatibility for dependent tests. Set Request settings to enable
-    // callbacks to maintain the previous functionality of this test contract.
     function requestAndProposePriceFor(
         bytes32 _identifier,
         uint32 _timestamp,
@@ -71,19 +44,17 @@ contract SkinnyOptimisticRequesterTest {
         address proposer,
         int256 proposedPrice
     ) external {
-        requestAndProposePriceFor(
+        uint256 finalFee = _getStore().computeFinalFee(address(currency)).rawValue;
+
+        currency.approve(address(optimisticOracle), reward.add(bond).add(finalFee));
+        optimisticOracle.requestAndProposePriceFor(
             _identifier,
             _timestamp,
             _ancillaryData,
             currency,
             reward,
-            SkinnyOptimisticOracleV2Interface.RequestSettings({
-                callbackOnPriceProposed: true,
-                callbackOnPriceDisputed: true,
-                callbackOnPriceSettled: true,
-                bond: bond,
-                customLiveness: customLiveness
-            }),
+            bond,
+            customLiveness,
             proposer,
             proposedPrice
         );
@@ -95,37 +66,11 @@ contract SkinnyOptimisticRequesterTest {
         bytes memory _ancillaryData,
         IERC20 currency,
         uint256 reward,
-        SkinnyOptimisticOracleV2Interface.RequestSettings memory requestSettings
-    ) public {
-        currency.approve(address(optimisticOracle), reward);
-        optimisticOracle.requestPrice(_identifier, _timestamp, _ancillaryData, currency, reward, requestSettings);
-    }
-
-    // Overload this method to enable backwards compatibility for dependent tests. Set Request settings to enable
-    // callbacks to maintain the previous functionality of this test contract.
-    function requestPrice(
-        bytes32 _identifier,
-        uint32 _timestamp,
-        bytes memory _ancillaryData,
-        IERC20 currency,
-        uint256 reward,
         uint256 bond,
         uint256 customLiveness
     ) external {
-        requestPrice(
-            _identifier,
-            _timestamp,
-            _ancillaryData,
-            currency,
-            reward,
-            SkinnyOptimisticOracleV2Interface.RequestSettings({
-                callbackOnPriceProposed: true,
-                callbackOnPriceDisputed: true,
-                callbackOnPriceSettled: true,
-                bond: bond,
-                customLiveness: customLiveness
-            })
-        );
+        currency.approve(address(optimisticOracle), reward);
+        optimisticOracle.requestPrice(_identifier, _timestamp, _ancillaryData, currency, reward, bond, customLiveness);
     }
 
     function setExpirationTimestamp(uint256 _expirationTimestamp) external {
@@ -140,7 +85,7 @@ contract SkinnyOptimisticRequesterTest {
         bytes32 _identifier,
         uint32 _timestamp,
         bytes memory _ancillaryData,
-        SkinnyOptimisticOracleV2.Request memory _request
+        SkinnyOptimisticOracle.Request memory _request
     ) external {
         require(!shouldRevert);
         identifier = _identifier;
@@ -153,7 +98,7 @@ contract SkinnyOptimisticRequesterTest {
         bytes32 _identifier,
         uint32 _timestamp,
         bytes memory _ancillaryData,
-        SkinnyOptimisticOracleV2.Request memory _request
+        SkinnyOptimisticOracle.Request memory _request
     ) external {
         require(!shouldRevert);
         identifier = _identifier;
@@ -166,7 +111,7 @@ contract SkinnyOptimisticRequesterTest {
         bytes32 _identifier,
         uint32 _timestamp,
         bytes memory _ancillaryData,
-        SkinnyOptimisticOracleV2.Request memory _request
+        SkinnyOptimisticOracle.Request memory _request
     ) external {
         require(!shouldRevert);
         identifier = _identifier;
