2bdd293ee062913f2862f227abe36a12cf6f0291
Matt Rice <matthewcrice32@gmail.com>
2022-07-18 10:16:17 -0400

    fix tests
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/core/deploy/052_deploy_votingV2.js
+++ packages/core/deploy/052_deploy_votingV2.js
@@ -42,7 +42,7 @@ const func = async function (hre) {
         unstakeCooldown,
         phaseLength,
         minRollToNextRoundLength,
-        gat,
+        gat.toString(),
         VotingToken.address,
         Finder.address,
         SlashingLibrary.address,
