003ad9b7e3832599bebf72ba6b08ab8be50ae3e9
Pablo Maldonado <pablo@umaproject.org>
2022-08-04 16:58:42 +0100

    feat: fix solidity freeze version
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/core/contracts/oracle/implementation/ProposerV2.sol
+++ packages/core/contracts/oracle/implementation/ProposerV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 import "./Finder.sol";
 import "./GovernorV2.sol";
--- packages/core/contracts/oracle/implementation/ResultComputationV2.sol
+++ packages/core/contracts/oracle/implementation/ResultComputationV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 /**
  * @title Computes vote results.
