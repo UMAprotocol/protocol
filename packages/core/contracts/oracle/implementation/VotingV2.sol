97ca84895e094a825c55c4b905e262c771a6c80a
Pablo Maldonado <pablo@umaproject.org>
2022-08-05 12:00:19 +0100

    fix: add uint32 cast
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/core/contracts/oracle/implementation/VotingV2.sol
+++ packages/core/contracts/oracle/implementation/VotingV2.sol
@@ -978,7 +978,9 @@ contract VotingV2 is
 
         uint256 proposalId = spamDeletionProposals.length - 1;
 
-        bytes32 identifier = SpamGuardIdentifierLib._constructIdentifier(proposalId);
+        // Note that for proposalId>= 10^11 the generated identifier will no longer be unique but the manner
+        // in which the priceRequest id is encoded in _encodePriceRequest guarantees its uniqueness.
+        bytes32 identifier = SpamGuardIdentifierLib._constructIdentifier(SafeCast.toUint32(proposalId));
 
         _requestPrice(identifier, currentTime, "", true);
 
@@ -992,7 +994,8 @@ contract VotingV2 is
     function executeSpamDeletion(uint256 proposalId) public {
         require(spamDeletionProposals[proposalId].executed == false);
         spamDeletionProposals[proposalId].executed = true;
-        bytes32 identifier = SpamGuardIdentifierLib._constructIdentifier(proposalId);
+
+        bytes32 identifier = SpamGuardIdentifierLib._constructIdentifier(SafeCast.toUint32(proposalId));
 
         (bool hasPrice, int256 resolutionPrice, ) =
             _getPriceOrError(identifier, spamDeletionProposals[proposalId].requestTime, "");
