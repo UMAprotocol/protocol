160c985d9560b78d41b2998d2ca40f4c1ef297b4
chrismaree <christopher.maree@gmail.com>
2022-08-08 10:43:48 +0200

    fix: [N25]: Remove Unnecessary imports
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/core/contracts/oracle/implementation/Staker.sol
+++ packages/core/contracts/oracle/implementation/Staker.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "../../common/implementation/Testable.sol";
-
 import "./VotingToken.sol";
 import "../interfaces/StakerInterface.sol";
 
