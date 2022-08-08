ea641e0cc2f5b6e1bd65df104467a5e66f7b6c65
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2022-08-08 06:48:23 -0700

    feat: [L09] Unlocked Solidity version pragma (#4075)
    * feat: fix solidity freeze version
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * feat: bump to solidity 0.8.15
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * feat: update hardhat config
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    Co-authored-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/core/contracts/oracle/implementation/DesignatedVotingV2Factory.sol
+++ packages/core/contracts/oracle/implementation/DesignatedVotingV2Factory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.0;
+pragma solidity 0.8.15;
 
 import "../../common/implementation/Withdrawable.sol";
 import "./DesignatedVotingV2.sol";
