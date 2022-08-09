98151e116b8defd7ed85e985996c6fddadaf9201
Pablo Maldonado <pablo@umaproject.org>
2022-08-09 16:39:29 +0200

    feat: add slashing library sol update
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/core/contracts/oracle/implementation/SlashingLibrary.sol
+++ packages/core/contracts/oracle/implementation/SlashingLibrary.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.0;
+pragma solidity 0.8.16;
 
 /**
  * @title Slashing Library contract. Returns the how much a voter should be slashed per staked token as a function of
