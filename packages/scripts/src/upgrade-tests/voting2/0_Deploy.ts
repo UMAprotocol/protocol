3075a6323da94f8f47c20b4a57c01bd25b8300a6
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2022-08-22 09:53:47 -0700

    feat: add migration scripts to voting v2 (#4121)
    * feat: add migration scripts
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * feat: add new voting upgrader contract (#4132)
    
    * feat: add new voting upgrader contract
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * feat: add migration scripts
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * feat: undo simulateVote changes
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * feat: update verify script
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * fix: imports
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * refactor: comments
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * feat: update readme
    
    * feat: simplify migration scripts
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    Co-authored-by: Pablo Maldonado <pablo@umaproject.org>
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    Co-authored-by: Pablo Maldonado <pablo@umaproject.org>

---
+++ packages/scripts/src/upgrade-tests/voting2/0_Deploy.ts
@@ -0,0 +1,102 @@
+// This script deploys SlashingLibrary, VotingV2 and VotingUpgrader.
+// This script can be run against a mainnet fork by spinning a node in a separate terminal with:
+// HARDHAT_CHAIN_ID=1 yarn hardhat node --fork https://mainnet.infura.io/v3/<YOUR-INFURA-KEY> --port 9545 --no-deploy
+// and then running this script with:
+// yarn hardhat run ./src/upgrade-tests/voting2/0_Deploy.ts --network localhost
+
+const hre = require("hardhat");
+
+import {
+  FinderEthers,
+  GovernorEthers,
+  GovernorV2Ethers__factory,
+  SlashingLibraryEthers__factory,
+  VotingEthers,
+  VotingTokenEthers,
+  VotingUpgraderV2Ethers__factory,
+  VotingV2Ethers__factory,
+} from "@uma/contracts-node";
+import { getContractInstance } from "../../utils/contracts";
+import { getMultiRoleContracts, getOwnableContracts } from "./migrationUtils";
+
+const { getContractFactory } = hre.ethers;
+
+async function main() {
+  console.log("Running VotingV2 Deployments🔥");
+
+  const networkId = Number(await hre.getChainId());
+
+  const finder = await getContractInstance<FinderEthers>("Finder");
+  const governor = await getContractInstance<GovernorEthers>("Governor");
+  const existingVoting = await getContractInstance<VotingEthers>("Voting");
+  const votingToken = await getContractInstance<VotingTokenEthers>("VotingToken");
+
+  console.log("1. DEPLOYING SLASHING LIBRARY");
+  const slashingLibraryFactory: SlashingLibraryEthers__factory = await getContractFactory("SlashingLibrary");
+  const slashingLibrary = await slashingLibraryFactory.deploy();
+
+  console.log("Deployed SlashingLibrary: ", slashingLibrary.address);
+
+  console.log("2. DEPLOYING VOTING V2");
+  const votingV2Factory: VotingV2Ethers__factory = await getContractFactory("VotingV2");
+  const emissionRate = "640000000000000000"; // 0.64 UMA per second.
+  const spamDeletionProposalBond = hre.web3.utils.toWei("10000", "ether");
+  const unstakeCooldown = 60 * 60 * 24 * 7; // 7 days
+  const phaseLength = "86400";
+  const minRollToNextRoundLength = "7200";
+  const gat = hre.web3.utils.toBN(hre.web3.utils.toWei("5500000", "ether")); // Set the GAT to 5.5 million tokens.
+
+  const votingV2 = await votingV2Factory.deploy(
+    emissionRate,
+    spamDeletionProposalBond,
+    unstakeCooldown,
+    phaseLength,
+    minRollToNextRoundLength,
+    gat.toString(),
+    "0", // Starting request index of 0 (no offset).
+    votingToken.address,
+    finder.address,
+    slashingLibrary.address
+  );
+
+  console.log("Deployed VotingV2: ", votingV2.address);
+
+  console.log("3. DEPLOYING GOVERNOR V2");
+
+  const governorV2Factory: GovernorV2Ethers__factory = await getContractFactory("GovernorV2");
+
+  const governorV2 = await governorV2Factory.deploy(finder.address, 0);
+
+  console.log("Deployed GovernorV2: ", governorV2.address);
+
+  console.log("4. DEPLOYING VOTING UPGRADER");
+
+  const ownableContractsToMigrate = await getOwnableContracts(networkId);
+
+  const multicallContractsToMigrate = await getMultiRoleContracts(networkId);
+
+  const votingUpgraderFactoryV2: VotingUpgraderV2Ethers__factory = await getContractFactory("VotingUpgraderV2");
+  const votingUpgrader = await votingUpgraderFactoryV2.deploy(
+    governor.address,
+    governorV2.address,
+    existingVoting.address,
+    votingV2.address,
+    finder.address,
+    ownableContractsToMigrate,
+    multicallContractsToMigrate
+  );
+
+  console.log("Deployed VotingUpgrader: ", votingUpgrader.address);
+
+  console.log("Deployment done!🎉");
+}
+
+main().then(
+  () => {
+    process.exit(0);
+  },
+  (err) => {
+    console.error(err);
+    process.exit(1);
+  }
+);
---
+++ packages/scripts/src/upgrade-tests/voting2/1_Propose.ts
@@ -0,0 +1,147 @@
+// This script generates and submits the upgrade transactions from Voting to VotingV2.
+// It is intended to be run after 0_Deploy.ts where the VotingV2 and VotingUpgrader contracts are deployed.
+// This script can be run against a mainnet fork by spinning a node in a separate terminal with:
+// HARDHAT_CHAIN_ID=1 yarn hardhat node --fork https://mainnet.infura.io/v3/<YOUR-INFURA-KEY> --port 9545 --no-deploy
+// and then running this script with:
+// VOTING_UPGRADER_ADDRESS= <VOTING-UPGRADER-ADDRESS> \
+// VOTING_V2_ADDRESS= <VOTING-V2-ADDRESS> \
+// GOVERNOR_V2_ADDRESS= <GOVERNOR-V2-ADDRESS> \
+// yarn hardhat run ./src/upgrade-tests/voting2/1_Propose.ts --network localhost
+
+const hre = require("hardhat");
+
+import { BigNumberish } from "@ethersproject/bignumber";
+import { BytesLike } from "@ethersproject/bytes";
+import {
+  FinderEthers,
+  GovernorEthers,
+  ProposerEthers,
+  VotingEthers,
+  VotingTokenEthers,
+  VotingUpgraderV2Ethers__factory,
+  VotingV2Ethers__factory,
+} from "@uma/contracts-node";
+
+import { getContractInstance } from "../../utils/contracts";
+import { getMultiRoleContracts, getOwnableContracts } from "./migrationUtils";
+const { getAbi } = require("@uma/contracts-node");
+
+const { getContractFactory } = hre.ethers;
+
+const proposerWallet = "0x2bAaA41d155ad8a4126184950B31F50A1513cE25";
+
+async function main() {
+  const proposerSigner = await hre.ethers.getSigner(proposerWallet);
+
+  const networkId = Number(await hre.getChainId());
+
+  const votingUpgraderAddress = process.env["VOTING_UPGRADER_ADDRESS"];
+  const votingV2Address = process.env["VOTING_V2_ADDRESS"];
+  const governorV2Address = process.env["GOVERNOR_V2_ADDRESS"];
+
+  if (!votingV2Address) throw new Error("VOTING_V2_ADDRESS not set");
+  if (!votingUpgraderAddress) throw new Error("VOTING_UPGRADER_ADDRESS not set");
+  if (!governorV2Address) throw new Error("GOVERNOR_V2_ADDRESS not set");
+
+  console.log("Running Voting Upgrade🔥");
+  console.log("1. LOADING DEPLOYED CONTRACT STATE");
+
+  const finder = await getContractInstance<FinderEthers>("Finder");
+  const governor = await getContractInstance<GovernorEthers>("Governor");
+  const proposer = await getContractInstance<ProposerEthers>("Proposer");
+  const existingVoting = await getContractInstance<VotingEthers>("Voting");
+  const votingToken = await getContractInstance<VotingTokenEthers>("VotingToken");
+
+  const votingV2Factory: VotingV2Ethers__factory = await getContractFactory("VotingV2");
+  const votingV2 = await votingV2Factory.attach(votingV2Address);
+
+  const votingUpgraderFactory: VotingUpgraderV2Ethers__factory = await getContractFactory("VotingUpgraderV2");
+  const votingUpgrader = await votingUpgraderFactory.attach(votingUpgraderAddress);
+
+  const ownableContractsToMigrate = await getOwnableContracts(networkId);
+
+  const multicallContractsToMigrate = await getMultiRoleContracts(networkId);
+
+  const adminProposalTransactions: {
+    to: string;
+    value: BigNumberish;
+    data: BytesLike;
+  }[] = [];
+
+  console.log("2. TRANSFERRING OWNERSHIP OF NEW VOTING TO GOVERNORV2");
+
+  await votingV2.transferOwnership(governorV2Address);
+
+  console.log("3. CRAFTING GOVERNOR TRANSACTIONS");
+
+  // Add VotingV2 contract as a minter, so rewards can be minted in the existing token.
+  // Note: this transaction must come before the owner is moved to the new Governor.
+  const minter = "1";
+  const addVotingV2AsTokenMinterTx = await votingToken.populateTransaction.addMember(minter, votingV2.address);
+  if (!addVotingV2AsTokenMinterTx.data) throw "addVotingV2AsTokenMinterTx.data is null";
+  adminProposalTransactions.push({ to: votingToken.address, value: 0, data: addVotingV2AsTokenMinterTx.data });
+  console.log("3.a. Add minting roll to new voting contract:", addVotingV2AsTokenMinterTx.data);
+
+  const transferFinderOwnershipTx = await finder.populateTransaction.transferOwnership(votingUpgrader.address);
+  if (!transferFinderOwnershipTx.data) throw "transferFinderOwnershipTx.data is null";
+  adminProposalTransactions.push({ to: finder.address, value: 0, data: transferFinderOwnershipTx.data });
+  console.log("3.b. Transfer ownership of finder to voting upgrader:", transferFinderOwnershipTx.data);
+
+  const transferExistingVotingOwnershipTx = await existingVoting.populateTransaction.transferOwnership(
+    votingUpgrader.address
+  );
+  if (!transferExistingVotingOwnershipTx.data) throw "transferExistingVotingOwnershipTx.data is null";
+  adminProposalTransactions.push({
+    to: existingVoting.address,
+    value: 0,
+    data: transferExistingVotingOwnershipTx.data,
+  });
+  console.log("3.c. Transfer ownership of existing voting to voting upgrader:", transferExistingVotingOwnershipTx.data);
+
+  // Transfer Ownable contracts to VotingUpgraderV2
+  for (const ownableToMigrate of Object.entries(ownableContractsToMigrate)) {
+    const contractAddress = ownableToMigrate[1];
+    const contractName = ownableToMigrate[0];
+    const iface = new hre.ethers.utils.Interface(getAbi("Ownable"));
+    const data = iface.encodeFunctionData("transferOwnership", [votingUpgraderAddress]);
+    adminProposalTransactions.push({ to: contractAddress, value: 0, data });
+    console.log(`3.d.  Ownable: transfer ownership of ${contractName} to voting upgrader`, data);
+  }
+
+  // Transfer Multirole contracts to new VotingUpgraderV2
+  for (const multiRoleToMigrate of Object.entries(multicallContractsToMigrate)) {
+    const contractAddress = multiRoleToMigrate[1];
+    const contractName = multiRoleToMigrate[0];
+    const iface = new hre.ethers.utils.Interface(getAbi("MultiRole"));
+    const data = iface.encodeFunctionData("resetMember", [0, votingUpgraderAddress]);
+    adminProposalTransactions.push({ to: contractAddress, value: 0, data });
+    console.log(`3.e.  Multirole: transfer owner role of ${contractName} to voting upgrader`, data);
+  }
+
+  const resetMemberGovernorTx = await governor.populateTransaction.resetMember(0, votingUpgraderAddress);
+  if (!resetMemberGovernorTx.data) throw "resetMemberGovernorTx.data is null";
+  adminProposalTransactions.push({ to: governor.address, value: 0, data: resetMemberGovernorTx.data });
+  console.log("3.f.  Reset governor member to voting upgrader:", resetMemberGovernorTx.data);
+
+  const upgraderExecuteUpgradeTx = await votingUpgrader.populateTransaction.upgrade();
+  if (!upgraderExecuteUpgradeTx.data) throw "upgraderExecuteUpgradeTx.data is null";
+  adminProposalTransactions.push({ to: votingUpgrader.address, value: 0, data: upgraderExecuteUpgradeTx.data });
+  console.log("3.g. Execute upgrade of voting:", upgraderExecuteUpgradeTx.data);
+
+  console.log("4. SENDING PROPOSAL TXS TO GOVERNOR");
+
+  const tx = await proposer.connect(proposerSigner).propose(adminProposalTransactions);
+
+  console.log("Proposal done!🎉");
+  console.log("\nProposal data:\n", tx.data);
+}
+
+main().then(
+  () => {
+    process.exit(0);
+  },
+  (err) => {
+    console.error(err);
+    process.exit(1);
+  }
+);
