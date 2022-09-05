e4eb7213f1954afb2fc4d8d4e498562efedfcc1e
Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
2022-09-05 18:42:16 +0300

    feat(core): implement floored linear fpl (#4119)
    * feat(core): implement floored linear fpl
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * fix merge issues
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * fix merge issues
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * fix merge issues
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * fix merge
    
    * fix DCO
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * test floored linear lsp fpl
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * Update packages/core/contracts/financial-templates/common/financial-product-libraries/long-short-pair-libraries/FlooredLinearLongShortPairFinancialProductLibrary.sol
    
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>
    Co-authored-by: Chris Maree <christopher.maree@gmail.com>

--- packages/core/contracts/financial-templates/common/financial-product-libraries/long-short-pair-libraries/FlooredLinearLongShortPairFinancialProductLibrary.sol
+++ packages/core/contracts/financial-templates/common/financial-product-libraries/long-short-pair-libraries/FlooredLinearLongShortPairFinancialProductLibrary.sol
@@ -7,26 +7,29 @@ import "./LongShortPairFinancialProductLibrary.sol";
 import "../../../../common/implementation/Lockable.sol";
 
 /**
- * @title Linear Long Short Pair Financial Product Library.
- * @notice Adds settlement logic to create linear LSPs. The contract will payout a scaled amount of collateral
- * depending on where the settlement price lands within a price range between an upperBound and a lowerBound. If
- * settlement price is within the price range then the expiryPercentLong is defined by
- * (expiryPrice - lowerBound) / (upperBound - lowerBound). This number represent the amount of collateral from the
- * collateralPerPair that will be sent to the long and short side. If the price is higher than the upperBound then
- * expiryPercentLong = 1. if the price is lower than the lower bound then expiryPercentLong = 0. For example, consider
- * a linear LSP on the price of ETH collateralized in USDC with an upperBound = 4000 and lowerBound = 2000 with a
- * collateralPerPair of 1000 (i.e each pair of long and shorts is worth 1000 USDC). At settlement the expiryPercentLong
- * would equal 1 (each long worth 1000 and short worth 0) if ETH price was > 4000 and it would equal 0 if < 2000
- * (each long is worthless and each short is worth 1000). If between the two (say 3500) then expiryPercentLong
- * = (3500 - 2000) / (4000 - 2000) = 0.75. Therefore each long is worth 750 and each short is worth 250.
+ * @title Floored Linear Long Short Pair Financial Product Library.
+ * @notice Adds settlement logic to create floored linear LSPs. The contract will payout a scaled amount of collateral
+ * depending on where the settlement price lands within a price range between a lowerBound and an upperBound with
+ * configured minimum floorPercentage payout for all prices below lowerBound. If settlement price is within the price
+ * range then the expiryPercentLong is defined by (expiryPrice - lowerBound) / (upperBound - lowerBound) *
+ * (1 - floorPercentage) + floorPercentage. This number represents the amount of collateral from the collateralPerPair
+ * that will be sent to the long side. If the price is at or higher than the upperBound then expiryPercentLong = 1.
+ * If the price is at or lower than the lowerBound then expiryPercentLong = floorPercentage. For example, consider a
+ * floored linear LSP on the price of ETH collateralized in USDC with an upperBound = 4000, lowerBound = 2000 and
+ * floorPercentage = 20% with a collateralPerPair of 1000 (i.e each pair of long and shorts is worth 1000 USDC). At
+ * settlement the expiryPercentLong would equal 1 (each long worth 1000 and short worth 0) if ETH price was >= 4000 and
+ * it would equal 0.2 if <= 2000 (each long is worth minimum 200 and each short is worth maximum 800). If between the
+ * two (say 3500) then expiryPercentLong = (3500 - 2000) / (4000 - 2000) * (1 - 0.2) + 0.2 = 0.8. Therefore each long
+ * is worth 800 and each short is worth 200.
  */
-contract CopiedFromLinearLongShortPairFinancialProductLibrary is LongShortPairFinancialProductLibrary, Lockable {
+contract FlooredLinearLongShortPairFinancialProductLibrary is LongShortPairFinancialProductLibrary, Lockable {
     using FixedPoint for FixedPoint.Unsigned;
     using SignedSafeMath for int256;
 
     struct LinearLongShortPairParameters {
         int256 upperBound;
         int256 lowerBound;
+        uint256 floorPercentage;
     }
 
     mapping(address => LinearLongShortPairParameters) public longShortPairParameters;
@@ -36,32 +39,38 @@ contract CopiedFromLinearLongShortPairFinancialProductLibrary is LongShortPairFi
      * @param longShortPair address of the LSP contract.
      * @param upperBound the upper price that the linear LSP will operate within.
      * @param lowerBound the lower price that the linear LSP will operate within.
+     * @param floorPercentage the lowest possible payout percentage from collateralPerPair to each long token expressed
+     * with 1e18 decimals. E.g., a 20% floor percentage should be expressed as 200000000000000000.
      * @dev Note: a) Any address can set these parameters b) existing LSP parameters for address not set.
      * c) upperBound > lowerBound.
-     * d) parameters can only be set once to prevent the deployer from changing the parameters after the fact.
-     * e) For safety, parameters should be set before depositing any synthetic tokens in a liquidity pool.
-     * f) longShortPair must expose an expirationTimestamp method to validate it is correctly deployed.
+     * d) floorPercentage <= 1e18 (no need to check >= 0 as floorPercentage is unsigned).
+     * e) parameters can only be set once to prevent the deployer from changing the parameters after the fact.
+     * f) For safety, parameters should be set before depositing any synthetic tokens in a liquidity pool.
+     * g) longShortPair must expose an expirationTimestamp method to validate it is correctly deployed.
      */
     function setLongShortPairParameters(
         address longShortPair,
         int256 upperBound,
-        int256 lowerBound
+        int256 lowerBound,
+        uint256 floorPercentage
     ) public nonReentrant() {
         require(ExpiringContractInterface(longShortPair).expirationTimestamp() != 0, "Invalid LSP address");
         require(upperBound > lowerBound, "Invalid bounds");
+        require(floorPercentage <= 1e18, "Invalid floor percentage");
 
         LinearLongShortPairParameters memory params = longShortPairParameters[longShortPair];
         require(params.upperBound == 0 && params.lowerBound == 0, "Parameters already set");
 
         longShortPairParameters[longShortPair] = LinearLongShortPairParameters({
             upperBound: upperBound,
-            lowerBound: lowerBound
+            lowerBound: lowerBound,
+            floorPercentage: floorPercentage
         });
     }
 
     /**
-     * @notice Returns a number between 0 and 1e18 to indicate how much collateral each long and short token is entitled
-     * to per collateralPerPair.
+     * @notice Returns a number between floorPercentage and 1e18 to indicate how much collateral each long and short
+     * token is entitled to per collateralPerPair.
      * @param expiryPrice price from the optimistic oracle for the LSP price identifier.
      * @return expiryPercentLong to indicate how much collateral should be sent between long and short tokens.
      */
@@ -75,15 +84,18 @@ contract CopiedFromLinearLongShortPairFinancialProductLibrary is LongShortPairFi
         LinearLongShortPairParameters memory params = longShortPairParameters[msg.sender];
         require(params.upperBound != 0 || params.lowerBound != 0, "Params not set for calling LSP");
 
-        if (expiryPrice >= params.upperBound) return FixedPoint.fromUnscaledUint(1).rawValue;
+        if (expiryPrice >= params.upperBound) return 1e18;
 
-        if (expiryPrice <= params.lowerBound) return FixedPoint.fromUnscaledUint(0).rawValue;
+        if (expiryPrice <= params.lowerBound) return params.floorPercentage;
 
-        // if not exceeding bounds, expiryPercentLong = (expiryPrice - lowerBound) / (upperBound - lowerBound)
+        // if not exceeding bounds, expiryPercentLong = (expiryPrice - lowerBound) / (upperBound - lowerBound) *
+        // (1 - floorPercentage) + floorPercentage
         return
             FixedPoint
                 .Unsigned(uint256(expiryPrice - params.lowerBound))
                 .div(FixedPoint.Unsigned(uint256(params.upperBound - params.lowerBound)))
+                .mul(FixedPoint.Unsigned(1e18 - params.floorPercentage))
+                .add(FixedPoint.Unsigned(params.floorPercentage))
                 .rawValue;
     }
 }
