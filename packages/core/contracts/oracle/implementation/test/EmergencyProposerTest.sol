53d3502256393a7b1fbd06d6094325f771746bb7
Chris Maree <christopher.maree@gmail.com>
2022-09-13 12:02:27 +0200

    fix: [N12]: Remove Unused variables (#4167)

--- packages/core/contracts/oracle/implementation/test/EmergencyProposerTest.sol
+++ packages/core/contracts/oracle/implementation/test/EmergencyProposerTest.sol
@@ -9,10 +9,9 @@ contract EmergencyProposerTest is EmergencyProposer, Testable {
         IERC20 _token,
         uint256 _quorum,
         GovernorV2 _governor,
-        Finder _finder,
         address _executor,
         address _timerAddress
-    ) EmergencyProposer(_token, _quorum, _governor, _finder, _executor) Testable(_timerAddress) {}
+    ) EmergencyProposer(_token, _quorum, _governor, _executor) Testable(_timerAddress) {}
 
     function getCurrentTime() public view override(EmergencyProposer, Testable) returns (uint256) {
         return Testable.getCurrentTime();
