67df04f9ccaeed055e123485db1bd235d591cd96
Pablo Maldonado <pablo@umaproject.org>
2022-09-28 12:30:22 +0100

    fix: add missing transfer of voting token
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/scripts/src/upgrade-tests/voting2/0_Deploy.ts
+++ packages/scripts/src/upgrade-tests/voting2/0_Deploy.ts
@@ -53,7 +53,7 @@ async function main() {
     phaseLength,
     minRollToNextRoundLength,
     gat.toString(),
-    "0", // Starting request index of 0 (no offset).
+    "0", // Starting request index of 0 (no offset). TODO change this to the correct number
     votingToken.address,
     finder.address,
     slashingLibrary.address,
