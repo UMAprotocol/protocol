9a12433c8bfcb323f146805281c4fdc444c04441
David <david@umaproject.org>
2022-09-30 10:46:28 -0400

    chore: remove affiliates package
    Signed-off-by: David <david@umaproject.org>

--- packages/affiliates/apps/GenerateDappMiningPr.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { makeUnixPipe, dappMiningPrTemplate } = require("../libs/affiliates/utils");
-
-const App = async (params) => {
-  const prTemplate = await dappMiningPrTemplate(params);
-  return { ...params, prTemplate };
-};
-
-makeUnixPipe(App).then(console.log).catch(console.error);
