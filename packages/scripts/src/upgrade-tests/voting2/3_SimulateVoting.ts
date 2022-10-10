f1de0f13333eddfb511f2048012d0076b2c15cb7
Reinis Martinsons <reinis@umaproject.org>
2022-10-10 14:51:09 +0000

    feat: simulate staking and unstaking
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/scripts/src/upgrade-tests/voting2/3_SimulateVoting.ts
+++ packages/scripts/src/upgrade-tests/voting2/3_SimulateVoting.ts
@@ -8,8 +8,18 @@ import { FinderEthers, StoreEthers, VotingTokenEthers, VotingV2Ethers } from "@u
 import { FOUNDATION_WALLET, getContractInstance } from "../../utils/contracts";
 import { isVotingV2Instance } from "./migrationUtils";
 
+// Initial voter balances relative to GAT.
+const voter1RelativeGatFunding = parseEther("0.6");
+const voter2RelativeGatFunding = parseEther("0.55");
+const voter3RelativeGatFunding = parseEther("0.5");
+
+const increaseEvmTime = async (time: number) => {
+  await hre.ethers.provider.send("evm_increaseTime", [time]);
+  await hre.ethers.provider.send("evm_mine", []);
+};
+
 async function main() {
-  console.log("Running Voting Simulation🎭");
+  console.log("🎭 Running Voting Simulation after V2 upgrade");
 
   if (hre.network.name != "localhost") throw new Error("Voting should be only tested in simulation!");
 
@@ -25,29 +35,43 @@ async function main() {
   const votingV2 = await getContractInstance<VotingV2Ethers>("VotingV2", votingV2Address);
 
   const gat = await votingV2.gat();
+  const unstakeCoolDown = await votingV2.unstakeCoolDown();
   const finalFee = (await store.computeFinalFee(votingToken.address)).rawValue;
 
   let foundationBalance = await votingToken.balanceOf(FOUNDATION_WALLET);
-  console.log(` Foundation has ${formatEther(foundationBalance)} UMA, funding requester and voters...`);
+  console.log(` 1. Foundation has ${formatEther(foundationBalance)} UMA, funding requester and voters...`);
 
-  // There will be 3 voters with balances 0.6, 0.55 and 0.5 relative to GAT (total 1.65 * GAT), and for two price
+  // There will be 3 voters with initial balances set relative to GAT, and for two price
   // price requests 4 * finalFee amount will be needed.
-  if (foundationBalance.lt(gat.mul(parseEther("1.65")).div(parseEther("1")).add(finalFee.mul(4))))
+  if (
+    foundationBalance.lt(
+      gat
+        .mul(voter1RelativeGatFunding.add(voter2RelativeGatFunding.add(voter3RelativeGatFunding)))
+        .div(parseEther("1"))
+        .add(finalFee.mul(4))
+    )
+  )
     throw new Error("Foundation balance too low for simulation!");
 
   const foundationSigner = await hre.ethers.getSigner(FOUNDATION_WALLET);
   const [requesterSigner, voter1Signer, voter2Signer, voter3Signer] = await hre.ethers.getSigners();
 
-  await votingToken.connect(foundationSigner).transfer(requesterSigner.address, finalFee.mul(4));
-  await votingToken
-    .connect(foundationSigner)
-    .transfer(voter1Signer.address, gat.mul(parseEther("0.6")).div(parseEther("1")));
-  await votingToken
-    .connect(foundationSigner)
-    .transfer(voter2Signer.address, gat.mul(parseEther("0.55")).div(parseEther("1")));
-  await votingToken
-    .connect(foundationSigner)
-    .transfer(voter3Signer.address, gat.mul(parseEther("0.5")).div(parseEther("1")));
+  await (await votingToken.connect(foundationSigner).transfer(requesterSigner.address, finalFee.mul(4))).wait();
+  await (
+    await votingToken
+      .connect(foundationSigner)
+      .transfer(voter1Signer.address, gat.mul(voter1RelativeGatFunding).div(parseEther("1")))
+  ).wait();
+  await (
+    await votingToken
+      .connect(foundationSigner)
+      .transfer(voter2Signer.address, gat.mul(voter2RelativeGatFunding).div(parseEther("1")))
+  ).wait();
+  await (
+    await votingToken
+      .connect(foundationSigner)
+      .transfer(voter3Signer.address, gat.mul(voter3RelativeGatFunding).div(parseEther("1")))
+  ).wait();
 
   const [requesterBalance, voter1Balance, voter2Balance, voter3Balance] = await Promise.all(
     [requesterSigner, voter1Signer, voter2Signer, voter3Signer].map((signer) => {
@@ -55,12 +79,40 @@ async function main() {
     })
   );
 
-  console.log(`  Requester now has ${formatEther(requesterBalance)} UMA.`);
-  console.log(`  Voter 1 now has ${formatEther(voter1Balance)} UMA.`);
-  console.log(`  Voter 2 now has ${formatEther(voter2Balance)} UMA.`);
-  console.log(`  Voter 3 now has ${formatEther(voter3Balance)} UMA.`);
+  console.log(`✅ Requester now has ${formatEther(requesterBalance)} UMA.`);
+  console.log(`✅ Voter 1 now has ${formatEther(voter1Balance)} UMA.`);
+  console.log(`✅ Voter 2 now has ${formatEther(voter2Balance)} UMA.`);
+  console.log(`✅ Voter 3 now has ${formatEther(voter3Balance)} UMA.`);
 
-  console.log(" Returning all UMA to the foundation...");
+  console.log(" 2. Voters are approving UMA for staking...");
+  await (await votingToken.connect(voter1Signer).approve(votingV2.address, voter1Balance)).wait();
+  await (await votingToken.connect(voter2Signer).approve(votingV2.address, voter2Balance)).wait();
+  await (await votingToken.connect(voter3Signer).approve(votingV2.address, voter3Balance)).wait();
+  console.log("✅ Approvals on VotingV2 done!");
+
+  console.log(" 3. Voters are staking all their UMA...");
+  await (await votingV2.connect(voter1Signer).stake(voter1Balance)).wait();
+  await (await votingV2.connect(voter2Signer).stake(voter2Balance)).wait();
+  await (await votingV2.connect(voter3Signer).stake(voter3Balance)).wait();
+  console.log("✅ Voters have staked all their UMA!");
+
+  console.log(" 4. Requesting unstake...");
+  await (await votingV2.connect(voter1Signer).requestUnstake(voter1Balance)).wait();
+  await (await votingV2.connect(voter2Signer).requestUnstake(voter2Balance)).wait();
+  await (await votingV2.connect(voter3Signer).requestUnstake(voter3Balance)).wait();
+  console.log("✅ Voters requested unstake of all UMA!");
+
+  console.log(" 5. Waiting for unstake cooldown...");
+  await increaseEvmTime(unstakeCoolDown.toNumber());
+  console.log(`✅ Unstake colldown of ${unstakeCoolDown.toNumber()} seconds has passed!`);
+
+  console.log(" 6. Executing unstake");
+  await (await votingV2.connect(voter1Signer).executeUnstake()).wait();
+  await (await votingV2.connect(voter2Signer).executeUnstake()).wait();
+  await (await votingV2.connect(voter3Signer).executeUnstake()).wait();
+  console.log("✅ Voters have unstaked all UMA!");
+
+  console.log(" 7. Returning all UMA to the foundation...");
   await votingToken
     .connect(requesterSigner)
     .transfer(foundationSigner.address, await votingToken.balanceOf(requesterSigner.address));
@@ -75,7 +127,9 @@ async function main() {
     .transfer(foundationSigner.address, await votingToken.balanceOf(voter3Signer.address));
 
   foundationBalance = await votingToken.balanceOf(FOUNDATION_WALLET);
-  console.log(` Foundation has ${formatEther(foundationBalance)} UMA.`);
+  console.log(`✅ Foundation has ${formatEther(foundationBalance)} UMA.`);
+
+  console.log("\n✅ Verified! The upgraded DVM is functional.");
 }
 
 main().then(
