f37bd70b0b6e0152fa53010ef55f81b59d109df2
chrismaree <christopher.maree@gmail.com>
2022-10-25 14:39:28 +0200

    clean
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/core/contracts/cross-chain-oracle/chain-adapters/test/Arbitrum_inboxMock.sol
+++ packages/core/contracts/cross-chain-oracle/chain-adapters/test/Arbitrum_inboxMock.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.0;
-import "../../external/avm/interfaces/iArbitrum_Inbox.sol";
-import "../../cross-chain-oracle/chain-adapters/Arbitrum_ParentMessenger.sol";
+
+import "../../../external/avm/interfaces/iArbitrum_Inbox.sol";
+import "../../../cross-chain-oracle/chain-adapters/Arbitrum_ParentMessenger.sol";
 
 contract Arbitrum_OutboxMock {
     function l2ToL1Sender() external view returns (address) {
