4674de01696ec2e3ba80795de4390bf53e74be36
Evaldo Felipe <contato@evaldofelipe.com>
2022-10-31 16:25:06 -0300

    Fix(CI): Change node version (#4213)
    * Fix: CI node version
    
    Signed-off-by: Evaldo Felipe <contato@evaldofelipe.com>

--- ci/tests/test-packages.sh
+++ ci/tests/test-packages.sh
@@ -5,7 +5,7 @@ PACKAGE=$1
 cat << EOF
   test-${PACKAGE:5}:
     docker:
-      - image: circleci/node:lts
+      - image: cimg/node:16.17.0
       - image: trufflesuite/ganache-cli
         command: ganache-cli -i 1234 -l 9000000 -p 9545
     working_directory: ~/protocol
