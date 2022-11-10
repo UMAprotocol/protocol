6176089f1b7e9f00b5b7ddbbf98dc5a4a20a54d7
Reinis Martinsons <reinis@umaproject.org>
2022-11-10 18:51:19 +0000

    test: disable dvm as oa oracle
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/core/test/foundry/optimistic-assertor/OptimisticAssertor.SsmPolicies.t.sol
+++ packages/core/test/foundry/optimistic-assertor/OptimisticAssertor.SsmPolicies.t.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.8.0;
 
 import "../fixtures/optimistic-assertor/OptimisticAssertorFixture.sol";
 import "../fixtures/common/TestAddress.sol";
-import "../../../contracts/optimistic-assertor/implementation/sovereign-security-manager/BaseSovereignSecurityManager.sol";
+import "../../../contracts/oracle/test/MockOracleAncillary.sol";
 
 contract SovereignSecurityManagerPoliciesEnforced is Test {
+    struct OracleRequest {
+        bytes32 identifier;
+        uint256 time;
+        bytes ancillaryData;
+    }
+
     OptimisticAssertor optimisticAssertor;
-    SovereignSecurityManagerInterface mockedSovereignSecurityManager;
+    MockOracleAncillary mockOracle;
+    address mockedSovereignSecurityManager = address(0xff);
     TestnetERC20 defaultCurrency;
     uint256 defaultBond;
     uint256 defaultLiveness;
@@ -17,11 +24,10 @@ contract SovereignSecurityManagerPoliciesEnforced is Test {
         OptimisticAssertorFixture.OptimisticAsserterContracts memory oaContracts =
             new OptimisticAssertorFixture().setUp();
         optimisticAssertor = oaContracts.optimisticAssertor;
+        mockOracle = oaContracts.mockOracle;
         defaultCurrency = oaContracts.defaultCurrency;
         defaultBond = optimisticAssertor.defaultBond();
         defaultLiveness = optimisticAssertor.defaultLiveness();
-
-        mockedSovereignSecurityManager = new BaseSovereignSecurityManager();
 
         // Fund Account1 for making assertion.
         vm.startPrank(TestAddress.account1);
@@ -42,17 +48,23 @@ contract SovereignSecurityManagerPoliciesEnforced is Test {
         // Block any assertion.
         _mockSsmPolicies(false, true, true);
 
-        vm.prank(TestAddress.account1);
         vm.expectRevert("Assertion not allowed");
-        optimisticAssertor.assertTruthFor(
-            bytes(claimAssertion),
-            address(0),
-            address(0),
-            address(mockedSovereignSecurityManager),
-            defaultCurrency,
-            defaultBond,
-            defaultLiveness
-        );
+        _assertWithSsm();
+        vm.clearMockedCalls();
+    }
+
+    function test_DisableDvmAsOracle() public {
+        // Use SSM as oracle.
+        _mockSsmPolicies(true, false, true);
+
+        bytes32 assertionId = _assertWithSsm();
+        OptimisticAssertorInterface.Assertion memory assertion = optimisticAssertor.readAssertion(assertionId);
+        assertFalse(assertion.useDvmAsOracle);
+
+        // Dispute, mock resolve assertion truethful through SSM as Oracle and verify on Optimistic Asserter.
+        OracleRequest memory oracleRequest = _disputeAndGetOracleRequest(assertionId);
+        _mockOracleResolved(mockedSovereignSecurityManager, oracleRequest, true);
+        assertTrue(optimisticAssertor.settleAndGetAssertion(assertionId));
         vm.clearMockedCalls();
     }
 
@@ -63,7 +75,7 @@ contract SovereignSecurityManagerPoliciesEnforced is Test {
     ) internal {
         // Mock getAssertionPolicies call to block assertion. No need to pass assertionId as mockCall uses loose matching.
         vm.mockCall(
-            address(mockedSovereignSecurityManager),
+            mockedSovereignSecurityManager,
             abi.encodePacked(SovereignSecurityManagerInterface.getAssertionPolicies.selector),
             abi.encode(
                 SovereignSecurityManagerInterface.AssertionPolicies({
@@ -73,5 +85,55 @@ contract SovereignSecurityManagerPoliciesEnforced is Test {
                 })
             )
         );
+    }
+
+    function _mockOracleResolved(
+        address oracle,
+        OracleRequest memory oracleRequest,
+        bool assertionTruthful
+    ) internal {
+        vm.mockCall(
+            oracle,
+            abi.encodeWithSelector(
+                MockOracleAncillary.getPrice.selector,
+                oracleRequest.identifier,
+                oracleRequest.time,
+                oracleRequest.ancillaryData
+            ),
+            abi.encode(assertionTruthful ? int256(1e18) : int256(0))
+        );
+    }
+
+    function _assertWithSsm() internal returns (bytes32) {
+        vm.prank(TestAddress.account1);
+        return
+            optimisticAssertor.assertTruthFor(
+                bytes(claimAssertion),
+                address(0),
+                address(0),
+                mockedSovereignSecurityManager,
+                defaultCurrency,
+                defaultBond,
+                defaultLiveness
+            );
+    }
+
+    function _disputeAndGetOracleRequest(bytes32 assertionId) internal returns (OracleRequest memory) {
+        // Get expected oracle request on dispute.
+        OptimisticAssertorInterface.Assertion memory assertion = optimisticAssertor.readAssertion(assertionId);
+        OracleRequest memory oracleRequest =
+            OracleRequest({
+                identifier: optimisticAssertor.identifier(),
+                time: assertion.assertionTime,
+                ancillaryData: optimisticAssertor.stampAssertion(assertionId)
+            });
+
+        // Fund Account2 and make dispute.
+        vm.startPrank(TestAddress.account2);
+        defaultCurrency.allocateTo(TestAddress.account2, optimisticAssertor.defaultBond());
+        defaultCurrency.approve(address(optimisticAssertor), optimisticAssertor.defaultBond());
+        optimisticAssertor.disputeAssertionFor(assertionId, TestAddress.account2);
+        vm.stopPrank();
+        return oracleRequest;
     }
 }
