e5b9107d798065bdb76097822abc938260ca1e09
chrismaree <christopher.maree@gmail.com>
2022-11-16 15:23:51 +0200

    test
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- ci/install_node_npm.sh
+++ ci/install_node_npm.sh
@@ -2,3 +2,6 @@
 
 curl -sSL "https://nodejs.org/dist/v14.15.4/node-v14.15.4-linux-x64.tar.xz" | sudo tar --strip-components=2 -xJ -C /usr/local/bin/ node-v12.16.2-linux-x64/bin/node
 curl https://www.npmjs.com/install.sh | sudo bash
+curl -L https://foundry.paradigm.xyz | bash
+export PATH="$PATH:/home/circleci/.foundry/bin"
+foundryup
