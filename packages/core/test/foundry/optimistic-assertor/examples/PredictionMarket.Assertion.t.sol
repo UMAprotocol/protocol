32e7605b79868db7cf06525f10c4b9c91bd71661
Reinis Martinsons <reinis@umaproject.org>
2022-11-23 18:29:55 +0000

    refactor prediction market tests
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

---
+++ packages/core/test/foundry/optimistic-assertor/examples/PredictionMarket.Assertion.t.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import "./PredictionMarket.Common.sol";
+
+contract PredictionMarketAssertionTest is PredictionMarketTestCommon {
+    bytes32 marketId;
+
+    function setUp() public {
+        _commonPredictionMarketSetUp();
+        marketId = _initializeMarket();
+    }
+
+    function test_RevertIf_InvalidAssertionParameters() public {
+        _fundAssertionBond();
+        vm.expectRevert("Market does not exist");
+        vm.prank(TestAddress.account1);
+        predictionMarket.assertMarket(bytes32(0), outcome1);
+
+        vm.expectRevert("Invalid asserted outcome");
+        vm.prank(TestAddress.account1);
+        predictionMarket.assertMarket(marketId, "Invalid");
+    }
+
+    function test_RevertIf_DuplicateActiveAssertion() public {
+        _fundAssertionBond();
+        vm.prank(TestAddress.account1);
+        predictionMarket.assertMarket(marketId, outcome1);
+
+        _fundAssertionBond();
+        vm.expectRevert("Assertion active or resolved");
+        vm.prank(TestAddress.account1);
+        predictionMarket.assertMarket(marketId, outcome1);
+    }
+
+    function test_AssertionMade() public {
+        uint256 oaBalanceBefore = defaultCurrency.balanceOf(address(optimisticAssertor));
+
+        // Make assertion and verify bond posted to Optimistic Asseror.
+        bytes32 assertionId = _assertMarket(marketId, outcome1);
+        assertEq(defaultCurrency.balanceOf(address(optimisticAssertor)), oaBalanceBefore + requiredBond);
+
+        // Verify PredictionMarket storage.
+        PredictionMarket.Market memory market = predictionMarket.getMarket(marketId);
+        assertEq(market.assertedOutcomeId, keccak256(bytes(outcome1)));
+        (address storedAsserter, bytes32 storedMarketId) = predictionMarket.assertedMarkets(assertionId);
+        assertEq(storedAsserter, TestAddress.account1);
+        assertEq(storedMarketId, marketId);
+
+        // Verify OptimisticAssertor storage.
+        OptimisticAssertorInterface.Assertion memory assertion = optimisticAssertor.readAssertion(assertionId);
+        assertEq(assertion.proposer, TestAddress.account1);
+        assertEq(assertion.callbackRecipient, address(predictionMarket));
+        assertEq(address(assertion.currency), address(defaultCurrency));
+        assertEq(assertion.bond, requiredBond);
+        assertEq(assertion.assertionTime, block.timestamp);
+        assertEq(assertion.expirationTime, block.timestamp + defaultLiveness);
+        assertEq(assertion.identifier, defaultIdentifier);
+        assertEq(assertion.ssmSettings.assertingCaller, address(predictionMarket));
+    }
+
+    function test_DisputeCallbackReceived() public {
+        bytes32 assertionId = _assertMarket(marketId, outcome1);
+
+        vm.expectCall(address(predictionMarket), abi.encodeCall(predictionMarket.assertionDisputed, (assertionId)));
+        _disputeAndGetOracleRequest(assertionId, requiredBond);
+    }
+}
