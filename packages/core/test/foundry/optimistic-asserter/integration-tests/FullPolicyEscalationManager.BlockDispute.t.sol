b623ddc3908628e1a33de69096463818c96df602
Reinis Martinsons <reinis@umaproject.org>
2022-12-13 18:44:12 +0000

    fix: remove redundant imports
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/core/test/foundry/optimistic-asserter/integration-tests/FullPolicyEscalationManager.BlockDispute.t.sol
+++ packages/core/test/foundry/optimistic-asserter/integration-tests/FullPolicyEscalationManager.BlockDispute.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "./FullPolicyEscalationManager.Common.t.sol";
-import "../../../../contracts/optimistic-asserter/implementation/escalation-manager/FullPolicyEscalationManager.sol";
 
 contract FullPolicyEscalationManagerBlockDisputeTest is FullPolicyEscalationManagerCommon {
     function test_ValidateDisputersEnabled() public {
