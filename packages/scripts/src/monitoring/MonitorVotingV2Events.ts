fe0e89910c14a6e990f1622968630cb353c24a85
chrismaree <christopher.maree@gmail.com>
2022-12-15 12:13:05 +0200

    wip
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/scripts/src/monitoring/MonitorVotingV2Events.ts
+++ packages/scripts/src/monitoring/MonitorVotingV2Events.ts
@@ -11,6 +11,8 @@ const hre = require("hardhat");
 import { VotingV2Ethers } from "@uma/contracts-node";
 import { getContractInstance } from "../utils/contracts";
 
+import { increaseEvmTime } from "../utils/utils";
+
 async function main() {
   const networkId = Number(await hre.getChainId());
   if (networkId != 1 && networkId != 5) throw new Error("This script should be run on mainnet or goerli");
@@ -31,14 +33,39 @@ async function main() {
       .reduce((a, b) => a.add(b), hre.ethers.BigNumber.from(0));
   };
 
+  const getSlashedVotersAddresses = async () => {
+    const uniqueSlashedVoters: string[] = [];
+    const voterSlashedEvents = await votingV2.queryFilter(votingV2.filters.VoterSlashed(), 0, "latest");
+
+    for (const event of voterSlashedEvents)
+      if (!uniqueSlashedVoters.includes(event.args.voter)) uniqueSlashedVoters.push(event.args.voter);
+    return uniqueSlashedVoters;
+  };
+
+  const unstakeFromStakedAccount = async (voters: string[]) => {
+    for (const voter of voters) {
+      const stakeBalance = await votingV2.callStatic.getVoterStakePostUpdate(voter);
+      const tx = await votingV2.requestUnstake(stakeBalance);
+      await tx.wait();
+    }
+
+    await increaseEvmTime(60);
+
+    console.log("UU");
+  };
+
   console.log("VotingV2 address: ", votingV2.address);
 
-  const stakedEvents = await votingV2.queryFilter(votingV2.filters.Staked(null, null, null));
+  const stakedEvents = await votingV2.queryFilter(votingV2.filters.Staked(null, null, null), 0, "latest");
+  const delegateSetEvents = await votingV2.queryFilter(votingV2.filters.DelegateSet(null, null), 0, "latest");
+  console.log("delegateSetEvents", delegateSetEvents);
 
   const uniqueVoters: string[] = [];
-  for (const event of stakedEvents) {
-    if (!uniqueVoters.includes(event.args.voter)) uniqueVoters.push(event.args.voter);
-  }
+  for (const event of stakedEvents) if (!uniqueVoters.includes(event.args.voter)) uniqueVoters.push(event.args.voter);
+  for (const event of delegateSetEvents)
+    if (!uniqueVoters.includes(event.args.delegator)) uniqueVoters.push(event.args.delegator);
+  for (const event of delegateSetEvents)
+    if (!uniqueVoters.includes(event.args.delegate)) uniqueVoters.push(event.args.delegate);
 
   console.log("Unique voters: ", uniqueVoters.length);
   console.log("Voters: ", uniqueVoters);
@@ -59,6 +86,10 @@ async function main() {
   const sumSlashEvents = await getSumSlashedEvents();
 
   console.log("Sum of slashedTokens between all stakeholder after update trackers: ", sumSlashEvents.toString());
+
+  console.log("getSlashedVotersAddresses", await getSlashedVotersAddresses());
+
+  await unstakeFromStakedAccount(uniqueVoters);
 
   if (!sumSlashEvents.eq(0)) {
     throw new Error("The sum of slashedTokens between all stakeholder should be 0");
