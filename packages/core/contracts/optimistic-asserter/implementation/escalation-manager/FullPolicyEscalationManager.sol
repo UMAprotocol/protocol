778c256d951dcb76530f6d030d3322595c5222b9
chrismaree <christopher.maree@gmail.com>
2022-12-22 09:33:23 +0200

    fix
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/core/contracts/optimistic-asserter/implementation/escalation-manager/FullPolicyEscalationManager.sol
+++ packages/core/contracts/optimistic-asserter/implementation/escalation-manager/FullPolicyEscalationManager.sol
@@ -153,7 +153,7 @@ contract FullPolicyEscalationManager is BaseEscalationManager, Ownable {
         bool arbitrationResolution
     ) public onlyOwner {
         bytes32 requestId = keccak256(abi.encode(identifier, time, ancillaryData));
-        require(arbitrationResolutions[requestId].valueSet == false, "Arbitration resolution already set");
+        require(arbitrationResolutions[requestId].valueSet == false, "Arbitration already resolved");
         arbitrationResolutions[requestId] = ArbitrationResolution(true, arbitrationResolution);
         emit ArbitrationResolutionSet(identifier, time, ancillaryData, arbitrationResolution);
     }
