7f00348ee90f28e30dc934709f2b104f2f2b1078
nicholaspai <9457025+nicholaspai@users.noreply.github.com>
2022-12-28 11:56:34 -0500

    improve: Update blockfinder to be faster for chains with new average time estimates: {10, 288, 42161} (#4351)
    * lint
    
    * add and fix tests
    
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>
    
    * fix
    
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>
    
    * Over estimate estimatedBlocksElapsed
    
    * Update TimeUtils.ts
    
    * use cushion 2
    
    * Update blockFinder.ts
    
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>
    Co-authored-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/sdk/src/blockFinder.ts
+++ packages/sdk/src/blockFinder.ts
@@ -8,7 +8,8 @@ export type WithoutStringTimestamp<T extends { timestamp: number | string }> = T
 export default class BlockFinder<T extends { number: number; timestamp: number | string }> {
   constructor(
     private readonly requestBlock: (requestedBlock: string | number) => Promise<T>,
-    private readonly blocks: T[] = []
+    private readonly blocks: T[] = [],
+    private readonly chainId: number = 1
   ) {
     assert(requestBlock, "requestBlock function must be provided");
   }
@@ -29,9 +30,16 @@ export default class BlockFinder<T extends { number: number; timestamp: number |
     // Check the first block. If it's grater than our timestamp, we need to find an earlier block.
     if (this.blocks[0].timestamp > timestamp) {
       const initialBlock = this.blocks[0] as WithoutStringTimestamp<T>;
-      const cushion = 1.1;
-      // Ensure the increment block distance is _at least_ a single block to prevent an infinite loop.
-      const incrementDistance = Math.max(await estimateBlocksElapsed(initialBlock.timestamp - timestamp, cushion), 1);
+      // We use a 2x cushion to reduce the number of iterations in the following loop and increase the chance
+      // that the first block we find sets a floor for the target timestamp. The loop converges on the correct block
+      // slower than the following incremental search performed by `findBlock`, so we want to minimize the number of
+      // loop iterations in favor of searching more blocks over the `findBlock` search.
+      const cushion = 1;
+      const incrementDistance = Math.max(
+        // Ensure the increment block distance is _at least_ a single block to prevent an infinite loop.
+        await estimateBlocksElapsed(initialBlock.timestamp - timestamp, cushion, this.chainId),
+        1
+      );
 
       // Search backwards by a constant increment until we find a block before the timestamp or hit block 0.
       for (let multiplier = 1; ; multiplier++) {
