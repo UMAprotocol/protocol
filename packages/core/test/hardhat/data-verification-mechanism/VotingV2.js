0c0888739bee1df6dae59c6471feaba01874aa01
Pablo Maldonado <pablo@umaproject.org>
2023-01-03 16:56:12 +0100

    test: never allow total stakes to exceed cumulative stake
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/core/test/hardhat/data-verification-mechanism/VotingV2.js
+++ packages/core/test/hardhat/data-verification-mechanism/VotingV2.js
@@ -30,7 +30,7 @@ const VotingToken = getContract("VotingToken");
 const Timer = getContract("Timer");
 const SlashingLibrary = getContract("SlashingLibrary");
 const ZeroedSlashingSlashingLibraryTest = getContract("ZeroedSlashingSlashingLibraryTest");
-// const PunitiveSlashingLibraryTest = getContract("PunitiveSlashingLibraryTest");
+const PunitiveSlashingLibraryTest = getContract("PunitiveSlashingLibraryTest");
 
 const { utf8ToHex, padRight } = web3.utils;
 
@@ -98,35 +98,7 @@ describe("VotingV2", function () {
     if (!voting.events["VoterSlashed"]) return;
     const events = await voting.getPastEvents("VoterSlashed", { fromBlock: 0, toBlock: "latest" });
     const sum = events.map((e) => Number(web3.utils.fromWei(e.returnValues.slashedTokens))).reduce((a, b) => a + b, 0);
-
-    console.log("VoterSlashed events sum to", sum);
-    // assert(Math.abs(sum) < 10e-10, `VoterSlashed events should sum to 0, but sum is ${sum}`);
-
-    await voting.methods.setUnstakeCoolDown(0).send({ from: account1 });
-
-    await voting.methods
-      .requestUnstake(await voting.methods.getVoterStakePostUpdate(account1).call())
-      .send({ from: account1 });
-    await voting.methods
-      .requestUnstake(await voting.methods.getVoterStakePostUpdate(account2).call())
-      .send({ from: account2 });
-    await voting.methods
-      .requestUnstake(await voting.methods.getVoterStakePostUpdate(account3).call())
-      .send({ from: account3 });
-    await voting.methods
-      .requestUnstake(await voting.methods.getVoterStakePostUpdate(account4).call())
-      .send({ from: account4 });
-    await voting.methods.requestUnstake(await voting.methods.getVoterStakePostUpdate(rand).call()).send({ from: rand });
-
-    await voting.methods.executeUnstake().send({ from: account1 });
-    await voting.methods.executeUnstake().send({ from: account2 });
-    await voting.methods.executeUnstake().send({ from: account3 });
-    await voting.methods.executeUnstake().send({ from: account4 });
-    await voting.methods.executeUnstake().send({ from: rand });
-
-    // log voting v2 token balance
-    const votingTokenBalance = await votingToken.methods.balanceOf(voting.options.address).call();
-    console.log("votingTokenBalance", votingTokenBalance);
+    assert(Math.abs(sum) < 10e-10, `VoterSlashed events should sum to 0, but sum is ${sum}`);
   });
 
   it("Constructor", async function () {
@@ -4511,19 +4483,32 @@ describe("VotingV2", function () {
     assert.equal(finalContractBalance, "0");
   });
 
-  it("Ensure Users Are Never Slashed Below 0", async function () {
+  it("Should never allow total stakes to exceed cumulative stake", async function () {
+    // This test proves how the total user stakes never excede the cumulativeStake parameter. This is important because
+    // otherwise the VotingV2 contract could not pay the users stakes and also would issue more rewards than it should.
+
+    // To check this we will use a more punitive slashing library that slashes 99% of staked tokens of users who vote
+    // incorrectly or do not vote at all. Account1 will be the only one to vote and therefore receive the tokens of the
+    // other users which should reach 0 staked balance.
+
     // Set up contracts
-    // const punitiveSlashingLibraryTest = await PunitiveSlashingLibraryTest.new().send({ from: accounts[0] });
-    // await voting.methods.setSlashingLibrary(punitiveSlashingLibraryTest.options.address).send({ from: accounts[0] });
+    const punitiveSlashingLibraryTest = await PunitiveSlashingLibraryTest.new().send({ from: accounts[0] });
+    await voting.methods.setSlashingLibrary(punitiveSlashingLibraryTest.options.address).send({ from: accounts[0] });
     const identifier = padRight(utf8ToHex("test"), 64);
     const time = "1000";
     await supportedIdentifiers.methods.addSupportedIdentifier(identifier).send({ from: accounts[0] });
 
-    // In this test account2 has 32M UMA tokens staked and will remain innactive during all the votes. Account1 will vote
-    // with his 32M tokens so reaching the gat and then receiving the slashing rewards comming from account2.
-    // We check that account2 is never slashed below 0.
+    const accountOneStartingStakedBalance = await voting.methods.getVoterStakePostUpdate(account1).call(); // 32M
+    const restOfStakedBalance = toBN(await votingToken.methods.balanceOf(voting.options.address).call()).sub(
+      toBN(accountOneStartingStakedBalance)
+    ); // 68M
 
-    for (let i = 0; i < 7; i++) {
+    // Number of active voting rounds to test with
+    const activeVotingRounds = 30;
+
+    // Account1 will vote with 32M tokens so reaching the gat and then receiving the slashing rewards comming from
+    // account2, account3 and account4
+    for (let i = 0; i < activeVotingRounds; i++) {
       const newTime = Number(time) + i;
       await voting.methods.requestPrice(identifier, newTime).send({ from: registeredContract });
       await moveToNextRound(voting, accounts[0]);
@@ -4539,14 +4524,59 @@ describe("VotingV2", function () {
       // Reveal vote
       await moveToNextPhase(voting, accounts[0]);
       await voting.methods.revealVote(identifier, newTime, price, salt).send({ from: account1 });
-
-      // Check user stake
-      await moveToNextRound(voting, accounts[0]);
-      const accountTwoStake = await voting.methods.getVoterStakePostUpdate(account2).call();
-      console.log("account2 stake", accountTwoStake.toString());
-      // Rand stake is more or equal to 0
-      assert(toBN(accountTwoStake).gt(toBN("0")) || toBN(accountTwoStake).eq(toBN("0")));
     }
+
+    // Update the trackers of all accounts
+    for (const ac of [account1, account2, account3, account4, rand]) {
+      await voting.methods.updateTrackers(ac).send({ from: account1 });
+    }
+
+    const events = await voting.getPastEvents("VoterSlashed", { fromBlock: 0, toBlock: "latest" });
+
+    // Sum of all the positive slashed tokens received by account1
+    const sumPositive = events
+      .map((e) => e.returnValues.slashedTokens)
+      .filter((e) => toBN(e).gt(toBN(0)))
+      .reduce((a, b) => toBN(a).add(toBN(b)), toBN(0));
+
+    // Sum of all the negative slashed tokens removed to the rest of accounts
+    const sumNegative = events
+      .map((e) => e.returnValues.slashedTokens)
+      .filter((e) => toBN(e).lt(toBN(0)))
+      .reduce((a, b) => toBN(a).add(toBN(b)), toBN(0));
+
+    // Check that the sum of all the positive slashed tokens is equal to the total staked balance minus the account1
+    // starting balance. Due to rounding drifts, we allow 1 WEI of error per round.
+    assert(restOfStakedBalance.sub(sumPositive).lt(toBN(activeVotingRounds)));
+
+    // Check that the sum of all the negative slashed tokens is equal to the total staked balance minus the account1
+    // starting balance. Due to rounding drifts, we allow 1 WEI of error per round.
+    assert(restOfStakedBalance.add(sumNegative).lt(toBN(activeVotingRounds)));
+
+    // The sum of all the positive and negative slashed tokens should be equal ~ 0. Due to rounding drifts, we allow 1
+    // WEI of error per round.
+    assert(sumPositive.add(sumNegative).lt(toBN(activeVotingRounds)));
+
+    // Check that the total user stakes never excede the cumulativeStake
+    const cumulativeStake = await voting.methods.cumulativeStake().call();
+
+    const account1Stake = await voting.methods.getVoterStakePostUpdate(account1).call(); // ~ 100M
+    const account2Stake = await voting.methods.getVoterStakePostUpdate(account2).call(); // ~ 0
+    const account3Stake = await voting.methods.getVoterStakePostUpdate(account3).call(); // ~ 0
+    const account4Stake = await voting.methods.getVoterStakePostUpdate(account4).call(); // ~ 0
+
+    const totalUserStakes = toBN(account1Stake)
+      .add(toBN(account2Stake))
+      .add(toBN(account3Stake))
+      .add(toBN(account4Stake));
+
+    // Abs value of difference between totalUserStakes and cumulativeStake
+    const diff = toBN(totalUserStakes).gt(toBN(cumulativeStake))
+      ? toBN(totalUserStakes).sub(toBN(cumulativeStake))
+      : toBN(cumulativeStake).sub(toBN(totalUserStakes)); // ~ 0
+
+    // Due to rounding drifts, we allow 1 WEI of error per round.
+    assert(diff.lte(toBN(activeVotingRounds)));
   });
 
   const addNonSlashingVote = async () => {
