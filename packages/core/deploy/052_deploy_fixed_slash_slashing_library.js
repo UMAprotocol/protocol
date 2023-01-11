a5e47b2620fd61ce348d64644198959f014b4264
chrismaree <christopher.maree@gmail.com>
2023-01-11 13:51:14 +0200

    fix
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

---
+++ packages/core/deploy/052_deploy_fixed_slash_slashing_library.js
@@ -0,0 +1,22 @@
+const func = async function (hre) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  // baseSlashAmount: amount slashed for missing a vote or voting wrong.
+  const baseSlashAmount = web3.utils.toWei("0.0016", "ether");
+
+  // governanceSlashAmount: amount slashed for voting wrong in a governance vote.
+  const governanceSlashAmount = web3.utils.toWei("0", "ether");
+
+  await deploy("FixedSlashSlashingLibrary", {
+    from: deployer,
+    args: [baseSlashAmount, governanceSlashAmount],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
+};
+module.exports = func;
+func.tags = ["FixedSlashSlashingLibrary"];
+func.dependencies = [];
