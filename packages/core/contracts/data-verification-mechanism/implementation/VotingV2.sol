ce3adceea05d294ebfc9652be7563c20a3c34dde
chrismaree <christopher.maree@gmail.com>
2023-01-12 12:43:22 +0200

    feat: Address excessive intra round slashing
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/core/contracts/data-verification-mechanism/implementation/VotingV2.sol
+++ packages/core/contracts/data-verification-mechanism/implementation/VotingV2.sol
@@ -55,6 +55,7 @@ contract VotingV2 is Staker, OracleInterface, OracleAncillaryInterface, OracleGo
         uint256 minAgreementRequirement; // Minimum staked tokens that must agree on an outcome to resolve a request.
         uint256 cumulativeStakeAtRound; // Total staked tokens at the start of the round.
         uint64 resolvedIndex; // Index of pendingPriceRequestsIds that has been traversed this round.
+        uint32 requestToVoteOnInThisRound; // The number of requests that have been voted on in this round.
     }
 
     // Represents the status a price request has.
@@ -106,6 +107,8 @@ contract VotingV2 is Staker, OracleInterface, OracleAncillaryInterface, OracleGo
     uint256 public gat; // GAT: A minimum number of tokens that must participate to resolve a vote.
 
     uint256 public spat; // SPAT: Minimum percentage of staked tokens that must agree on the answer to resolve a vote.
+
+    uint32 public maxRequestsPerRound = 1000; // The maximum number of requests that can be enqueued in a single round.
 
     uint64 private constant UINT64_MAX = type(uint64).max; // Max value of an unsigned integer.
 
@@ -286,7 +289,7 @@ contract VotingV2 is Staker, OracleInterface, OracleAncillaryInterface, OracleGo
         // Price has never been requested.
         uint32 currentRoundId = uint32(getCurrentRoundId());
         if (_getRequestStatus(priceRequest, currentRoundId) == RequestStatus.NotRequested) {
-            uint32 roundIdToVoteOn = currentRoundId + 1; // Vote on request in the following round.
+            uint32 roundIdToVoteOn = getRoundIdToVoteOnRequest(currentRoundId + 1);
             priceRequest.identifier = identifier;
             priceRequest.time = SafeCast.toUint64(time);
             priceRequest.ancillaryData = ancillaryData;
@@ -296,6 +299,11 @@ contract VotingV2 is Staker, OracleInterface, OracleAncillaryInterface, OracleGo
             pendingPriceRequestsIds.push(priceRequestId);
             emit RequestAdded(msg.sender, roundIdToVoteOn, identifier, time, ancillaryData, isGovernance);
         }
+    }
+
+    function getRoundIdToVoteOnRequest(uint32 targetRoundId) public view returns (uint32) {
+        while (rounds[targetRoundId].requestToVoteOnInThisRound > maxRequestsPerRound) targetRoundId++;
+        return targetRoundId;
     }
 
     /**
@@ -988,7 +996,8 @@ contract VotingV2 is Staker, OracleInterface, OracleAncillaryInterface, OracleGo
                 continue;
             }
             // Else, the request should be rolled. This involves only moving forward the lastVotingRound.
-            request.lastVotingRound = currentRoundId;
+            request.lastVotingRound = getRoundIdToVoteOnRequest(currentRoundId);
+            ++rounds[request.lastVotingRound].requestToVoteOnInThisRound;
             emit RequestRolled(request.identifier, request.time, request.ancillaryData, request.rollCount);
             requestIndex = unsafe_inc_64(requestIndex);
         }
