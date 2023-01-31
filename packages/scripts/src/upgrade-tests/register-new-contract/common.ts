fc7427d7300ab78ee020a50f90c0f0cdcb10567b
Pablo Maldonado <pablo@umaproject.org>
2023-01-31 14:17:23 +0000

    refactor: group imports
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/scripts/src/upgrade-tests/register-new-contract/common.ts
+++ packages/scripts/src/upgrade-tests/register-new-contract/common.ts
@@ -15,28 +15,32 @@ import {
   ParentMessengerBaseEthers,
   ProposerEthers,
   RegistryEthers,
+  getAddress as _getAddress,
 } from "@uma/contracts-node";
 import { BaseContract, PopulatedTransaction, Signer } from "ethers";
 import { getContractInstance, getContractInstanceByUrl } from "../../utils/contracts";
-import { fundArbitrumParentMessengerForRelays, relayGovernanceMessages } from "../../utils/relay";
+import {
+  fundArbitrumParentMessengerForRelays,
+  relayGovernanceMessages,
+  decodeData,
+  decodeRelayMessages,
+  ProposedTransaction,
+  RelayTransaction,
+} from "../../utils/relay";
 
 import { interfaceName, RegistryRolesEnum } from "@uma/common";
-import { getAddress as _getAddress } from "@uma/contracts-node";
 
 import { strict as assert } from "assert";
 
-import { decodeData, decodeRelayMessages, ProposedTransaction, RelayTransaction } from "../../utils/relay";
-
 import { ParamType } from "ethers/lib/utils";
 import { forkNetwork } from "../../utils/utils";
-
-// CONSTANTS
-const newContractName = interfaceName.OptimisticAsserter;
 
 const getAddress = (contractName: string, networkId: number): Promise<string> => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return _getAddress(contractName as any, networkId);
 };
+
+const newContractName = interfaceName.OptimisticAsserter;
 
 export {
   hre,
