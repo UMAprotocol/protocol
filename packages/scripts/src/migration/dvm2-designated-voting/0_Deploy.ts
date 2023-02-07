c1f17d545c84242f904548b3dce9329684e181e0
chrismaree <christopher.maree@gmail.com>
2023-02-07 10:05:24 +0100

    fix
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/scripts/src/migration/dvm2-designated-voting/0_Deploy.ts
+++ packages/scripts/src/migration/dvm2-designated-voting/0_Deploy.ts
@@ -9,8 +9,6 @@ import {
 } from "@uma/contracts-node";
 import { getContractInstance } from "../../utils/contracts";
 
-const { getContractFactory } = hre.ethers;
-
 async function main() {
   console.log("Running DesignatedVotingV2 deployments🔥");
 
@@ -21,7 +19,7 @@ async function main() {
   const owner = process.env.OWNER_TO_MIGRATE || "";
 
   const factoryV1 = await getContractInstance<DesignatedVotingFactoryEthers>("DesignatedVotingFactory");
-  const factoryV2 = await getContractInstance<DesignatedVotingV2FactoryEthers>("DesignatedVotingV2Factory");
+  // const factoryV2 = await getContractInstance<DesignatedVotingV2FactoryEthers>("DesignatedVotingV2Factory");
   const votingToken = await getContractInstance<VotingTokenEthers>("VotingToken");
   // Step 1: find all DesignatedVoting contracts created by the DesignatedVotingFactory.
   const designatedVotingContracts = await _fetchDesignatedVotingContractsCreatedByFactory(factoryV1);
@@ -39,30 +37,39 @@ async function main() {
 
   // Step 3: Find all owners and hot wallets for all DesignatedVoting contracts that have UMA. Construct one condensed
   // data structure that contains all key information. Remove any that are not owned by the provided owner.
-  const owners = await Promise.all(designatedVotingContractsWithTokens.map((contract) => contract.getMember(0)));
-  const hotWallets = await Promise.all(designatedVotingContractsWithTokens.map((contract) => contract.getMember(1)));
 
-  const designatedVotingData = designatedVotingContractsWithTokens
-    .map((contract, index) => {
-      return { designatedVoting: contract.address, owner: owners[index], hotWallet: hotWallets[index] };
-    })
-    .filter((contract) => contract.owner === owner);
+  const designatedVotingData = (
+    await Promise.all(
+      designatedVotingContractsWithTokens.map(async (contract) => {
+        const contractOwner = await contract.getMember(0);
+        const hotWallet = await contract.getMember(1);
+        // Typescript knows these values are all defined.
+        return { designatedVoting: contract.address, owner: contractOwner, hotWallet };
+      })
+    )
+  ).filter((contract) => contract.owner === owner);
 
   // Log all designated voting and the associated hot wallets. remove the owner element from the object to keep it short.
   console.log(`Found the following DesignatedVoting to migrate owned by ${owner}:`);
   console.table(
-    designatedVotingData.map((e: any) => {
+    designatedVotingData.map((e: { designatedVoting: string; owner?: string; hotWallet: string }) => {
       delete e.owner;
       return e;
     })
   );
 
   // Step 4: Deploy new DesignatedVotingV2 contracts for each hot wallet.
-  const multiCallPayload = designatedVotingData.map(async (data) => {
-    await factoryV2.populateTransaction.newDesignatedVoting(owner, data.hotWallet);
-  });
+  // const multiCallPayload = designatedVotingData.map(async (data) => {
+  //   await factoryV2.populateTransaction.newDesignatedVoting(owner, data.hotWallet);
+  // });
 
-  console.log("multiCallPayload", multiCallPayload);
+  // console.log("multiCallPayload", multiCallPayload);
+}
+
+interface EtherscanTransactionListResponse {
+  input: string;
+  from: string;
+  functionName: string;
 }
 
 async function _fetchDesignatedVotingContractsCreatedByFactory(
@@ -73,16 +80,16 @@ async function _fetchDesignatedVotingContractsCreatedByFactory(
   );
 
   const createCallers = data
-    .filter((tx: any) => tx.functionName.includes("newDesignatedVoting")) // choose only newDesignatedVoting calls.
-    .map((tx: any) => tx.from as string); // extract the caller address.
+    .filter((tx) => tx.functionName.includes("newDesignatedVoting")) // choose only newDesignatedVoting calls.
+    .map((tx) => tx.from as string); // extract the caller address.
 
   const designatedVotingFromCreation = await Promise.all(
     createCallers.map((creator: string) => factoryV1.designatedVotingContracts(creator))
   );
 
   const setDesignatedVotingInputs = data
-    .filter((tx: any) => tx.functionName.includes("setDesignatedVoting")) // choose only setDesignatedVoting calls.
-    .map((tx: any) => tx.input as string); // extract the input calldata.
+    .filter((tx) => tx.functionName.includes("setDesignatedVoting")) // choose only setDesignatedVoting calls.
+    .map((tx) => tx.input as string); // extract the input calldata.
 
   // Now, extract the target designated voting contract from the input calldata.
   const designatedVotingFromSetDesignatedVoting = setDesignatedVotingInputs.map(
@@ -90,11 +97,10 @@ async function _fetchDesignatedVotingContractsCreatedByFactory(
   );
 
   // Merge the two sets and return the unique set of designated voting contracts.
-
-  return [...new Set([...designatedVotingFromCreation, ...designatedVotingFromSetDesignatedVoting])] as string[];
+  return [...new Set([...designatedVotingFromCreation, ...designatedVotingFromSetDesignatedVoting])];
 }
 
-async function _runEtherscanApiCall(url: string) {
+async function _runEtherscanApiCall(url: string): Promise<EtherscanTransactionListResponse[]> {
   const response = await fetch(`${url}&apikey=${process.env.ETHERSCAN_API_KEY}}`, {
     method: "GET",
     headers: { Accept: "application/json", "Content-Type": "application/json" },
