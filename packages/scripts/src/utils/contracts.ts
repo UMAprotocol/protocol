2f3959cd7a600e202425fe1857eb14d124ec9a1d
Reinis Martinsons <reinis@umaproject.org>
2023-02-16 15:45:26 +0000

    fix: use ts for getAddress
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/scripts/src/utils/contracts.ts
+++ packages/scripts/src/utils/contracts.ts
@@ -1,4 +1,4 @@
-const { getAddress } = require("@uma/contracts-node");
+import { ContractName, DeploymentName, getAddress } from "@uma/contracts-node";
 const hre = require("hardhat");
 import { Contract, ContractFactory } from "ethers";
 import { Provider } from "@ethersproject/abstract-provider";
@@ -7,7 +7,11 @@ export const FOUNDATION_WALLET = "0x8180d59b7175d4064bdfa8138a58e9babffda44a";
 export const SECONDS_PER_DAY = 86400;
 export const YES_VOTE = "1";
 
-export const getContractInstance = async <T>(contractName: string, address?: string, chainId?: number): Promise<T> => {
+export const getContractInstance = async <T>(
+  contractName: DeploymentName | ContractName,
+  address?: string,
+  chainId?: number
+): Promise<T> => {
   const networkId = chainId ? chainId : await hre.getChainId();
   const factory = await hre.ethers.getContractFactory(contractName);
   if (address) return (await factory.attach(address)) as T;
@@ -16,7 +20,7 @@ export const getContractInstance = async <T>(contractName: string, address?: str
 };
 
 export const getContractInstanceByUrl = async <T extends Contract>(
-  contractName: string,
+  contractName: DeploymentName | ContractName,
   rpcUrl: string
 ): Promise<T> => {
   const provider: Provider = new hre.ethers.providers.JsonRpcProvider(rpcUrl);
