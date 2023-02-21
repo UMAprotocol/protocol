b5ea8b46d50a5026b5f9ac4b059ae88ea267a645
Reinis Martinsons <reinis@umaproject.org>
2023-02-21 14:02:59 +0000

    fix: update hardhat config
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/monitor-v2/hardhat.config.ts
+++ packages/monitor-v2/hardhat.config.ts
@@ -1,14 +1,21 @@
-// Minimal config just to get bot tests to run.
+import { getHardhatConfig } from "@uma/common";
+import path from "path";
 
-import { HardhatUserConfig } from "hardhat/config";
-
-import "hardhat-deploy";
+// Hardhat plugins used in monitor-v2 package tests.
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
 
-const config: HardhatUserConfig = {
-  networks: {
-    hardhat: {},
+const coreWkdir = path.dirname(require.resolve("@uma/core/package.json"));
+const packageWkdir = path.dirname(require.resolve("./package.json"));
+
+const configOverride = {
+  paths: {
+    root: coreWkdir,
+    sources: `${coreWkdir}/contracts`,
+    artifacts: `${coreWkdir}/artifacts`,
+    cache: `${coreWkdir}/cache`,
+    tests: `${packageWkdir}/test`,
   },
 };
 
-export default config;
+export default getHardhatConfig(configOverride, coreWkdir);
