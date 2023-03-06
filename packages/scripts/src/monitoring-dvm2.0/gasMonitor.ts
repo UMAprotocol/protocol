ec5d937ef0218c64d46f3fa4b5642c7277719f6b
chrismaree <christopher.maree@gmail.com>
2023-03-06 13:32:05 +0100

    fix
    Signed-off-by: chrismaree <christopher.maree@gmail.com>

--- packages/scripts/src/monitoring-dvm2.0/gasMonitor.ts
+++ packages/scripts/src/monitoring-dvm2.0/gasMonitor.ts
@@ -1,6 +1,9 @@
 // This Script verifies that potentially high gas cost actions are bounded in their max gas cost. In particular, check
 // how much gas it would cost to run processResolvablePriceRequests and updateTrackers for each voter. If the gas cost
 // is exorbitant, then the script will throw an error. and this should be dealt with by the UMA team.
+// It can be run on a mainnet or goerli fork, with the following command and environment variables:
+// CUSTOM_NODE_URL=https://<goerli OR mainnet>.infura.io/v3/<YOUR-INFURA-KEY> \
+// yarn hardhat run ./src/monitoring-dvm2.0/gasMonitor.ts
 
 import { getUniqueVoters, getVotingContracts } from "../utils/votingv2-utils";
 
