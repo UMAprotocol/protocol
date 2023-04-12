696a7a95020a5b2691306f40e7599cedcbff9f69
Reinis Martinsons <reinis@umaproject.org>
2023-04-12 12:58:33 +0000

    feat: simulate vote by all real stakers
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/scripts/src/admin-proposals/simulateVoteV2.ts
+++ packages/scripts/src/admin-proposals/simulateVoteV2.ts
@@ -7,9 +7,19 @@ import { BigNumberish, BytesLike, Signer } from "ethers";
 import { REQUIRED_SIGNER_ADDRESSES, SECONDS_PER_DAY, YES_VOTE } from "../utils/constants";
 import { getContractInstance } from "../utils/contracts";
 import { increaseEvmTime } from "../utils/utils";
+import { getUniqueVoters } from "../utils/votingv2-utils";
 
 const { ethers } = hre;
 const FOUNDATION_WALLET = REQUIRED_SIGNER_ADDRESSES.foundation;
+
+interface VoteRequest {
+  identifier: BytesLike;
+  time: BigNumberish;
+  salt: BigNumberish;
+  ancillaryData: BytesLike;
+  price: BigNumberish;
+  voteHash: BytesLike;
+}
 
 require("dotenv").config();
 const assert = require("assert").strict;
@@ -22,6 +32,8 @@ const assert = require("assert").strict;
 // VOTING_V2_ADDRESS=<VOTING-V2-ADDRESS> \
 // GOVERNOR_V2_ADDRESS=<GOVERNOR-V2-ADDRESS> \
 // EXECUTOR_ADDRESS=<EXECUTOR-ADDRESS> \  Optional. If not provided, the first signer will be used.
+// SKIP_EXECUTE=1 \  Optional. If 1, then the script will not execute the proposal. Default is 0.
+// REAL_VOTERS=1 \  Optional. If 1, then the script simulate vote from real stakers. Default is 0.
 // NODE_URL_1=http://127.0.0.1:9545/ \
 // yarn hardhat run ./src/admin-proposals/simulateVoteV2.ts --network localhost
 
@@ -38,18 +50,20 @@ async function simulateVoteV2() {
   } else {
     executorSigner = (await hre.ethers.getSigners())[0];
   }
+  const skipExecute = process.env["SKIP_EXECUTE"] === "1" ? true : false;
+  const realVoters = process.env["REAL_VOTERS"] === "1" ? true : false;
 
   const governorV2 = await getContractInstance<GovernorV2Ethers>("GovernorV2", governorV2Address);
   const votingV2 = await getContractInstance<VotingV2Ethers>("VotingV2", votingV2Address);
   const votingToken = await getContractInstance<VotingTokenEthers>("VotingToken");
 
-  const signers: Signer[] = await ethers.getSigners();
-
-  // Stake foundation balance
-  const foundationBalance = await votingToken.balanceOf(FOUNDATION_WALLET);
-  if (foundationBalance.gt(0)) {
-    await votingToken.connect(foundationSigner).approve(votingV2.address, foundationBalance);
-    await votingV2.connect(foundationSigner).stake(foundationBalance);
+  // Stake foundation balance if not using real voters.
+  if (!realVoters) {
+    const foundationBalance = await votingToken.balanceOf(FOUNDATION_WALLET);
+    if (foundationBalance.gt(0)) {
+      await votingToken.connect(foundationSigner).approve(votingV2.address, foundationBalance);
+      await votingV2.connect(foundationSigner).stake(foundationBalance);
+    }
   }
 
   console.group("\n🗳 Simulating Admin Proposal Vote");
@@ -89,66 +103,76 @@ async function simulateVoteV2() {
   console.groupEnd();
   assert(votingStats.pendingRequests.length >= 1, "Must be at least 1 pending admin price request to vote on");
 
-  // Vote affirmatively from Foundation wallet.
-  const requestsToVoteOn: {
-    identifier: BytesLike;
-    time: BigNumberish;
-    salt: BigNumberish;
-    ancillaryData: BytesLike;
-    price: BigNumberish;
-    voteHash: BytesLike;
-  }[] = [];
-  console.group("\n🏗 Constructing vote hashes");
-  for (let i = 0; i < votingStats.pendingRequests.length; i++) {
-    const request = votingStats.pendingRequests[i];
-    const identifier = request.identifier.toString();
-    const time = request.time.toString();
-    const price = ethers.utils.parseEther(YES_VOTE);
-
-    const salt = getRandomSignedInt();
-    // Main net DVM uses the commit reveal scheme of hashed concatenation of price and salt
-    const voteHashData = {
-      price,
-      salt: salt.toString(),
-      account: FOUNDATION_WALLET,
-      time,
-      roundId: votingStats.roundId,
-      identifier,
-    };
-    console.table(voteHashData);
-
-    const voteHash = computeVoteHashAncillary({
-      price,
-      salt,
-      account: FOUNDATION_WALLET,
-      time,
-      roundId: votingStats.roundId,
-      identifier,
-      ancillaryData: request.ancillaryData,
-    });
-    requestsToVoteOn.push({
-      identifier,
-      salt: salt.toString(),
-      time,
-      price,
-      voteHash,
-      ancillaryData: request.ancillaryData,
-    });
+  const voterRequests: { [key: string]: VoteRequest[] } = {};
+  if (realVoters) {
+    // Fetch all voters.
+    const uniqueVoters = await getUniqueVoters(votingV2);
+    for (const voter of uniqueVoters) {
+      voterRequests[voter] = [];
+    }
+  } else {
+    // If not using real voters, then vote from the foundation wallet.
+    voterRequests[FOUNDATION_WALLET] = [];
   }
+  // Set voter balances to 10 ETH for covering gas costs
+  for (const voter in voterRequests) {
+    await hre.network.provider.send("hardhat_setBalance", [voter, hre.ethers.utils.parseEther("10.0").toHexString()]);
+  }
+
+  // Vote affirmatively from voters.
+  console.group("\n🏗 Constructing vote hashes");
+  for (const voter in voterRequests) {
+    for (let i = 0; i < votingStats.pendingRequests.length; i++) {
+      const request = votingStats.pendingRequests[i];
+      const identifier = request.identifier.toString();
+      const time = request.time.toString();
+      const price = ethers.utils.parseEther(YES_VOTE);
+
+      const salt = getRandomSignedInt();
+      // Main net DVM uses the commit reveal scheme of hashed concatenation of price and salt
+      const voteHashData = {
+        price,
+        salt: salt.toString(),
+        account: voter,
+        time,
+        roundId: votingStats.roundId,
+        identifier,
+      };
+      if (!realVoters) console.table(voteHashData);
+
+      const voteHash = computeVoteHashAncillary({
+        price,
+        salt,
+        account: voter,
+        time,
+        roundId: votingStats.roundId,
+        identifier,
+        ancillaryData: request.ancillaryData,
+      });
+      voterRequests[voter].push({
+        identifier,
+        salt: salt.toString(),
+        time,
+        price,
+        voteHash,
+        ancillaryData: request.ancillaryData,
+      });
+    }
+  }
+  const requestsToVoteOn = voterRequests[Object.keys(voterRequests)[0]];
   console.groupEnd();
   console.group(`\n📝  Commit ${requestsToVoteOn.length} Admin requests`);
-  // send the foundation wallet some eth to submit the Tx
-  await signers[0].sendTransaction({
-    to: FOUNDATION_WALLET,
-    value: ethers.utils.parseEther("10"),
-  });
-  for (let i = 0; i < requestsToVoteOn.length; i++) {
-    const request = requestsToVoteOn[i];
-    const txn = await votingV2
-      .connect(foundationSigner)
-      .functions.commitVote(request.identifier, request.time, request.ancillaryData, request.voteHash);
-    console.log(`- [${i + 1}/${requestsToVoteOn.length}] Commit transaction hash: ${txn.hash}`);
+  for (const voter in voterRequests) {
+    const impersonatedSigner = await ethers.getImpersonatedSigner(voter);
+    for (let i = 0; i < voterRequests[voter].length; i++) {
+      const request = voterRequests[voter][i];
+      const txn = await votingV2
+        .connect(impersonatedSigner)
+        .functions.commitVote(request.identifier, request.time, request.ancillaryData, request.voteHash);
+      if (!realVoters) console.log(`- [${i + 1}/${requestsToVoteOn.length}] Commit transaction hash: ${txn.hash}`);
+    }
   }
+  if (realVoters) console.log(`- Committed ${requestsToVoteOn.length} Admin requests from real voters`);
   console.groupEnd();
 
   console.group("\n⏱ Advancing voting phase to reveal phase (i.e. advancing to next voting round)");
@@ -157,13 +181,17 @@ async function simulateVoteV2() {
   console.groupEnd();
 
   console.group(`\n✅ Reveal ${requestsToVoteOn.length} Admin requests`);
-  for (let i = 0; i < requestsToVoteOn.length; i++) {
-    const request = requestsToVoteOn[i];
-    const txn = await votingV2
-      .connect(foundationSigner)
-      .functions.revealVote(request.identifier, request.time, request.price, request.ancillaryData, request.salt);
-    console.log(`- [${i + 1}/${requestsToVoteOn.length}] Reveal transaction hash: ${txn.hash}`);
+  for (const voter in voterRequests) {
+    const impersonatedSigner = await ethers.getImpersonatedSigner(voter);
+    for (let i = 0; i < voterRequests[voter].length; i++) {
+      const request = voterRequests[voter][i];
+      const txn = await votingV2
+        .connect(impersonatedSigner)
+        .functions.revealVote(request.identifier, request.time, request.price, request.ancillaryData, request.salt);
+      if (!realVoters) console.log(`- [${i + 1}/${requestsToVoteOn.length}] Reveal transaction hash: ${txn.hash}`);
+    }
   }
+  if (realVoters) console.log(`- Revealed ${requestsToVoteOn.length} Admin requests from real voters`);
   console.groupEnd();
 
   console.group("\n⏱ Advancing voting phase to conclude vote");
@@ -187,41 +215,45 @@ async function simulateVoteV2() {
     );
   }
 
-  // Execute the most recent admin votes that we just passed through.
-  console.group("\n📢 Executing Governor Proposals");
-  console.group("\nNumber of proposals:", requestsToVoteOn.length);
-  for (let i = requestsToVoteOn.length; i > 0; i--) {
-    // Set `proposalId` to index of most recent proposal in voting.sol
-    const proposalId = Number(await governorV2.numProposals()) - i;
-    console.group("\nProposal ID:", proposalId.toString());
-    const proposal = await governorV2.getProposal(proposalId.toString());
-    for (let j = 0; j < proposal.transactions.length; j++) {
-      console.log(`- Submitting transaction #${j + 1} from proposal #${proposalId}`);
-      try {
-        const txn = await governorV2.connect(executorSigner).executeProposal(proposalId.toString(), j.toString());
-        console.log(`    - Success, receipt: ${txn.hash}`);
-      } catch (err) {
-        console.error("    - Failure: Txn was likely executed previously, skipping to next one");
-        continue;
+  if (!skipExecute) {
+    // Execute the most recent admin votes that we just passed through.
+    console.group("\n📢 Executing Governor Proposals");
+    console.group("\nNumber of proposals:", requestsToVoteOn.length);
+    for (let i = requestsToVoteOn.length; i > 0; i--) {
+      // Set `proposalId` to index of most recent proposal in voting.sol
+      const proposalId = Number(await governorV2.numProposals()) - i;
+      console.group("\nProposal ID:", proposalId.toString());
+      const proposal = await governorV2.getProposal(proposalId.toString());
+      for (let j = 0; j < proposal.transactions.length; j++) {
+        console.log(`- Submitting transaction #${j + 1} from proposal #${proposalId}`);
+        try {
+          const txn = await governorV2.connect(executorSigner).executeProposal(proposalId.toString(), j.toString());
+          console.log(`    - Success, receipt: ${txn.hash}`);
+        } catch (err) {
+          console.error("    - Failure: Txn was likely executed previously, skipping to next one");
+          continue;
+        }
       }
+      console.groupEnd();
     }
     console.groupEnd();
+    console.groupEnd();
   }
-  console.groupEnd();
-  console.groupEnd();
 
-  // Unstake all tokens from the foundation wallet.
-  console.group("\n📢 Unstaking all tokens from foundation wallet");
-  const foundationStakes = await votingV2.voterStakes(FOUNDATION_WALLET);
-  await votingV2.connect(foundationSigner).requestUnstake(foundationStakes.stake);
-  const stakeCooldown = await votingV2.unstakeCoolDown();
+  // Unstake all tokens from the foundation wallet if not using real voters.
+  if (!realVoters) {
+    console.group("\n📢 Unstaking all tokens from foundation wallet");
+    const foundationStakes = await votingV2.voterStakes(FOUNDATION_WALLET);
+    await votingV2.connect(foundationSigner).requestUnstake(foundationStakes.stake);
+    const stakeCooldown = await votingV2.unstakeCoolDown();
 
-  await increaseEvmTime(stakeCooldown.toNumber());
+    await increaseEvmTime(stakeCooldown.toNumber());
 
-  await votingV2.connect(foundationSigner).executeUnstake();
+    await votingV2.connect(foundationSigner).executeUnstake();
 
-  console.log(`Successfully unstaked ${foundationStakes.stake} tokens from foundation wallet`);
-  console.groupEnd();
+    console.log(`Successfully unstaked ${foundationStakes.stake} tokens from foundation wallet`);
+    console.groupEnd();
+  }
 
   console.log("\n😇 Success!");
   console.groupEnd();
