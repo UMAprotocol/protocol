e842530ee38312ffaac9af30c9b64da1c75d943b
Reinis Martinsons <reinis@umaproject.org>
2023-04-27 11:01:21 +0000

    feat: ui links in oov3 monitor
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/monitor-v2/src/monitor-oo-v3/MonitorSettlements.ts
+++ packages/monitor-v2/src/monitor-oo-v3/MonitorSettlements.ts
@@ -8,6 +8,7 @@ export async function monitorSettlements(logger: typeof Logger, params: Monitori
     await oo.queryFilter(oo.filters.AssertionSettled(), params.blockRange.start, params.blockRange.end)
   ).map(async (event) => ({
     tx: event.transactionHash,
+    eventIndex: event.logIndex,
     assertionId: event.args.assertionId,
     claim: (await oo.queryFilter(oo.filters.AssertionMade(event.args.assertionId))).map((event) => event.args.claim)[0],
     assertionData: await oo.getAssertion(event.args.assertionId),
