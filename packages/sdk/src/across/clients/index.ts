5e5d8e9fff1bbd4f3105ee92bc30437e7766d3a4
Matt Rice <matthewcrice32@gmail.com>
2023-05-03 03:17:53 -0400

    remove parts of sdk that depend on across contracts
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- packages/sdk/src/across/clients/index.ts
+++ packages/sdk/src/across/clients/index.ts
@@ -1,3 +1,2 @@
-export * as bridgePool from "./bridgePool";
 export * as optimismBridge from "./optimismBridge";
 export * as bobaBridge from "./bobaBridge";
--- packages/sdk/src/across/index.ts
+++ packages/sdk/src/across/index.ts
@@ -1,6 +1,5 @@
 export * as feeCalculator from "./feeCalculator";
 export * as rateModel from "./rateModel";
-export { default as LpFeeCalculator } from "./lpFeeCalculator";
 export * as gasFeeCalculator from "./gasFeeCalculator";
 export * as utils from "./utils";
 export * as constants from "./constants";
