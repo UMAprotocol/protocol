75f951942292e778a05efcb1b829e6a532182168
Pablo Maldonado <pablo@umaproject.org>
2023-06-22 18:57:52 +0200

    refactor: remove unused interface
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/financial-templates-lib/src/helpers/GasEstimator.ts
+++ packages/financial-templates-lib/src/helpers/GasEstimator.ts
@@ -21,21 +21,6 @@ interface GasEstimatorMapping {
     backupUrl?: string;
   };
 }
-interface EthGasStationResponse {
-  fast: number;
-  fastest: number;
-  safeLow: number;
-  average: number;
-  block_time: number;
-  blockNum: number;
-  speed: number;
-  safeLowWait: number;
-  avgWait: number;
-  fastWait: number;
-  fastestWait: number;
-  gasPriceRange: Record<string, number>;
-}
-
 interface EtherscanGasResponse {
   status: string;
   message: string;
