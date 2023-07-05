4c96a46514886ccd12580c318cb838ad282fc5fa
Pablo Maldonado <pablo@umaproject.org>
2023-07-05 12:13:58 +0100

    feat: add comments
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/monitor-v2/src/llm-bot/common.ts
+++ packages/monitor-v2/src/llm-bot/common.ts
@@ -14,28 +14,60 @@ export type OptimisticOracleRequest = {
   disputeTx?: string; // Transaction hash of the dispute.
 };
 
+/**
+ * Represents a client to interact with an Optimistic Oracle and store the requests.
+ */
 export class OptimisticOracleClient {
   protected provider: Provider;
   protected requests: OptimisticOracleRequest[];
+  protected fetchedBlockRange: [number, number];
 
+  /**
+   * Constructs a new OptimisticOracleClient instance.
+   * @param _provider The provider used for interacting with the blockchain.
+   * @param _requests (Optional) The list of Optimistic Oracle requests.
+   */
   constructor(_provider: Provider, _requests: OptimisticOracleRequest[] = []) {
     this.provider = _provider;
     this.requests = _requests;
+    this.fetchedBlockRange = [0, 0];
   }
 
-  protected async getOracleRequests(blockRange: [number, number]): Promise<OptimisticOracleRequest[]> {
+  /**
+   * Retrieves Optimistic Oracle requests within the specified block range.
+   * @param blockRange The block range to fetch requests from.
+   * @returns A Promise that resolves to an array of OptimisticOracleRequest objects.
+   */
+  protected async fetchOracleRequests(blockRange: [number, number]): Promise<OptimisticOracleRequest[]> {
     // TODO: Implement this.
     blockRange;
     return [];
   }
 
-  async update(blockRange: [number, number]): Promise<OptimisticOracleClient> {
-    const newRequests = await this.getOracleRequests(blockRange);
+  /**
+   * Updates the OptimisticOracleClient instance by fetching new Oracle requests within the specified block range. Returns a new instance.
+   * @param blockRange The block range to fetch new requests from.
+   * @returns A Promise that resolves to a new OptimisticOracleClient instance with updated requests.
+   */
+  async updateWithBlockRange(blockRange: [number, number]): Promise<OptimisticOracleClient> {
+    const newRequests = await this.fetchOracleRequests(blockRange);
     return new OptimisticOracleClient(this.provider, newRequests);
   }
 
+  /**
+   * Returns the list of Optimistic Oracle requests.
+   * @returns An array of OptimisticOracleRequest objects.
+   */
   getRequests(): OptimisticOracleRequest[] {
-    return this.requests;
+    return this.requests || [];
+  }
+
+  /**
+   * Returns the block range of the fetched requests.
+   * @returns An array of two numbers representing the block range.
+   */
+  getFetchedBlockRange(): [number, number] {
+    return this.fetchedBlockRange;
   }
 }
 
@@ -44,7 +76,7 @@ export class OptimisticOracleClientV2 extends OptimisticOracleClient {
     super(_provider, _requests);
   }
 
-  protected async getOracleRequests(blockRange: [number, number]): Promise<OptimisticOracleRequest[]> {
+  protected async fetchOracleRequests(blockRange: [number, number]): Promise<OptimisticOracleRequest[]> {
     // TODO: Implement this for the OptimisticOracleV2
     blockRange;
     return [];
@@ -56,7 +88,7 @@ export class OptimisticOracleClientV3 extends OptimisticOracleClient {
     super(_provider, _requests);
   }
 
-  protected async getOracleRequests(blockRange: [number, number]): Promise<OptimisticOracleRequest[]> {
+  protected async fetchOracleRequests(blockRange: [number, number]): Promise<OptimisticOracleRequest[]> {
     // TODO: Implement this for the OptimisticOracleV3
     blockRange;
     return [];
