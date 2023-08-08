6cbc3aa7e2dc1c276bb0775228e4fdf878befb5e
Reinis Martinsons <reinis@umaproject.org>
2023-08-08 12:35:32 +0000

    feat(financial-templates-lib): try preserve url in truncated discord ticket
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/financial-templates-lib/src/logger/DiscordTicketTransport.ts
+++ packages/financial-templates-lib/src/logger/DiscordTicketTransport.ts
@@ -33,6 +33,7 @@ export function createConfig(config: unknown): Config {
 }
 
 const DISCORD_MAX_CHAR_LIMIT = 2000;
+const TRUNCATED = " [TRUNCATED] ";
 
 // Interface for log info object.
 interface DiscordTicketInfo {
@@ -104,7 +105,12 @@ export class DiscordTicketTransport extends Transport {
 
         // Prepend the $ticket command and concatenate message title and content separated by newline.
         // Also remove anchor text from links and truncate the message to Discord's max character limit.
-        const message = this.truncateMessage(`$ticket ${info.message}\n${removeAnchorTextFromLinks(info.mrkdwn)}`);
+        const header = `$ticket ${info.message}\n`;
+        const content = this.truncateMessage(
+          removeAnchorTextFromLinks(info.mrkdwn),
+          DISCORD_MAX_CHAR_LIMIT - header.length
+        );
+        const message = header + content;
 
         // Add the message to the queue and process it.
         this.logQueue.push({ channel, message });
@@ -150,9 +156,57 @@ export class DiscordTicketTransport extends Transport {
     this.isQueueBeingExecuted = false;
   }
 
-  // Truncate the message if it exceeds the Discord character limit.
-  private truncateMessage(message: string): string {
-    if (message.length <= DISCORD_MAX_CHAR_LIMIT) return message;
-    return message.slice(0, DISCORD_MAX_CHAR_LIMIT - 12) + " [TRUNCATED]";
+  // Truncate the message if it exceeds the provided character limit. Try to preserve URLs.
+  private truncateMessage(message: string, limit: number): string {
+    if (limit < TRUNCATED.length) throw new Error("Invalid truncated message limit!");
+
+    // If the message is short enough, return it as is.
+    if (message.length <= limit) return message;
+
+    // Regular expression to match URLs.
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    const urls = message.match(urlRegex);
+
+    // If there are no URLs, just truncate the end of the message.
+    if (urls === null) return message.slice(0, limit - TRUNCATED.length) + TRUNCATED;
+
+    // Split the message into chunks around URLs for further processing.
+    const messageChunks = message.split(urlRegex);
+
+    // Truncate chunks until the message is short enough to fit the character limit. Cycle through the chunks backwards
+    // in two rounds. The first round truncates the chunks that are not URLs. The second round also truncates the URL
+    // chunks. This is done to preserve URLs as much as possible.
+    let truncatedMessageLength = message.length;
+    let isUrlRound = false;
+    for (let i = messageChunks.length - 1; i >= 0; i--) {
+      if (truncatedMessageLength <= limit) break; // Stop if the message is short enough.
+
+      if (urls.includes(messageChunks[i]) && !isUrlRound) continue; // Keep URLs from being truncated in the first round.
+
+      // Truncate the chunk and update the truncated message length if it shortens the message. In the second round
+      // truncate the whole URL chunk as there is no point in keeping a part of it.
+      if (messageChunks[i].length > TRUNCATED.length) {
+        const retainedChunkLength = Math.max(
+          0,
+          isUrlRound ? 0 : messageChunks[i].length - TRUNCATED.length - (truncatedMessageLength - limit)
+        );
+        truncatedMessageLength -= messageChunks[i].length - retainedChunkLength - TRUNCATED.length;
+        messageChunks[i] = messageChunks[i].slice(0, retainedChunkLength) + TRUNCATED;
+      }
+
+      // If the first round is done, reset the counter for the second round truncating URLs.
+      if (!isUrlRound && i === 0) {
+        isUrlRound = true;
+        i = messageChunks.length;
+      }
+    }
+
+    // Concatenate the chunks back together.
+    const truncatedMessage = messageChunks.join("");
+
+    // In case there are too many chunks to even fit the TRUNCATED values we have to truncate the whole message without
+    // giving any priority to URLs.
+    if (truncatedMessage.length <= limit) return truncatedMessage;
+    return message.slice(0, limit - TRUNCATED.length) + TRUNCATED;
   }
 }
