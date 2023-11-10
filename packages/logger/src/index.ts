052e1bc833a84964e3f6892effa7d7f2ccaec836
Pablo Maldonado <pablo@umaproject.org>
2023-11-10 14:42:51 +0000

    feat: clean up
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./helpers/delay";
-export * from "./helpers/math";
 export * from "./logger/Logger";
 export * from "./logger/SpyTransport";
 export * from "./logger/ConsoleTransport";
