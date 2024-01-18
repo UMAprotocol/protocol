9c73cd690f58fd6dd1c074a79dc4e52f991b72e9
Reinis Martinsons <reinis@umaproject.org>
2024-01-18 10:41:48 +0000

    feat: api simulates osnap execution
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/api/src/libs/osnap/utils.ts
+++ packages/api/src/libs/osnap/utils.ts
@@ -1,28 +1,63 @@
 import assert from "assert";
+import { utils } from "ethers";
 import { Json } from "../../types";
-import type { OsnapPluginData, Transaction } from "./interfaces";
+import type { GnosisSafe, OsnapPluginData } from "./interfaces";
 import { simulateTenderlyTx, TenderlySimulationParams } from "@uma/common";
+import { getAddress, OptimisticGovernorEthers__factory } from "@uma/contracts-node";
 
-export async function simulateOsnapTx(space: OsnapPluginData, transactionIndex = 0): Promise<Json> {
+export async function simulateOsnapProposal(space: OsnapPluginData): Promise<Json> {
   const { safe } = space;
   assert(safe, "Requires a safe");
-  const { network, transactions } = safe;
-  assert(transactions.length > transactionIndex, "Cannot simulate not existent transaction");
+  assert(safe.transactions.length > 0, "Cannot simulate empty transactions");
 
-  // we only expect a single tx
-  const transaction = transactions[transactionIndex];
-  const simParams = mapOsnapTxToTenderlySim(transaction, Number(network));
+  const ooAddress = await getAddress("OptimisticOracleV3", Number(safe.network));
+
+  const simParams = mapOsnapSafeToTenderlySim(safe, ooAddress);
   // we are passing the result straight to express, so we are casting this as json. this is fully
   // compatible with json type since all values are primitives.
   return ((await simulateTenderlyTx(simParams)) as unknown) as Json;
 }
-export function mapOsnapTxToTenderlySim(tx: Transaction, chainId: number): TenderlySimulationParams {
+
+// Creates Tenderly simulation parameters for oSnap proposal execution with required state overrides:
+// - OptimisticGovernor knows of proposalHash pointing to non-zero asserionId in the assertionIds mapping;
+// - OptimisticOracleV3 has the Assertion with the above assertionId marked as settled.
+export function mapOsnapSafeToTenderlySim(safe: GnosisSafe, ooAddress: string): TenderlySimulationParams {
+  const transactions = safe.transactions.map((tx) => {
+    const [to, operation, value, data] = tx.formatted;
+    return { to, operation, data, value };
+  });
+
+  // Calculate proposalHash from proposed transaction contents.
+  const ogInterface = OptimisticGovernorEthers__factory.createInterface();
+  const proposalHash = utils.keccak256(
+    utils.defaultAbiCoder.encode(ogInterface.getFunction("executeProposal").inputs, [transactions])
+  );
+
+  // assertionIds mapping pointer is at slot 110 in the OptimisticGovernor contract.
+  const assertionIdsSlot = utils.keccak256(utils.defaultAbiCoder.encode(["bytes32", "uint256"], [proposalHash, 110]));
+
+  // assertionId needs to be the same non-zero value in both OptimisticGovernor and OptimisticOracleV3.
+  const assertionId = "0x1";
+
+  // assertions mapping pointer is at slot 4 in the OptimisticOracleV3 contract. We need to override settled property
+  // that is 28 bytes offset at slot 2 within the Assertion struct.
+  const assertionSettledSlot =
+    "0x" +
+    (
+      BigInt(
+        utils.keccak256(utils.defaultAbiCoder.encode(["bytes32", "uint256"], [utils.hexZeroPad(assertionId, 32), 4]))
+      ) + 2n
+    ).toString(16);
+  const assertionSettledOffset = 28;
+  const assertionSettledValue = "0x1"; // Represents settled as true.
+
   return {
-    chainId,
-    to: tx.to,
-    // TODO: Need to encode executeProposal here
-    input: tx.data,
-    // TODO: figure out state overrides here
-    // state_objects: {},
+    chainId: Number(safe.network),
+    to: safe.moduleAddress,
+    input: ogInterface.encodeFunctionData("executeProposal", [transactions]),
+    stateOverrides: [
+      { address: safe.moduleAddress, slot: assertionIdsSlot, value: assertionId },
+      { address: ooAddress, slot: assertionSettledSlot, offset: assertionSettledOffset, value: assertionSettledValue },
+    ],
   };
 }
