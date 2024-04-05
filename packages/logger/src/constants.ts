8a45181d18256f356f3d7d78dff145ff3ea921f7
Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
2024-04-05 15:46:00 +0300

    Revert "fix: use persistent log queue for discord tickets (#4734)"
    This reverts commit 932bbed4a2cab4d9234bde5af9c02aaa260c4132.

--- packages/logger/src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const noBotId = "NO_BOT_ID";
--- packages/logger/src/helpers/typeGuards.ts
+++
@@ -1,4 +0,0 @@
-// Helper type guard for dictionary objects. Useful when dealing with any info type passed to log method.
-export const isDictionary = (arg: unknown): arg is Record<string, unknown> => {
-  return typeof arg === "object" && arg !== null && !Array.isArray(arg);
-};
