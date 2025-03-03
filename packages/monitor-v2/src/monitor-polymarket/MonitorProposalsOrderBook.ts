991fbf6ac35c8f38bdc54f60639f7233b1a65b75
Pablo Maldonado <pablo@umaproject.org>
2025-03-03 09:55:37 +0100

    tmp
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- packages/monitor-v2/src/monitor-polymarket/MonitorProposalsOrderBook.ts
+++ packages/monitor-v2/src/monitor-polymarket/MonitorProposalsOrderBook.ts
@@ -19,7 +19,9 @@ import {
   storeNotifiedProposals,
   retryAsync,
 } from "./common";
-
+import { decodeMultipleQueryPriceAtIndex } from "./helpers";
+import { ethers } from "ethers";
+import umaSportsOracleAbi from "./abi/umaSportsOracle.json";
 export async function monitorTransactionsProposedOrderBook(
   logger: typeof Logger,
   params: MonitoringParams
@@ -57,6 +59,7 @@ export async function monitorTransactionsProposedOrderBook(
 
 async function processMarketProposal(market: OptimisticPriceRequest, params: MonitoringParams, logger: typeof Logger) {
   const networker = new Networker(logger);
+  const isSportsMarket = market.requester == params.ctfSportsOracleAddress;
   const questionID = calculatePolymarketQuestionID(market.ancillaryData);
   // set this to retry twice and wait 5 seconds between retries.
   const markets = await retryAsync(
@@ -72,56 +75,69 @@ async function processMarketProposal(market: OptimisticPriceRequest, params: Mon
         polymarketInfo.clobTokenIds,
         Number(market.requestBlockNumber)
       );
+      if (!isSportsMarket) {
+        const proposedOutcome = market.proposedPrice.eq(ONE_SCALED) ? 0 : 1;
+        const complementaryOutcome = proposedOutcome === 0 ? 1 : 0;
 
-      const proposedOutcome = market.proposedPrice.eq(ONE_SCALED) ? 0 : 1;
-      const complementaryOutcome = proposedOutcome === 0 ? 1 : 0;
+        const thresholds = {
+          asks: Number(process.env["THRESHOLD_ASKS"]) || 1,
+          bids: Number(process.env["THRESHOLD_BIDS"]) || 0,
+          volume: Number(process.env["THRESHOLD_VOLUME"]) || 500000,
+        };
 
-      const thresholds = {
-        asks: Number(process.env["THRESHOLD_ASKS"]) || 1,
-        bids: Number(process.env["THRESHOLD_BIDS"]) || 0,
-        volume: Number(process.env["THRESHOLD_VOLUME"]) || 500000,
-      };
+        const sellingWinnerSide = orderBook[proposedOutcome].asks.find((ask) => ask.price < thresholds.asks);
+        const buyingLoserSide = orderBook[complementaryOutcome].bids.find((bid) => bid.price > thresholds.bids);
 
-      const sellingWinnerSide = orderBook[proposedOutcome].asks.find((ask) => ask.price < thresholds.asks);
-      const buyingLoserSide = orderBook[complementaryOutcome].bids.find((bid) => bid.price > thresholds.bids);
-
-      const soldWinnerSide = orderFilledEvents[proposedOutcome].filter(
-        (event) => event.type == "sell" && event.price < thresholds.asks
-      );
-      const boughtLoserSide = orderFilledEvents[complementaryOutcome].filter(
-        (event) => event.type == "buy" && event.price > thresholds.bids
-      );
-
-      let notified = false;
-      const notificationData = {
-        txHash: market.proposalHash,
-        question: polymarketInfo.question,
-        proposedPrice: market.proposedPrice,
-        requestTimestamp: market.requestTimestamp,
-      };
-
-      if (polymarketInfo.volumeNum > thresholds.volume) {
-        await logProposalHighVolume(logger, { ...market, ...polymarketInfo }, params);
-        notified = true;
-      }
-
-      if (sellingWinnerSide || buyingLoserSide || soldWinnerSide.length > 0 || boughtLoserSide.length > 0) {
-        await logMarketSentimentDiscrepancy(
-          logger,
-          {
-            ...market,
-            ...polymarketInfo,
-            sellingWinnerSide,
-            buyingLoserSide,
-            soldWinnerSide,
-            boughtLoserSide,
-          },
-          params
+        const soldWinnerSide = orderFilledEvents[proposedOutcome].filter(
+          (event) => event.type == "sell" && event.price < thresholds.asks
         );
-        notified = true;
-      }
+        const boughtLoserSide = orderFilledEvents[complementaryOutcome].filter(
+          (event) => event.type == "buy" && event.price > thresholds.bids
+        );
 
-      return { notified, notifiedProposal: notified ? notificationData : null };
+        let notified = false;
+        const notificationData = {
+          txHash: market.proposalHash,
+          question: polymarketInfo.question,
+          proposedPrice: market.proposedPrice,
+          requestTimestamp: market.requestTimestamp,
+        };
+
+        if (polymarketInfo.volumeNum > thresholds.volume) {
+          await logProposalHighVolume(logger, { ...market, ...polymarketInfo }, params);
+          notified = true;
+        }
+
+        if (sellingWinnerSide || buyingLoserSide || soldWinnerSide.length > 0 || boughtLoserSide.length > 0) {
+          await logMarketSentimentDiscrepancy(
+            logger,
+            {
+              ...market,
+              ...polymarketInfo,
+              sellingWinnerSide,
+              buyingLoserSide,
+              soldWinnerSide,
+              boughtLoserSide,
+            },
+            params
+          );
+          notified = true;
+        }
+
+        return { notified, notifiedProposal: notified ? notificationData : null };
+      } else {
+        const sportsMarketType = "winner";
+        const proposedPrice = market.proposedPrice;
+        const score1 = decodeMultipleQueryPriceAtIndex(proposedPrice, 0);
+        const score2 = decodeMultipleQueryPriceAtIndex(proposedPrice, 1);
+
+        const umaSportsOracle = new ethers.Contract(params.ctfSportsOracleAddress, umaSportsOracleAbi, params.provider);
+
+
+
+        return { notified: false, notifiedProposal: null };
+      }
     })
+
   );
 }
--- packages/monitor-v2/src/monitor-polymarket/common.ts
+++ packages/monitor-v2/src/monitor-polymarket/common.ts
@@ -341,7 +341,7 @@ export const initMonitoringParams = async (env: NodeJS.ProcessEnv): Promise<Moni
   const ctfAdapterAddress = "0x6A9D222616C90FcA5754cd1333cFD9b7fb6a4F74";
   const ctfAdapterAddressV2 = "0x2f5e3684cb1f318ec51b00edba38d79ac2c0aa9d";
   const ctfExchangeAddress = "0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E";
-  const ctfSportsOracleAddress = "0x0000000000000000000000000000000000000000"; // TODO: add this
+  const ctfSportsOracleAddress = "0xbec046ae23fee91643a45b03f12c6afdb2628d9c";
 
   const graphqlEndpoint = "https://gamma-api.polymarket.com/query";
   const apiEndpoint = "https://clob.polymarket.com";
