993e4a30b9cb249daa7f5ac7d623b637c50a3d5e
Reinis Martinsons <reinis@umaproject.org>
2025-09-08 16:39:05 +0000

    fix: add oov3 cache syncer
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

---
+++ packages/monitor-v2/src/oo-v3-cache-syncer/common.ts
@@ -0,0 +1,28 @@
+import { Multicall } from "@uma/sdk";
+import { ethers } from "ethers";
+import { BaseMonitoringParams, initBaseMonitoringParams, startupLogLevel as baseStartup } from "../bot-utils/base";
+
+export { Logger } from "@uma/financial-templates-lib";
+
+export interface MonitoringParams extends BaseMonitoringParams {
+  multicall: Multicall;
+}
+
+export const initMonitoringParams = async (env: NodeJS.ProcessEnv): Promise<MonitoringParams> => {
+  const base = await initBaseMonitoringParams(env);
+
+  // Default to Ethereum mainnet Multicall3 address
+  const multicallAddress = env.MULTICALL_ADDRESS ?? "0xcA11bde05977b3631167028862bE2a173976CA11";
+  if (ethers.utils.isAddress(multicallAddress) === false) {
+    throw new Error(`Invalid MULTICALL_ADDRESS: ${multicallAddress}`);
+  }
+  const multicall = new Multicall(multicallAddress, base.provider);
+  return {
+    ...base,
+    multicall,
+  };
+};
+
+export const startupLogLevel = (params: MonitoringParams): "debug" | "info" => {
+  return baseStartup(params);
+};
