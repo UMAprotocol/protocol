aeed93931ad9165f7054eb74b0b4df89bd0aefb3
Paul <108695806+pxrl@users.noreply.github.com>
2025-09-19 15:47:58 +0200

    improve(serverless): Cleanup tests & depends (#4857)
    - Use native shell tooling to simulate serverlessCommand pass/fail.
    - Use uma/logger directly rather than importing logger via 
      financial-templates-lib.
    - Drop unnecessary imports & superfluous functionalities.
    
    This change is motivated by the desire to decouple the Across build &
    deployment pipeline from UMA infrastructre, which should allow both to
    operate with more flexibility.
    
    Also a quick node on verification of this change; it's been run in a
    sandbox environment and both success & failure cases have been
    exercised, as well as block number tracking. No issue were observed.
    
    As a side benefit, these changes seem to cut about 1 minute from the
    test run.
    
    ...more to follow.
    
    Signed-off-by: Paul <108695806+pxrl@users.noreply.github.com>

--- packages/serverless-orchestration/test/ServerlessSpoke.js
+++ packages/serverless-orchestration/test/ServerlessSpoke.js
@@ -1,6 +1,5 @@
-const hre = require("hardhat");
+const { web3 } = require("hardhat");
 const { assert } = require("chai");
-const { web3, getContract } = hre;
 
 // Enables testing http requests to an express spoke.
 const request = require("supertest");
@@ -8,18 +7,12 @@ const request = require("supertest");
 // Script to test
 const spoke = require("../src/ServerlessSpoke");
 
-// Contract to monitor
-const OptimisticOracleV2 = getContract("OptimisticOracleV2");
-
 // Custom winston transport module to monitor winston log outputs
 const winston = require("winston");
 const sinon = require("sinon");
-const { SpyTransport, lastSpyLogIncludes } = require("@uma/financial-templates-lib");
-const { runDefaultFixture } = require("@uma/common");
+const { SpyTransport, lastSpyLogIncludes } = require("@uma/logger");
 
 describe("ServerlessSpoke.js", function () {
-  let optimisticOracleV2Address;
-
   let spy;
   let spyLogger;
   let testPort = 8080;
@@ -28,10 +21,6 @@ describe("ServerlessSpoke.js", function () {
   const sendRequest = (body) => {
     return request(`http://localhost:${testPort}`).post("/").send(body).set("Accept", "application/json");
   };
-
-  before(async function () {
-    await runDefaultFixture(hre);
-  });
 
   beforeEach(async function () {
     // Create a sinon spy and give it to the SpyTransport as the winston logger. Use this to check all winston logs.
@@ -43,9 +32,6 @@ describe("ServerlessSpoke.js", function () {
 
     // Start the Serverless spoke instance with the spy logger injected.
     spokeInstance = await spoke.Poll(spyLogger, testPort);
-
-    // Get deployed OptimisticOracleV2 address to monitor.
-    optimisticOracleV2Address = (await OptimisticOracleV2.deployed()).options.address;
   });
   afterEach(async function () {
     spokeInstance.close();
@@ -73,13 +59,8 @@ describe("ServerlessSpoke.js", function () {
   });
   it("Serverless Spoke can correctly execute bot logic with valid body", async function () {
     const validBody = {
-      serverlessCommand: "yarn --silent monitors --network test",
-      environmentVariables: {
-        CUSTOM_NODE_URL: web3.currentProvider.host, // ensures that script runs correctly in tests & CI.
-        POLLING_DELAY: 0,
-        OPTIMISTIC_ORACLE_ADDRESS: optimisticOracleV2Address,
-        OPTIMISTIC_ORACLE_TYPE: "OptimisticOracleV2",
-      },
+      serverlessCommand: "true", // Force command success.
+      environmentVariables: { CUSTOM_NODE_URL: web3.currentProvider.host },
     };
 
     const validResponse = await sendRequest(validBody);
@@ -97,60 +78,20 @@ describe("ServerlessSpoke.js", function () {
   it("Serverless Spoke can execute multiple chained commands with &&", async function () {
     const validBody = {
       serverlessCommand: "cd . && echo TEST_VAR=${TEST_VAR}",
-      environmentVariables: {
-        TEST_VAR: "test_value",
-      },
+      environmentVariables: { TEST_VAR: "test_value" },
     };
     const validResponse = await sendRequest(validBody);
     assert.equal(validResponse.res.statusCode, 200); // error code
     assert.isTrue(lastSpyLogIncludes(spy, "Process exited with no error")); // Verify the process completed successfully
   });
-  it("Serverless Spoke can correctly returns errors over http calls(invalid path)", async function () {
-    // Invalid path should error out when trying to run an executable that does not exist
-    const invalidPathBody = {
-      serverlessCommand: "yarn --silent INVALID --network test",
-      environmentVariables: {
-        CUSTOM_NODE_URL: web3.currentProvider.host,
-        POLLING_DELAY: 0,
-        OPTIMISTIC_ORACLE_ADDRESS: optimisticOracleV2Address,
-        OPTIMISTIC_ORACLE_TYPE: "OptimisticOracleV2",
-      },
+  it("Serverless Spoke can correctly returns errors over http calls", async function () {
+    const body = {
+      serverlessCommand: "false",
+      environmentVariables: { CUSTOM_NODE_URL: web3.currentProvider.host },
     };
 
-    const invalidPathResponse = await sendRequest(invalidPathBody);
-    assert.equal(invalidPathResponse.res.statusCode, 500); // error code
-    // Expected error text from an invalid path
-    assert.isTrue(lastSpyLogIncludes(spy, "Process exited with error")); // Check the process logger contains exit error.
-  });
-  it("Serverless Spoke can correctly returns errors over http calls(invalid body)", async function () {
-    // Invalid config should error out before entering the main while loop in the bot.
-    const invalidConfigBody = {
-      serverlessCommand: "yarn --silent monitors --network test",
-      environmentVariables: {
-        CUSTOM_NODE_URL: web3.currentProvider.host,
-        POLLING_DELAY: 0,
-        // missing OPTIMISTIC_ORACLE_ADDRESS. Should error before entering main while loop.
-      },
-    };
-
-    const invalidConfigResponse = await sendRequest(invalidConfigBody);
-    assert.equal(invalidConfigResponse.res.statusCode, 500); // error code
-    assert.isTrue(lastSpyLogIncludes(spy, "Process exited with error")); // Check the process logger contains exit error.
-  });
-  it("Serverless Spoke can correctly returns errors over http calls(invalid oo)", async function () {
-    // Invalid OPTIMISTIC_ORACLE_ADDRESS address should error out when trying to retrieve on-chain data.
-    const invalidOOAddressBody = {
-      serverlessCommand: "yarn --silent monitors --network test",
-      environmentVariables: {
-        CUSTOM_NODE_URL: web3.currentProvider.host,
-        POLLING_DELAY: 0,
-        OPTIMISTIC_ORACLE_ADDRESS: "0x0000000000000000000000000000000000000000", // Invalid address that should generate an error
-      },
-    };
-
-    const invalidOOAddressResponse = await sendRequest(invalidOOAddressBody);
-    assert.equal(invalidOOAddressResponse.res.statusCode, 500); // error code
-    // Expected error text from loading in an OO from an invalid address
+    const response = await sendRequest(body);
+    assert.equal(response.res.statusCode, 500); // error code
     assert.isTrue(lastSpyLogIncludes(spy, "Process exited with error")); // Check the process logger contains exit error.
   });
 });
