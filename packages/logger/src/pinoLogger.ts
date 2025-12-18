752036baa850e26a9bc591b48d2751e88226f4e0
Reinis Martinsons <reinis@umaproject.org>
2025-12-18 17:47:52 +0000

    fix: pass pino config to fastify
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- packages/logger/src/pinoLogger.ts
+++ packages/logger/src/pinoLogger.ts
@@ -1,11 +1,12 @@
-import { pino, LevelWithSilentOrString, Logger as PinoLogger } from "pino";
+import { pino, LevelWithSilentOrString, Logger as PinoLogger, LoggerOptions as PinoLoggerOptions } from "pino";
 import { createGcpLoggingPinoConfig } from "@google-cloud/pino-logging-gcp-config";
 import { noBotId } from "./constants";
 import { generateRandomRunId } from "./logger/Logger";
 
 export type { PinoLogger };
+export type { PinoLoggerOptions };
 
-type PinoLoggerOptions = {
+type CustomPinoLoggerOptions = {
   botIdentifier: string;
   runIdentifier: string;
   level: LevelWithSilentOrString;
@@ -15,11 +16,17 @@ export function createPinoLogger({
   botIdentifier = process.env.BOT_IDENTIFIER || noBotId,
   runIdentifier = process.env.RUN_IDENTIFIER || generateRandomRunId(),
   level = "info",
-}: Partial<PinoLoggerOptions> = {}): PinoLogger {
-  return pino(
-    createGcpLoggingPinoConfig(undefined, {
-      level,
-      base: { "bot-identifier": botIdentifier, "run-identifier": runIdentifier },
-    })
-  );
+}: Partial<CustomPinoLoggerOptions> = {}): PinoLogger {
+  return pino(createPinoConfig({ botIdentifier, runIdentifier, level }));
+}
+
+export function createPinoConfig({
+  botIdentifier = process.env.BOT_IDENTIFIER || noBotId,
+  runIdentifier = process.env.RUN_IDENTIFIER || generateRandomRunId(),
+  level = "info",
+}: Partial<CustomPinoLoggerOptions> = {}): PinoLoggerOptions {
+  return createGcpLoggingPinoConfig(undefined, {
+    level,
+    base: { "bot-identifier": botIdentifier, "run-identifier": runIdentifier },
+  });
 }
